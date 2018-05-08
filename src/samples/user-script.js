fa1814e0362dabd9ee7a5ea59e127d33083b9e38
Patricio Palladino <email@patriciopalladino.com>
2018-05-07 23:54:39 -0300

    Update sample user-script

--- src/samples/user-script.js
+++ src/samples/user-script.js
@@ -1,11 +1,13 @@
 console.log("Sool env's web3 provider's host:", web3.currentProvider.host);
-
-web3.eth.getAccounts((err, accounts) => {
+pweb3.eth.getAccounts().then(accounts => {
   accounts.map(addr => {
-    web3.eth.getBalance(addr, (err2, balance) => {
+    web3.eth.getBalance(addr, (err, balance) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
       console.log(`${addr}: ${balance.toString(10)}`);
     });
   });
 });
-
-getContract("Contract").then(console.log);
