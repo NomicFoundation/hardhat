7285f8c7991a1e52171b129c2c7bd18fa9bace66
Patricio Palladino <email@patriciopalladino.com>
2018-05-14 20:21:12 -0300

    Make console task work despite cache folder not being created yet

--- src/builtin-tasks/console.js
+++ src/builtin-tasks/console.js
@@ -1,8 +1,10 @@
 const path = require("path");
+const fs = require("fs-extra");
 const repl = require("repl");
 const replHistory = require("repl.history");
 
 task("console", "Opens a buidler console", async () => {
+  await fs.ensureDir(config.paths.cache);
   const historyFile = path.join(config.paths.cache, "console_history");
 
   const theRepl = repl.start({ useGlobal: true, ignoreUndefined: true });
