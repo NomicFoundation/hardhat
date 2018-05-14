c26c9f116dbf7c453103f453bb0b5dea97b16fd0
Patricio Palladino <email@patriciopalladino.com>
2018-05-14 15:29:07 -0300

    First version of project creation script

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -3,13 +3,72 @@
 
 const fs = require("fs-extra");
 const path = require("path");
+const chalk = require("chalk");
+const inquirer = require('inquirer');
 
-const { getConfig } = require("../core/config");
+
+const { getConfig, getUserConfigPath } = require("../core/config");
 const { getTaskDefinitions } = require("../core/tasks");
 const { parseArguments } = require("../core/arguments");
 const { createEnvironment } = require("../core/environment");
 
 const DEFAULT_TASK_NAME = "help";
+
+function insideProject() {
+  try {
+    getUserConfigPath();
+
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+function supportsEmoji(builderArguments) {
+  return process.stdout.isTTY && (process.platform === 'darwin' || process.argv.includes("--emoji"));
+}
+
+async function initProject() {
+  const packageInfo = await fs.readJson(
+    path.join(__dirname, "../../package.json")
+  );
+
+  console.log(chalk.blue(`888               d8b      888 888`));
+  console.log(chalk.blue(`888               Y8P      888 888`));
+  console.log(chalk.blue('888                        888 888'));
+  console.log(chalk.blue('88888b.  888  888 888  .d88888 888  .d88b.  888d888'));
+  console.log(chalk.blue('888 "88b 888  888 888 d88" 888 888 d8P  Y8b 888P"'));
+  console.log(chalk.blue('888  888 888  888 888 888  888 888 88888888 888'));
+  console.log(chalk.blue('888 d88P Y88b 888 888 Y88b 888 888 Y8b.     888'));
+  console.log(chalk.blue(`88888P"   "Y88888 888  "Y88888 888  "Y8888  888`));
+  console.log("");
+
+  console.log(chalk.cyan(`  Welcome to ${packageInfo.name} v${packageInfo.version}  \n`));
+
+  const {shouldCreateProject} = await inquirer.prompt([{
+    name: "shouldCreateProject",
+    type: "confirm",
+    message: "You are not inside a buidler project. Do you want to create a new one?"
+  }]);
+
+  if (!shouldCreateProject) {
+    return;
+  }
+
+  const {projectRoot} = await inquirer.prompt([{
+    name: "projectRoot",
+    default: process.cwd(),
+    message: "Buidler project root:"
+  }]);
+
+  await fs.outputFile(path.join(projectRoot, "buidler-config.js"),
+  `
+module.exports = {
+};
+  `);
+
+  console.log(chalk.cyan(`\n✨ Project created ✨`));
+}
 
 async function main() {
   // We first accept this argument anywhere, so we know if the user wants
@@ -17,6 +76,11 @@ async function main() {
   let showStackTraces = process.argv.includes("--showStackTraces");
 
   try {
+    if (!insideProject() && process.stdout.isTTY) {
+      await initProject();
+      return;
+    }
+
     const config = getConfig();
 
     const parsedArguments = parseArguments(
@@ -52,7 +116,7 @@ async function main() {
     if (showStackTraces) {
       console.error(error.stack);
     } else {
-      console.log("For more info run buidler again with --showStackTraces.");
+      console.error("For more info run buidler again with --showStackTraces.");
     }
   }
 }
