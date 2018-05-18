be1200cb083b06b925f026654cfe497f9839f4e9
Patricio Palladino <email@patriciopalladino.com>
2018-05-18 00:21:18 -0300

    Better error reporting in pweb3

--- src/core/web3/pweb3.js
+++ src/core/web3/pweb3.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const importLazy = require('import-lazy')(require);
+const importLazy = require("import-lazy")(require);
 const { promisify } = require("util");
 const Web3 = importLazy("web3");
 
@@ -10,11 +10,25 @@ function promisifyWeb3(web3) {
   const pweb3 = new Web3(web3.currentProvider);
 
   for (const module of WEB3_MODULES) {
-    for (const prop of Object.keys(web3[module])) {
-      const desc = Object.getOwnPropertyDescriptor(pweb3[module], prop);
+    pweb3[module] = {};
 
-      if (desc.get === undefined && pweb3[module][prop] instanceof Function) {
-        pweb3[module][prop] = promisify(pweb3[module][prop]);
+    for (const prop in web3[module]) {
+      const desc = Object.getOwnPropertyDescriptor(web3[module], prop);
+
+      if (desc === undefined) {
+        Object.defineProperty(pweb3[module], prop, {
+          get: () => {
+            throw new Error(`pweb3.${module}.${prop} is not supported`);
+          }
+        });
+      } else if (desc.get !== undefined) {
+        Object.defineProperty(pweb3[module], prop, {
+          get: () => {
+            throw new Error("Synchronous requests are not supported by pweb3.");
+          }
+        });
+      } else if (web3[module][prop] instanceof Function) {
+        pweb3[module][prop] = promisify(web3[module][prop].bind(web3[module]));
       }
     }
   }
