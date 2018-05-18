80f075e11d29f584ada9f302a81d29a3825cb958
Patricio Palladino <email@patriciopalladino.com>
2018-05-18 20:14:42 -0300

    Tidy up project creation

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -4,101 +4,19 @@
 const importLazy = require("import-lazy")(require);
 const fs = importLazy("fs-extra");
 const path = require("path");
-const chalk = importLazy("chalk");
 const inquirer = importLazy("inquirer");
 
 const { getConfig } = require("../core/config");
 const { getTaskDefinitions } = require("../core/tasks/dsl");
 const { createEnvironment } = require("../core/env/definition");
-const {
-  isCwdInsideProject,
-  getRecommendedGitIgnore
-} = require("../core/project-structure");
+const { isCwdInsideProject } = require("../core/project-structure");
+const { enableEmoji } = require("./emoji");
+const { createProject } = require("./project-creation");
 const {
   getMergedParamDefinitions,
   getArgumentsBeforeConfig,
   getArgumentsAfterConfig
 } = require("./params");
-const { enableEmoji, emoji } = require("./emoji");
-
-async function initProject() {
-  const packageInfo = await fs.readJson(
-    path.join(__dirname, "../../package.json")
-  );
-
-  console.log(chalk.blue(`888               d8b      888 888`));
-  console.log(chalk.blue(`888               Y8P      888 888`));
-  console.log(chalk.blue("888                        888 888"));
-  console.log(
-    chalk.blue("88888b.  888  888 888  .d88888 888  .d88b.  888d888")
-  );
-  console.log(chalk.blue('888 "88b 888  888 888 d88" 888 888 d8P  Y8b 888P"'));
-  console.log(chalk.blue("888  888 888  888 888 888  888 888 88888888 888"));
-  console.log(chalk.blue("888 d88P Y88b 888 888 Y88b 888 888 Y8b.     888"));
-  console.log(chalk.blue(`88888P"   "Y88888 888  "Y88888 888  "Y8888  888`));
-  console.log("");
-
-  console.log(
-    chalk.cyan(
-      `${emoji("👷 ")}Welcome to ${packageInfo.name} v${
-        packageInfo.version
-      }${emoji(" 👷‍♀")}‍\n`
-    )
-  );
-
-  const { shouldCreateProject } = await inquirer.prompt([
-    {
-      name: "shouldCreateProject",
-      type: "confirm",
-      message:
-        "You are not inside a buidler project. Do you want to create a new one?"
-    }
-  ]);
-
-  if (!shouldCreateProject) {
-    return;
-  }
-
-  const { projectRoot, shouldAddGitIgnore } = await inquirer.prompt([
-    {
-      name: "projectRoot",
-      default: process.cwd(),
-      message: "Buidler project root:"
-    },
-    {
-      name: "shouldAddGitIgnore",
-      type: "confirm",
-      message: "Do you want to add a .gitignore?"
-    }
-  ]);
-
-  await fs.ensureDir(projectRoot);
-  await fs.copy(
-    path.join(__dirname, "..", "..", "sample-project"),
-    projectRoot
-  );
-
-  if (shouldAddGitIgnore) {
-    const gitIgnorePath = path.join(projectRoot, ".gitignore");
-
-    let content = await getRecommendedGitIgnore();
-
-    if (await fs.pathExists(gitIgnorePath)) {
-      const existingContent = fs.readFile(gitIgnorePath, "utf-8");
-      content = (await existingContent) + "\n" + content;
-    }
-
-    await fs.writeFile(gitIgnorePath, content);
-  }
-
-  console.log(chalk.cyan(`\n${emoji("✨ ")}Project created${emoji(" ✨")}`));
-
-  console.log(`\nTry running running the following tasks:`);
-  console.log(`  buidler compile`);
-  console.log(`  buidler test`);
-  console.log(`  buidler run scripts/deploy.js`);
-  console.log(`  buidler help`);
-}
 
 function printVersionMessage() {
   const packageInfo = fs.readJsonSync(
@@ -126,7 +44,7 @@ async function main() {
     }
 
     if (!isCwdInsideProject() && process.stdout.isTTY) {
-      await initProject(globalArguments);
+      await createProject();
       return;
     }
 
---
+++ src/cli/project-creation.js
@@ -0,0 +1,132 @@
+const importLazy = require("import-lazy")(require);
+const fs = importLazy("fs-extra");
+const path = require("path");
+const chalk = importLazy("chalk");
+const inquirer = importLazy("inquirer");
+
+const { getRecommendedGitIgnore } = require("../core/project-structure");
+const { emoji } = require("./emoji");
+
+async function removeProjectDirIfPresent(projectRoot, dirName) {
+  const dirPath = path.join(projectRoot, dirName);
+  if (await fs.pathExists(dirPath)) {
+    await fs.remove(dirPath);
+  }
+}
+
+async function removeTempFilesIfPresent(projectRoot) {
+  await removeProjectDirIfPresent(projectRoot, "cache");
+  await removeProjectDirIfPresent(projectRoot, "artifacts");
+}
+
+function printAsciiLogo() {
+  console.log(chalk.blue(`888               d8b      888 888`));
+  console.log(chalk.blue(`888               Y8P      888 888`));
+  console.log(chalk.blue("888                        888 888"));
+  console.log(
+    chalk.blue("88888b.  888  888 888  .d88888 888  .d88b.  888d888")
+  );
+  console.log(chalk.blue('888 "88b 888  888 888 d88" 888 888 d8P  Y8b 888P"'));
+  console.log(chalk.blue("888  888 888  888 888 888  888 888 88888888 888"));
+  console.log(chalk.blue("888 d88P Y88b 888 888 Y88b 888 888 Y8b.     888"));
+  console.log(chalk.blue(`88888P"   "Y88888 888  "Y88888 888  "Y8888  888`));
+  console.log("");
+}
+
+async function printWelcomeMessage() {
+  const packageInfo = await fs.readJson(
+    path.join(__dirname, "../../package.json")
+  );
+
+  console.log(
+    chalk.cyan(
+      `${emoji("👷 ")}Welcome to ${packageInfo.name} v${
+        packageInfo.version
+      }${emoji(" 👷‍♀")}‍\n`
+    )
+  );
+}
+
+async function confirmProjectCreation() {
+  const { shouldCreateProject } = await inquirer.prompt([
+    {
+      name: "shouldCreateProject",
+      type: "confirm",
+      message:
+        "You are not inside a buidler project. Do you want to create a new one?"
+    }
+  ]);
+
+  return shouldCreateProject;
+}
+
+async function copySampleProject(projectRoot) {
+  await fs.ensureDir(projectRoot);
+  await fs.copy(
+    path.join(__dirname, "..", "..", "sample-project"),
+    projectRoot
+  );
+
+  await removeTempFilesIfPresent(projectRoot);
+}
+
+async function addGitIgnore(projectRoot) {
+  const gitIgnorePath = path.join(projectRoot, ".gitignore");
+
+  let content = await getRecommendedGitIgnore();
+
+  if (await fs.pathExists(gitIgnorePath)) {
+    const existingContent = fs.readFile(gitIgnorePath, "utf-8");
+    content = (await existingContent) + "\n" + content;
+  }
+
+  await fs.writeFile(gitIgnorePath, content);
+}
+
+function printSuggestedCommands() {
+  console.log(`Try running running the following tasks:`);
+  console.log(`  buidler compile`);
+  console.log(`  buidler test`);
+  console.log(`  buidler deploy`);
+  console.log(`  node scripts/scripted-deployment.js`);
+  console.log(`  buidler help`);
+}
+
+async function createProject() {
+  printAsciiLogo();
+
+  await printWelcomeMessage();
+
+  const shouldCreateProject = await confirmProjectCreation();
+
+  if (!shouldCreateProject) {
+    return;
+  }
+
+  const { projectRoot, shouldAddGitIgnore } = await inquirer.prompt([
+    {
+      name: "projectRoot",
+      default: process.cwd(),
+      message: "Buidler project root:"
+    },
+    {
+      name: "shouldAddGitIgnore",
+      type: "confirm",
+      message: "Do you want to add a .gitignore?"
+    }
+  ]);
+
+  await copySampleProject(projectRoot);
+
+  if (shouldAddGitIgnore) {
+    await addGitIgnore(projectRoot);
+  }
+
+  console.log(chalk.cyan(`\n${emoji("✨ ")}Project created${emoji(" ✨")}`));
+
+  console.log(``);
+
+  printSuggestedCommands();
+}
+
+module.exports = { createProject };
