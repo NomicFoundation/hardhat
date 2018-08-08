e869a0e6668de66c43004c021e9c3a3b199758ea
Patricio Palladino <email@patriciopalladino.com>
2018-08-08 17:13:33 -0300

    Replace arrow functions in sample test.
    Using arrow functions in Mocha is discouraged.
    Closes #7.

--- sample-project/test/greeter.js
+++ sample-project/test/greeter.js
@@ -1,7 +1,7 @@
 const Greeter = artifacts.require("Greeter");
 
 describe("Greeter contract", () => {
-  it("Should return the right greeting", async () => {
+  it("Should return the right greeting", async function () {
     const expectedGreeting = "Hello, buidler!";
 
     const greeter = await Greeter.new(expectedGreeting);
