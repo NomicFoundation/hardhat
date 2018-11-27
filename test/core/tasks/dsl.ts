c442a6e4352851784f470f22fbd191b9434aafec
Patricio Palladino <email@patriciopalladino.com>
2018-11-27 19:50:14 -0300

    Fix failing test

--- test/core/tasks/dsl.ts
+++ test/core/tasks/dsl.ts
@@ -1,5 +1,7 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import { TasksDSL } from "../../../src/core/tasks/dsl";
+import { TaskDefinition } from "../../../src/core/tasks/TaskDefinition";
+import { BuidlerError, ERRORS } from "../../../src/core/errors";
 
 describe("TasksDSL", () => {
   let dsl: TasksDSL;
@@ -8,37 +10,42 @@ describe("TasksDSL", () => {
   });
 
   it("should add a task", () => {
-    const taskName = "compile"
-    const description = "compiler task description"
+    const taskName = "compile";
+    const description = "compiler task description";
     const action = async () => {};
 
     const task = dsl.task(taskName, description, action);
-    
-    assert.equal(task.name, taskName)
-    assert.equal(task.description, description)
-    assert.equal(task.action, action)
-    assert.isFalse(task.isInternal)
-    
+
+    assert.equal(task.name, taskName);
+    assert.equal(task.description, description);
+    assert.equal(task.action, action);
+    assert.isFalse(task.isInternal);
   });
 
   it("should add an internal task", () => {
     const action = async () => {};
-    const task = dsl.internalTask("compile", "compiler task description", action);
-    assert.isTrue(task.isInternal)
+    const task = dsl.internalTask(
+      "compile",
+      "compiler task description",
+      action
+    );
+    assert.isTrue(task.isInternal);
   });
 
   it("should add a task without description", () => {
     const action = async () => {};
     const task = dsl.task("compile", action);
-    assert.isUndefined(task.description)
-    assert.equal(task.action, action)
+    assert.isUndefined(task.description);
+    assert.equal(task.action, action);
   });
 
-  it("should add a task without action", () => {
-    const action = async () => {};
+  it("should add a task with default action", () => {
     const task = dsl.task("compile", "a description");
-    assert.isUndefined(task.action)
-    assert.isDefined(task.description)
+    assert.isDefined(task.description);
+    assert.isDefined(task.action);
+    expect(() => task.action({}, {} as any, async () => {}))
+      .to.throw(BuidlerError)
+      .with.property("number", ERRORS.TASKS_DEFINITION_NO_ACTION.number);
   });
 
   it("should overload task", () => {
@@ -46,18 +53,16 @@ describe("TasksDSL", () => {
 
     const builtin = dsl.task("compile", "built-in", action);
     let tasks = dsl.getTaskDefinitions();
-    assert.equal(tasks["compile"], builtin)
-    
+    assert.equal(tasks["compile"], builtin);
+
     const custom = dsl.task("compile", "custom", action);
     tasks = dsl.getTaskDefinitions();
-    assert.equal(tasks["compile"], custom)
+    assert.equal(tasks["compile"], custom);
   });
 
   it("should return added tasks", () => {
     const task = dsl.task("compile", "built-in");
     let tasks = dsl.getTaskDefinitions();
-    assert.deepEqual(tasks, {compile: task})
+    assert.deepEqual(tasks, { compile: task });
   });
-
-
 });
