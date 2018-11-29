3c3fe16e15ac3a79395bb14f9edbe392ec3290a8
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2018-11-29 18:28:49 -0300

    Forgot to uncomment the declare module.

--- src/@types/not-typed.d.ts
+++ src/@types/not-typed.d.ts
@@ -1,6 +1,6 @@
 declare module "solc/Wrapper";
 declare module "ethereumjs-util";
 declare module "web3";
-// declare module "deepmerge";
+declare module "deepmerge";
 declare module "repl.history";
 declare module "inquirer";
