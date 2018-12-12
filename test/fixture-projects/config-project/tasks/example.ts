9712cfa06c4e6c7cc097bfcac7d3d6dccafc5a33
Patricio Palladino <email@patriciopalladino.com>
2018-12-12 11:24:12 -0300

    Run prettier

--- test/fixture-projects/config-project/tasks/example.ts
+++ test/fixture-projects/config-project/tasks/example.ts
@@ -2,7 +2,4 @@ import tasks from "../../../../src/core/importable-tasks-dsl";
 
 tasks.task("example2", "example task", async ret => 28);
 
-
 tasks.task("example", "example task", async (__, { run }) => run("example2"));
-
-
