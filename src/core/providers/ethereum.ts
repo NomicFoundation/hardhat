5c41030a639851998c65adc4f86a54b228a80dac
Patricio Palladino <email@patriciopalladino.com>
2018-12-17 15:52:13 -0300

    Remove provider adapters in favor of Web3X's ones

--- src/core/providers/ethereum.ts
+++ src/core/providers/ethereum.ts
@@ -1,65 +1,6 @@
 import { EventEmitter } from "events";
-import { promisify } from "util";
-import {
-  createJsonRpcPayload,
-  JsonRpcRequest,
-  JsonRpcResponse
-} from "web3x/providers/jsonrpc";
-import { LegacyProvider } from "web3x/providers/legacy-provider";
+import { EthereumProvider } from "web3x/providers";
 
-export interface IEthereumProvider extends EventEmitter {
-  send(method: string, params?: any[]): Promise<any>;
-  on(type: string, listener: (result: any) => void): this;
-}
-
-class EthereumProviderError extends Error {
-  constructor(
-    public readonly message: string,
-    public readonly code: number,
-    public readonly data: any
-  ) {
-    super(message);
-  }
-}
-
-export class EthereumProvider extends EventEmitter
-  implements IEthereumProvider {
-  constructor(private readonly provider: LegacyProvider) {
-    super();
-  }
-
-  public async send(method: string, params?: any[]): Promise<any> {
-    if (method === "eth_requestAccounts") {
-      throw new Error("'eth_requestAccounts' is not yet supported");
-    }
-
-    const payload = createJsonRpcPayload(method, params);
-
-    const promisifiedSend: (
-      payload: JsonRpcRequest
-    ) => Promise<JsonRpcResponse> = promisify(
-      this.provider.send.bind(this.provider)
-    );
-
-    const response = (await promisifiedSend(payload)) as JsonRpcResponse;
-
-    if (response.error === undefined) {
-      return response.result;
-    } else {
-      const error = response.error;
-
-      throw new EthereumProviderError(error.message, error.code, error.data);
-    }
-  }
-
-  public on(event: string | symbol, listener: (...args: any[]) => void): this {
-    throw new Error("Event listeners are not yet supported");
-  }
-
-  public once(
-    event: string | symbol,
-    listener: (...args: any[]) => void
-  ): this {
-    throw new Error("Event listeners are not yet supported");
-  }
-}
+// TODO: This may not be the correct interface, see
+// https://ethereum-magicians.org/t/eip-1193-ethereum-provider-javascript-api/640/31?u=alcuadrado
+export type IEthereumProvider = EthereumProvider & EventEmitter;
