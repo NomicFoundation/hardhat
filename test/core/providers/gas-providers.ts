f22976c583d9194ab1879fbcea161eb2e2738939
Patricio Palladino <email@patriciopalladino.com>
2018-12-18 12:54:45 -0300

    Update gas estimation/price providers to the new API

--- test/core/providers/gas-providers.ts
+++ test/core/providers/gas-providers.ts
@@ -1,237 +1,206 @@
 import { assert } from "chai";
 
+import { IEthereumProvider } from "../../../src/core/providers/ethereum";
 import {
-  AutomaticGasProvider,
-  FixedGasProvider
+  createAutomaticGasPriceProvider,
+  createAutomaticGasProvider,
+  createFixedGasPriceProvider,
+  createFixedGasProvider
 } from "../../../src/core/providers/gas-providers";
 
 import { ParamsReturningProvider } from "./mocks";
 
-describe("FixedGasProvider", () => {
-  let mockedProvider: ParamsReturningProvider;
+describe("createAutomaticGasProvider", () => {
+  const FIXED_GAS_LIMIT = 1231;
+  let provider: IEthereumProvider;
 
   beforeEach(() => {
-    mockedProvider = new ParamsReturningProvider();
+    const mockedProvider = new ParamsReturningProvider();
+    const fixedGasProvider = createFixedGasProvider(
+      mockedProvider,
+      FIXED_GAS_LIMIT
+    );
+    provider = createAutomaticGasProvider(fixedGasProvider);
   });
 
-  describe("With fixed gasLimit and without gasPrice", () => {
-    let provider: FixedGasProvider;
-    const FIXED_GAS_LIMIT = 123;
-
-    beforeEach(() => {
-      provider = new FixedGasProvider(mockedProvider, FIXED_GAS_LIMIT);
-    });
-
-    it("should return the fixed gas when estimating", async () => {
-      const ret1 = await provider.send("eth_estimateGas", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          gasPrice: 5678,
-          value: 1
-        }
-      ]);
-
-      assert.equal(ret1, FIXED_GAS_LIMIT);
-
-      const ret2 = await provider.send("eth_estimateGas", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          gasPrice: 5678,
-          value: 1,
-          gas: 78990878
-        }
-      ]);
-
-      assert.equal(ret1, FIXED_GAS_LIMIT);
-    });
-
-    it("Should add gas to a tx if non is present, but not a gasPrice", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          value: 1
-        }
-      ]);
-
-      assert.equal(tx.gas, FIXED_GAS_LIMIT);
-      assert.isUndefined(tx.gasPrice);
-    });
-
-    it("Shouldn't change the gas nor the gasPrice", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          gasPrice: 5678,
-          value: 1,
-          gas: 678
-        }
-      ]);
-
-      assert.equal(tx.gas, 678);
-      assert.equal(tx.gasPrice, 5678);
-    });
-
-    it("Shouldn't return another gasPrice", async () => {
-      const [mockedPrice] = await provider.send("eth_gasPrice", [678]);
-
-      assert.equal(mockedPrice, 678);
-    });
-  });
-
-  describe("With fixed gasPrice and no gasLimit", () => {
-    let provider: FixedGasProvider;
-    const FIXED_GAS_PRICE = 123;
-
-    beforeEach(() => {
-      provider = new FixedGasProvider(
-        mockedProvider,
-        undefined,
-        FIXED_GAS_PRICE
-      );
-    });
-
-    it("Shouldn return the fixed gasPrice", async () => {
-      const price = await provider.send("eth_gasPrice");
-
-      assert.equal(price, FIXED_GAS_PRICE);
-    });
-
-    it("Shouldn't modify the gas estimation", async () => {
-      const inputTx = {
+  it("Should estimate gas automatically if not present", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
         from: "0x0000000000000000000000000000000000000011",
         to: "0x0000000000000000000000000000000000000011",
-        gasPrice: 5678,
         value: 1
-      };
+      }
+    ]);
 
-      const [tx] = await provider.send("eth_estimateGas", [inputTx]);
+    assert.equal(tx.gas, FIXED_GAS_LIMIT);
+  });
 
-      assert.deepEqual(tx, inputTx);
-    });
+  it("Shouldn't replace the provided gas", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1,
+        gas: 567
+      }
+    ]);
 
-    it("Should add a gasPrice and not gas if they are missing", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          value: 1
-        }
-      ]);
+    assert.equal(tx.gas, 567);
+  });
 
-      assert.equal(tx.gasPrice, FIXED_GAS_PRICE);
-      assert.isUndefined(tx.gas);
-    });
+  it("Should forward the other calls", async () => {
+    const input = [1, 2, 3];
+    const returned = await provider.send("A", input);
 
-    it("Shouldn't change the gas nor the gasPrice", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          gasPrice: 5678,
-          value: 1,
-          gas: 678
-        }
-      ]);
-
-      assert.equal(tx.gas, 678);
-      assert.equal(tx.gasPrice, 5678);
-    });
+    assert.deepEqual(returned, input);
   });
 });
 
-describe("AutomaticGasProvider", () => {
-  const FIXED_GAS_LIMIT = 123;
-  const FIXED_GAS_PRICE = 678;
-
-  let mockedProvider: ParamsReturningProvider;
+describe("createAutomaticGasPriceProvider", () => {
+  const FIXED_GAS_PRICE = 1232;
+  let provider: IEthereumProvider;
 
   beforeEach(() => {
-    mockedProvider = new ParamsReturningProvider();
+    const mockedProvider = new ParamsReturningProvider();
+    const fixedGasProvider = createFixedGasPriceProvider(
+      mockedProvider,
+      FIXED_GAS_PRICE
+    );
+    provider = createAutomaticGasPriceProvider(fixedGasProvider);
   });
 
-  describe("With automatic gasLimit and not gasPrice", () => {
-    let provider: AutomaticGasProvider;
-    let fixedGasProvider: FixedGasProvider;
+  it("Should obtain the gas price automatically if not present", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1
+      }
+    ]);
 
-    beforeEach(() => {
-      fixedGasProvider = new FixedGasProvider(mockedProvider, FIXED_GAS_LIMIT);
-
-      provider = new AutomaticGasProvider(fixedGasProvider, true, false);
-    });
-
-    it("Should add gas to a tx if non is present, but not a gasPrice", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          value: 1
-        }
-      ]);
-
-      assert.equal(tx.gas, FIXED_GAS_LIMIT);
-      assert.isUndefined(tx.gasPrice);
-    });
-
-    it("Shouldn't change the gas nor the gasPrice", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          gasPrice: 5678,
-          value: 1,
-          gas: 678
-        }
-      ]);
-
-      assert.equal(tx.gas, 678);
-      assert.equal(tx.gasPrice, 5678);
-    });
+    assert.equal(tx.gasPrice, FIXED_GAS_PRICE);
   });
 
-  describe("With automatic gasPrice and not gasLimit", () => {
-    let provider: AutomaticGasProvider;
-    let fixedGasProvider: FixedGasProvider;
+  it("Shouldn't replace the provided gasPrice", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1,
+        gasPrice: 456
+      }
+    ]);
 
-    beforeEach(() => {
-      fixedGasProvider = new FixedGasProvider(
-        mockedProvider,
-        undefined,
-        FIXED_GAS_PRICE
-      );
+    assert.equal(tx.gasPrice, 456);
+  });
 
-      provider = new AutomaticGasProvider(fixedGasProvider, false, true);
-    });
+  it("Should forward the other calls", async () => {
+    const input = [1, 2, 3, 4];
+    const returned = await provider.send("A", input);
 
-    it("Should add a gasPrice and not gas if they are missing", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          value: 1
-        }
-      ]);
+    assert.deepEqual(returned, input);
+  });
+});
 
-      assert.equal(tx.gasPrice, FIXED_GAS_PRICE);
-      assert.isUndefined(tx.gas);
-    });
+describe("createFixedGasProvider", () => {
+  const FIXED_GAS_LIMIT = 1233;
+  let provider: IEthereumProvider;
 
-    it("Shouldn't change the gas nor the gasPrice", async () => {
-      const [tx] = await provider.send("eth_sendTransaction", [
-        {
-          from: "0x0000000000000000000000000000000000000011",
-          to: "0x0000000000000000000000000000000000000011",
-          gasPrice: 5678,
-          value: 1,
-          gas: 678
-        }
-      ]);
+  beforeEach(() => {
+    const mockedProvider = new ParamsReturningProvider();
+    provider = createFixedGasProvider(mockedProvider, FIXED_GAS_LIMIT);
+  });
 
-      assert.equal(tx.gas, 678);
-      assert.equal(tx.gasPrice, 5678);
-    });
+  it("Should set the fixed gas if not present", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1
+      }
+    ]);
+
+    assert.equal(tx.gas, FIXED_GAS_LIMIT);
+  });
+
+  it("Shouldn't replace the provided gas", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1,
+        gas: 1456
+      }
+    ]);
+
+    assert.equal(tx.gas, 1456);
+  });
+
+  it("Should return the fixed gas when estimating", async () => {
+    const estimated = await provider.send("eth_estimateGas", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1,
+        gas: 1456123
+      }
+    ]);
+
+    assert.equal(estimated, FIXED_GAS_LIMIT);
+  });
+
+  it("Should forward the other calls", async () => {
+    const input = [1, 2, 3, 4, 5];
+    const returned = await provider.send("A", input);
+
+    assert.deepEqual(returned, input);
+  });
+});
+
+describe("createFixedGasPriceProvider", () => {
+  const FIXED_GAS_PRICE = 1234;
+  let provider: IEthereumProvider;
+
+  beforeEach(() => {
+    const mockedProvider = new ParamsReturningProvider();
+    provider = createFixedGasPriceProvider(mockedProvider, FIXED_GAS_PRICE);
+  });
+
+  it("Should set the fixed gasPrice if not present", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1
+      }
+    ]);
+
+    assert.equal(tx.gasPrice, FIXED_GAS_PRICE);
+  });
+
+  it("Shouldn't replace the provided gasPrice", async () => {
+    const [tx] = await provider.send("eth_sendTransaction", [
+      {
+        from: "0x0000000000000000000000000000000000000011",
+        to: "0x0000000000000000000000000000000000000011",
+        value: 1,
+        gasPrice: 14567
+      }
+    ]);
+
+    assert.equal(tx.gasPrice, 14567);
+  });
+
+  it("Should return the fixed gas price when requested", async () => {
+    const price = await provider.send("eth_gasPrice");
+
+    assert.equal(price, FIXED_GAS_PRICE);
+  });
+
+  it("Should forward the other calls", async () => {
+    const input = [1, 2, 3, 4, 5, 6];
+    const returned = await provider.send("A", input);
+
+    assert.deepEqual(returned, input);
   });
 });
