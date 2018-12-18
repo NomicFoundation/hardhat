30b5d8d8cbf12b7cfb5937a435218db7e5a4b95c
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2018-12-18 13:19:45 -0300

    Update fixture config-project buidler config

--- test/fixture-projects/config-project/buidler-config.js
+++ test/fixture-projects/config-project/buidler-config.js
@@ -2,11 +2,12 @@ module.exports = {
   networks: {
     custom: {
       host: "128.0.0.1",
-      port: 8545
+      port: 8545,
     },
     develop: {
-      host: "128.0.0.1",
-      port: 1337
+      host: "127.0.0.1",
+      port: 8545,
+      accounts: ["a95f9e3e7ae4e4865c5968828fe7c03fffa8a9f3bb52d36d26243f4c868ee166"]
     }
   }
 };
