14f7dd2fdc0b6d4d1b0cb599f9d2c5b46a0a5777
Patricio Palladino <email@patriciopalladino.com>
2018-12-19 23:10:15 -0300

    Add a root-level config.ts
    This file is there to make importing the config environment easier. This
    way using require("buidler/config") works.

---
+++ config.ts
@@ -0,0 +1,7 @@
+import * as argumentTypes from "./dist/src/core/argumentTypes";
+import dsl from "./dist/src/core/importable-tasks-dsl";
+
+export const types = argumentTypes;
+export const tasks = dsl;
+
+export { BuidlerConfig } from "./dist/src/types";
