36b40de25c42ae9dc55cad684ec68be9ea7e2519
Patricio Palladino <email@patriciopalladino.com>
2018-12-27 19:02:07 -0300

    Use the new helper in buidler-lib's tests

--- test/lib/buidler-lib.ts
+++ test/lib/buidler-lib.ts
@@ -1,40 +1,34 @@
 import { assert } from "chai";
 
-import { BuidlerRuntimeEnvironment } from "../../src/core/runtime-environment";
+import { useEnvironment } from "../helpers/environment";
 import { useFixtureProject } from "../helpers/project";
 
 describe("Buidler lib", () => {
-  let environment: BuidlerRuntimeEnvironment;
   useFixtureProject("config-project");
+  useEnvironment();
 
-  before(() => {
-    environment = require("../../src/lib/buidler-lib").default;
+  it("should load environment", function() {
+    assert.isDefined(this.env.config.networks.custom);
   });
 
-  it("should load environment", () => {
-    assert.isDefined(environment.config.networks.custom);
+  it("should load task user defined task", async function() {
+    assert.isDefined(this.env.tasks.example2);
+    assert.equal(await this.env.run("example2"), 28);
   });
 
-  it("should load task user defined task", async () => {
-    assert.isDefined(environment.tasks.example2);
-    assert.equal(await environment.run("example2"), 28);
-  });
-
-  it("should reuse global state", async () => {
-    let e: any;
-    e = environment;
-    environment = require("../../src/lib/buidler-lib").default;
-    assert.isTrue(e === environment);
+  it("should reuse global state", async function() {
+    let environment = require("../../src/lib/buidler-lib").default;
+    assert.isTrue(this.env === environment);
 
     // delete the cached version of buidler lib exported module.
     delete require.cache[require.resolve("../../src/lib/buidler-lib")];
 
     environment = require("../../src/lib/buidler-lib").default;
     assert.equal(await environment.run("example"), 28);
-    assert.isFalse(e === environment);
+    assert.isFalse(this.env === environment);
   });
 
-  it("should reuse global state", async () => {
-    assert.equal(await environment.run("example"), 28);
+  it("should reuse global state", async function() {
+    assert.equal(await this.env.run("example"), 28);
   });
 });
