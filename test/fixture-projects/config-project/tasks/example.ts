236d89ddb5a3de63f33908178954a268a608cf4a
Patricio Palladino <email@patriciopalladino.com>
2018-12-29 15:56:40 -0300

    Make mocha exclude helpers and fixtures.
    This commit also fixes a fixture that was based on that behaviour

--- test/fixture-projects/config-project/tasks/example.ts
+++
@@ -1,5 +0,0 @@
-import { task } from "../../../../src/core/config/config-env";
-
-task("example2", "example task", async ret => 28);
-
-task("example", "example task", async (__, { run }) => run("example2"));
