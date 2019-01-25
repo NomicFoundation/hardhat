0a47111a06343c5d4a6df8cec7cd1d686d28952f
Patricio Palladino <email@patriciopalladino.com>
2019-01-25 20:09:28 -0300

    Use script runner in the console to save the history

--- src/builtin-tasks/console.ts
+++ src/builtin-tasks/console.ts
@@ -1,21 +1,21 @@
 import { task } from "../internal/core/config/config-env";
+import { runScript } from "../internal/util/scripts-runner";
 
 task("console", "Opens a buidler console")
   .addFlag("noCompile", "Don't compile before running this task")
   .setAction(async ({ noCompile }: { noCompile: boolean }, { config, run }) => {
     const path = await import("path");
     const fsExtra = await import("fs-extra");
-    const repl = await import("repl");
-    const { default: replHistory } = await import("repl.history");
 
     if (!noCompile) {
       await run("compile");
     }
 
     await fsExtra.ensureDir(config.paths.cache);
-    const historyFile = path.join(config.paths.cache, "console_history");
+    const historyFile = path.join(config.paths.cache, "console-history.txt");
 
-    const theRepl = repl.start({ useGlobal: true, ignoreUndefined: true });
-
-    replHistory(theRepl, historyFile);
+    // Running the script "" is like running `node`, so this starts the repl
+    await runScript("", [], ["--require", __dirname + "/../register"], {
+      NODE_REPL_HISTORY: historyFile
+    });
   });
