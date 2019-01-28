3335e7a3d8c294a37ec0e344c03e39986b05eb11
Patricio Palladino <email@patriciopalladino.com>
2019-01-28 13:03:23 -0300

    Remove unnecessary newline

--- src/builtin-tasks/flatten.ts
+++ src/builtin-tasks/flatten.ts
@@ -58,7 +58,7 @@ internalTask(
     const packageJson = await getPackageJson();
     flattened += `// Sources flattened with buidler v${
       packageJson.version
-    } https://getbuidler.com\n`;
+    } https://getbuidler.com`;
 
     const graph = await run(TASK_COMPILE_GET_DEPENDENCY_GRAPH);
     const sortedFiles = getSortedFiles(graph);
