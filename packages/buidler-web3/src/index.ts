84a9c20a181da60569884f2aa84641ec2bfc6da5
Patricio Palladino <email@patriciopalladino.com>
2019-02-01 17:37:38 -0300

    [buidler-web3] Load web3 lazily

--- packages/buidler-web3/src/index.ts
+++ packages/buidler-web3/src/index.ts
@@ -1,5 +1,5 @@
 import { extendEnvironment } from "@nomiclabs/buidler/config";
-import Web3 from "web3";
+import { lazyFunction, lazyObject } from "@nomiclabs/buidler/plugins";
 
 import { Web3HTTPProviderAdapter } from "./web3-provider-adapter";
 
@@ -11,6 +11,22 @@ declare module "@nomiclabs/buidler/types" {
 }
 
 extendEnvironment(env => {
-  env.Web3 = Web3;
-  env.web3 = new Web3(new Web3HTTPProviderAdapter(env.provider));
+  env.Web3 = lazyFunction(() => require("web3"));
+  env.web3 = lazyObject(() => {
+    // This function is designed to import and initialize web3 in a lazy fashion.
+    // It has to remove web3 from global, as some web3 files access global.web3,
+    // which will trigger this again recrusively and fail.
+    const globalAsAny = global as any;
+    const hasGlobalWeb3 = typeof globalAsAny.web3 !== "undefined";
+    globalAsAny.web3 = undefined;
+
+    const Web3 = require("web3");
+    const web3 = new Web3(new Web3HTTPProviderAdapter(env.provider));
+
+    if (hasGlobalWeb3) {
+      globalAsAny.web3 = web3;
+    }
+
+    return web3;
+  });
 });
