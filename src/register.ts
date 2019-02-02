471675445bb155dad972766c5488c1e30f1bb1b2
Patricio Palladino <email@patriciopalladino.com>
2019-02-02 20:06:45 -0300

    Disable showProxy from repl's writer (#165)

--- src/register.ts
+++ src/register.ts
@@ -2,12 +2,20 @@ import { loadConfigAndTasks } from "./internal/core/config/config-loading";
 import { BUIDLER_PARAM_DEFINITIONS } from "./internal/core/params/buidler-params";
 import { getEnvBuidlerArguments } from "./internal/core/params/env-variables";
 import { Environment } from "./internal/core/runtime-environment";
+import {
+  disableReplWriterShowProxy,
+  isNodeCalledWithoutAScript
+} from "./internal/util/console";
 
 const globalAsAny = global as any;
 
 if (globalAsAny.env === undefined) {
   // tslint:disable-next-line no-var-requires
   require("source-map-support/register");
+
+  if (isNodeCalledWithoutAScript()) {
+    disableReplWriterShowProxy();
+  }
 
   const buidlerArguments = getEnvBuidlerArguments(
     BUIDLER_PARAM_DEFINITIONS,
