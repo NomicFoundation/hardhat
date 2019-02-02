8164880cb9a89659f98b0c0a5c4f389c09cbe541
Patricio Palladino <email@patriciopalladino.com>
2019-02-02 20:46:51 -0300

    [buidler-truffle4] Make env.artifacts lazy

--- packages/buidler-truffle4/src/index.ts
+++ packages/buidler-truffle4/src/index.ts
@@ -1,5 +1,6 @@
 import "@nomiclabs/buidler-web3-legacy";
 import { extendEnvironment, internalTask } from "@nomiclabs/buidler/config";
+import { lazyObject } from "@nomiclabs/buidler/plugins";
 
 import { TruffleEnvironmentArtifacts } from "./artifacts";
 import { LazyTruffleContractProvisioner } from "./provisioner";
@@ -11,15 +12,17 @@ declare module "@nomiclabs/buidler/types" {
 }
 
 extendEnvironment(env => {
-  const provisioner = new LazyTruffleContractProvisioner(
-    env.web3,
-    env.config.networks[env.buidlerArguments.network].from
-  );
+  env.artifacts = lazyObject(() => {
+    const provisioner = new LazyTruffleContractProvisioner(
+      env.web3,
+      env.config.networks[env.buidlerArguments.network].from
+    );
 
-  env.artifacts = new TruffleEnvironmentArtifacts(
-    env.config.paths.artifacts,
-    provisioner
-  );
+    return new TruffleEnvironmentArtifacts(
+      env.config.paths.artifacts,
+      provisioner
+    );
+  });
 });
 
 internalTask("builtin:setup-test-environment", async (_, { pweb3 }) => {
