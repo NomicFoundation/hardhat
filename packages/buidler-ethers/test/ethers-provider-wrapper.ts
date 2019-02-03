af84f2b46c19e8ed2fbc9465e43b6644bcda537c
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-02-03 19:04:24 -0300

    [buidler-ethers] add ethers provider wrapper tests.

---
+++ packages/buidler-ethers/test/ethers-provider-wrapper.ts
@@ -0,0 +1,43 @@
+import { assert } from "chai";
+import { JsonRpcProvider } from "ethers/providers";
+
+import { EthersProviderWrapper } from "../src/ethers-provider-wrapper";
+
+describe("Ethers provider wrapper", function() {
+  let realProvider: JsonRpcProvider;
+  let wrapper: EthersProviderWrapper;
+
+  before("Setup buidler project", function() {
+    process.chdir(__dirname);
+    process.env.BUIDLER_NETWORK = "develop";
+  });
+
+  beforeEach(function() {
+    delete require.cache[require.resolve("@nomiclabs/buidler")];
+    this.env = require("@nomiclabs/buidler");
+    realProvider = new JsonRpcProvider();
+    wrapper = new EthersProviderWrapper(this.env.provider);
+  });
+
+  it("Should return the same as the real provider", async () => {
+    const response = await realProvider.send("eth_accounts", []);
+    const response2 = await wrapper.send("eth_accounts", []);
+
+    assert.deepEqual(response, response2);
+  });
+
+  it("Should return the same error", done => {
+    realProvider
+      .send("error_please", [])
+      .then(_ => assert.fail())
+      .catch(err => {
+        wrapper
+          .send("error_please", [])
+          .then(_ => assert.fail())
+          .catch(err2 => {
+            assert.deepEqual(err2.message, err.message);
+            done();
+          });
+      });
+  });
+});
