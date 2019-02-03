47e0f8f872d030372c1e2c3d448fae63c7c1b4e0
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-02-03 18:02:39 -0300

    [buidler-ethers] implement environment extension.

--- packages/buidler-ethers/src/index.ts
+++ packages/buidler-ethers/src/index.ts
@@ -1,6 +1,7 @@
 import { extendEnvironment } from "@nomiclabs/buidler/config";
+import { readArtifactSync } from "@nomiclabs/buidler/plugins";
 import { BuidlerRuntimeEnvironment } from "@nomiclabs/buidler/types";
-import { ContractFactory, Signer } from "ethers";
+import { ContractFactory, ethers, Signer } from "ethers";
 
 import { EthersProviderWrapper } from "./ethers-provider-wrapper";
 
@@ -14,4 +15,19 @@ declare module "@nomiclabs/buidler/types" {
   }
 }
 
-extendEnvironment((env: BuidlerRuntimeEnvironment) => {});
+extendEnvironment((env: BuidlerRuntimeEnvironment) => {
+  const wrapper = new EthersProviderWrapper(env.provider);
+  env.ethers = {
+    provider: wrapper,
+    getContract: async (name: string): Promise<ContractFactory> => {
+      const artifact = readArtifactSync(env.config.paths.artifacts, name);
+      const bytecode = artifact.bytecode;
+      const signers = await env.ethers.signers();
+      return new ethers.ContractFactory(artifact.abi, bytecode, signers[0]);
+    },
+    signers: async (): Promise<Signer[]> => {
+      const accounts = await env.provider.send("eth_accounts");
+      return accounts.map((account: string) => wrapper.getSigner(account));
+    }
+  };
+});
