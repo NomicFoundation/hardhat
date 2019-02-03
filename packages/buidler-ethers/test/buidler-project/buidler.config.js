f6aded3cbd36a17fdf101010c3b742e109ee55c4
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-02-03 18:25:53 -0300

    [buidler-ethers] add provider's accounts config into the buidler's config.

--- packages/buidler-ethers/test/buidler-project/buidler.config.js
+++ packages/buidler-ethers/test/buidler-project/buidler.config.js
@@ -1,3 +1,12 @@
 require("../../src/index");
 
-module.exports = {};
+module.exports = {
+    networks: {
+      develop: {
+        accounts: [
+          "0xa95f9e3e7ae4e4865c5968828fe7c03fffa8a9f3bb52d36d26243f4c868ee166"
+        ]
+      }
+    }
+  };
+  
