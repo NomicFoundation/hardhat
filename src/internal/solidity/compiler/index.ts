50b8519853c2d4e19dc07a63081327197c89a11d
Patricio Palladino <email@patriciopalladino.com>
2019-02-05 16:48:23 -0300

    Fix two bugs (#168)
    * Fix bug in test task glob pattern
    
    * Make the Compiler bypass any require hook

--- src/internal/solidity/compiler/index.ts
+++ src/internal/solidity/compiler/index.ts
@@ -78,12 +78,38 @@ export class Compiler {
     );
 
     const { default: solcWrapper } = await import("solc/wrapper");
-    this.loadedSolc = solcWrapper(await import(compilerPath));
+    this.loadedSolc = solcWrapper(this.loadCompilerSources(compilerPath));
 
     return this.loadedSolc;
   }
 
   private isUsingLocalSolcVersion() {
     return this.version === this.localSolcVersion;
+  }
+
+  /**
+   * This function loads the compiler sources bypassing any require hook.
+   *
+   * The compiler is a huge asm.js file, and using a simple require may trigger
+   * babel/register and hang the process.
+   */
+  private loadCompilerSources(compilerPath: string) {
+    const Module = module.constructor as any;
+    const previousHook = Module._extensions[".js"];
+
+    Module._extensions[".js"] = function(
+      module: NodeJS.Module,
+      filename: string
+    ) {
+      const fs = require("fs");
+      const content = fs.readFileSync(filename, "utf8");
+      Object.getPrototypeOf(module)._compile.call(module, content, filename);
+    };
+
+    const loadedSolc = require(compilerPath);
+
+    Module._extensions[".js"] = previousHook;
+
+    return loadedSolc;
   }
 }
