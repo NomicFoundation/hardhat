30ad75655554d7f380f7bcda1d7e8783adef56d3
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-02-06 13:22:59 -0300

    [buidler-ethers] get rid of unneeded types.

--- packages/buidler-ethers/src/ethers-provider-wrapper.ts
+++ packages/buidler-ethers/src/ethers-provider-wrapper.ts
@@ -1,51 +1,23 @@
 import { IEthereumProvider } from "@nomiclabs/buidler/types";
 import { JsonRpcProvider } from "ethers/providers";
 
-export interface JsonRpcRequest {
-  jsonrpc: string;
-  method: string;
-  params: any[];
-  id: number;
-}
-
-export interface JsonRpcResponse {
-  jsonrpc: string;
-  id: number;
-  result?: any;
-  error?: {
-    code: number;
-    message: string;
-    data?: any;
-  };
-}
-
-let nextId = 1;
-
-// This can be avoided if the ethers' request creation is extracted to another method.
-function createJsonRpcRequest(
-  method: string,
-  params: any[] = []
-): JsonRpcRequest {
-  return {
-    id: nextId++,
-    jsonrpc: "2.0",
-    method,
-    params
-  };
-}
-
 export class EthersProviderWrapper extends JsonRpcProvider {
   constructor(private readonly provider: IEthereumProvider) {
     super();
   }
 
-  public async send(method: string, params: any): Promise<JsonRpcResponse> {
+  public async send(method: string, params: any): Promise<any> {
     const result = await this.provider.send(method, params);
 
     // We replicate ethers' behavior.
     this.emit("debug", {
       action: "send",
-      request: createJsonRpcRequest(method, params),
+      request: {
+        id: 42,
+        jsonrpc: "2.0",
+        method,
+        params
+      },
       response: result,
       provider: this
     });
