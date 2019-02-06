3d61a504b01899c3e2854d502b8fc169f5a75416
Franco Zeoli <franco@nomiclabs.io>
2019-02-06 14:52:19 -0300

    Use the nicer API for the sample task.

--- sample-project/buidler.config.js
+++ sample-project/buidler.config.js
@@ -1,5 +1,4 @@
-task("accounts", "Prints a list of the available accounts")
- .setAction(async taskArgs => {
+task("accounts", "Prints a list of the available accounts", async taskArgs => {
      const accounts = await env.provider.send("eth_accounts");
 
      console.log("Accounts:", accounts);
