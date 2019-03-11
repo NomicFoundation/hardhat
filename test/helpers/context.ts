ee2c8144de40daf3c7e473d8d7c7cd7ca7004337
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-03-11 15:29:29 -0300

    Add context tests.

---
+++ test/helpers/context.ts
@@ -0,0 +1,34 @@
+import { BuidlerContext } from "../../src/internal/context";
+import { glob } from "../../src/internal/util/glob";
+
+export async function resetBuidlerContext() {
+  if (BuidlerContext.isCreated()) {
+    const ctx = BuidlerContext.getBuidlerContext();
+    const globalAsAny = global as any;
+    if (ctx.environment !== undefined) {
+      for (const key of Object.keys(ctx.environment)) {
+        globalAsAny[key] = undefined;
+      }
+      // unload config file too.
+      unloadModule(ctx.environment.config.paths.configFile);
+    }
+    BuidlerContext.deleteBuidlerContext();
+  }
+  unloadModule("../../src/register");
+  unloadModule("../../src/internal/cli/cli");
+  unloadModule("../../src/internal/lib/buidler-lib");
+  unloadModule("../../src/internal/core/config/config-env");
+  unloadModule("../../src/internal/core/tasks/builtin-tasks");
+  const tasks = await glob(__dirname + "/../../src/builtin-tasks/**/*");
+  tasks.forEach((task: string) => {
+    unloadModule(task);
+  });
+}
+
+export function unloadModule(path: string) {
+  try {
+    delete require.cache[require.resolve(path)];
+  } catch (err) {
+    // module wasn't loaded
+  }
+}
