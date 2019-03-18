8af0b641b6b325980b731e9f82d689201123867d
Marin Petrunić <marin.petrunic@gmail.com>
2019-03-18 11:50:04 +0100

    get flatten contract source

--- packages/buidler-etherscan/src/index.ts
+++ packages/buidler-etherscan/src/index.ts
@@ -1,6 +1,7 @@
 import { extendEnvironment } from "@nomiclabs/buidler/config";
-import { lazyObject } from "@nomiclabs/buidler/plugins";
+import { lazyObject, BuidlerPluginError } from "@nomiclabs/buidler/plugins";
 import {task} from "@nomiclabs/buidler/internal/core/config/config-env"
+import {TASK_FLATTEN_GET_FLATTENED_SOURCE} from "@nomiclabs/buidler/builtin-tasks/task-names"
 
 export class EtherscanBuidlerEnvironment {
     constructor(
@@ -23,10 +24,6 @@ declare module "@nomiclabs/buidler/types" {
   }
 }
 
-task('verify', "Verifies contract on etherscan", async (taskArgs, env) => {
-  console.log({etherscan: env.etherscan})
-});
-
 extendEnvironment(env => {
   env.etherscan = lazyObject(
       () => new EtherscanBuidlerEnvironment(
@@ -34,4 +31,12 @@ extendEnvironment(env => {
           env.config.etherscan.token
       )
   );
+});
+
+
+task('verify', "Verifies contract on etherscan", async (taskArgs, {config, run}) => {
+    if(!config.etherscan.token || !config.etherscan.token.trim()) {
+        throw new BuidlerPluginError('Please provide etherscan api token via buidler.config.js (etherscan.token)')
+    }
+    const flattenedSource = await run(TASK_FLATTEN_GET_FLATTENED_SOURCE);
 });
