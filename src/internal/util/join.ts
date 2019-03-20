cdf6ae0e5461496bdad83cbc32685320951695b3
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-03-20 17:30:15 -0300

    Add adhoc join function to avoid missing the initial dot in relatives paths.

---
+++ src/internal/util/join.ts
@@ -0,0 +1,6 @@
+import path from "path";
+
+export function join(...args: string[]): string {
+  const p = path.join(...args);
+  return args.length > 0 && args[0] === "." ? "." + path.sep + p : p;
+}
