c26a6b9d9b2b110602b113f9281208970f152bc9
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-03-21 02:06:42 -0300

    Use path.sep for library tests.

--- packages/buidler-core/src/internal/util/join.ts
+++ packages/buidler-core/src/internal/util/join.ts
@@ -2,5 +2,5 @@ import path from "path";
 
 export function join(...args: string[]): string {
   const p = path.join(...args);
-  return args.length > 0 && args[0] === "." ? "." + path.sep + p : p;
+  return args.length > 0 && args[0] === "." ? args[0] + path.sep + p : p;
 }
