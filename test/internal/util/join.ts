ecc38fa97bbec0888657b094e47a5bcc5f616b11
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-03-22 12:21:31 -0300

    Use OS' separator instead of slash.

--- test/internal/util/join.ts
+++ test/internal/util/join.ts
@@ -1,15 +1,26 @@
 import { assert } from "chai";
+import { sep } from "path";
 
 import { join } from "../../../src/internal/util/join";
 
 describe.only("join", () => {
+  function getPath(parts: string[]): string {
+    return parts.reduce((currentPath, part) => currentPath + sep + part);
+  }
+
   it("should join paths as expected", () => {
-    assert.equal(join("a", "b", "c"), "a/b/c");
-    assert.equal(join("a"), "a");
-    assert.equal(join(".", "a"), "./a");
-    assert.equal(join(".", "a", "b"), "./a/b");
-    assert.equal(join(".", "a", "..", "b"), "./b");
-    assert.equal(join(".", "..", "..", "a", "b"), "./../../a/b");
-    assert.equal(join(".", "..", "..", "a", "..", "b"), "./../../b");
+    assert.equal(join("a", "b", "c"), getPath(["a", "b", "c"]));
+    assert.equal(join("a"), getPath(["a"]));
+    assert.equal(join(".", "a"), getPath([".", "a"]));
+    assert.equal(join(".", "a", "b"), getPath([".", "a", "b"]));
+    assert.equal(join(".", "a", "..", "b"), getPath([".", "b"]));
+    assert.equal(
+      join(".", "..", "..", "a", "b"),
+      getPath([".", "..", "..", "a", "b"])
+    );
+    assert.equal(
+      join(".", "..", "..", "a", "..", "b"),
+      getPath([".", "..", "..", "b"])
+    );
   });
 });
