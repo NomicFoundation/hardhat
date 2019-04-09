29a493fd91c13ca68790efb60c152c61bc9ca793
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2019-04-09 17:20:46 -0300

    Include contracts in test directory in truffle plugins. Resolves #219 and #226.

--- packages/buidler-truffle4/src/index.ts
+++ packages/buidler-truffle4/src/index.ts
@@ -1,7 +1,13 @@
 import "@nomiclabs/buidler-web3-legacy";
-import { TASK_TEST_SETUP_TEST_ENVIRONMENT } from "@nomiclabs/buidler/builtin-tasks/task-names";
+import {
+  TASK_COMPILE_GET_SOURCE_PATHS,
+  TASK_TEST_SETUP_TEST_ENVIRONMENT
+} from "@nomiclabs/buidler/builtin-tasks/task-names";
 import { extendEnvironment, internalTask } from "@nomiclabs/buidler/config";
 import { lazyObject } from "@nomiclabs/buidler/plugins";
+import { join } from "path";
+
+import { glob } from "../../buidler-core/internal/util/glob";
 
 import { TruffleEnvironmentArtifacts } from "./artifacts";
 import { LazyTruffleContractProvisioner } from "./provisioner";
@@ -35,4 +41,10 @@ internalTask(TASK_TEST_SETUP_TEST_ENVIRONMENT, async (_, { pweb3 }) => {
     describe(description, () => {
       definition(accounts);
     });
+});
+
+internalTask(TASK_COMPILE_GET_SOURCE_PATHS, async (_, { config }) => {
+  const sources = await glob(join(config.paths.sources, "**/*.sol"));
+  const tests = await glob(join(config.paths.sources, "**/*.sol"));
+  return [...sources, ...tests];
 });
--- packages/buidler-truffle5/src/index.ts
+++ packages/buidler-truffle5/src/index.ts
@@ -1,7 +1,13 @@
 import "@nomiclabs/buidler-web3";
-import { TASK_TEST_SETUP_TEST_ENVIRONMENT } from "@nomiclabs/buidler/builtin-tasks/task-names";
+import {
+  TASK_COMPILE_GET_SOURCE_PATHS,
+  TASK_TEST_SETUP_TEST_ENVIRONMENT
+} from "@nomiclabs/buidler/builtin-tasks/task-names";
 import { extendEnvironment, internalTask } from "@nomiclabs/buidler/config";
 import { BuidlerPluginError, lazyObject } from "@nomiclabs/buidler/plugins";
+import { join } from "path";
+
+import { glob } from "../../buidler-core/internal/util/glob";
 
 import { TruffleEnvironmentArtifacts } from "./artifacts";
 import { LazyTruffleContractProvisioner } from "./provisioner";
@@ -70,4 +76,10 @@ internalTask(TASK_TEST_SETUP_TEST_ENVIRONMENT, async (_, { web3 }) => {
     describe(description, () => {
       definition(accounts);
     });
+});
+
+internalTask(TASK_COMPILE_GET_SOURCE_PATHS, async (_, { config }) => {
+  const sources = await glob(join(config.paths.sources, "**/*.sol"));
+  const tests = await glob(join(config.paths.sources, "**/*.sol"));
+  return [...sources, ...tests];
 });
