0c7148656392828289ed5729f415268fb150f1fe
Patricio Palladino <email@patriciopalladino.com>
2019-04-29 19:25:10 -0300

    Update build-site.sh

--- docs/build-site.sh
+++ docs/build-site.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd ../
+npm install
+npx lerna bootstrap
+
+cd "$DIR"
 npm install
 npm run apidocs
 bash fix-api-docs.sh
