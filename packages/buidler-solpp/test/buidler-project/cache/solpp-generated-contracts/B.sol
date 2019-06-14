e69f6d78c25d5982a65614b318ff18125eb0aa9d
Patricio Palladino <email@patriciopalladino.com>
2019-06-14 17:05:50 -0300

    Delete cache dirs from a plugin's tests

--- packages/buidler-solpp/test/buidler-project/cache/solpp-generated-contracts/B.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity >=0.4.21 <0.6.0;
-
-contract B {
-   function bar(uint256 x) pure returns (uint256) {
-     // Repeat code with a a for loop.
-     return x + 2+ 3+ 4; // -> return x + 1 + 2 + 3;
-   }
-}
