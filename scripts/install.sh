7962b044eb63034c9497927f52b499b3dc2afce3
Patricio Palladino <email@patriciopalladino.com>
2019-08-23 17:45:42 -0300

    Modify scripts/install.sh so it deletes unwanted .bin dirs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 npm install --no-package-lock
 npx lerna bootstrap --no-package-lock --no-ci
+
+# We delete this .bin folders because lerna toches them and then npm doesn't link the packages
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR/.."
+rm -rf packages/*/node_modules/.bin
