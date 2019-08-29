e39704a385f0a14a15205e2e24bd5a0c3bcccf7f
Marto <martinlsanchez@gmail.com>
2019-08-29 18:30:34 -0300

    [buidler-ganache] Add port and host default configs for user

--- packages/buidler-ganache/src/ganache-service.ts
+++ packages/buidler-ganache/src/ganache-service.ts
@@ -1,29 +1,39 @@
 import Ganache from "ganache-core";
+import {string} from "@nomiclabs/buidler/internal/core/params/argumentTypes";
 
 export class GanacheService {
   private readonly _server: any;
+  private readonly _verbose: boolean;
+  private _options: GanacheOptions;
 
-  constructor(configs?: {}, env?: {}) {
+  constructor(env?: any) {
     console.log(">> Ganache Service > Initializing server");
 
     // Only for debug
     // console.log(configs);
-    // console.log(env);
+    // console.log(env.config);
 
-    this._server = Ganache.server();
+    this._verbose = env.buidlerArguments.verbose;
+    this._options = {
+      hostname: "127.0.0.1",
+      port: 8545
+    };
+    this._server = Ganache.server(this._options);
   }
 
   public async send(method: string, params: any): Promise<any> {
     return this._server.send(method, params);
   }
 
-  public startServer(configs?: {}, env?: {}) {
+  public startServer() {
     console.log(">> Ganache Service > Starting server");
 
     // Only for debug
     // console.log(env);
 
-    this._server.listen(8545, function(err: any, blockchain: any) {
+    const port = this._options.port;
+    const hostname = this._options.hostname;
+    const listeningListener = (err: any, blockchain: any) => {
       if (err) {
         console.log(">> Ganache Error:\n");
         console.log(err);
@@ -31,17 +41,48 @@ export class GanacheService {
 
       // Only for debug
       // console.log(blockchain);
-    });
+    };
+
+    this._server.listen(port, hostname, listeningListener);
   }
 
   public stopServer() {
     console.log(">> Ganache Service > Stopping server");
+
     // TODO Make this function
     this._server.close();
   }
 
-  public processConfigs(configs?: {}, env?: {}) {
-    console.log(">> Ganache Service > Processing configs");
-    // TODO Make this function
-  }
+  public processConfigs(configs?: any) {}
+}
+
+export interface GanacheOptions {
+  // Details: https://github.com/trufflesuite/ganache-core#options
+  account_keys_path?: string;
+  accounts?: object[];
+  allowUnlimitedContractSize?: boolean;
+  blockTime?: number;
+  db_path?: string;
+  debug?: boolean;
+  default_balance_ether?: number;
+  fork?: string | object;
+  fork_block_number?: string | number;
+  gasLimit?: number;
+  gasPrice?: string;
+  hardfork?: "byzantium" | "constantinople" | "petersburg";
+  hd_path?: string;
+  locked?: boolean;
+  logger?: { log(msg: string): void; };
+  mnemonic?: string;
+  network_id?: number;
+  networkId?: number;
+  hostname?: string;
+  port?: number;
+  seed?: any;
+  time?: Date;
+  total_accounts?: number;
+  unlocked_accounts?: string[];
+  verbose?: boolean;
+  vmErrorsOnRPCResponse?: boolean;
+  ws?: boolean;
 }
--- packages/buidler-ganache/src/index.ts
+++ packages/buidler-ganache/src/index.ts
@@ -2,7 +2,7 @@ import {
   TASK_RUN,
   TASK_TEST
 } from "@nomiclabs/buidler/builtin-tasks/task-names";
-import { task } from "@nomiclabs/buidler/config";
+import { BuidlerConfig, extendConfig, task } from "@nomiclabs/buidler/config";
 import {
   ensurePluginLoadedWithUsePlugin,
   lazyObject
@@ -18,6 +18,29 @@ import { GanacheService } from "./ganache-service";
 ensurePluginLoadedWithUsePlugin();
 
 export default function() {
+  // Original parameters: resolvedConfig: ResolvedBuidlerConfig, config: DeepReadonly<BuidlerConfig>)
+  extendConfig((resolvedConfig: any, config: any) => {
+    // TODO Extract all this to a GanacheService function
+    // Set ganache as default network if no value was given
+    if (!config.defaultNetwork) {
+      resolvedConfig.defaultNetwork = "ganache";
+    }
+
+    // Get all default values
+    resolvedConfig.networks.ganache = { url: "http://127.0.0.1:8545" };
+
+    // Override ganache network with custom user values if needed
+    if (config.networks && config.networks.ganache) {
+      if (config.networks.ganache.host) {
+        resolvedConfig.networks.ganache.url = `${config.networks.ganache.host}:8545`;
+      }
+
+      if (config.networks.ganache.port) {
+        resolvedConfig.networks.ganache.url = `http://127.0.0.1:${config.networks.ganache.port}`;
+      }
+    }
+  });
+
   task(TASK_TEST, async (args, env, runSuper) => {
     return handlePluginTask(args, env, runSuper);
   });
@@ -32,15 +55,15 @@ async function handlePluginTask(
   env: BuidlerRuntimeEnvironment,
   runSuper: RunSuperFunction<TaskArguments>
 ) {
-  if (env.network.name === "ganache") {
-    console.log(">> Buidler Ganache > NO Handling Task");
+  if (env.network.name !== "ganache") {
+    console.log(">> Buidler Ganache > No handling Task\n");
     return runSuper();
   }
 
   console.log(">> Buidler Ganache > Handling Task");
 
   // Init ganache service with current configs
-  const ganacheService = lazyObject(() => new GanacheService(args, env));
+  const ganacheService = lazyObject(() => new GanacheService(env));
 
   // Start ganache server and log errors
   ganacheService.startServer();
