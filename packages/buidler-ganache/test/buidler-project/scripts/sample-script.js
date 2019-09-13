9a7b12fd0f5978202aa8fed22862882a7b47c6d2
Marto <martinlsanchez@gmail.com>
2019-09-13 16:42:33 -0300

    [buidler-ganache] WIP Rebase Integration tests with sample data

---
+++ packages/buidler-ganache/test/buidler-project/scripts/sample-script.js
@@ -0,0 +1,15 @@
+// We require the Buidler Runtime Environment explicitly here. This is optional.
+const env = require("@nomiclabs/buidler");
+
+async function main() {
+  await env.run("compile");
+  const accounts = await env.network.provider.send("eth_accounts");
+  console.log(">> Accounts detected! ", accounts.length);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
