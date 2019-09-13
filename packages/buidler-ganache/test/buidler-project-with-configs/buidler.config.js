5fbf1679862485c8a9ef5f073de18060a0f78ac6
Marto <martinlsanchez@gmail.com>
2019-09-13 15:38:58 -0300

    [buidler-ganache] Add draft base for integration tests

---
+++ packages/buidler-ganache/test/buidler-project-with-configs/buidler.config.js
@@ -0,0 +1,19 @@
+import {TASK_RUN, TASK_TEST} from "@nomiclabs/buidler/builtin-tasks/task-names";
+
+const { loadPluginFile } = require("@nomiclabs/buidler/plugins-testing");
+
+loadPluginFile(__dirname + "/../../src/index");
+
+module.exports = {
+  defaultNetwork: "ganache",
+  networks: {
+    ganache: {
+      url: "http://127.0.0.1:8555",
+      gasPrice: 20000000000,
+      gasLimit: 6000000000,
+      defaultBalanceEther: 9,
+      totalAccounts: 3,
+      mnemonic: "polar velvet stereo oval echo senior cause cruel tube hobby exact angry",
+    },
+  }
+};
