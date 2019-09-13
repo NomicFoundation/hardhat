af5e964fa7d2a6a4f2ae2bc504cd93c98eb47e8b
Marto <martinlsanchez@gmail.com>
2019-09-13 16:42:33 -0300

    [buidler-ganache] WIP Rebase Integration tests with sample data

--- packages/buidler-ganache/test/index.ts
+++ packages/buidler-ganache/test/index.ts
@@ -28,34 +28,14 @@ describe("Ganache plugin with empty configs", function() {
   });
 
   it("Should run Buidler TEST task using Ganache", async function() {
-    const accounts = await this.env.run("test");
+    await this.env.run("test", { noCompile: true, testFiles: [] });
   });
 
-  it("Should get accounts from Ganache", async function() {
-    const accounts = await this.env.ethereum.send("eth_accounts");
-    assert.equal(accounts.length, 10);
+  // TODO This test pass but with some errors, should be fixed
+  it("Should run Buidler RUN task using Ganache", async function() {
+    await this.env.run("run", {
+      noCompile: true,
+      script: "scripts/sample-script.js"
+    });
   });
 });
-
-// describe("Ganache plugin with custom configs", function() {
-//   useEnvironment(path.join(__dirname, "buidler-project-with-configs"));
-//
-//   it("Should set ganache as default network", function() {
-//     assert.equal(this.env.config.defaultNetwork, "ganache");
-//   });
-//
-//   it("Should add ganache network to buidler runtime environment", function() {
-//     assert.isDefined(this.env.config.networks.ganache);
-//   });
-//
-//   it("Should expose ganache custom condfigs in buidler runtime environment", function() {
-//     assert.isDefined(this.env.config.networks.ganache);
-//     const defaultOptions = GanacheService.getDefaultOptions() as any;
-//     const options = this.env.config.networks.ganache as any;
-//
-//     // Iterate over all default options and assert equality
-//     for (const [key, value] of Object.entries(defaultOptions)) {
-//       assert.equal(options[key], value);
-//     }
-//   });
-// });
