570610b63a8badd367056f72366bad37f5611db6
Marto <martinlsanchez@gmail.com>
2019-09-16 13:58:34 -0300

    [buidler-ganache] Add running integration tests and update readme

--- packages/buidler-ganache/scripts/run-tests.sh
+++ packages/buidler-ganache/scripts/run-tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-../../node_modules/.bin/mocha --exit
+../../node_modules/.bin/mocha --timeout 20000 --exit
--- packages/buidler-ganache/test/index.ts
+++ packages/buidler-ganache/test/index.ts
@@ -16,7 +16,7 @@ describe("Ganache plugin with empty configs", function() {
     assert.isDefined(this.env.config.networks.ganache);
   });
 
-  it("Should expose ganache defaults condfigs in buidler runtime environment", function() {
+  it("Should expose ganache defaults configs in buidler runtime environment", function() {
     assert.isDefined(this.env.config.networks.ganache);
     const defaultOptions = GanacheService.getDefaultOptions() as any;
     const options = this.env.config.networks.ganache as any;
@@ -31,10 +31,68 @@ describe("Ganache plugin with empty configs", function() {
     await this.env.run("test", { noCompile: true, testFiles: [] });
   });
 
-  it("Should run Buidler RUN task using Ganache", async function() {
+  it("Should run Buidler RUN task 'accounts-sample.js' using Ganache", async function() {
     await this.env.run("run", {
       noCompile: true,
-      script: "scripts/sample-script.js"
+      script: "scripts/accounts-sample.js"
+    });
+
+    assert.equal(process.exitCode, 0);
+  });
+
+  it("Should run Buidler RUN task 'delayed-sample.js' using Ganache", async function() {
+    await this.env.run("run", {
+      noCompile: true,
+      script: "scripts/delayed-sample.js"
+    });
+
+    assert.equal(process.exitCode, 0);
+  });
+});
+
+describe("Ganache plugin with custom configs", function() {
+  useEnvironment(path.join(__dirname, "buidler-project-with-configs"));
+
+  it("Should add ganache network to buidler runtime environment", function() {
+    assert.isDefined(this.env.config.networks.ganache);
+  });
+
+  it("Should load custom configs in buidler runtime environment", function() {
+    assert.isDefined(this.env.config.networks.ganache);
+    const customConfigs = require("./buidler-project-with-configs/buidler.config.js");
+
+    assert.isDefined(customConfigs.networks.ganache);
+    const customOptions = customConfigs.networks.ganache;
+
+    const options = this.env.config.networks.ganache as any;
+
+    // Iterate over all custom options and assert equality
+    for (const [key, value] of Object.entries(customOptions)) {
+      assert.equal(options[key], value);
+    }
+  });
+
+  it("Should expose merged (custom + defaults) configs in buidler runtime environment", function() {
+    assert.isDefined(this.env.config.networks.ganache);
+    const customConfigs = require("./buidler-project-with-configs/buidler.config.js");
+    const defaultOptions = GanacheService.getDefaultOptions() as any;
+
+    assert.isDefined(customConfigs.networks.ganache);
+    const customOptions = customConfigs.networks.ganache;
+    const mergedOptions = { ...defaultOptions, ...customOptions };
+
+    const options = this.env.config.networks.ganache as any;
+
+    // Iterate over all custom options and assert equality
+    for (const [key, value] of Object.entries(mergedOptions)) {
+      assert.equal(options[key], value);
+    }
+  });
+
+  it("Should run Buidler RUN task using Ganache with custom configs", async function() {
+    await this.env.run("run", {
+      noCompile: true,
+      script: "scripts/custom-accounts-sample.js"
     });
 
     assert.equal(process.exitCode, 0);
