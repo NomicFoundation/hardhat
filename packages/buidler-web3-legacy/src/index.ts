93da5ba142152d77a27c22256d3c55285fe3d424
Patricio Palladino <email@patriciopalladino.com>
2019-09-17 17:24:00 -0300

    Make web3 plugins more robust wrt lazyObject/Function bugs

--- packages/buidler-web3-legacy/src/index.ts
+++ packages/buidler-web3-legacy/src/index.ts
@@ -7,9 +7,10 @@ import { Web3HTTPProviderAdapter } from "./web3-provider-adapter";
 export default function() {
   extendEnvironment(env => {
     env.Web3 = lazyFunction(() => require("web3"));
-    env.web3 = lazyObject(
-      () => new env.Web3(new Web3HTTPProviderAdapter(env.network.provider))
-    );
+    env.web3 = lazyObject(() => {
+      const Web3 = require("web3");
+      return new Web3(new Web3HTTPProviderAdapter(env.network.provider));
+    });
     env.pweb3 = lazyObject(() => promisifyWeb3(env.web3));
   });
 }
