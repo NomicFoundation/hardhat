fae99de71dd294e38d1628ccec343496f67899c5
Patricio Palladino <email@patriciopalladino.com>
2019-10-02 12:28:35 -0300

    Add favicon

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,9 +4,7 @@ const pluginsChildren = [];
 
 plugins.forEach(plugin => {
   let readmePath =
-    "/plugins/" +
-    plugin.name.replace("/", "-").replace(/^@/, "") +
-    ".md";
+    "/plugins/" + plugin.name.replace("/", "-").replace(/^@/, "") + ".md";
 
   pluginsChildren.push([readmePath, plugin.name, 0]);
 });
@@ -88,9 +86,102 @@ module.exports = {
   },
   head: [
     [
-      "meta ",
+      "meta",
       { name: "Cache-Control", content: "public, max-age=0, must-revalidate" }
-    ]
+    ],
+
+    ["link", { rel: "shortcut icon", href: "/favicon.ico" }],
+    ["link", { rel: "icon", sizes: "16x16 32x32 64x64", href: "/favicon.ico" }],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "196x196",
+        href: "/favicon-192.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "160x160",
+        href: "/favicon-160.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicon-96.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "64x64",
+        href: "/favicon-64.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16.png"
+      }
+    ],
+    ["link", { rel: "apple-touch-icon", href: "/favicon-57.png" }],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "114x114", href: "/favicon-114.png" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "72x72", href: "/favicon-72.png" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "144x144", href: "/favicon-144.png" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "60x60", href: "/favicon-60.png" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "120x120", href: "/favicon-120.png" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "76x76", href: "/favicon-76.png" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "152x152", href: "/favicon-152.png" }
+    ],
+    [
+      "link",
+      { rel: "apple-touch-icon", sizes: "180x180", href: "/favicon-180.png" }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#FFFFFF" }],
+    ["meta", { name: "msapplication-TileImage", content: "/favicon-144.png" }],
+    ["meta", { name: "msapplication-config", content: "/browserconfig.xml" }]
   ],
   markdown: {
     slugify: title => {
