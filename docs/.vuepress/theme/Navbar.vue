d9e7d6fc24d590d447ce171dc93d29f6ad21bc3f
Patricio Palladino <email@patriciopalladino.com>
2019-10-02 16:29:35 -0300

    Fix banner in mobile

--- docs/.vuepress/theme/Navbar.vue
+++ docs/.vuepress/theme/Navbar.vue
@@ -5,7 +5,8 @@
   >
     <div style="position: relative">
       <div class="banner">
-        <a href="https://medium.com/nomic-labs-blog/better-solidity-debugging-stack-traces-are-finally-here-dd80a56f92bb">Better Solidity debugging: stack traces are finally here →</a>
+        <a class="not-narrow" href="https://medium.com/nomic-labs-blog/better-solidity-debugging-stack-traces-are-finally-here-dd80a56f92bb">Better Solidity debugging: stack traces are finally here →</a>
+        <a class="only-narrow" href="https://medium.com/nomic-labs-blog/better-solidity-debugging-stack-traces-are-finally-here-dd80a56f92bb">Solidity stack traces are here →</a>
       </div>
       <div style="position: relative">
         <header>
