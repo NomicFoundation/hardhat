8c82f2cce7d7fec2635424652f354771f0d1c2da
Patricio Palladino <email@patriciopalladino.com>
2019-10-31 12:12:21 -0300

    Fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@
 set -x
 set -e
 
-npm install --no-package-lock
-npx lerna bootstrap --no-package-lock --no-ci
+npm install
+npx lerna bootstrap --no-ci
 
 # We delete these .bin folders because lerna creates them and then npm doesn't link the packages
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
