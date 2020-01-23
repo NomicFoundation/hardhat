2c28d007046c76010de25f144af15d372fc9c6a5
Patricio Palladino <email@patriciopalladino.com>
2020-01-23 12:39:06 -0300

    Add test with Solidity 0.6 to buidler-truffle5

---
+++ packages/buidler-truffle5/test/buidler-project-solc-0.6/buidler.config.js
@@ -0,0 +1,14 @@
+const { loadPluginFile } = require("@nomiclabs/buidler/plugins-testing");
+loadPluginFile(__dirname + "/../../src/index");
+
+module.exports = {
+  networks: {
+    withoutAccounts: {
+      url: "http://localhost:8545",
+      accounts: []
+    }
+  },
+  solc: {
+    version: "0.6.1"
+  }
+};
