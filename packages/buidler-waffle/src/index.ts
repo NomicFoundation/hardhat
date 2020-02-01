ec2f8b59a21ab30107009e24f26b38f78119ebcb
Patricio Palladino <email@patriciopalladino.com>
2020-02-01 13:40:24 -0300

    Remove commented out code

--- packages/buidler-waffle/src/index.ts
+++ packages/buidler-waffle/src/index.ts
@@ -1,45 +1,8 @@
-import {
-  extendConfig,
-  extendEnvironment,
-  usePlugin
-} from "@nomiclabs/buidler/config";
+import { extendEnvironment, usePlugin } from "@nomiclabs/buidler/config";
 
 import { WaffleMockProviderAdapter } from "./waffle-provider-adapter";
 
 export default function() {
-  // const waffleProviderModulePath = require.resolve(
-  //   "@ethereum-waffle/provider",
-  //   {
-  //     paths: [require.resolve("ethereum-waffle")]
-  //   }
-  // );
-  //
-  // const {
-  //   defaultWaffleAccounts
-  // }: {
-  //   defaultWaffleAccounts: Array<{
-  //     balance: string;
-  //     secretKey: string;
-  //   }>;
-  // } = require(waffleProviderModulePath);
-  //
-  // extendConfig((resolvedConfig, userConfig) => {
-  //   if (
-  //     userConfig.networks !== undefined &&
-  //     userConfig.networks.buidlerevm !== undefined &&
-  //     userConfig.networks.buidlerevm.accounts !== undefined
-  //   ) {
-  //     return;
-  //   }
-  //
-  //   resolvedConfig.networks.buidlerevm.accounts = defaultWaffleAccounts.map(
-  //     acc => ({
-  //       privateKey: acc.secretKey,
-  //       balance: acc.balance
-  //     })
-  //   );
-  // });
-
   extendEnvironment(bre => {
     // There's some problems with the `options` private property of Waffle's MockProvider
     // it can't be correctly implemented, possibly because it depends on Ganache types.
