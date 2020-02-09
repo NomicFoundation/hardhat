b62b95aa34a239281cf714b933590985481648c9
Franco Victorio <victorio.franco@gmail.com>
2020-02-09 15:01:18 -0300

    Make shelljs throw if some command fails

--- scripts/install.js
+++ scripts/install.js
@@ -1,5 +1,7 @@
 const shell = require('shelljs')
 
+shell.config.fatal = true // throw if a command fails
+
 shell.exec('npx lerna bootstrap --no-ci')
 
 // We delete these .bin folders because lerna creates them and then npm doesn't link the packages
--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,5 +1,7 @@
 const shell = require('shelljs')
 
+shell.config.fatal = true // throw if a command fails
+
 shell.exec('npx lerna exec -- npm run build')
 shell.exec('npx lerna exec -- npm run build-test')
 shell.exec('npx lerna exec --concurrency 1 -- npm run test', {
