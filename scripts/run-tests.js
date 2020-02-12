cbc0cdef54735456400668b9cb90094d4b734f97
Franco Victorio <victorio.franco@gmail.com>
2020-02-12 18:09:45 -0300

    Don't run buidler-solpp tests in windows

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,12 +1,18 @@
-const os = require('os')
-const shell = require('shelljs')
+const os = require("os");
+const shell = require("shelljs");
 
-shell.config.fatal = true // throw if a command fails
+shell.config.fatal = true; // throw if a command fails
 
-const isLinux = os.type() === "Linux"
+const isLinux = os.type() === "Linux";
+const isWindows = os.type() === "Windows_NT";
 
-shell.exec('npx lerna exec -- npm run build')
-shell.exec('npx lerna exec -- npm run build-test')
-shell.exec(`npx lerna exec ${isLinux ? '' : '--ignore "@nomiclabs/buidler-vyper"'} --concurrency 1 -- npm run test`, {
-  TS_NODE_TRANSPILE_ONLY: 'true'
-})
+shell.exec("npx lerna exec -- npm run build");
+shell.exec("npx lerna exec -- npm run build-test");
+shell.exec(
+  `npx lerna exec ${!isLinux ? '--ignore "@nomiclabs/buidler-vyper"' : ""} ${
+    isWindows ? '--ignore "@nomiclabs/buidler-solpp"' : ""
+  } --concurrency 1 -- npm run test`,
+  {
+    TS_NODE_TRANSPILE_ONLY: "true"
+  }
+);
