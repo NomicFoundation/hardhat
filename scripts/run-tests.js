4f6245e7d25dac116dfa81d39d75224a74222f34
Franco Victorio <victorio.franco@gmail.com>
2020-02-11 21:10:36 -0300

    Only run vyper tests in linux

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,9 +1,12 @@
+const os = require('os')
 const shell = require('shelljs')
 
 shell.config.fatal = true // throw if a command fails
 
+const isLinux = os.type() === "Linux"
+
 shell.exec('npx lerna exec -- npm run build')
 shell.exec('npx lerna exec -- npm run build-test')
-shell.exec('npx lerna exec --concurrency 1 -- npm run test', {
+shell.exec(`npx lerna exec ${isLinux ? '' : '--ignore "@nomiclabs/buidler-vyper"'} --concurrency 1 -- npm run test`, {
   TS_NODE_TRANSPILE_ONLY: 'true'
 })
