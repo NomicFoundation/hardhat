45bd6b3fe8b98865b2069e40b874b8b001842b45
Patricio Palladino <email@patriciopalladino.com>
2020-02-12 20:04:48 -0300

    Delete .bin on install.js and force colors

--- scripts/install.js
+++ scripts/install.js
@@ -1,8 +1,11 @@
-const shell = require('shelljs')
+const process = require("process");
+const shell = require("shelljs");
 
-shell.config.fatal = true // throw if a command fails
+process.env.FORCE_COLOR = "3";
 
-shell.exec('npx lerna bootstrap --no-ci')
+shell.config.fatal = true; // throw if a command fails
+
+shell.exec("npx lerna bootstrap --no-ci");
 
 // We delete these .bin folders because lerna creates them and then npm doesn't link the packages
-// shell.rm('-rf', 'packages/*/node_modules/.bin')
+shell.rm("-rf", "packages/*/node_modules/.bin");
