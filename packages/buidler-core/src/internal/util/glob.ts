9984472c3d1f6d3e0b2617fae0def3233192b287
Franco Victorio <victorio.franco@gmail.com>
2020-02-12 18:47:17 -0300

    Use realpath in glob helpers

--- packages/buidler-core/src/internal/util/glob.ts
+++ packages/buidler-core/src/internal/util/glob.ts
@@ -2,9 +2,9 @@ import util from "util";
 
 export async function glob(pattern: string): Promise<string[]> {
   const { default: globModule } = await import("glob");
-  return util.promisify(globModule)(pattern);
+  return util.promisify(globModule)(pattern, { realpath: true });
 }
 
 export function globSync(pattern: string): string[] {
-  return require("glob").sync(pattern);
+  return require("glob").sync(pattern, { realpath: true });
 }
