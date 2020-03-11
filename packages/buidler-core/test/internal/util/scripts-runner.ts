744ea0dce993b0ba82b142ea23d46199001bb81b
Tomas Milar <tomasmilar4@gmail.com>
2020-03-11 13:31:14 -0300

    Parallelize runScript() calls in scripts-runner tests for a faster execution time.

--- packages/buidler-core/test/internal/util/scripts-runner.ts
+++ packages/buidler-core/test/internal/util/scripts-runner.ts
@@ -12,12 +12,18 @@ describe("Scripts runner", function() {
   useFixtureProject("project-with-scripts");
 
   it("Should pass params to the script", async function() {
-    const statusCode = await runScript("./params-script.js", ["a", "b", "c"]);
-    assert.equal(statusCode, 0);
+    const [
+      statusCodeWithScriptParams,
+      statusCodeWithNoParams
+    ] = await Promise.all([
+      runScript("./params-script.js", ["a", "b", "c"]),
+      runScript("./params-script.js")
+    ]);
+
+    assert.equal(statusCodeWithScriptParams, 0);
 
     // We check here that the script is correctly testing this:
-    const statusCode2 = await runScript("./params-script.js");
-    assert.notEqual(statusCode2, 0);
+    assert.notEqual(statusCodeWithNoParams, 0);
   });
 
   it("Should run the script to completion", async function() {
@@ -31,48 +37,88 @@ describe("Scripts runner", function() {
   it("Should resolve to the status code of the script run", async function() {
     const buidlerRegisterPath = resolveBuidlerRegisterPath();
 
-    const statusCode1 = await runScript(
-      "./async-script.js",
-      [],
-      ["--require", buidlerRegisterPath]
-    );
-    assert.equal(statusCode1, 0);
+    const extraNodeArgs = ["--require", buidlerRegisterPath];
+    const scriptArgs: string[] = [];
 
-    const statusCode2 = await runScript("./failing-script.js");
-    assert.equal(statusCode2, 123);
+    const runScriptCases = [
+      {
+        scriptPath: "./async-script.js",
+        extraNodeArgs,
+        expectedStatusCode: 0
+      },
+      {
+        scriptPath: "./failing-script.js",
+        expectedStatusCode: 123
+      },
+      {
+        scriptPath: "./successful-script.js",
+        extraNodeArgs,
+        expectedStatusCode: 0
+      }
+    ];
 
-    const statusCode3 = await runScript(
-      "./successful-script.js",
-      [],
-      ["--require", buidlerRegisterPath]
+    const runScriptTestResults = await Promise.all(
+      runScriptCases.map(
+        async ({ scriptPath, extraNodeArgs: _extraNodeArgs }) => {
+          const statusCode =
+            _extraNodeArgs === undefined
+              ? await runScript(scriptPath)
+              : await runScript(scriptPath, scriptArgs, _extraNodeArgs);
+          return { scriptPath, statusCode };
+        }
+      )
     );
-    assert.equal(statusCode3, 0);
+
+    const expectedResults = runScriptCases.map(
+      ({ expectedStatusCode, scriptPath }) => ({
+        scriptPath,
+        statusCode: expectedStatusCode
+      })
+    );
+
+    assert.deepEqual(runScriptTestResults, expectedResults);
   });
 
   it("Should pass env variables to the script", async function() {
-    const statusCode = await runScript("./env-var-script.js", [], [], {
-      TEST_ENV_VAR: "test"
-    });
-    assert.equal(statusCode, 0);
+    const [statusCodeWithEnvVars, statusCodeWithNoEnvArgs] = await Promise.all([
+      runScript("./env-var-script.js", [], [], {
+        TEST_ENV_VAR: "test"
+      }),
+      runScript("./env-var-script.js")
+    ]);
 
-    // We check here that the script is correctly testing this:
-    const statusCode2 = await runScript("./env-var-script.js");
-    assert.notEqual(statusCode2, 0);
+    assert.equal(
+      statusCodeWithEnvVars,
+      0,
+      "Status code with env vars should be 0"
+    );
+
+    assert.notEqual(
+      statusCodeWithNoEnvArgs,
+      0,
+      "Status code with no env vars should not be 0"
+    );
   });
 
   describe("runWithBuidler", function() {
     useEnvironment();
 
     it("Should load buidler/register successfully", async function() {
-      const statusCode = await runScriptWithBuidler(
-        this.env.buidlerArguments,
-        "./successful-script.js"
-      );
-      assert.equal(statusCode, 0);
+      const [
+        statusCodeWithBuidler,
+        statusCodeWithoutBuidler
+      ] = await Promise.all([
+        runScriptWithBuidler(
+          this.env.buidlerArguments,
+          "./successful-script.js"
+        ),
+        runScript("./successful-script.js")
+      ]);
+
+      assert.equal(statusCodeWithBuidler, 0);
 
       // We check here that the script is correctly testing this:
-      const statusCode2 = await runScript("./successful-script.js");
-      assert.notEqual(statusCode2, 0);
+      assert.notEqual(statusCodeWithoutBuidler, 0);
     });
 
     it("Should forward all the buidler arguments", async function() {
