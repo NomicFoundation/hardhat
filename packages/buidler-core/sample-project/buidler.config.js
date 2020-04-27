79c014ab5dc57a59627d03becb443d4a2c31da83
viarnes <viarnes@protonmail.com>
2020-04-27 15:48:44 -0300

    Add solc config option to sample project's config file

--- packages/buidler-core/sample-project/buidler.config.js
+++ packages/buidler-core/sample-project/buidler.config.js
@@ -10,4 +10,13 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
-module.exports = {};
+// You have to export an object to set up your config
+// This object can have the following optional entries:
+// defaultNetwork, networks, solc, and paths.
+// Go to https://buidler.dev/config/ to learn more
+module.exports = {
+  // This is a sample solc configuration that specifies which version of solc to use
+  solc: {
+    version: "0.5.15",
+  },
+};
