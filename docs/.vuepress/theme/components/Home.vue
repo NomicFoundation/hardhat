236554880f5675d9d4ae464433ef4d70244b7933
viarnes <viarnes@protonmail.com>
2020-05-04 16:00:49 -0300

    Add prev/next to slide + replace pngs with svgs

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -48,50 +48,44 @@
         <h3>Who's using Buidler?</h3>
         <div class="users">
           <div class="logo">
-            <a href="https://kyber.network/" target="_blank">
-              <img src="/users/kyber-logo.png" alt="Kyber" />
-            </a>
+            <img src="/users/kyber-logo.svg" alt="Kyber" />
           </div>
           <div class="logo">
-            <a href="https://www.synthetix.io/" target="_blank">
-              <img src="/users/synthetix-logo.png" alt="Synthetix" />
-            </a>
+            <img src="/users/synthetix-logo.svg" alt="Synthetix" />
           </div>
           <div class="logo">
-            <a href="https://aave.com/" target="_blank">
-              <img src="/users/aave-logo.png" alt="AAVE" />
-            </a>
+            <img src="/users/aave-logo.svg" alt="AAVE" />
           </div>
           <div class="logo">
-            <a href="https://aragon.one/" target="_blank">
-              <img src="/users/aone-logo.png" alt="Aragon One" />
-            </a>
+            <img src="/users/aone-logo.svg" alt="Aragon One" />
           </div>
           <div class="logo">
-            <a href="https://connext.network/" target="_blank">
-              <img src="/users/connext-logo.png" alt="Connext" />
-            </a>
+            <img src="/users/connext-logo.svg" alt="Connext" />
           </div>
           <div class="logo">
-            <a href="https://www.celer.network/" target="_blank">
-              <img src="/users/celer-logo.png" alt="Celer" />
-            </a>
+            <img src="/users/celer-logo.svg" alt="Celer" />
           </div>
           <div class="logo">
-            <a href="https://decentraland.org/" target="_blank">
-              <img src="/users/decentraland-logo.png" alt="Decentraland" />
-            </a>
+            <img src="/users/decentraland-logo.svg" alt="Decentraland" />
           </div>
           <div class="logo">
-            <a href="https://skale.network/" target="_blank">
-              <img src="/users/skale-logo.png" alt="Skale" />
-            </a>
+            <img src="/users/skale-logo.svg" alt="Skale" />
           </div>
         </div>
       </div>
 
       <div class="testimonial-slider">
-        <component v-if="carousel && slide" :is="carousel" :perPage="1">
+        <component 
+          v-if="carousel && slide" 
+          :is="carousel" 
+          :perPage="1"
+          :autoplay="true"
+          :autoplayTimeout=5000
+          :navigationEnabled="true"
+          :autoplayHoverPause="true"
+          navigationNextLabel=""
+          navigationPrevLabel=""
+        >
           <component
             v-if="carousel && slide"
             :is="slide"
@@ -136,7 +130,7 @@
         <div class="ef">
           <span>Supported by</span>
           <img
-            src="/ethereum_fundation.png"
+            src="/ethereum-foundation.svg"
             alt="Supported by Ethereum Foundation"
           />
         </div>
@@ -164,7 +158,7 @@ export default {
           author: "Rahul Sethuram",
           role: "CTO",
           company: "Connext Network",
-          companyImg: "/users/connext-logo-testimonial.png",
+          companyImg: "/users/connext-logo-testimonial.svg",
           text:
             "Builder has become an essential part of our development and Continuous Integration stack. At Connext, we develop and test complicated smart contract systems for our state channel implementations, making proper Solidity tooling a key to our productivity and success. Buidler's state-of-the-art Solidity stack trace and console.log features saved us considerable development time. As a user, it's clear that Buidler prioritizes a great developer experience, which aligns fully with Connext's values. We enjoy interacting with the team and we have even made contributions to the project."
         },
@@ -173,7 +167,7 @@ export default {
           author: "Brett Sun",
           role: "CTO",
           company: "Aragon One",
-          companyImg: "/users/aone-logo-testimonial.png",
+          companyImg: "/users/aone-logo-testimonial.svg",
           text:
             "Our interest in Buidler was driven by our own experience of building and maintaining developer tooling for the Aragon ecosystem. Not only were these efforts time consuming, difficult, and error-prone, we also found ourselves constantly re-inventing the wheel in areas we did not want to care about or force opinions on (e.g. Ganache connections, Truffle providers, test strategy). Buidler, with its plugin ecosystem, has effectively eliminated many of these problems for us. We feel confident piggybacking on the best for the underlying layers so that we can focus our attention on exposing the power of the Aragon ecosystem to our community."
         },
@@ -182,7 +176,7 @@ export default {
           author: "Justin J. Moses",
           role: "CTO",
           company: "Synthetix",
-          companyImg: "/users/synthetix-logo-testimonial.png",
+          companyImg: "/users/synthetix-logo-testimonial.svg",
           text:
             "Tired of battling other testing frameworks, I tried buidler on a whim one afternoon to see just how hard it might be to port Synthetix over to it. After fifteen minutes I had one of our specs running nearly 10x faster that what I’d become used to; from that moment I was hooked. Since then, we’ve integrated coverage, supported multiple versions of solc and even set up legacy testing through injection - all without having to wait for features to be added by the buidler team. It’s been built using its own extensible task system, dogfooding its own plugin architecture. Fast test turnarounds, extensible architecture and solidity stack traces - my dream of smart contract TDD has become a lot more real!"
         },
@@ -191,7 +185,7 @@ export default {
           author: "Esteban Ordano",
           role: "CTO",
           company: "Decentraland",
-          companyImg: "/users/decentraland-logo-testimonial.png",
+          companyImg: "/users/decentraland-logo-testimonial.svg",
           text:
             "Buidler's extensibility, clean interface and excellent design is the most significant advancement in the professionalization of tools for Ethereum of the past year. Our development experience improved significantly, and the quality of the development process is reflected in the fact that our team went from fearing updating packages to the latest version to watching out for the next release."
         },
@@ -200,7 +194,7 @@ export default {
           author: "Victor Tran",
           role: "CTO",
           company: "Kyber",
-          companyImg: "/users/kyber-logo-testimonial.png",
+          companyImg: "/users/kyber-logo-testimonial.svg",
           text:
             "Working with Buidler has been a great experience. Thanks to its flexibility we were able to test across different Solidity versions without duplicating our setup. Kyber has been around for long enough to have legacy contracts deployed with different Solidity versions in our architecture, so this kind of flexibility is important for such a mature project. The collaboration between the Kyber and Buidler teams to fix issues and implement new features has been fast and smooth, which helped our internal timelines a lot."
         }
