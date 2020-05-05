628487221da0454d4aa3da7b07d7c27a4b6960cc
Tomas Milar <tomasmilar4@gmail.com>
2020-05-05 16:54:14 -0300

    error reporter: implement Sentry client integration.

--- packages/buidler-core/src/internal/error-reporter/error-reporter.ts
+++ packages/buidler-core/src/internal/error-reporter/error-reporter.ts
@@ -1,3 +1,4 @@
+import * as Sentry from "@sentry/node";
 import debug from "debug";
 import os from "os";
 
@@ -53,6 +54,99 @@ interface ErrorReporterClient {
   sendErrorReport(error: Error): Promise<void>;
 }
 
+class SentryClient implements ErrorReporterClient {
+  public static SENTRY_FLUSH_TIMEOUT = 3000;
+  private readonly _SENTRY_DSN =
+    "https://08e9ea013b3f45cd87f6047ac0693ca2@o385006.ingest.sentry.io/5221401";
+  private readonly _log = debug("buidler:core:analytics:sentry");
+
+  constructor(
+    projectId: string,
+    clientId: string,
+    userType: UserType,
+    userAgent: string,
+    buidlerVersion: string
+  ) {
+    // init bugsnag client
+    Sentry.init({ dsn: this._SENTRY_DSN });
+
+    // setup metadata to be included in all reports by default
+    Sentry.configureScope((scope) => {
+      scope.setUser({ id: clientId, type: userType });
+      scope.setTag("projectId", projectId);
+      scope.setTag("version", buidlerVersion);
+      scope.setTag("os", os.type());
+      scope.setTag("node", process.version);
+      scope.setTag("userAgent", userAgent);
+      scope.setExtra("platform", os.platform());
+      scope.setExtra("os release", os.release());
+    });
+
+    this._log("Sentry client init");
+  }
+
+  public async sendMessage(message: string, context: any) {
+    this._log("Sending task hit...");
+
+    Sentry.withScope(function (scope) {
+      scope.setExtras(context);
+
+      Sentry.captureMessage(message);
+    });
+
+    await Sentry.flush(SentryClient.SENTRY_FLUSH_TIMEOUT);
+    this._log("Task hit sent");
+  }
+
+  public async sendErrorReport(error: Error): Promise<void> {
+    this._log("Sending error report...");
+    const errorContextData = contextualizeError(error);
+
+    const {
+      errorType,
+      pluginName,
+      title,
+      description,
+      name,
+      number,
+      message,
+      category,
+      contextMessage,
+    } = errorContextData;
+
+    Sentry.withScope(function (scope) {
+      scope.setTag("errorType", errorType);
+      scope.setExtra("message", message);
+      if (pluginName !== undefined) {
+        scope.setTag("pluginName", pluginName);
+      }
+      if (name !== undefined) {
+        scope.setTag("name", name);
+      }
+      if (number !== undefined) {
+        scope.setTag("number", String(number));
+      }
+      if (title !== undefined) {
+        scope.setExtra("title", title);
+      }
+      if (contextMessage !== undefined) {
+        scope.setExtra("contextMessage", contextMessage);
+      }
+      if (category !== undefined) {
+        scope.setTag("category.name", category.name);
+        scope.setExtra("category.title", category.title);
+      }
+      if (description !== undefined) {
+        scope.setExtra("description", description);
+      }
+
+      Sentry.captureException(error);
+    });
+    await Sentry.flush(SentryClient.SENTRY_FLUSH_TIMEOUT);
+    this._log(`Successfully sent report: '${message}'`);
+  }
+}
+
 export class ErrorReporter implements ErrorReporterClient {
   public static async getInstance(rootPath: string, enabled: boolean) {
     const [buidlerVersion, clientId] = await Promise.all([
@@ -96,7 +190,15 @@ export class ErrorReporter implements ErrorReporterClient {
   }) {
     this._enabled = enabled && !isLocalDev();
 
-    this._clients = [];
+    const sentryClient = new SentryClient(
+      projectId,
+      clientId,
+      userType,
+      userAgent,
+      buidlerVersion
+    );
+
+    this._clients = [sentryClient];
   }
 
   public async sendMessage(message: string, context: any) {
