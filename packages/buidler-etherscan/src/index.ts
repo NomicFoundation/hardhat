0c719451b06ee091b230a2d66feaf34b2c18f99f
nmlinaric <nikola.mlinaric93@gmail.com>
2020-07-07 09:29:08 +0200

    Addressed PR comments - fixed naming, restored default logic

--- packages/buidler-etherscan/src/index.ts
+++ packages/buidler-etherscan/src/index.ts
@@ -3,40 +3,49 @@ import {
   TASK_COMPILE_GET_COMPILER_INPUT,
   TASK_FLATTEN_GET_FLATTENED_SOURCE,
 } from "@nomiclabs/buidler/builtin-tasks/task-names";
-import { task } from "@nomiclabs/buidler/config";
-import { BuidlerPluginError, readArtifact } from "@nomiclabs/buidler/plugins";
+import {task} from "@nomiclabs/buidler/config";
+import {BuidlerPluginError, readArtifact} from "@nomiclabs/buidler/plugins";
 
 import AbiEncoder from "./AbiEncoder";
-import { getDefaultEtherscanConfig } from "./config";
+import {getDefaultEtherscanConfig} from "./config";
 import {
   getVerificationStatus,
   verifyContract,
 } from "./etherscan/EtherscanService";
-import { toRequest } from "./etherscan/EtherscanVerifyContractRequest";
-import { getLongVersion } from "./solc/SolcVersions";
-import { EtherscanConfig } from "./types";
-import { Deployments } from "./types";
+import {toRequest} from "./etherscan/EtherscanVerifyContractRequest";
+import {getLongVersion} from "./solc/SolcVersions";
+import {EtherscanConfig} from "./types";
+import {DeploymentsDir} from "./types";
+import path from "path";
 
 
 task("verify-contract", "Verifies contract on etherscan")
   .addParam("contractName", "Name of the deployed contract")
-  .addOptionalParam("deployments", "File path for smart contracts deployments")
+  .addParam("address", "Deployed address of smart contract")
+  .addOptionalParam("deploymentsDir", "Path to directory with deployment files")
   .addOptionalParam(
     "libraries",
     'Stringified JSON object in format of {library1: "0x2956356cd2a2bf3202f771f50d3d14a367b48071"}'
+  )
+  .addOptionalVariadicPositionalParam(
+    "constructorArguments",
+    "arguments for contract constructor",
+    []
   )
   .setAction(
     async (
       taskArgs: {
         contractName: string;
-        deployments: string;
+        address: string;
+        deploymentsDir: string;
         libraries: string;
         source: string;
+        constructorArguments: string[];
       },
-      { config, run }
+      {config, run}
     ) => {
       const etherscan: EtherscanConfig = getDefaultEtherscanConfig(config);
-      let deployments: Deployments = {
+      let deploymentsDir: DeploymentsDir = {
         address: "",
         args: []
       };
@@ -46,10 +55,12 @@ task("verify-contract", "Verifies contract on etherscan")
         );
       }
 
-      if (taskArgs.deployments) {
-        deployments = JSON.parse(taskArgs.deployments);
+      if (taskArgs.deploymentsDir) {
+        deploymentsDir = JSON.parse(path.join(taskArgs.deploymentsDir, taskArgs.contractName + ".json"));
+        taskArgs.address = deploymentsDir.address;
+        taskArgs.constructorArguments = deploymentsDir.args;
       } else {
-        throw new Error("Please provide deployments location")
+        throw new Error(`Failed to obtain contract ${taskArgs.contractName} deployments file ${taskArgs.deploymentsDir}`);
       }
 
       const index: number = taskArgs.contractName.indexOf(":");
@@ -72,7 +83,7 @@ task("verify-contract", "Verifies contract on etherscan")
 
       const request = toRequest({
         apiKey: etherscan.apiKey,
-        contractAddress: deployments.address,
+        contractAddress: taskArgs.address,
         sourceCode: source,
         contractName: `${etherscanContractName}`,
         compilerVersion: config.solc.fullVersion,
@@ -80,7 +91,7 @@ task("verify-contract", "Verifies contract on etherscan")
         // runs: config.solc.optimizer.runs,
         constructorArguments: AbiEncoder.encodeConstructor(
           abi,
-          deployments.args
+          taskArgs.constructorArguments
         ),
         libraries: taskArgs.libraries,
       });
@@ -88,7 +99,7 @@ task("verify-contract", "Verifies contract on etherscan")
       const response = await verifyContract(etherscan.url, request);
 
       console.log(
-        `Successfully submitted contract at ${deployments.address} for verification on etherscan. Waiting for verification result...`
+        `Successfully submitted contract at ${deploymentsDir.address} for verification on etherscan. Waiting for verification result...`
       );
 
       await getVerificationStatus(etherscan.url, response.message);
