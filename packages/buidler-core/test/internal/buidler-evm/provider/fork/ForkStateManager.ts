5a3829d43f4c8ee9ef4d4336da55e726072faeae
Michał Sieczkowski <michal@ethworks.io>
2020-07-27 13:55:59 +0200

    Extract JsonRpcClient and ForkStateManager setup to beforeEach hook

--- packages/buidler-core/test/internal/buidler-evm/provider/fork/ForkStateManager.ts
+++ packages/buidler-core/test/internal/buidler-evm/provider/fork/ForkStateManager.ts
@@ -7,42 +7,38 @@ import { ForkStateManager } from "../../../../../src/internal/buidler-evm/provid
 // reused from ethers.js
 const INFURA_URL = `https://mainnet.infura.io/v3/84842078b09946638c03157f83405213`;
 
+const DAI_ADDRESS = Buffer.from(
+  "6b175474e89094c44da98b954eedeac495271d0f",
+  "hex"
+);
+
 describe("ForkStateManager", () => {
+  let client: JsonRpcClient;
+  let blockNumber: BN;
+  let fsm: ForkStateManager;
+
+  beforeEach(async () => {
+    client = JsonRpcClient.forUrl(INFURA_URL);
+    blockNumber = await client.getLatestBlockNumber();
+    fsm = new ForkStateManager(client, blockNumber);
+  });
+
   it("can be constructed", () => {
-    const client = JsonRpcClient.forUrl(INFURA_URL);
-    const fsm = new ForkStateManager(client, new BN(0));
     assert.instanceOf(fsm, ForkStateManager);
   });
 
   it("can get contract code", async () => {
-    const client = JsonRpcClient.forUrl(INFURA_URL);
-    const blockNumber = await client.getLatestBlockNumber();
-    const fsm = new ForkStateManager(client, blockNumber);
-
-    const daiAddress = Buffer.from(
-      "6b175474e89094c44da98b954eedeac495271d0f",
-      "hex"
-    );
-
-    const remoteCode = await client.getCode(daiAddress, blockNumber);
-    const fsmCode = await fsm.getContractCode(daiAddress);
+    const remoteCode = await client.getCode(DAI_ADDRESS, blockNumber);
+    const fsmCode = await fsm.getContractCode(DAI_ADDRESS);
 
     assert.equal(fsmCode.toString("hex"), remoteCode.toString("hex"));
   });
 
   it("can override contract code", async () => {
-    const client = JsonRpcClient.forUrl(INFURA_URL);
-    const blockNumber = await client.getLatestBlockNumber();
-    const fsm = new ForkStateManager(client, blockNumber);
-
-    const daiAddress = Buffer.from(
-      "6b175474e89094c44da98b954eedeac495271d0f",
-      "hex"
-    );
     const code = Buffer.from("deadbeef", "hex");
 
-    await fsm.putContractCode(daiAddress, code);
-    const fsmCode = await fsm.getContractCode(daiAddress);
+    await fsm.putContractCode(DAI_ADDRESS, code);
+    const fsmCode = await fsm.getContractCode(DAI_ADDRESS);
 
     assert.equal(fsmCode.toString("hex"), code.toString("hex"));
   });
