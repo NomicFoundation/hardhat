7d25c5b3c00346949379333b35816bb17fbc33e8
Michał Sieczkowski <michal@ethworks.io>
2020-08-12 15:54:23 +0200

    Add tests for eth_getBalance call on forked provider

--- packages/buidler-core/test/internal/buidler-evm/helpers/conversions.ts
+++ packages/buidler-core/test/internal/buidler-evm/helpers/conversions.ts
@@ -2,6 +2,15 @@
  * Transforms a QUANTITY to a number. It should only be used if you are 100% sure that the value
  * fits in a number.
  */
+import { BN } from "ethereumjs-util";
+
+import { decode } from "../../../../src/internal/buidler-evm/jsonrpc/types";
+import { rpcQuantity } from "../../../../src/internal/buidler-evm/provider/input";
+
 export function quantityToNumber(quantity: string): number {
   return parseInt(quantity.substring(2), 16);
+}
+
+export function quantityToBN(quantity: string): BN {
+  return decode(quantity, rpcQuantity);
 }
--- packages/buidler-core/test/internal/buidler-evm/provider/forked-provider.ts
+++ packages/buidler-core/test/internal/buidler-evm/provider/forked-provider.ts
@@ -1,7 +1,14 @@
 import { assert } from "chai";
-import { addHexPrefix, BN, toBuffer } from "ethereumjs-util";
+import { addHexPrefix, BN, bufferToHex, toBuffer } from "ethereumjs-util";
 
-import { DAI_ADDRESS, INFURA_URL } from "../helpers/constants";
+import { assertQuantity } from "../helpers/assertions";
+import {
+  DAI_ADDRESS,
+  EMPTY_ACCOUNT_ADDRESS,
+  INFURA_URL,
+  WETH_ADDRESS,
+} from "../helpers/constants";
+import { quantityToBN } from "../helpers/conversions";
 import { setCWD } from "../helpers/cwd";
 import { useForkedProvider } from "../helpers/useForkedProvider";
 
@@ -23,7 +30,7 @@ describe("Forked provider", () => {
   });
 
   describe("eth_call", function () {
-    it("should return DAI total supply", async function () {
+    it("should be able to return DAI total supply", async function () {
       const daiTotalSupplySelector = "0x18160ddd";
       const daiAddress = addHexPrefix(DAI_ADDRESS.toString("hex"));
 
@@ -33,6 +40,24 @@ describe("Forked provider", () => {
 
       const bnResult = new BN(toBuffer(result));
       assert.isTrue(bnResult.gtn(0));
+    });
+  });
+
+  describe("get_balance", function () {
+    it("should return 0 for empty accounts", async function () {
+      assertQuantity(
+        await this.provider.send("eth_getBalance", [
+          bufferToHex(EMPTY_ACCOUNT_ADDRESS),
+        ]),
+        0
+      );
+    });
+
+    it("should return balance of WETH contract", async function () {
+      const result = await this.provider.send("eth_getBalance", [
+        bufferToHex(WETH_ADDRESS),
+      ]);
+      assert.isTrue(quantityToBN(result).gtn(0));
     });
   });
 });
