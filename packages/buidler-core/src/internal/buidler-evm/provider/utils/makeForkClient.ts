9b55afad70cdac6843d52047118e70ff0589b531
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-08-21 15:33:41 +0200

    Improve reliability on infura

--- packages/buidler-core/src/internal/buidler-evm/provider/utils/makeForkClient.ts
+++ packages/buidler-core/src/internal/buidler-evm/provider/utils/makeForkClient.ts
@@ -5,9 +5,18 @@ import { JsonRpcClient } from "../../jsonrpc/client";
 
 export async function makeForkClient(forkConfig: ForkConfig) {
   const forkClient = JsonRpcClient.forUrl(forkConfig.jsonRpcUrl);
-  const forkBlockNumber =
-    forkConfig.blockNumber !== undefined
-      ? new BN(forkConfig.blockNumber)
-      : await forkClient.getLatestBlockNumber();
+  let forkBlockNumber;
+  if (forkConfig.blockNumber !== undefined) {
+    forkBlockNumber = new BN(forkConfig.blockNumber);
+  } else {
+    forkBlockNumber = await forkClient.getLatestBlockNumber();
+    if (forkConfig.jsonRpcUrl.includes("infura")) {
+      // Probably because of load balancing infura nodes can differ in their
+      // view of the blockchain. This makes it possible to query for a block
+      // with the latest returned block number and get null.
+      // To prevent this we use a past block number.
+      forkBlockNumber = forkBlockNumber.subn(2);
+    }
+  }
   return { forkClient, forkBlockNumber };
 }
