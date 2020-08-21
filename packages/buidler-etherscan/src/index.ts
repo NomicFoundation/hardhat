18b79111785beacad600b75765a028e811fb51b1
Sebastián Claudio Nale <sebinale@gmail.com>
2020-08-20 21:02:50 -0300

    Extract ABI encoding to a separate module.

--- packages/buidler-etherscan/src/index.ts
+++ packages/buidler-etherscan/src/index.ts
@@ -165,32 +165,13 @@ Common causes:
     throw new NomicLabsBuidlerPluginError(pluginName, message);
   }
 
-  const { Interface } = await import("@ethersproject/abi");
-  const { isABIArgumentLengthError, isABIArgumentTypeError } = await import(
-    "./ABITypes"
+  const { encodeArguments } = await import("./ABIEncoder");
+  const deployArgumentsEncoded = await encodeArguments(
+    contractInformation.contract.abi,
+    contractInformation.contractFilename,
+    contractInformation.contractName,
+    constructorArguments
   );
-  const contractInterface = new Interface(contractInformation.contract.abi);
-  let deployArgumentsEncoded;
-  try {
-    deployArgumentsEncoded = contractInterface
-      .encodeDeploy(constructorArguments)
-      .replace("0x", "");
-  } catch (error) {
-    if (isABIArgumentLengthError(error)) {
-      const { contractName, contractFilename } = contractInformation;
-      // TODO: add a list of types and constructor arguments to the error message?
-      const message = `The constructor for ${contractFilename}:${contractName} has ${error.count.types} parameters
- but ${error.count.values} arguments were provided instead.`;
-      throw new NomicLabsBuidlerPluginError(pluginName, message, error);
-    }
-    if (isABIArgumentTypeError(error)) {
-      const message = `Value ${error.value} cannot be encoded for the parameter ${error.argument}.
-Encoder error reason: ${error.reason}`;
-      throw new NomicLabsBuidlerPluginError(pluginName, message, error);
-    }
-    // Should be unreachable.
-    throw error;
-  }
 
   // Ensure the linking information is present in the compiler input;
   compilerInput.settings.libraries = contractInformation.libraryLinks;
