3424a8e53e156c1c8fec720af6e464bbb8b15a8e
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2020-09-23 14:54:09 +0200

    Finish BatchRequestService

--- packages/buidler-core/src/internal/buidler-evm/jsonrpc/http.ts
+++ packages/buidler-core/src/internal/buidler-evm/jsonrpc/http.ts
@@ -3,12 +3,17 @@ import type { default as nodeFetch } from "node-fetch";
 import { BuidlerError } from "../../core/errors";
 import { ERRORS } from "../../core/errors-list";
 import {
+  isSuccessfulJsonResponse,
   JsonRpcRequest,
   parseBatchJsonResponse,
-  SuccessfulJsonRpcResponse,
 } from "../../util/jsonrpc";
 
-type ResultOrError = any | Error;
+type ResultOrError = unknown | Error;
+
+interface RpcError extends Error {
+  code?: number;
+  data?: any;
+}
 
 export interface HttpRequestService {
   send(request: JsonRpcRequest[]): Promise<ResultOrError[]>;
@@ -22,6 +27,10 @@ export class BatchHttpRequestService implements HttpRequestService {
   ) {}
 
   public async send(request: JsonRpcRequest[]): Promise<ResultOrError[]> {
+    // We create the error here to capture the stack traces at this point,
+    // the async call that follows would probably loose of the stack trace
+    const errors: RpcError[] = request.map(() => new Error());
+
     try {
       const response = await this._fetch(this._url, {
         method: "POST",
@@ -33,10 +42,27 @@ export class BatchHttpRequestService implements HttpRequestService {
         },
       });
 
-      const jsonResponse = parseBatchJsonResponse(await response.text());
-      return jsonResponse.map(
-        (res) => (res as SuccessfulJsonRpcResponse).result
-      );
+      const text = await response.text();
+      const jsonResponse = parseBatchJsonResponse(text);
+      const orderedResponse = request.map(({ id }) => {
+        const matched = jsonResponse.find((el) => el.id === id);
+        if (matched === undefined) {
+          throw new BuidlerError(ERRORS.NETWORK.INVALID_JSON_RESPONSE, {
+            response: text,
+          });
+        }
+        return matched;
+      });
+      return orderedResponse.map((res, i) => {
+        if (isSuccessfulJsonResponse(res)) {
+          return res.result;
+        }
+        const error = errors[i];
+        error.message = res.error.message;
+        error.code = res.error.code;
+        error.data = res.error.data;
+        return error;
+      });
     } catch (error) {
       if (error.code === "ECONNREFUSED") {
         throw new BuidlerError(ERRORS.NETWORK.NODE_IS_NOT_RUNNING, {}, error);
--- packages/buidler-core/test/internal/buidler-evm/jsonrpc/http.ts
+++ packages/buidler-core/test/internal/buidler-evm/jsonrpc/http.ts
@@ -5,27 +5,29 @@ import sinon, { SinonStub } from "sinon";
 import { BatchHttpRequestService } from "../../../../src/internal/buidler-evm/jsonrpc/http";
 import { JsonRpcRequest } from "../../../../src/internal/util/jsonrpc";
 
-function getRequest(method: string, params: any[] = []): JsonRpcRequest[] {
-  return [
-    {
-      jsonrpc: "2.0",
-      id: 1,
-      method,
-      params,
-    },
-  ];
+function getRequest(
+  items: Array<{ method: string; params?: any[] }> = []
+): JsonRpcRequest[] {
+  return items.map(({ method, params = [] }, index) => ({
+    jsonrpc: "2.0",
+    id: index + 1,
+    method,
+    params,
+  }));
 }
 
-function getResponse(result: any) {
+function getResponse(
+  items: Array<{ result: any; id?: number } | { error: any; id?: number }> = []
+) {
   return {
     text: async () =>
-      JSON.stringify([
-        {
+      JSON.stringify(
+        items.map((item, index) => ({
           jsonrpc: "2.0",
-          id: 1,
-          result,
-        },
-      ]),
+          id: index + 1,
+          ...item,
+        }))
+      ),
   };
 }
 
@@ -40,25 +42,114 @@ describe("BatchHttpRequestService", () => {
 
   describe("send", () => {
     it("can fetch a single request", async () => {
-      fetchMock = sinon.mock().resolves(getResponse("0xdeadbeef"));
-      const response = await service.send(getRequest("eth_blockNumber"));
-      assert.deepEqual(response, ["0xdeadbeef"]);
+      const request = getRequest([{ method: "eth_blockNumber" }]);
+      const response = getResponse([{ result: "0xdeadbeef" }]);
+      fetchMock = sinon.mock().resolves(response);
+      const result = await service.send(request);
+      assert.deepEqual(result, ["0xdeadbeef"]);
     });
 
-    it.skip("can fetch multiple requests", async () => {
-      // TODO
+    it("can fetch multiple requests", async () => {
+      const request = getRequest([
+        { method: "eth_blockNumber" },
+        { method: "eth_coinbase" },
+      ]);
+      const response = getResponse([
+        { result: "0xdeadbeef" },
+        { result: "0x1337" },
+      ]);
+      fetchMock = sinon.mock().resolves(response);
+      const result = await service.send(request);
+      assert.deepEqual(result, ["0xdeadbeef", "0x1337"]);
     });
 
-    it.skip("handles errors", async () => {
-      // TODO
+    it("handles errors", async () => {
+      const request = getRequest([
+        { method: "net_version" },
+        { method: "eth_blockNumber" },
+        { method: "eth_coinbase" },
+      ]);
+      const response = getResponse([
+        { error: { message: "Nope", code: -1234, data: "foo" } },
+        { result: "0xdeadbeef" },
+        { error: { message: "Sorry", code: 42 } },
+      ]);
+      fetchMock = sinon.mock().resolves(response);
+      const result: any[] = await service.send(request);
+
+      const error1: any = new Error();
+      error1.message = "Nope";
+      error1.code = -1234;
+      error1.data = "foo";
+      const error3: any = new Error();
+      error3.message = "Sorry";
+      error3.code = 42;
+      error3.data = undefined;
+
+      assert.lengthOf(result, 3);
+      assert.deepEqual({ ...result[0] }, { ...error1 });
+      assert.deepEqual(result[1], "0xdeadbeef");
+      assert.deepEqual({ ...result[2] }, { ...error3 });
     });
 
-    it.skip("returns the right responses for the right requests", async () => {
-      // TODO match responses with requests by ids
+    it("returns the right responses for the right requests", async () => {
+      const request = getRequest([
+        { method: "net_version" },
+        { method: "eth_blockNumber" },
+        { method: "eth_coinbase" },
+      ]);
+      const response = getResponse([
+        { result: "a", id: 2 },
+        { result: "b", id: 1 },
+        { result: "c", id: 3 },
+      ]);
+      fetchMock = sinon.mock().resolves(response);
+      const result = await service.send(request);
+      assert.deepEqual(result, ["b", "a", "c"]);
     });
 
-    it.skip("validates responses correctly", async () => {
-      // TODO
+    it("rejects on mismatched number of responses", async () => {
+      const request = getRequest([
+        { method: "net_version" },
+        { method: "eth_blockNumber" },
+        { method: "eth_coinbase" },
+      ]);
+      const response = getResponse([{ result: "a" }, { result: "b" }]);
+      fetchMock = sinon.mock().resolves(response);
+      await assert.isRejected(service.send(request));
+    });
+
+    it("rejects on mismatched ids of responses", async () => {
+      const request = getRequest([
+        { method: "net_version" },
+        { method: "eth_blockNumber" },
+        { method: "eth_coinbase" },
+      ]);
+      const response = getResponse([
+        { result: "a", id: 1 },
+        { result: "b", id: 2 },
+        { result: "c", id: 1000 },
+      ]);
+      fetchMock = sinon.mock().resolves(response);
+      await assert.isRejected(service.send(request));
+    });
+
+    it("rejects on non-json response", async () => {
+      const request = getRequest([{ method: "net_version" }]);
+      const response = {
+        text: async () => "I am not JSON, sorry.",
+      };
+      fetchMock = sinon.mock().resolves(response);
+      await assert.isRejected(service.send(request));
+    });
+
+    it("rejects on invalid json response", async () => {
+      const request = getRequest([{ method: "net_version" }]);
+      const response = {
+        text: async () => '[{"foo":123}]',
+      };
+      fetchMock = sinon.mock().resolves(response);
+      await assert.isRejected(service.send(request));
     });
   });
 });
