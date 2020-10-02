7e5f246103135bc54140cb5609f78fcea6778570
Franco Victorio <victorio.franco@gmail.com>
2020-10-02 16:39:39 -0300

    Properly pluralize compilation message

--- packages/hardhat-core/src/builtin-tasks/compile.ts
+++ packages/hardhat-core/src/builtin-tasks/compile.ts
@@ -28,6 +28,7 @@ import { ResolvedFile, Resolver } from "../internal/solidity/resolver";
 import { localPathToSourceName } from "../internal/solidity/source-names";
 import { glob } from "../internal/util/glob";
 import { getCompilersDir } from "../internal/util/global-dir";
+import { pluralize } from "../internal/util/strings";
 import { unsafeObjectEntries, unsafeObjectKeys } from "../internal/util/unsafe";
 import { SolcInput } from "../types";
 
@@ -846,7 +847,9 @@ export default function () {
             .filter((file) => job.emitsArtifacts(file)).length;
         }
 
-        console.log(`Compiling ${count} files with ${solcVersion}`);
+        console.log(
+          `Compiling ${count} ${pluralize(count, "file")} with ${solcVersion}`
+        );
       }
     );
 
