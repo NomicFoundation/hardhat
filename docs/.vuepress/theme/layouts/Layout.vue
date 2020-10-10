07addaeb7e435bddfa8ffee32950208e6aab808d
Patricio Palladino <email@patriciopalladino.com>
2020-10-10 14:49:27 -0300

    Fix prisim import

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -136,5 +136,5 @@
   };
 </script>
 
-<style src="../../../node_modules/prismjs/themes/prism-tomorrow.css"></style>
+<style src="prismjs/themes/prism-tomorrow.css"></style>
 <style src="../styles/theme.styl" lang="stylus"></style>
