23060264163679f7dedb6ba566ae9c15a3283b33
Franco Victorio <victorio.franco@gmail.com>
2020-10-15 14:18:41 -0300

    Remove getProvider from JsonRpcServer interface

--- packages/hardhat-core/src/types/builtin-tasks/node.ts
+++ packages/hardhat-core/src/types/builtin-tasks/node.ts
@@ -1,8 +1,4 @@
-import { EIP1193Provider } from "../provider";
-
 export interface JsonRpcServer {
-  getProvider(name: string): EIP1193Provider;
-
   listen(): Promise<{ address: string; port: number }>;
   waitUntilClosed(): Promise<void>;
 
