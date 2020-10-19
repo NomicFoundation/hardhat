2a645859699e1cea0edd10799a40c2563e69a4e9
Franco Victorio <victorio.franco@gmail.com>
2020-10-19 16:41:02 -0300

    Use .address instead of .getAddress() in sample project

--- packages/hardhat-core/sample-project/hardhat.config.js
+++ packages/hardhat-core/sample-project/hardhat.config.js
@@ -6,7 +6,7 @@ task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(await account.getAddress());
+    console.log(account.address);
   }
 });
 
