20b13c18611aa1e235d0a8fd25e4ddeb981dc9ab
Patricio Palladino <email@patriciopalladino.com>
2020-10-20 14:40:21 -0300

    Small improvements to the getting started section

--- packages/hardhat-core/sample-project/scripts/sample-script.js
+++ packages/hardhat-core/sample-project/scripts/sample-script.js
@@ -1,13 +1,16 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional 
 // but useful for running the script in a standalone fashion through `node <script>`.
+//
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts through it. 
-  // If this runs in a standalone fashion you may want to call compile manually 
-  // to make sure everything is compiled
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile 
+  // manually to make sure everything is compiled
   // await hre.run('compile');
 
   // We get the contract to deploy
