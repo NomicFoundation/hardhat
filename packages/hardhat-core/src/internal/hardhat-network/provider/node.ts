925e195214faebbe8248c683cff29d6f1fadcd56
Michał Sieczkowski <michal@ethworks.io>
2020-10-22 16:00:24 +0200

    WIP

--- packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
@@ -262,6 +262,14 @@ export class HardhatNode extends EventEmitter {
     error?: Error;
     consoleLogMessages: string[];
   }> {
+    // Geth throws this error if a tx is sent twice
+    if (this._transactionWasSuccessful(tx)) {
+      throw new InvalidInputError(
+        `known transaction: ${bufferToHex(tx.hash(true)).toString()}`
+      );
+    }
+
+    await this._txPool.addTransaction(tx);
     await this._validateTransaction(tx);
     await this._notifyPendingTransaction(tx);
 
@@ -1154,12 +1162,7 @@ export class HardhatNode extends EventEmitter {
   }
 
   private async _validateTransaction(tx: Transaction) {
-    // Geth throws this error if a tx is sent twice
-    if (this._transactionWasSuccessful(tx)) {
-      throw new InvalidInputError(
-        `known transaction: ${bufferToHex(tx.hash(true)).toString()}`
-      );
-    }
+    
 
     if (!tx.verifySignature()) {
       throw new InvalidInputError("Invalid transaction signature");
