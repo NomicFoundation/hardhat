a6e10ea1acd9e3d94b1c4a3e6f6feae3786b8076
Patricio Palladino <email@patriciopalladino.com>
2020-10-23 10:48:48 -0300

    Update site banner

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -5,8 +5,8 @@
   >
     <div style="position: relative">
       <div class="banner">
-        <a class="not-narrow" href="https://medium.com/nomic-labs-blog/better-solidity-debugging-console-log-is-finally-here-fc66c54f2c4a">Better Solidity debugging: stack traces and console.log are finally here →</a>
-        <a class="only-narrow" href="https://medium.com/nomic-labs-blog/better-solidity-debugging-console-log-is-finally-here-fc66c54f2c4a">Stack traces and console.log are here →</a>
+        <a class="not-narrow" href="https://medium.com/nomic-labs-blog/buidler-has-evolved-introducing-hardhat-4bccd13bc931">Buidler is now Hardhat. Learn more about the latest release and rebrand.</a>
+        <a class="only-narrow" href="https://medium.com/nomic-labs-blog/buidler-has-evolved-introducing-hardhat-4bccd13bc931">Buidler is now Hardhat.</a>
       </div>
       <div style="position: relative">
         <header>
