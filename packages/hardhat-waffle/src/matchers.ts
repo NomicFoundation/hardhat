49b541bf985564d06a21bbaac2439938283e625e
Sebastián Claudio Nale <sebinale@gmail.com>
2020-10-29 14:44:53 -0300

    Remove unnecessary workaround in hardhat-waffle.

--- packages/hardhat-waffle/src/matchers.ts
+++ packages/hardhat-waffle/src/matchers.ts
@@ -1,19 +1,13 @@
-import path from "path";
+import type ChaiStatic from "chai";
+
+import type { waffleChai as waffleChaiT } from "./waffle-chai";
 
 export function initializeWaffleMatchers(projectRoot: string) {
   try {
-    let chaiPath = require.resolve("chai");
-
-    // When using this plugin linked from sources, we'd end up with the chai
-    // used to test it, not the project's version of chai, so we correct it.
-    if (chaiPath.startsWith(path.join(__dirname, "..", "node_modules"))) {
-      chaiPath = require.resolve("chai", {
-        paths: [projectRoot],
-      });
-    }
-
-    const chai = require(chaiPath);
-    const { waffleChai } = require("./waffle-chai");
+    const chai: typeof ChaiStatic = require("chai");
+    const {
+      waffleChai,
+    }: { waffleChai: typeof waffleChaiT } = require("./waffle-chai");
 
     chai.use(waffleChai);
   } catch (error) {
