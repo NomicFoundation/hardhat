5a8a2b905d63742366e839dc6448d2ef71ab16d2
Michał Sieczkowski <michal@ethworks.io>
2020-11-04 15:44:51 +0100

    Add two more mineBlock tests and divide the tests into 2 describes

--- packages/hardhat-core/test/internal/hardhat-network/provider/node.ts
+++ packages/hardhat-core/test/internal/hardhat-network/provider/node.ts
@@ -37,7 +37,8 @@ describe("HardhatNode", () => {
   beforeEach(async () => {
     let common: Common;
     [common, node] = await HardhatNode.create(config);
-    createTestTransaction = (txData) => new FakeTransaction(txData, { common });
+    createTestTransaction = (txData) =>
+      new FakeTransaction({ gasPrice: 1, ...txData }, { common });
   });
 
   describe("constructor", () => {
@@ -82,131 +83,191 @@ describe("HardhatNode", () => {
       );
     }
 
-    it("can mine an empty block", async () => {
-      const beforeBlock = await node.getLatestBlockNumber();
-      await node.mineBlock();
-      const currentBlock = await node.getLatestBlockNumber();
-      assert.equal(currentBlock.toString(), beforeBlock.addn(1).toString());
+    describe("basic tests", () => {
+      it("can mine an empty block", async () => {
+        const beforeBlock = await node.getLatestBlockNumber();
+        await node.mineBlock();
+        const currentBlock = await node.getLatestBlockNumber();
+        assert.equal(currentBlock.toString(), beforeBlock.addn(1).toString());
+      });
+
+      it("can mine a block with one transaction", async () => {
+        const tx = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 21_000,
+          value: 1234,
+        });
+        await node.runTransaction(tx);
+        await node.mineBlock();
+
+        await assertTransactionsWereMined([tx]);
+        const balance = await node.getAccountBalance(
+          EMPTY_ACCOUNT_ADDRESS,
+          null
+        );
+        assert.equal(balance.toString(), "1234");
+      });
+
+      it("can mine a block with two transactions from different senders", async () => {
+        const tx1 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 21_000,
+          value: 1234,
+        });
+        const tx2 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[1],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 21_000,
+          value: 1234,
+        });
+        await node.runTransaction(tx1);
+        await node.runTransaction(tx2);
+        await node.mineBlock();
+
+        await assertTransactionsWereMined([tx1, tx2]);
+        const balance = await node.getAccountBalance(
+          EMPTY_ACCOUNT_ADDRESS,
+          null
+        );
+        assert.equal(balance.toString(), "2468");
+      });
+
+      it("can mine a block with two transactions from the same sender", async () => {
+        const tx1 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 21_000,
+          value: 1234,
+        });
+        const tx2 = createTestTransaction({
+          nonce: 1,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 21_000,
+          value: 1234,
+        });
+        await node.runTransaction(tx1);
+        await node.runTransaction(tx2);
+        await node.mineBlock();
+
+        await assertTransactionsWereMined([tx1, tx2]);
+        const balance = await node.getAccountBalance(
+          EMPTY_ACCOUNT_ADDRESS,
+          null
+        );
+        assert.equal(balance.toString(), "2468");
+      });
+
+      it("sets correct gasUsed values", async () => {
+        const tx1 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 100_000,
+          value: 1234,
+        });
+        const tx2 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[1],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 100_000,
+          value: 1234,
+        });
+        await node.runTransaction(tx1);
+        await node.runTransaction(tx2);
+        await node.mineBlock();
+
+        const tx1Receipt = await node.getTransactionReceipt(tx1.hash());
+        const tx2Receipt = await node.getTransactionReceipt(tx2.hash());
+        assertQuantity(tx1Receipt?.gasUsed, 21_000);
+        assertQuantity(tx2Receipt?.gasUsed, 21_000);
+
+        const block = await node.getLatestBlock();
+        assert.equal(bufferToInt(block.header.gasUsed), 42_000);
+      });
     });
 
-    it("can mine a block with one transaction", async () => {
-      const tx = createTestTransaction({
-        nonce: 0,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[0],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 21_000,
-        value: 1234,
+    describe("gas limit tests", () => {
+      it("mines only as many transactions as would fit in a block", async () => {
+        node.setBlockGasLimit(30_000);
+        const tx1 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 21_000,
+        });
+        const tx2 = createTestTransaction({
+          nonce: 1,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 21_000,
+        });
+        await node.runTransaction(tx1);
+        await node.runTransaction(tx2);
+        await node.mineBlock();
+
+        await assertTransactionsWereMined([tx1]);
+        assert.isUndefined(await node.getTransactionReceipt(tx2.hash()));
       });
-      await node.runTransaction(tx);
-      await node.mineBlock();
 
-      await assertTransactionsWereMined([tx]);
-      const balance = await node.getAccountBalance(EMPTY_ACCOUNT_ADDRESS, null);
-      assert.equal(balance.toString(), "1234");
-    });
+      it("uses gasUsed value for determining if two transactions will fit in a block", async () => {
+        node.setBlockGasLimit(50_000);
+        const tx1 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 40_000, // actual gas used is 21_000
+        });
+        const tx2 = createTestTransaction({
+          nonce: 1,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 40_000, // actual gas used is 21_000
+        });
+        await node.runTransaction(tx1);
+        await node.runTransaction(tx2);
+        await node.mineBlock();
 
-    it("can mine a block with two transactions from different senders", async () => {
-      const tx1 = createTestTransaction({
-        nonce: 0,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[0],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 21_000,
-        value: 1234,
+        await assertTransactionsWereMined([tx1, tx2]);
       });
-      const tx2 = createTestTransaction({
-        nonce: 0,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[1],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 21_000,
-        value: 1234,
+
+      xit("puts as many transactions as it can in a block", async () => {
+        node.setBlockGasLimit(42_000);
+        const tx1 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 40_000, // actual gas used is 21_000
+        });
+        const expensiveTx2 = createTestTransaction({
+          nonce: 0,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[1],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 40_000,
+          data: Buffer.alloc(1024, 1), // actual gas used is 37_384
+        });
+        const tx3 = createTestTransaction({
+          nonce: 1,
+          from: DEFAULT_ACCOUNTS_ADDRESSES[0],
+          to: EMPTY_ACCOUNT_ADDRESS,
+          gasLimit: 40_000, // actual gas used is 21_000
+        });
+        await node.runTransaction(tx1);
+        await node.runTransaction(expensiveTx2);
+        await node.runTransaction(tx3);
+        await node.mineBlock();
+
+        await assertTransactionsWereMined([tx1, tx3]);
+        assert.isUndefined(
+          await node.getTransactionReceipt(expensiveTx2.hash())
+        );
       });
-      await node.runTransaction(tx1);
-      await node.runTransaction(tx2);
-      await node.mineBlock();
-
-      await assertTransactionsWereMined([tx1, tx2]);
-      const balance = await node.getAccountBalance(EMPTY_ACCOUNT_ADDRESS, null);
-      assert.equal(balance.toString(), "2468");
-    });
-
-    it("can mine a block with two transactions from the same sender", async () => {
-      const tx1 = createTestTransaction({
-        nonce: 0,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[0],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 21_000,
-        value: 1234,
-      });
-      const tx2 = createTestTransaction({
-        nonce: 1,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[0],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 21_000,
-        value: 1234,
-      });
-      await node.runTransaction(tx1);
-      await node.runTransaction(tx2);
-      await node.mineBlock();
-
-      await assertTransactionsWereMined([tx1, tx2]);
-      const balance = await node.getAccountBalance(EMPTY_ACCOUNT_ADDRESS, null);
-      assert.equal(balance.toString(), "2468");
-    });
-
-    it("sets correct gasUsed values", async () => {
-      const tx1 = createTestTransaction({
-        nonce: 0,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[0],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 100_000,
-        value: 1234,
-      });
-      const tx2 = createTestTransaction({
-        nonce: 0,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[1],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 100_000,
-        value: 1234,
-      });
-      await node.runTransaction(tx1);
-      await node.runTransaction(tx2);
-      await node.mineBlock();
-
-      const tx1Receipt = await node.getTransactionReceipt(tx1.hash());
-      const tx2Receipt = await node.getTransactionReceipt(tx2.hash());
-      assertQuantity(tx1Receipt?.gasUsed, 21_000);
-      assertQuantity(tx2Receipt?.gasUsed, 21_000);
-
-      const block = await node.getLatestBlock();
-      assert.equal(bufferToInt(block.header.gasUsed), 42_000);
-    });
-
-    it("respects block gas limit", async () => {
-      node.setBlockGasLimit(21_000);
-      const tx1 = createTestTransaction({
-        nonce: 0,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[0],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 21_000,
-        value: 1234,
-      });
-      const tx2 = createTestTransaction({
-        nonce: 1,
-        from: DEFAULT_ACCOUNTS_ADDRESSES[0],
-        to: EMPTY_ACCOUNT_ADDRESS,
-        gasLimit: 21_000,
-        value: 1234,
-      });
-      await node.runTransaction(tx1);
-      await node.runTransaction(tx2);
-      await node.mineBlock();
-
-      await assertTransactionsWereMined([tx1]);
-      assert.isUndefined(await node.getTransactionReceipt(tx2.hash()));
-    });
-
-    xit("can mine two transactions which gasLimit sum exceeds block gas limit but actual gas used does not", async () => {
-      // TODO
     });
   });
 });
