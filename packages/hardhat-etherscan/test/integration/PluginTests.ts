a01bfc93af39d4ba01bce12a324f9d108c6a81d7
Sebastián Claudio Nale <sebinale@gmail.com>
2020-12-10 23:20:15 -0300

    Add a test for the verify subtask.
    Improve some old tests by mocking the Rinkeby network as needed.

--- packages/hardhat-etherscan/test/integration/PluginTests.ts
+++ packages/hardhat-etherscan/test/integration/PluginTests.ts
@@ -1,4 +1,4 @@
-import { assert } from "chai";
+import { assert, expect } from "chai";
 import {
   TASK_COMPILE,
   TASK_COMPILE_SOLIDITY_GET_COMPILER_INPUT,
@@ -6,6 +6,8 @@ import {
 import type { task as taskT } from "hardhat/config";
 import { NomicLabsHardhatPluginError } from "hardhat/plugins";
 import type { CompilerInput } from "hardhat/types";
+// tslint:disable-next-line: no-implicit-dependencies
+import nock from "nock";
 import path from "path";
 
 import { TASK_VERIFY_GET_MINIMUM_BUILD } from "../../src/constants";
@@ -13,24 +15,24 @@ import { useEnvironment } from "../helpers";
 
 // These are skipped because they can't currently be run in CI
 describe("Plugin integration tests", function () {
-  this.timeout(1000000);
-  this.beforeAll(function () {
-    if (process.env.RUN_ETHERSCAN_TESTS !== "yes") {
-      this.skip();
-    } else {
-      if (
-        process.env.WALLET_PRIVATE_KEY === undefined ||
-        process.env.WALLET_PRIVATE_KEY === ""
-      ) {
-        throw new Error("missing WALLET_PRIVATE_KEY env variable");
-      }
-    }
-  });
-
   describe("Using a normal Hardhat project", function () {
+    this.timeout(1000000);
+
+    before(function () {
+      if (process.env.RUN_ETHERSCAN_TESTS !== "yes") {
+        this.skip();
+      } else {
+        if (
+          process.env.WALLET_PRIVATE_KEY === undefined ||
+          process.env.WALLET_PRIVATE_KEY === ""
+        ) {
+          throw new Error("missing WALLET_PRIVATE_KEY env variable");
+        }
+      }
+    });
     useEnvironment("hardhat-project", "testnet");
 
-    this.beforeEach(function () {
+    beforeEach(async function () {
       const mutation = getRandomString(this.env);
       const { task }: { task: typeof taskT } = require("hardhat/config");
 
@@ -46,11 +48,12 @@ describe("Plugin integration tests", function () {
           return solcInput;
         }
       );
+
+      // We force compilation to make sure that Hardhat introduces a new random payload.
+      await this.env.run(TASK_COMPILE, { force: true });
     });
 
     it("Should verify deployed inner contract on etherscan", async function () {
-      await this.env.run(TASK_COMPILE, { force: true });
-
       const deployedAddress = await deployContract(
         "InnerContract",
         [],
@@ -59,13 +62,10 @@ describe("Plugin integration tests", function () {
 
       return this.env.run("verify", {
         address: deployedAddress,
-        constructorArgsParams: [],
       });
     });
 
     it("Should verify deployed contract with name clash on etherscan", async function () {
-      await this.env.run(TASK_COMPILE, { force: true });
-
       const deployedAddress = await deployContract(
         "TestReentrancyGuardLocal",
         [],
@@ -74,66 +74,18 @@ describe("Plugin integration tests", function () {
 
       return this.env.run("verify", {
         address: deployedAddress,
-        constructorArgsParams: [],
       });
     });
 
     it("Should verify deployed library on etherscan", async function () {
-      await this.env.run(TASK_COMPILE, { force: true });
-
       const deployedAddress = await deployContract("TestLibrary", [], this.env);
 
       return this.env.run("verify", {
         address: deployedAddress,
-        constructorArgsParams: [],
       });
     });
 
-    // The plugin doesn't look at deployment bytecode while inferring the contract
-    it("fail when the contract can only be singled out by its deploy bytecode", async function () {
-      await this.env.run(TASK_COMPILE, { force: true });
-
-      const amount = "20";
-
-      // We wait a single block because we just want the contract code to be available
-      // at the Ethereum node we're connected to.
-      const deployedAddress = await deployContract(
-        "TestContract1",
-        [amount],
-        this.env,
-        1
-      );
-
-      return this.env
-        .run("verify", {
-          address: deployedAddress,
-          constructorArgsParams: [amount],
-        })
-        .catch((reason) => {
-          assert.instanceOf(
-            reason,
-            NomicLabsHardhatPluginError,
-            "Ambiguous inferences should throw an error"
-          );
-
-          assert.isTrue(
-            reason.message.includes(
-              "use the contract parameter with one of the following contracts"
-            ),
-            "The error should contain an explanation of how to solve the ambiguous inference."
-          );
-          assert.isTrue(
-            reason.message.includes(
-              "contracts/TestContract1.sol:TestContract1"
-            ),
-            "The deployed contract should be among the presented options."
-          );
-        });
-    });
-
     it("Should verify deployed contract with a complex parameter list on etherscan", async function () {
-      await this.env.run(TASK_COMPILE, { force: true });
-
       const modulePath = path.join(process.cwd(), "paramList.js");
       const args = require(modulePath);
       const deployedAddress = await deployContract(
@@ -149,45 +101,7 @@ describe("Plugin integration tests", function () {
     });
 
     describe("With contract fully qualified name parameter", function () {
-      it("Should fail to verify contract with a different version", async function () {
-        await this.env.run(TASK_COMPILE, { force: true });
-
-        const deployedAddress = await deployContract(
-          "NewContract",
-          [],
-          this.env,
-          1
-        );
-
-        return this.env
-          .run("verify", {
-            address: deployedAddress,
-            constructorArgsParams: [],
-            contract: "contracts/TestContract1.sol:InnerContract",
-          })
-          .catch((reason) => {
-            assert.instanceOf(
-              reason,
-              NomicLabsHardhatPluginError,
-              "Ambiguous inferences should throw an error"
-            );
-
-            assert.isTrue(
-              reason.message.includes(
-                "the contract found in the address provided as argument has its bytecode marked with"
-              ),
-              "The error should describe the version found in the bytecode metadata."
-            );
-            assert.isTrue(
-              reason.message.includes("0.7.5"),
-              "The version inferred from the bytecode should be shown."
-            );
-          });
-      });
-
       it("should verify when passing the correct fully qualified name", async function () {
-        await this.env.run(TASK_COMPILE, { force: true });
-
         const amount = "20";
 
         const deployedAddress = await deployContract(
@@ -204,32 +118,34 @@ describe("Plugin integration tests", function () {
       });
     });
   });
-});
 
-function getRandomString({ ethers }: any): string {
-  return ethers.Wallet.createRandom().address;
-}
+  describe("Using a Hardhat project that mocks the rinkeby network", function () {
+    useEnvironment("hardhat-project-rinkeby-mock");
 
-async function deployContract(
-  contractName: string,
-  constructorArguments: string[],
-  { ethers }: any,
-  confirmations: number = 5,
-  signer?: any
-) {
-  if (signer === undefined) {
-    signer = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, ethers.provider);
-  }
+    let signer: any;
 
-  const factory = await ethers.getContractFactory(contractName, signer);
-  const contract = await factory.deploy(...constructorArguments);
-  await contract.deployTransaction.wait(confirmations);
-  return contract.address;
-}
+    // This environment variable allows us to avoid the network name check for this mock environment.
+    before(function () {
+      process.env.HARDHAT_ETHERSCAN_MOCK_NETWORK_TESTS = "yes";
+    });
 
-describe("Plugin subtask test", function () {
-  describe("Using a normal Hardhat project", function () {
-    useEnvironment("hardhat-project-only-contracts");
+    after(function () {
+      process.env.HARDHAT_ETHERSCAN_MOCK_NETWORK_TESTS = "no";
+    });
+
+    beforeEach(async function () {
+      // We ensure that the compile task was run at least once before disallowing net connections.
+      // This avoids failure due to compiler downloads.
+      await this.env.run(TASK_COMPILE, { quiet: true });
+      nock.disableNetConnect();
+      const { ethers } = this.env as any;
+      const signers = await ethers.getSigners();
+      signer = signers[0];
+    });
+
+    afterEach(function () {
+      nock.enableNetConnect();
+    });
 
     it("Minimum build subtask should work with simple project", async function () {
       const sourceName = "contracts/TestContract.sol";
@@ -244,5 +160,141 @@ describe("Plugin subtask test", function () {
         "contracts/ReentrancyGuard.sol",
       ]);
     });
+
+    it("fail when the deployed bytecode matches with more than one contract in the project", async function () {
+      const amount = "20";
+
+      const deployedAddress = await deployContract(
+        "TestContract1",
+        [amount],
+        this.env,
+        1,
+        signer
+      );
+
+      return this.env
+        .run("verify", {
+          address: deployedAddress,
+          constructorArgsParams: [amount],
+        })
+        .then(() => {
+          assert.fail("The plugin should throw an error");
+        })
+        .catch((reason) => {
+          expect(reason).to.be.an.instanceOf(
+            NomicLabsHardhatPluginError,
+            "Ambiguous inferences should throw a plugin error"
+          );
+
+          expect(reason.message)
+            .to.be.a("string")
+            .and.include(
+              "use the contract parameter with one of the following contracts",
+              "The error should contain an explanation of how to solve the ambiguous inference."
+            )
+            .and.include(
+              "contracts/TestContract1.sol:TestContract1",
+              "The deployed contract should be among the presented options."
+            );
+        });
+    });
+
+    it("fail when the verify subtask receives an invalid list of constructor arguments", async function () {
+      const constructorArguments: any[] = [];
+
+      const deployedAddress = await deployContract(
+        "InnerContract",
+        constructorArguments,
+        this.env,
+        1,
+        signer
+      );
+
+      return this.env
+        .run("verify:verify", {
+          address: deployedAddress,
+          constructorArguments: null,
+        })
+        .then(() => {
+          assert.fail(
+            "The verify subtask should throw an error when passed a null constructor arguments list."
+          );
+        })
+        .catch((reason) => {
+          expect(reason).to.be.an.instanceOf(
+            NomicLabsHardhatPluginError,
+            "An invalid list of constructor arguments should cause a Hardhat plugin error"
+          );
+
+          expect(reason.message)
+            .to.be.a("string")
+            .and.include(
+              "The constructorArguments parameter should be an array",
+              "The error message should communicate the type error."
+            );
+        });
+    });
+
+    describe("With contract fully qualified name parameter", function () {
+      it("Should fail to verify contract with a different version", async function () {
+        const deployedAddress = await deployContract(
+          "NewContract",
+          [],
+          this.env,
+          1,
+          signer
+        );
+
+        return this.env
+          .run("verify", {
+            address: deployedAddress,
+            constructorArgsParams: [],
+            contract: "contracts/TestContract1.sol:InnerContract",
+          })
+          .then(() =>
+            assert.fail(
+              "The verify task should fail when the deployed bytecode is marked with an unexpected compiler version."
+            )
+          )
+          .catch((reason) => {
+            expect(reason).to.be.an.instanceOf(
+              NomicLabsHardhatPluginError,
+              "The verify task should throw a plugin error when there is a version mismatch between the deployed bytecode and compiler used for a particular contract"
+            );
+
+            expect(reason.message)
+              .to.be.a("string")
+              .and.include(
+                "the contract found in the address provided as argument has its bytecode marked with",
+                "The error should describe the version found in the bytecode metadata."
+              )
+              .and.include(
+                "0.7.5",
+                "The version inferred from the bytecode should be shown."
+              );
+          });
+      });
+    });
   });
 });
+
+function getRandomString({ ethers }: any): string {
+  return ethers.Wallet.createRandom().address;
+}
+
+async function deployContract(
+  contractName: string,
+  constructorArguments: any[],
+  { ethers }: any,
+  confirmations: number = 5,
+  signer?: any
+) {
+  if (signer === undefined) {
+    signer = new ethers.Wallet(process.env.WALLET_PRIVATE_KEY, ethers.provider);
+  }
+
+  const factory = await ethers.getContractFactory(contractName, signer);
+  const contract = await factory.deploy(...constructorArguments);
+  await contract.deployTransaction.wait(confirmations);
+  return contract.address;
+}
