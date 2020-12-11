42e891d047491bd8c5b6f06278d054af9f3a55fd
Sebastián Claudio Nale <sebinale@gmail.com>
2020-12-10 23:20:15 -0300

    Add a test for the verify subtask.
    Improve some old tests by mocking the Rinkeby network as needed.

--- packages/hardhat-etherscan/test/fixture-projects/hardhat-project/contracts/NewContract.sol
+++ packages/hardhat-etherscan/test/fixture-projects/hardhat-project/contracts/NewContract.sol
@@ -1,8 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.7.5;
 
 contract NewContract {
 
     string public mark = "placeholder";
 
-    constructor() public {}
+    constructor() {}
 }
