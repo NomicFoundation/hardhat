31014f53dab07bd9974eb5c1b873cb6917b0f4d8
Sebastián Claudio Nale <sebinale@gmail.com>
2020-12-17 15:42:24 -0300

    Remove compiler several warnings in hardhat-etherscan tests.

--- packages/hardhat-etherscan/test/fixture-projects/hardhat-project/contracts/TestLibrary.sol
+++ packages/hardhat-etherscan/test/fixture-projects/hardhat-project/contracts/TestLibrary.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.5.15;
 
 library TestLibrary {
-    function libDo(uint256 n) external returns (uint256) {
+    function libDo(uint256 n) pure external returns (uint256) {
         return n * 2;
     }
 
-    function libID() external returns (string memory) {
+    function libID() pure external returns (string memory) {
         return "placeholder";
     }
 }
