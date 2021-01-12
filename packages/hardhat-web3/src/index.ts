30bfc10e17b222afd5286f8c7e33189e71d743f3
Sebastián Claudio Nale <sebinale@gmail.com>
2021-01-12 16:46:50 -0300

    Rename environment to HRE.

--- packages/hardhat-web3/src/index.ts
+++ packages/hardhat-web3/src/index.ts
@@ -4,10 +4,10 @@ import { lazyFunction, lazyObject } from "hardhat/plugins";
 import "./type-extensions";
 import { Web3HTTPProviderAdapter } from "./web3-provider-adapter";
 
-extendEnvironment((env) => {
-  env.Web3 = lazyFunction(() => require("web3"));
-  env.web3 = lazyObject(() => {
+extendEnvironment((hre) => {
+  hre.Web3 = lazyFunction(() => require("web3"));
+  hre.web3 = lazyObject(() => {
     const Web3 = require("web3");
-    return new Web3(new Web3HTTPProviderAdapter(env.network.provider));
+    return new Web3(new Web3HTTPProviderAdapter(hre.network.provider));
   });
 });
