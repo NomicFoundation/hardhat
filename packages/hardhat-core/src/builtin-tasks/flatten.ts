66d81f47ae6c7c81399f22dfccf9bd0dc6f09341
Ignacio Mazzara <nachomazzara@gmail.com>
2021-02-03 15:06:59 -0300

    chore: lint

--- packages/hardhat-core/src/builtin-tasks/flatten.ts
+++ packages/hardhat-core/src/builtin-tasks/flatten.ts
@@ -1,11 +1,11 @@
-import * as fs from "fs"
+import * as fs from "fs";
 
-import { subtask, task, types } from "../internal/core/config/config-env"
-import { HardhatError } from "../internal/core/errors"
-import { ERRORS } from "../internal/core/errors-list"
-import { DependencyGraph } from "../internal/solidity/dependencyGraph"
-import { ResolvedFile, ResolvedFilesMap } from "../internal/solidity/resolver"
-import { getPackageJson } from "../internal/util/packageInfo"
+import { subtask, task, types } from "../internal/core/config/config-env";
+import { HardhatError } from "../internal/core/errors";
+import { ERRORS } from "../internal/core/errors-list";
+import { DependencyGraph } from "../internal/solidity/dependencyGraph";
+import { ResolvedFile, ResolvedFilesMap } from "../internal/solidity/resolver";
+import { getPackageJson } from "../internal/util/packageInfo";
 
 import {
   TASK_COMPILE_SOLIDITY_GET_DEPENDENCY_GRAPH,
@@ -14,51 +14,55 @@ import {
   TASK_FLATTEN,
   TASK_FLATTEN_GET_DEPENDENCY_GRAPH,
   TASK_FLATTEN_GET_FLATTENED_SOURCE,
-} from "./task-names"
+} from "./task-names";
 
-type FlattenInput = { files?: string[], removeLicences?: boolean, license?: string }
+type FlattenInput = {
+  files?: string[];
+  removeLicences?: boolean;
+  license?: string;
+};
 
 function getSortedFiles(dependenciesGraph: DependencyGraph) {
-  const tsort = require("tsort")
-  const graph = tsort()
+  const tsort = require("tsort");
+  const graph = tsort();
 
-  const filesMap: ResolvedFilesMap = {}
-  const resolvedFiles = dependenciesGraph.getResolvedFiles()
-  resolvedFiles.forEach((f) => (filesMap[f.sourceName] = f))
+  const filesMap: ResolvedFilesMap = {};
+  const resolvedFiles = dependenciesGraph.getResolvedFiles();
+  resolvedFiles.forEach((f) => (filesMap[f.sourceName] = f));
 
   for (const [from, deps] of dependenciesGraph.entries()) {
     for (const to of deps) {
-      graph.add(to.sourceName, from.sourceName)
+      graph.add(to.sourceName, from.sourceName);
     }
   }
 
   try {
-    const topologicalSortedNames: string[] = graph.sort()
+    const topologicalSortedNames: string[] = graph.sort();
 
     // If an entry has no dependency it won't be included in the graph, so we
     // add them and then dedup the array
     const withEntries = topologicalSortedNames.concat(
       resolvedFiles.map((f) => f.sourceName)
-    )
+    );
 
-    const sortedNames = [...new Set(withEntries)]
-    return sortedNames.map((n) => filesMap[n])
+    const sortedNames = [...new Set(withEntries)];
+    return sortedNames.map((n) => filesMap[n]);
   } catch (error) {
     if (error.toString().includes("Error: There is a cycle in the graph.")) {
-      throw new HardhatError(ERRORS.BUILTIN_TASKS.FLATTEN_CYCLE, error)
+      throw new HardhatError(ERRORS.BUILTIN_TASKS.FLATTEN_CYCLE, error);
     }
 
     // tslint:disable-next-line only-hardhat-error
-    throw error
+    throw error;
   }
 }
 
 function getFileWithoutImports(resolvedFile: ResolvedFile) {
-  const IMPORT_SOLIDITY_REGEX = /^\s*import(\s+)[\s\S]*?;\s*$/gm
+  const IMPORT_SOLIDITY_REGEX = /^\s*import(\s+)[\s\S]*?;\s*$/gm;
 
   return resolvedFile.content.rawContent
     .replace(IMPORT_SOLIDITY_REGEX, "")
-    .trim()
+    .trim();
 }
 
 subtask(
@@ -68,39 +72,44 @@ subtask(
   .addOptionalParam("files", undefined, undefined, types.any)
   .addOptionalParam("removeLicences", undefined, false, types.boolean)
   .addOptionalParam("license", undefined, undefined, types.string)
-  .setAction(async ({ files, removeLicences, license }: FlattenInput, { run }) => {
-    const dependencyGraph: DependencyGraph = await run(
-      TASK_FLATTEN_GET_DEPENDENCY_GRAPH,
-      { files }
-    )
+  .setAction(
+    async ({ files, removeLicences, license }: FlattenInput, { run }) => {
+      const dependencyGraph: DependencyGraph = await run(
+        TASK_FLATTEN_GET_DEPENDENCY_GRAPH,
+        { files }
+      );
 
-    let flattened = ""
+      let flattened = "";
 
-    if (dependencyGraph.getResolvedFiles().length === 0) {
-      return flattened
+      if (dependencyGraph.getResolvedFiles().length === 0) {
+        return flattened;
+      }
+
+      const packageJson = await getPackageJson();
+      flattened += `// Sources flattened with hardhat v${packageJson.version} https://hardhat.org`;
+
+      const sortedFiles = getSortedFiles(dependencyGraph);
+
+      for (const file of sortedFiles) {
+        flattened += `\n\n// File ${file.getVersionedName()}\n`;
+        flattened += `\n${getFileWithoutImports(file)}\n`;
+      }
+
+      if (removeLicences || license) {
+        // Remove every line started with "// SPDX-License-Identifier:"
+        flattened = flattened.replace(
+          /^\/\/ SPDX-License-Identifier:.*\n?/m,
+          ""
+        );
+      }
+
+      if (license) {
+        flattened = `// SPDX-License-Identifier: ${license}\n${flattened}`;
+      }
+
+      return flattened.trim();
     }
-
-    const packageJson = await getPackageJson()
-    flattened += `// Sources flattened with hardhat v${packageJson.version} https://hardhat.org`
-
-    const sortedFiles = getSortedFiles(dependencyGraph)
-
-    for (const file of sortedFiles) {
-      flattened += `\n\n// File ${file.getVersionedName()}\n`
-      flattened += `\n${getFileWithoutImports(file)}\n`
-    }
-
-    if (removeLicences || license) {
-      // Remove every line started with "// SPDX-License-Identifier:"
-      flattened = flattened.replace(/^\/\/ SPDX-License-Identifier:.*\n?/m, '')
-    }
-
-    if (license) {
-      flattened = `// SPDX-License-Identifier: ${license}\n${flattened}`
-    }
-
-    return flattened.trim()
-  })
+  );
 
 subtask(TASK_FLATTEN_GET_DEPENDENCY_GRAPH)
   .addOptionalParam("files", undefined, undefined, types.any)
@@ -108,22 +117,22 @@ subtask(TASK_FLATTEN_GET_DEPENDENCY_GRAPH)
     const sourcePaths: string[] =
       files === undefined
         ? await run(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS)
-        : files.map((f) => fs.realpathSync(f))
+        : files.map((f) => fs.realpathSync(f));
 
     const sourceNames: string[] = await run(
       TASK_COMPILE_SOLIDITY_GET_SOURCE_NAMES,
       {
         sourcePaths,
       }
-    )
+    );
 
     const dependencyGraph: DependencyGraph = await run(
       TASK_COMPILE_SOLIDITY_GET_DEPENDENCY_GRAPH,
       { sourceNames }
-    )
+    );
 
-    return dependencyGraph
-  })
+    return dependencyGraph;
+  });
 
 task(TASK_FLATTEN, "Flattens and prints contracts and their dependencies")
   .addOptionalVariadicPositionalParam(
@@ -138,12 +147,15 @@ task(TASK_FLATTEN, "Flattens and prints contracts and their dependencies")
     undefined,
     types.string
   )
-  .addOptionalParam(
-    "license",
-    "License for each file",
-    undefined,
-    types.string
-  )
-  .setAction(async ({ files, removeLicences, license }: FlattenInput, { run }) => {
-    console.log(await run(TASK_FLATTEN_GET_FLATTENED_SOURCE, { files, removeLicences, license }))
-  })
+  .addOptionalParam("license", "License for each file", undefined, types.string)
+  .setAction(
+    async ({ files, removeLicences, license }: FlattenInput, { run }) => {
+      console.log(
+        await run(TASK_FLATTEN_GET_FLATTENED_SOURCE, {
+          files,
+          removeLicences,
+          license,
+        })
+      );
+    }
+  );
--- packages/hardhat-waffle/test/fixture-projects/hardhat-project/contracts/Token.sol
+++ packages/hardhat-waffle/test/fixture-projects/hardhat-project/contracts/Token.sol
@@ -1,5 +1,5 @@
 // Example taken from https://gist.github.com/jonchurch/c71de291b3ffa49ae17e416393f87135 and modified
-// Original code's license: BokkyPooBah 2017. The MIT Licence.
+// Original code's license: BokkyPooBah 2017. The MIT License.
 
 pragma solidity ^0.7.0;
 
