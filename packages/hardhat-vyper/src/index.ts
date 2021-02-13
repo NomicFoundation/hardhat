aaf1be3bf16a08638064b4d05a1d729bd57f4487
transferAndCall <transferAndCall@yaxis.io>
2021-02-13 10:33:01 -0600

    Support projects with Solidity and Vyper files
    Signed-off-by: transferAndCall <transferAndCall@yaxis.io>

--- packages/hardhat-vyper/src/index.ts
+++ packages/hardhat-vyper/src/index.ts
@@ -1,6 +1,10 @@
-import { TASK_COMPILE } from "hardhat/builtin-tasks/task-names";
-import { extendConfig, task } from "hardhat/internal/core/config/config-env";
+import {
+  TASK_COMPILE_GET_COMPILATION_TASKS,
+  TASK_COMPILE_SOLIDITY,
+} from "hardhat/builtin-tasks/task-names";
+import { extendConfig, subtask } from "hardhat/internal/core/config/config-env";
 
+import { TASK_COMPILE_VYPER } from "./task-names";
 import "./type-extensions";
 
 extendConfig((config) => {
@@ -8,7 +12,14 @@ extendConfig((config) => {
   config.vyper = { ...defaultConfig, ...config.vyper };
 });
 
-task(TASK_COMPILE, async (_, { config, artifacts }) => {
+subtask(
+  TASK_COMPILE_GET_COMPILATION_TASKS,
+  async (): Promise<string[]> => {
+    return [TASK_COMPILE_SOLIDITY, TASK_COMPILE_VYPER];
+  }
+);
+
+subtask(TASK_COMPILE_VYPER, async (_, { config, artifacts }) => {
   const { compile } = await import("./compilation");
 
   // This plugin is experimental, so this task isn't split into multiple
