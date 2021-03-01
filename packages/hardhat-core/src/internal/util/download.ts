ffdbfbcd6503cbab3f5ee4fe78a647807aa7d87d
Chris <cdesch@gmail.com>
2021-03-01 17:42:14 -0500

    Adding proxy for download.ts and tests for download.tx

--- packages/hardhat-core/src/internal/util/download.ts
+++ packages/hardhat-core/src/internal/util/download.ts
@@ -2,6 +2,12 @@ import fs from "fs";
 import fsExtra from "fs-extra";
 import path from "path";
 import util from "util";
+import HttpsProxyAgent from "https-proxy-agent";
+
+interface FetchOptions {
+  timeout: number;
+  agent?: undefined | HttpsProxyAgent.HttpsProxyAgent;
+}
 
 export async function download(
   url: string,
@@ -11,8 +17,17 @@ export async function download(
   const { pipeline } = await import("stream");
   const { default: fetch } = await import("node-fetch");
   const streamPipeline = util.promisify(pipeline);
+  const fetchOptions = <FetchOptions> { timeout: timeoutMillis, agent: undefined };
 
-  const response = await fetch(url, { timeout: timeoutMillis });
+  // Check if Proxy is set
+  if (process.env.http_proxy) {       
+    // Create the proxy from the environment variables
+    const proxy = process.env.http_proxy;
+    fetchOptions.agent = new HttpsProxyAgent.HttpsProxyAgent(proxy);
+  }
+
+  // Fetch the url
+  const response = await fetch(url, fetchOptions);
 
   if (response.ok && response.body !== null) {
     await fsExtra.ensureDir(path.dirname(filePath));
---
+++ packages/hardhat-core/test/internal/util/download.ts
@@ -0,0 +1,88 @@
+import { assert } from "chai";
+import fsExtra from "fs-extra";
+import path from "path";
+import Proxy from "proxy";
+import { download } from "../../../src/internal/util/download";
+import { ERRORS } from "../../../src/internal/core/errors-list";
+import { useTmpDir } from "../../helpers/fs";
+
+
+// The CompilerDownloader's logic is complex and has/depends on lots of
+// side-effects. This is not ideal, but enables many optimizations. In
+// particular, it doesn't download unnecessary files.
+//
+// To make it easier to test, CompilerDownloader exposes helper methods with
+// internal logic and they are tested individually here.
+
+
+describe("Compiler List download", function () {
+
+  useTmpDir("compiler-downloader");
+
+  describe("Compilers list download", function () {
+    it("Should call download with the right params", async function () {
+      const compilersDir = this.tmpDir;
+      const downloadPath = path.join(compilersDir, "downloadedCompiler");
+      const expectedUrl = `https://solc-bin.ethereum.org/wasm/list.json`;
+      
+      // download the file
+      await download(expectedUrl, downloadPath);
+      // Assert that the file exists
+      assert.isTrue(await fsExtra.pathExists(downloadPath));
+    });
+  });
+});
+
+
+describe("Compiler List download with proxy", function () {
+  let env: any;
+  let proxy: any;
+  let proxyPort: number;
+
+  useTmpDir("compiler-downloader");
+
+  before(function () {
+
+    // Setup Proxy Server
+    proxy = new Proxy();
+		proxy.listen(function() {
+			proxyPort = proxy.address().port;
+			// done();
+		});
+    
+  });
+
+
+  describe("Compilers list download", function () {
+    before(function () {
+      //Save the Environment Settings and Set
+      env = process.env;
+      process.env.http_proxy = `http://127.0.0.1:${proxyPort}`;
+    })
+
+    it("Should call download with the right params", async function () {
+      const compilersDir = this.tmpDir;
+      const downloadPath = path.join(compilersDir, "downloadedCompilerProxy");
+      const expectedUrl = `https://solc-bin.ethereum.org/wasm/list.json`;
+
+      // download the file
+      await download(expectedUrl, downloadPath);
+      // Assert that the file exists
+      assert.isTrue(await fsExtra.pathExists(downloadPath));
+    });
+
+
+    after(function () {
+      // restoring everything back to the environment
+      process.env = env;
+    });
+  });
+
+  after(function(done) {
+    //Shutdown Proxy Server
+		proxy.once('close', function() {
+			done();
+		});
+		proxy.close();
+	});
+});
