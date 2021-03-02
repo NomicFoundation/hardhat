a7c579e163dc664ef21eaab2b049c909dbd2e2b6
Chris <cdesch@gmail.com>
2021-03-02 00:11:45 -0500

    Fixing linting errors. tslint is very strict.

--- packages/hardhat-core/test/internal/util/download.ts
+++ packages/hardhat-core/test/internal/util/download.ts
@@ -1,22 +1,13 @@
 import { assert } from "chai";
 import fsExtra from "fs-extra";
 import path from "path";
-// @ts-ignore
+// tslint:disable-next-line no-implicit-dependencies
 import Proxy from "proxy";
+
 import { download } from "../../../src/internal/util/download";
 import { useTmpDir } from "../../helpers/fs";
 
-
-// The CompilerDownloader's logic is complex and has/depends on lots of
-// side-effects. This is not ideal, but enables many optimizations. In
-// particular, it doesn't download unnecessary files.
-//
-// To make it easier to test, CompilerDownloader exposes helper methods with
-// internal logic and they are tested individually here.
-
-
 describe("Compiler List download", function () {
-
   useTmpDir("compiler-downloader");
 
   describe("Compilers list download", function () {
@@ -24,7 +15,7 @@ describe("Compiler List download", function () {
       const compilersDir = this.tmpDir;
       const downloadPath = path.join(compilersDir, "downloadedCompiler");
       const expectedUrl = `https://solc-bin.ethereum.org/wasm/list.json`;
-      
+
       // download the file
       await download(expectedUrl, downloadPath);
       // Assert that the file exists
@@ -33,7 +24,6 @@ describe("Compiler List download", function () {
   });
 });
 
-
 describe("Compiler List download with proxy", function () {
   let env: any;
   let proxy: any;
@@ -41,21 +31,21 @@ describe("Compiler List download with proxy", function () {
 
   useTmpDir("compiler-downloader");
 
-  before(function () {
+  before(function (done) {
     // Setup Proxy Server
     proxy = new Proxy();
-		proxy.listen(function() {
-			proxyPort = proxy.address().port;
-			// done();
-		});
+    proxy.listen(function () {
+      proxyPort = proxy.address().port;
+      done();
+    });
   });
 
   describe("Compilers list download with HTTPS_PROXY", function () {
     before(function () {
-      //Save the Environment Settings and Set
+      // Save the Environment Settings and Set
       env = process.env;
       process.env.HTTPS_PROXY = `http://127.0.0.1:${proxyPort}`;
-    })
+    });
 
     it("Should call download with the right params", async function () {
       const compilersDir = this.tmpDir;
@@ -76,10 +66,10 @@ describe("Compiler List download with proxy", function () {
 
   describe("Compilers list download with HTTP_PROXY", function () {
     before(function () {
-      //Save the Environment Settings and Set
+      // Save the Environment Settings and Set
       env = process.env;
       process.env.HTTP_PROXY = `http://127.0.0.1:${proxyPort}`;
-    })
+    });
 
     it("Should call download with the right params", async function () {
       const compilersDir = this.tmpDir;
@@ -92,18 +82,17 @@ describe("Compiler List download with proxy", function () {
       assert.isTrue(await fsExtra.pathExists(downloadPath));
     });
 
-
     after(function () {
       // restoring everything back to the environment
       process.env = env;
     });
   });
 
-  after(function(done) {
-    //Shutdown Proxy Server
-		proxy.once('close', function() {
-			done();
-		});
-		proxy.close();
-	});
+  after(function (done) {
+    // Shutdown Proxy Server
+    proxy.once("close", function () {
+      done();
+    });
+    proxy.close();
+  });
 });
