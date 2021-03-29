57cb5e799d40f298a930fb14ad2e9be322961d7c
cgewecke <christophergewecke@gmail.com>
2021-03-29 08:37:58 -0700

    review -> remove fakeTransaction EIP155 and hash logic

--- packages/hardhat-core/src/internal/hardhat-network/provider/utils/fakeTransaction.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/utils/fakeTransaction.ts
@@ -60,44 +60,4 @@ export class FakeTransaction extends Transaction {
   public getSenderAddress(): Address {
     return this.fakeFrom;
   }
-
-  // Ported from ethereumjs-tx `tx.hash(false)`
-  public hash(): Buffer {
-    let items;
-
-    if (this._implementsEIP155()) {
-      items = [
-        ...this.raw().slice(0, 6),
-        toBuffer(this.common.chainId()),
-        unpadBuffer(toBuffer(0)),
-        unpadBuffer(toBuffer(0)),
-      ];
-    } else {
-      items = this.raw().slice(0, 6);
-    }
-
-    // create hash
-    return rlphash(items);
-  }
-
-  private _implementsEIP155(): boolean {
-    const onEIP155BlockOrLater = this.common.gteHardfork("spuriousDragon");
-
-    if (!this.isSigned()) {
-      // We sign with EIP155 all unsigned transactions after spuriousDragon
-      return onEIP155BlockOrLater;
-    }
-
-    // EIP155 spec:
-    // If block.number >= 2,675,000 and v = CHAIN_ID * 2 + 35 or v = CHAIN_ID * 2 + 36, then when computing
-    // the hash of a transaction for purposes of signing or recovering, instead of hashing only the first six
-    // elements (i.e. nonce, gasprice, startgas, to, value, data), hash nine elements, with v replaced by
-    // CHAIN_ID, r = 0 and s = 0.
-    const v = bufferToInt(this.v!.toBuffer());
-
-    const vAndChainIdMeetEIP155Conditions =
-      v === this.common.chainId() * 2 + 35 ||
-      v === this.common.chainId() * 2 + 36;
-    return vAndChainIdMeetEIP155Conditions && onEIP155BlockOrLater;
-  }
 }
