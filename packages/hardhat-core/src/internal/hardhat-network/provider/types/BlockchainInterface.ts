e026739e100abd87acca20f8a3ae453e9e8f5491
Franco Victorio <victorio.franco@gmail.com>
2021-03-31 11:19:13 -0300

    Use a single Blockchain interface and remove unused modules

--- packages/hardhat-core/src/internal/hardhat-network/provider/types/BlockchainInterface.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/types/BlockchainInterface.ts
@@ -5,10 +5,8 @@ import { BN } from "ethereumjs-util";
 import { FilterParams } from "../node-types";
 import { RpcLogOutput, RpcReceiptOutput } from "../output";
 
-import { Blockchain } from "./Blockchain";
-import { Callback } from "./Callback";
-
-export interface PBlockchain {
+// TODO: Replace this with BlockchainInterface from @ethereumjs/blockchain
+export interface BlockchainInterface {
   getLatestBlock(): Promise<Block>;
   getBlock(blockHashOrNumber: Buffer | number | BN): Promise<Block | undefined>;
   addBlock(block: Block): Promise<Block>;
@@ -27,40 +25,4 @@ export interface PBlockchain {
   ): Promise<RpcReceiptOutput | undefined>;
   addTransactionReceipts(receipts: RpcReceiptOutput[]): void;
   getLogs(filterParams: FilterParams): Promise<RpcLogOutput[]>;
-}
-
-export function toBlockchain(pb: PBlockchain): Blockchain {
-  async function getBlock(blockTag: number | Buffer | BN) {
-    const block = await pb.getBlock(blockTag);
-    if (block === undefined) {
-      // tslint:disable-next-line only-hardhat-error
-      throw new Error("Block not found");
-    }
-    return block;
-  }
-
-  function delBlock(blockHash: Buffer, cb: Callback) {
-    try {
-      pb.deleteBlock(blockHash);
-    } catch (e) {
-      return;
-    }
-  }
-
-  return {
-    getBlock,
-    putBlock: pb.addBlock.bind(pb),
-    delBlock,
-    getDetails,
-    iterator,
-  };
-}
-
-function getDetails(_: string, cb: Callback) {
-  cb(null);
-}
-
-function iterator() {
-  // tslint:disable-next-line only-hardhat-error
-  throw new Error(".iterator() is not supported");
 }
