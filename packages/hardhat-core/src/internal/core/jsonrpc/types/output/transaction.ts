941fdacb643cee46d113bff2320fe7a2485aea49
Patricio Palladino <email@patriciopalladino.com>
2021-04-02 21:33:57 -0300

    Make RpcTransaction accept undefined as to, because that's what Alchemy does

--- packages/hardhat-core/src/internal/core/jsonrpc/types/output/transaction.ts
+++ packages/hardhat-core/src/internal/core/jsonrpc/types/output/transaction.ts
@@ -1,6 +1,6 @@
 import * as t from "io-ts";
 
-import { nullable } from "../../../../util/io-ts";
+import { nullable, optional } from "../../../../util/io-ts";
 import { rpcAddress, rpcData, rpcHash, rpcQuantity } from "../base-types";
 
 export type RpcTransaction = t.TypeOf<typeof rpcTransaction>;
@@ -14,7 +14,7 @@ export const rpcTransaction = t.type(
     hash: rpcHash,
     input: rpcData,
     nonce: rpcQuantity,
-    to: nullable(rpcAddress),
+    to: optional(nullable(rpcAddress)),
     transactionIndex: nullable(rpcQuantity),
     value: rpcQuantity,
     v: rpcQuantity,
