d08833f3a1d2e76f5ef11459657202ec4411c599
Patricio Palladino <email@patriciopalladino.com>
2021-05-01 18:55:21 -0300

    Fix client-side sorting

--- docs/.vuepress/sorted-plugins.js
+++ docs/.vuepress/sorted-plugins.js
@@ -1,10 +1,11 @@
-const fs = require("fs");
 const plugins = require("./plugins.js");
 
-if (fs.existsSync(__dirname + "/plugin-downloads.json")) {
+try {
   const downloads = require("./plugin-downloads.json");
 
   plugins.sort((p1, p2) => downloads[p2.name] - downloads[p1.name]);
+} catch (e) {
+  // we just don't sort here
 }
 
 module.exports = plugins;
