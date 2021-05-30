0ef11c76e043ba44005d166a8482ce62e6104c43
Franco Victorio <victorio.franco@gmail.com>
2021-05-30 12:11:10 -0300

    Wait for tx to be mined in sample project

--- packages/hardhat-core/sample-project/test/sample-test.js
+++ packages/hardhat-core/sample-project/test/sample-test.js
@@ -4,11 +4,13 @@ describe("Greeter", function() {
   it("Should return the new greeting once it's changed", async function() {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
-    
     await greeter.deployed();
+
     expect(await greeter.greet()).to.equal("Hello, world!");
 
-    await greeter.setGreeting("Hola, mundo!");
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    await setGreetingTx.wait();
+
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
