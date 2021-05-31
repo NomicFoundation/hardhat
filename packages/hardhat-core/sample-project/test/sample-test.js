2d8cc5ef1943640b8cd2f083e4134361f869dbe5
Franco Victorio <victorio.franco@gmail.com>
2021-05-31 07:21:09 -0300

    Update sample-test.js

--- packages/hardhat-core/sample-project/test/sample-test.js
+++ packages/hardhat-core/sample-project/test/sample-test.js
@@ -9,6 +9,8 @@ describe("Greeter", function() {
     expect(await greeter.greet()).to.equal("Hello, world!");
 
     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    
+    // wait until the transaction is mined
     await setGreetingTx.wait();
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
