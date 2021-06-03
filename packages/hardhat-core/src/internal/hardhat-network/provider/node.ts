d2dcf0c9203a45fa4917b253a8f73b02e0d7ae49
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2021-06-03 15:32:09 -0600

    Handle irregular state transitions
    Fixes failing test added in https://github.com/nomiclabs/hardhat/pull/1435/commits/2bc4bad541a220db21db005bf4a6941b211e5458

--- packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/node.ts
@@ -255,6 +255,8 @@ Hardhat Network's forking functionality only works with blocks from at least spu
   private readonly _consoleLogger: ConsoleLogger = new ConsoleLogger();
   private _failedStackTraces = 0;
 
+  private _irregularStatesByBlockNum: Map<string, Buffer> = new Map(); // blockNumber as BN.toString() => state root
+
   private constructor(
     private readonly _vm: VM,
     private readonly _stateManager: StateManager,
@@ -938,6 +940,10 @@ Hardhat Network's forking functionality only works with blocks from at least spu
     const account = await this._stateManager.getAccount(address);
     account.balance = newBalance;
     await this._stateManager.putAccount(address, account);
+    this._irregularStatesByBlockNum.set(
+      (await this.getLatestBlock()).header.number.toString(),
+      await this._stateManager.getStateRoot()
+    );
   }
 
   public async setAccountCode(
@@ -1564,12 +1570,25 @@ Hardhat Network's forking functionality only works with blocks from at least spu
 
   private async _setBlockContext(block: Block): Promise<void> {
     if (this._stateManager instanceof ForkStateManager) {
+      if (this._irregularStatesByBlockNum.has(block.header.number.toString())) {
+        throw new InternalError(
+          "Irregular states are not supported on a forked chain"
+        );
+      }
       return this._stateManager.setBlockContext(
         block.header.stateRoot,
         block.header.number
       );
     }
-    return this._stateManager.setStateRoot(block.header.stateRoot);
+
+    const irregularStateOrUndefined = this._irregularStatesByBlockNum.get(
+      block.header.number.toString()
+    );
+    return this._stateManager.setStateRoot(
+      irregularStateOrUndefined !== undefined
+        ? irregularStateOrUndefined
+        : block.header.stateRoot
+    );
   }
 
   private async _restoreBlockContext(stateRoot: Buffer) {
