a0be7d45b7b8b29f59efc859254c98299c6a9919
Franco Victorio <victorio.franco@gmail.com>
2021-06-17 10:51:20 -0300

    Bump the supported solidity version range

--- packages/hardhat-core/src/internal/hardhat-network/stack-traces/solidityTracer.ts
+++ packages/hardhat-core/src/internal/hardhat-network/stack-traces/solidityTracer.ts
@@ -52,7 +52,7 @@ import {
   UnmappedSolc063RevertErrorStackTraceEntry,
 } from "./solidity-stack-trace";
 
-export const SUPPORTED_SOLIDITY_VERSION_RANGE = "<0.8.0";
+export const SUPPORTED_SOLIDITY_VERSION_RANGE = "<=0.8.4";
 export const FIRST_SOLC_VERSION_SUPPORTED = "0.5.1";
 
 export class SolidityTracer {
