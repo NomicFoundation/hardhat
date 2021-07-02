fba01aacdb5d84216d52bbcce4d8deb8bb68c9f3
istareatscreens <schar138@uottawa.ca>
2021-07-02 02:37:41 -0400

    Remove redundant check and moved to for loop

--- packages/hardhat-core/test/internal/hardhat-network/stack-traces/compilation.ts
+++ packages/hardhat-core/test/internal/hardhat-network/stack-traces/compilation.ts
@@ -128,11 +128,9 @@ async function compile(
 
   const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-  if ((output.errors ?? []).length !== 0) {
-    for (const error of output.errors) {
-      if (error.severity === "error") {
-        throw new Error(`Failed to compile: ${error.message}`);
-      }
+  for (const error of output?.errors ?? []) {
+    if (error.severity === "error") {
+      throw new Error(`Failed to compile: ${error.message}`);
     }
   }
 
