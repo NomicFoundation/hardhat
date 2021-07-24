7d7a8b4d69ba236b323917a19b1bb94aa6fc28e7
Julien Heller <5218949+flux627@users.noreply.github.com>
2021-07-24 15:58:10 -0400

    Give Waffle runtime augment its own interface

--- packages/hardhat-waffle/src/type-extensions.ts
+++ packages/hardhat-waffle/src/type-extensions.ts
@@ -10,21 +10,23 @@ import type { ContractJSON } from "ethereum-waffle/dist/esm/ContractJSON";
 import type { Contract, providers, Signer } from "ethers";
 import "hardhat/types/runtime";
 
+interface HardhatWaffle {
+  provider: MockProvider;
+  deployContract: (
+    signer: Signer,
+    contractJSON: ContractJSON,
+    args?: any[],
+    overrideOptions?: providers.TransactionRequest
+  ) => Promise<Contract>;
+  solidity: typeof solidity;
+  link: typeof link;
+  deployMockContract: (signer: Signer, abi: any[]) => Promise<MockContract>;
+  createFixtureLoader: typeof createFixtureLoader;
+  loadFixture: typeof loadFixture;
+}
+
 declare module "hardhat/types/runtime" {
   interface HardhatRuntimeEnvironment {
-    waffle: {
-      provider: MockProvider;
-      deployContract: (
-        signer: Signer,
-        contractJSON: ContractJSON,
-        args?: any[],
-        overrideOptions?: providers.TransactionRequest
-      ) => Promise<Contract>;
-      solidity: typeof solidity;
-      link: typeof link;
-      deployMockContract: (signer: Signer, abi: any[]) => Promise<MockContract>;
-      createFixtureLoader: typeof createFixtureLoader;
-      loadFixture: typeof loadFixture;
-    };
+    waffle: HardhatWaffle;
   }
 }
