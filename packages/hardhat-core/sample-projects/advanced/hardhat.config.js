b36cce776ce0e050efeda405f4df794836c8adc7
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2021-08-11 15:40:45 -0400

    Properly qualify ethers.getSigners()

--- packages/hardhat-core/sample-projects/advanced/hardhat.config.js
+++ packages/hardhat-core/sample-projects/advanced/hardhat.config.js
@@ -5,12 +5,10 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
-const { ethers } = require("ethers");
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
