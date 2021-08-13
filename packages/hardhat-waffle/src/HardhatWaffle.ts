11bb731dcb138e60537d6cd947c6e8999027ca07
Julien Heller <5218949+flux627@users.noreply.github.com>
2021-08-13 13:09:37 -0400

    Move HardhatWaffle to its own file

---
+++ packages/hardhat-waffle/src/HardhatWaffle.ts
@@ -0,0 +1,25 @@
+import type {
+  createFixtureLoader,
+  link,
+  loadFixture,
+  MockContract,
+  MockProvider,
+  solidity,
+} from "ethereum-waffle";
+import type { ContractJSON } from "ethereum-waffle/dist/esm/ContractJSON";
+import type { Contract, providers, Signer } from "ethers";
+
+export interface HardhatWaffle {
+  provider: MockProvider;
+  deployContract: (
+    signer: Signer,
+    contractJSON: ContractJSON,
+    args?: any[],
+    overrideOptions?: providers.TransactionRequest
+  ) => Promise<Contract>;
+  solidity: typeof solidity;
+  link: typeof link;
+  deployMockContract: (signer: Signer, abi: any[]) => Promise<MockContract>;
+  createFixtureLoader: typeof createFixtureLoader;
+  loadFixture: typeof loadFixture;
+}
