82c2eb15554baaff9e03f6c39863e701588ba8cd
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2021-10-17 16:32:57 -0600

    un-re-work makeForkCommon()

--- packages/hardhat-core/src/internal/hardhat-network/provider/utils/makeForkCommon.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/utils/makeForkCommon.ts
@@ -3,13 +3,12 @@ import Common from "@ethereumjs/common";
 import { ForkedNodeConfig } from "../node-types";
 
 export async function makeForkCommon(config: ForkedNodeConfig) {
-  const { chainId, networkId, networkName } = config;
   return Common.forCustomChain(
     "mainnet",
     {
-      chainId,
-      networkId,
-      name: networkName,
+      chainId: config.chainId,
+      networkId: config.networkId,
+      name: config.networkName,
     },
     config.hardfork
   );
