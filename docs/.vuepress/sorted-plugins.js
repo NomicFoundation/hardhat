f84fb57f27f51c13ce5cf279ef9d7b8b6e965909
Patricio Palladino <email@patriciopalladino.com>
2021-10-22 17:20:20 -0300

    Update how we sort plugins

--- docs/.vuepress/sorted-plugins.js
+++ docs/.vuepress/sorted-plugins.js
@@ -3,7 +3,13 @@ const plugins = require("./plugins.js");
 try {
   const downloads = require("./plugin-downloads.json");
 
-  plugins.sort((p1, p2) => downloads[p2.name] - downloads[p1.name]);
+  plugins.officialPlugins.sort(
+    (p1, p2) => downloads[p2.name] - downloads[p1.name]
+  );
+
+  plugins.communityPlugins.sort(
+    (p1, p2) => downloads[p2.name] - downloads[p1.name]
+  );
 } catch (e) {
   // we just don't sort here
 }
