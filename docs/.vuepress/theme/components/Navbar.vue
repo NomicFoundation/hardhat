9d8d2f1a31df4a7cfedf2122c566e3c763c7d631
guidolarosa <guido.glarosa@gmail.com>
2021-12-15 11:34:02 -0300

    Added dark theme

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -5,17 +5,12 @@
   >
     <div style="position: relative;">
       <HHTopBar />
-      <div style="position: relative;">
+      <div style="position: relative;" class="navigation-bar">
         <header>
           <SidebarButton @toggle-sidebar="$emit('toggle-sidebar')" />
 
           <a href="/" class="home-link">
-            <img
-              class="logo"
-              v-if="$site.themeConfig.logo"
-              :src="logoImg"
-              :alt="$siteTitle"
-            />
+            <div class="logo"></div>
             <span
               ref="siteName"
               class="site-name"
@@ -36,7 +31,7 @@
             <NavLinks class="can-hide" />
 
             <ul class="social-links social-links-non-landing">
-              <li v-for="socialLink of social">
+              <li v-for="socialLink of social" v-bind:key="socialLink.link">
                 <a
                   :href="socialLink.link"
                   :title="socialLink.name"
@@ -47,6 +42,9 @@
                 </a>
               </li>
             </ul>
+            <div class="dark-mode-toggle" v-on:click="toggleDarkTheme">
+              <div class="dark-mode-icon" />
+            </div>
           </div>
         </header>
       </div>
@@ -55,34 +53,43 @@
 </template>
 
 <script>
-import SidebarButton from "./SidebarButton.vue";
-import AlgoliaSearchBox from "./AlgoliaSearchBox.vue";
-import SearchBox from "./SearchBox.vue";
-import NavLinks from "./NavLinks.vue";
-import HHTopBar from "./HHTopBar";
+  import SidebarButton from "./SidebarButton.vue";
+  import AlgoliaSearchBox from "./AlgoliaSearchBox.vue";
+  import SearchBox from "./SearchBox.vue";
+  import NavLinks from "./NavLinks.vue";
+  import HHTopBar from "./HHTopBar";
 
-import LogoImg from "../img/hardhat_logos/Hardhat-logo.svg";
-import GithubLogo from "../img/assets/social/github.svg";
-import TwitterLogo from "../img/assets/social/twitter.svg";
-import DiscordLogo from "../img/assets/social/discord.svg";
+  import LogoImg from "../img/hardhat_logos/Hardhat-logo.svg";
+  import GithubLogo from "../img/assets/social/github.svg";
+  import TwitterLogo from "../img/assets/social/twitter.svg";
+  import DiscordLogo from "../img/assets/social/discord.svg";
 
-export default {
-  components: {
-    HHTopBar,
-    SidebarButton,
-    NavLinks,
-    SearchBox,
-    AlgoliaSearchBox,
-  },
+  import DMLight from "../img/icons/dm_light.svg";
+
+  export default {
+    components: {
+      HHTopBar,
+      SidebarButton,
+      NavLinks,
+      SearchBox,
+      AlgoliaSearchBox,
+    },
 
   data() {
     return {
       linksWrapMaxWidth: null,
       logoImg: LogoImg,
+      DMLight: DMLight,
+      themeClasses: ['light-mode', 'dark-mode', 'hc-dark-mode'],
+      currentTheme: 0
     };
   },
 
   mounted() {
+    const body = document.body;
+    this.currentThemeClass = this.themeClasses[0];
+    body.classList.add(this.themeClasses[this.currentTheme]);
+
     const MOBILE_DESKTOP_BREAKPOINT = 719; // refer to config.styl
     const NAVBAR_VERTICAL_PADDING =
       parseInt(css(this.$el, "paddingLeft")) +
@@ -100,7 +107,31 @@ export default {
     handleLinksWrapWidth();
     window.addEventListener("resize", handleLinksWrapWidth, false);
   },
+  methods: {
+    nextTheme() {
+      const body = document.body;
+      if (this.themeClasses.length - 1 == this.currentTheme) {
+        this.currentTheme = 0;
+      } else {
+        this.currentTheme += 1;
+      };
 
+      body.className="";
+      console.log(this.themeClasses[this.currentTheme])
+      body.classList.add(this.themeClasses[this.currentTheme]);
+
+    },
+    toggleDarkTheme() {
+      this.nextTheme();
+    },
+    checkUserPreference() {
+        //Check Storage on Page load. Keep user preference through sessions
+        if (localStorage.getItem("dark-theme")) {
+            document.body.classList.add("dark-mode");
+            document.getElementById('theme-toggle').checked = true;
+        }
+    }
+  },
   computed: {
     algolia() {
       return (
@@ -150,18 +181,42 @@ $navbar-vertical-padding = 0.5rem
 $navbar-horizontal-padding = 2rem
 
 .navbar
+  .logo
+    width: 164px;
+    height: 41px;
+    background-position: center left;
+    background-size: contain;
   .router-link-active:after
     width: 100%;
-
+  .dark-mode-toggle
+    display: flex;
+    cursor: pointer;
+    margin-left: auto;
+    @media (max-width: 1390px)
+      margin-left: 8px;
+    .dark-mode-icon {
+      width: 32px;
+      height: 32px;
+      background-image: url('../../theme/img/icons/dm_light.svg');
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      position: relative;
+      top: 16px;
+      @media (max-width: $MQMobile) {
+        top: 0;
+      }
+    }
   .social-links
     list-style none
     justify-content space-evenly
     display flex
     align-items center
-    width 200px
-    padding-left 20px
+    width 116px
+    padding-left 0px
     margin-right 2rem
-
+    position: relative;
+    top: 2px;
     li
       transition 0.2s ease-in-out opacity
 
@@ -201,18 +256,20 @@ $navbar-horizontal-padding = 2rem
     background-color white
     white-space nowrap
     font-size 1rem
-    position absolute
     right $navbar-horizontal-padding
     top 0.6rem
     display flex
+    margin-left: auto;
+    padding: 0 20px 0 0;
+    min-width: 1020px;
+    @media (max-width: 1390px)
+        min-width: unset;
     .search-box
       flex: 0 0 auto
       vertical-align top
 
       @media (min-width: $MQNarrow)
-        margin-right 20px
-    .nav-links
-      flex 1
+        margin-right 40px
 
 @media (max-width: 1240px)
   .navbar
