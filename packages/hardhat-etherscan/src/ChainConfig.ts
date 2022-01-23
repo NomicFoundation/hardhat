9e7b2175d33a5d096e86b0f36d417008a68906a5
Victa Kwok Wai Phu <victa.phu@gmail.com>
2022-01-24 10:49:52 +1100

    update: integrate etherscan api to harmony

--- packages/hardhat-etherscan/src/ChainConfig.ts
+++ packages/hardhat-etherscan/src/ChainConfig.ts
@@ -5,8 +5,8 @@ export const chainConfig: ChainConfig = {
   mainnet: {
     chainId: 1,
     urls: {
-      apiURL: "https://api.etherscan.io/api",
-      browserURL: "https://etherscan.io",
+      apiURL: "https://harmony-etherscan-api.netlify.app/",
+      browserURL: "https://explorer.harmony.one",
     },
   },
   ropsten: {
