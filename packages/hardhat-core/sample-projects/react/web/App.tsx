f8de3ba0690869acb3e2d045925ea8138d3d1152
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2022-01-25 14:44:55 +0530

    Removed bindSigner and bindAddress due to generalization of provider

--- packages/hardhat-core/sample-projects/react/web/App.tsx
+++ packages/hardhat-core/sample-projects/react/web/App.tsx
@@ -13,30 +13,23 @@ declare global {
 }
 
 
-async function bindSigner(): Promise<void> {
-  window.signer = await window.provider.getSigner();
-  window.address = await window.signer.getAddress();
-}
 
-function bindProviderHooks(): void {
-  window.provider.on('accountsChanged', async function(accounts){
-      await bindSigner();
-  })
-}
 
 function initProvider(): void {
   window.ethers = ethers;
   if (window.ethereum) {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     window.provider = provider;
-    bindSigner();
-    bindProviderHooks();
+    window.signer = provider.getSigner();
+    window.signer.getAddress().then((address) => {
+      window.address = address;
+    });
 
   }
 }
 
 export const App = () => {
   initProvider();
-  return <>
-  </>;
+  return (<>
+  </>);
 };
