cabbb112ea82eebd6227f9654d2ca961623b4d91
Franco Victorio <victorio.franco@gmail.com>
2022-02-02 18:43:19 -0300

    Use absolute paths for test task arguments
    Related to #2220.
    
    The `mocha.dispose()` call seems to throw when the test task is used like this:
    
    ```
    hh test ./test/test.js
    ```
    
    But this works:
    
    ```
    hh test test/test.js
    ```
    
    Converting the arguments to absolute paths (relative to process.cwd()) fixes the issue.

--- packages/hardhat-core/src/builtin-tasks/test.ts
+++ packages/hardhat-core/src/builtin-tasks/test.ts
@@ -26,7 +26,11 @@ subtask(TASK_TEST_GET_TEST_FILES)
   )
   .setAction(async ({ testFiles }: { testFiles: string[] }, { config }) => {
     if (testFiles.length !== 0) {
-      return testFiles;
+      const testFilesAbsolutePaths = testFiles.map((x) =>
+        path.resolve(process.cwd(), x)
+      );
+
+      return testFilesAbsolutePaths;
     }
 
     const jsFiles = await glob(path.join(config.paths.tests, "**/*.js"));
