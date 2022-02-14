76f253fd429a6c5492ae6a9b787beae2d8043d9c
Franco Victorio <victorio.franco@gmail.com>
2022-02-14 12:52:32 -0300

    Run prettier

--- packages/hardhat-core/test/fixture-projects/mnemonic-without-passphrase/hardhat.config.js
+++ packages/hardhat-core/test/fixture-projects/mnemonic-without-passphrase/hardhat.config.js
@@ -2,8 +2,9 @@ module.exports = {
   networks: {
     hardhat: {
       accounts: {
-        mnemonic: "drum alarm man enrich torch mule shine vessel struggle mention lend broken crowd six good"
-      }
+        mnemonic:
+          "drum alarm man enrich torch mule shine vessel struggle mention lend broken crowd six good",
+      },
     },
   },
   solidity: "0.5.15",
