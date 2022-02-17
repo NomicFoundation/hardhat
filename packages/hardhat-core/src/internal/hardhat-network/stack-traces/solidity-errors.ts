7b64df39b45f82299281640df1906db416e5be96
Jared Flatow <jflatow@gmail.com>
2022-02-17 13:49:05 -0800

    Logically patch the viaIR issue for running unit tests
    This attempts to patch the viaIR issue (as encountered in a particular case) in a logical way (as opposed to minimally).

--- packages/hardhat-core/src/internal/hardhat-network/stack-traces/solidity-errors.ts
+++ packages/hardhat-core/src/internal/hardhat-network/stack-traces/solidity-errors.ts
@@ -124,15 +124,8 @@ function encodeStackTraceEntry(
     case StackTraceEntryType.DIRECT_LIBRARY_CALL_ERROR:
     case StackTraceEntryType.UNMAPPED_SOLC_0_6_3_REVERT_ERROR:
     case StackTraceEntryType.CONTRACT_TOO_LARGE_ERROR:
-      return sourceReferenceToSolidityCallsite(stackTraceEntry.sourceReference);
-
     case StackTraceEntryType.UNRECOGNIZED_CREATE_CALLSTACK_ENTRY:
-      return new SolidityCallSite(
-        undefined,
-        UNRECOGNIZED_CONTRACT_NAME,
-        CONSTRUCTOR_FUNCTION_NAME,
-        undefined
-      );
+      break;
 
     case StackTraceEntryType.UNRECOGNIZED_CONTRACT_CALLSTACK_ENTRY:
       return new SolidityCallSite(
@@ -151,12 +144,7 @@ function encodeStackTraceEntry(
       );
 
     case StackTraceEntryType.UNRECOGNIZED_CREATE_ERROR:
-      return new SolidityCallSite(
-        undefined,
-        UNRECOGNIZED_CONTRACT_NAME,
-        CONSTRUCTOR_FUNCTION_NAME,
-        undefined
-      );
+      break;
 
     case StackTraceEntryType.UNRECOGNIZED_CONTRACT_ERROR:
       return new SolidityCallSite(
@@ -174,30 +162,19 @@ function encodeStackTraceEntry(
         undefined
       );
     case StackTraceEntryType.CONTRACT_CALL_RUN_OUT_OF_GAS_ERROR:
-      if (stackTraceEntry.sourceReference !== undefined) {
-        return sourceReferenceToSolidityCallsite(
-          stackTraceEntry.sourceReference
-        );
-      }
-
-      return new SolidityCallSite(
-        undefined,
-        UNRECOGNIZED_CONTRACT_NAME,
-        UNKNOWN_FUNCTION_NAME,
-        undefined
-      );
-
     case StackTraceEntryType.OTHER_EXECUTION_ERROR:
-      if (stackTraceEntry.sourceReference === undefined) {
-        return new SolidityCallSite(
-          undefined,
-          UNRECOGNIZED_CONTRACT_NAME,
-          UNKNOWN_FUNCTION_NAME,
-          undefined
-        );
-      }
+      break;
+  }
 
-      return sourceReferenceToSolidityCallsite(stackTraceEntry.sourceReference);
+  if (stackTraceEntry.sourceReference) {
+    return sourceReferenceToSolidityCallsite(stackTraceEntry.sourceReference);
+  } else {
+    return new SolidityCallSite(
+      undefined,
+      UNRECOGNIZED_CONTRACT_NAME,
+      UNKNOWN_FUNCTION_NAME,
+      undefined
+    );
   }
 }
 
