a9f0326835264210b83382299bd5c337d3c0884d
Psilon <pzixel@gmail.com>
2022-02-19 00:06:08 +0300

    fix lint

--- packages/hardhat-truffle5/test/types.ts
+++ packages/hardhat-truffle5/test/types.ts
@@ -1,25 +1,32 @@
-import {artifacts} from "hardhat";
+import { artifacts } from "hardhat";
 
 type Dummy = unknown;
 
 type IERC20Contract = Dummy;
 
-type Equals<X, Y> =
-    (<T>() => T extends X ? 1 : 2) extends
-    (<T>() => T extends Y ? 1 : 2) ? true : false;
+type Equals<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y
+  ? 1
+  : 2
+  ? true
+  : false;
 
-type Assert<_X extends true> = void;
+// eslint-disable-next-line @typescript-eslint/naming-convention
+type Assert<_ extends true> = void;
 
 // assert Artifacts can be extended
 
 declare module "hardhat/types/artifacts" {
   export interface Artifacts {
-    require(name: 'IERC20'): IERC20Contract;
+    require(name: "IERC20"): IERC20Contract;
   }
 }
 
-const ierc20Artifact = artifacts.require('IERC20');
-const unknownArtifact = artifacts.require('UnknownArtifact');
+const ierc20Artifact = artifacts.require("IERC20");
+const unknownArtifact = artifacts.require("UnknownArtifact");
 
-export type Assert_IERC20_Type_Is_NotAny = Assert<Equals<IERC20Contract, typeof ierc20Artifact>>;
-export type Assert_UnknownArtifact_Type_Is_Any = Assert<Equals<any, typeof unknownArtifact>>;
+export type AssertIERC20TypeIsNotAny = Assert<
+  Equals<IERC20Contract, typeof ierc20Artifact>
+>;
+export type AssertUnknownArtifactTypeIsAny = Assert<
+  Equals<any, typeof unknownArtifact>
+>;
