8d2b51639e57db5f0f191d1e58443110e44b03d2
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-03-03 17:17:18 -0600

    chai/types: remove unused stuff

--- packages/hardhat-test-helpers/src/chai/types.ts
+++ packages/hardhat-test-helpers/src/chai/types.ts
@@ -1,49 +1,5 @@
-/* eslint-disable @typescript-eslint/no-namespace */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-use-before-define */
-/// <reference types="chai" />
-// I cannot get ethers types to work for some reason
-
+// eslint-disable-next-line @typescript-eslint/no-namespace, @typescript-eslint/no-unused-vars
 declare namespace Chai {
-  interface Assertion
-    extends LanguageChains,
-      NumericComparison,
-      TypeComparison {
-    reverted: AsyncAssertion;
-    revertedWith(reason: string): AsyncAssertion;
-    emit(contract: any, eventName: string): EmitAssertion;
-    properHex(length: number): void;
-    hexEqual(other: string): void;
-    properPrivateKey: void;
-    properAddress: void;
-    /**
-     * @deprecated Use `changeEtherBalance()` instead.
-     */
-    changeBalance(account: any, balance: any): AsyncAssertion;
-    /**
-     * @deprecated Use `changeEtherBalances()` instead.
-     */
-    changeBalances(accounts: any[], balances: any[]): AsyncAssertion;
-    changeEtherBalance(
-      account: any,
-      balance: any,
-      options?: any
-    ): AsyncAssertion;
-    changeEtherBalances(
-      accounts: any[],
-      balances: any[],
-      options?: any
-    ): AsyncAssertion;
-    changeTokenBalance(token: any, account: any, balance: any): AsyncAssertion;
-    changeTokenBalances(
-      token: any,
-      accounts: any[],
-      balances: any[]
-    ): AsyncAssertion;
-    calledOnContract(contract: any): void;
-    calledOnContractWith(contract: any, parameters: any[]): void;
-  }
-
   interface NumericComparison {
     within(start: any, finish: any, message?: string): Assertion;
   }
@@ -56,11 +12,5 @@ declare namespace Chai {
   interface CloseTo {
     // eslint-disable-next-line
     (expected: any, delta: any, message?: string): Assertion;
-  }
-
-  interface AsyncAssertion extends Assertion, Promise<void> {}
-
-  interface EmitAssertion extends AsyncAssertion {
-    withArgs(...args: any[]): AsyncAssertion;
   }
 }
