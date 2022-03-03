e3825215cbb47621f5e76d43a3d999792c1c3824
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-03-03 14:52:37 -0600

    Ran lint:fix

--- packages/hardhat-test-helpers/test/chai/matchers/bigNumber.ts
+++ packages/hardhat-test-helpers/test/chai/matchers/bigNumber.ts
@@ -31,239 +31,239 @@ describe("BigNumber matchers", () => {
   }
 
   describe("with two arguments", function () {
-  function checkAll(
-    actual: number,
-    expected: number,
-    test: (
-      actual: number | string | bigint | BigNumberEthers | BigNumberJs | BN,
-      expected: number | string | bigint | BigNumberEthers | BigNumberJs | BN
-    ) => void
-  ) {
-    const conversions = [
-      (n: number) => n,
-      (n: number) => n.toString(),
-      ...numberToBigNumberConversions,
-    ];
-    for (const convertActual of conversions) {
-      for (const convertExpected of conversions) {
-        const convertedActual = convertActual(actual);
-        const convertedExpected = convertExpected(expected);
-        // a few particular combinations of types don't work:
-        if (
-          typeof convertedActual === "string" &&
-          !BigNumberEthers.isBigNumber(convertedExpected) &&
-          !BN.isBN(convertedExpected) &&
-          !BigNumberJs.isBigNumber(convertedExpected)
-        ) {
-          continue;
+    function checkAll(
+      actual: number,
+      expected: number,
+      test: (
+        actual: number | string | bigint | BigNumberEthers | BigNumberJs | BN,
+        expected: number | string | bigint | BigNumberEthers | BigNumberJs | BN
+      ) => void
+    ) {
+      const conversions = [
+        (n: number) => n,
+        (n: number) => n.toString(),
+        ...numberToBigNumberConversions,
+      ];
+      for (const convertActual of conversions) {
+        for (const convertExpected of conversions) {
+          const convertedActual = convertActual(actual);
+          const convertedExpected = convertExpected(expected);
+          // a few particular combinations of types don't work:
+          if (
+            typeof convertedActual === "string" &&
+            !BigNumberEthers.isBigNumber(convertedExpected) &&
+            !BN.isBN(convertedExpected) &&
+            !BigNumberJs.isBigNumber(convertedExpected)
+          ) {
+            continue;
+          }
+          if (
+            typeof convertedActual === "number" &&
+            typeof convertedExpected === "string"
+          ) {
+            continue;
+          }
+          test(convertedActual, convertedExpected);
         }
-        if (
-          typeof convertedActual === "number" &&
-          typeof convertedExpected === "string"
-        ) {
-          continue;
-        }
-        test(convertedActual, convertedExpected);
       }
     }
-  }
 
-  interface TestCase {
-    operator:
-      | "equal"
-      | "eq"
-      | "above"
-      | "below"
-      | "gt"
-      | "lt"
-      | "least"
-      | "most"
-      | "gte"
-      | "lte";
-    positiveSuccessOperands: [number, number];
-    positiveFailureOperands: [number, number];
-    positiveFailureMessage: string;
-    negativeSuccessOperands: [number, number];
-    negativeFailureOperands: [number, number];
-    negativeFailureMessage: string;
-  }
-  const equalsTestCase: Omit<TestCase, "operator"> = {
-    positiveSuccessOperands: [10, 10],
-    positiveFailureOperands: [10, 11],
-    positiveFailureMessage: "expected 10 to equal 11",
-    negativeSuccessOperands: [11, 10],
-    negativeFailureOperands: [10, 10],
-    negativeFailureMessage: "expected 10 to not equal 10",
-  };
-  const abovePartialTestCase: Omit<
-    TestCase,
-    "operator" | "negativeSuccessOperands"
-  > = {
-    positiveSuccessOperands: [10, 9],
-    positiveFailureOperands: [10, 10],
-    positiveFailureMessage: "expected 10 to be above 10",
-    negativeFailureOperands: [11, 10],
-    negativeFailureMessage: "expected 11 to be at most 10",
-  };
-  const aboveTestCase1: Omit<TestCase, "operator"> = {
-    ...abovePartialTestCase,
-    negativeSuccessOperands: [10, 10],
-  };
-  const aboveTestCase2: Omit<TestCase, "operator"> = {
-    ...abovePartialTestCase,
-    negativeSuccessOperands: [10, 11],
-  };
-  const belowPartialTestCase: Omit<
-    TestCase,
-    "operator" | "negativeSuccessOperands"
-  > = {
-    positiveSuccessOperands: [10, 11],
-    positiveFailureOperands: [11, 10],
-    positiveFailureMessage: "expected 11 to be below 10",
-    negativeFailureOperands: [10, 11],
-    negativeFailureMessage: "expected 10 to be at least 11",
-  };
-  const belowTestCase1: Omit<TestCase, "operator"> = {
-    ...belowPartialTestCase,
-    negativeSuccessOperands: [10, 10],
-  };
-  const belowTestCase2: Omit<TestCase, "operator"> = {
-    ...belowPartialTestCase,
-    negativeSuccessOperands: [10, 9],
-  };
-  const atLeastPartialTestCase: Omit<
-    TestCase,
-    "operator" | "positiveSuccessOperands"
-  > = {
-    positiveFailureOperands: [10, 11],
-    positiveFailureMessage: "expected 10 to be at least 11",
-    negativeSuccessOperands: [10, 11],
-    negativeFailureOperands: [11, 10],
-    negativeFailureMessage: "expected 11 to be below 10",
-  };
-  const atLeastTestCase1: Omit<TestCase, "operator"> = {
-    ...atLeastPartialTestCase,
-    positiveSuccessOperands: [10, 10],
-  };
-  const atLeastTestCase2: Omit<TestCase, "operator"> = {
-    ...atLeastPartialTestCase,
-    positiveSuccessOperands: [10, 9],
-  };
-  const atMostPartialTestCase: Omit<
-    TestCase,
-    "operator" | "positiveSuccessOperands"
-  > = {
-    positiveFailureOperands: [11, 10],
-    positiveFailureMessage: "expected 11 to be at most 10",
-    negativeSuccessOperands: [10, 9],
-    negativeFailureOperands: [10, 11],
-    negativeFailureMessage: "expected 10 to be above 11",
-  };
-  const atMostTestCase1: Omit<TestCase, "operator"> = {
-    ...atMostPartialTestCase,
-    positiveSuccessOperands: [10, 10],
-  };
-  const atMostTestCase2: Omit<TestCase, "operator"> = {
-    ...atMostPartialTestCase,
-    positiveSuccessOperands: [10, 11],
-  };
-  const testCases: TestCase[] = [
-    { operator: "equal", ...equalsTestCase },
-    { operator: "eq", ...equalsTestCase },
-    { operator: "above", ...aboveTestCase1 },
-    { operator: "above", ...aboveTestCase2 },
-    { operator: "gt", ...aboveTestCase1 },
-    { operator: "gt", ...aboveTestCase2 },
-    { operator: "below", ...belowTestCase1 },
-    { operator: "below", ...belowTestCase2 },
-    { operator: "lt", ...belowTestCase1 },
-    { operator: "lt", ...belowTestCase2 },
-    { operator: "least", ...atLeastTestCase1 },
-    { operator: "least", ...atLeastTestCase2 },
-    { operator: "gte", ...atLeastTestCase1 },
-    { operator: "gte", ...atLeastTestCase2 },
-    { operator: "most", ...atMostTestCase1 },
-    { operator: "most", ...atMostTestCase2 },
-    { operator: "lte", ...atMostTestCase1 },
-    { operator: "lte", ...atMostTestCase2 },
-  ];
-  for (const {
-    operator,
-    positiveSuccessOperands,
-    positiveFailureOperands,
-    positiveFailureMessage,
-    negativeSuccessOperands,
-    negativeFailureOperands,
-    negativeFailureMessage,
-  } of testCases) {
-    describe(`.to.${operator}`, () => {
-      checkAll(
-        positiveSuccessOperands[0],
-        positiveSuccessOperands[1],
-        (a, b) => {
-          it(`should work with ${typestr(a)} and ${typestr(b)}`, function () {
-            expect(a).to[operator](b);
-          });
-        }
-      );
-      describe("should throw the proper message on failure", () => {
+    interface TestCase {
+      operator:
+        | "equal"
+        | "eq"
+        | "above"
+        | "below"
+        | "gt"
+        | "lt"
+        | "least"
+        | "most"
+        | "gte"
+        | "lte";
+      positiveSuccessOperands: [number, number];
+      positiveFailureOperands: [number, number];
+      positiveFailureMessage: string;
+      negativeSuccessOperands: [number, number];
+      negativeFailureOperands: [number, number];
+      negativeFailureMessage: string;
+    }
+    const equalsTestCase: Omit<TestCase, "operator"> = {
+      positiveSuccessOperands: [10, 10],
+      positiveFailureOperands: [10, 11],
+      positiveFailureMessage: "expected 10 to equal 11",
+      negativeSuccessOperands: [11, 10],
+      negativeFailureOperands: [10, 10],
+      negativeFailureMessage: "expected 10 to not equal 10",
+    };
+    const abovePartialTestCase: Omit<
+      TestCase,
+      "operator" | "negativeSuccessOperands"
+    > = {
+      positiveSuccessOperands: [10, 9],
+      positiveFailureOperands: [10, 10],
+      positiveFailureMessage: "expected 10 to be above 10",
+      negativeFailureOperands: [11, 10],
+      negativeFailureMessage: "expected 11 to be at most 10",
+    };
+    const aboveTestCase1: Omit<TestCase, "operator"> = {
+      ...abovePartialTestCase,
+      negativeSuccessOperands: [10, 10],
+    };
+    const aboveTestCase2: Omit<TestCase, "operator"> = {
+      ...abovePartialTestCase,
+      negativeSuccessOperands: [10, 11],
+    };
+    const belowPartialTestCase: Omit<
+      TestCase,
+      "operator" | "negativeSuccessOperands"
+    > = {
+      positiveSuccessOperands: [10, 11],
+      positiveFailureOperands: [11, 10],
+      positiveFailureMessage: "expected 11 to be below 10",
+      negativeFailureOperands: [10, 11],
+      negativeFailureMessage: "expected 10 to be at least 11",
+    };
+    const belowTestCase1: Omit<TestCase, "operator"> = {
+      ...belowPartialTestCase,
+      negativeSuccessOperands: [10, 10],
+    };
+    const belowTestCase2: Omit<TestCase, "operator"> = {
+      ...belowPartialTestCase,
+      negativeSuccessOperands: [10, 9],
+    };
+    const atLeastPartialTestCase: Omit<
+      TestCase,
+      "operator" | "positiveSuccessOperands"
+    > = {
+      positiveFailureOperands: [10, 11],
+      positiveFailureMessage: "expected 10 to be at least 11",
+      negativeSuccessOperands: [10, 11],
+      negativeFailureOperands: [11, 10],
+      negativeFailureMessage: "expected 11 to be below 10",
+    };
+    const atLeastTestCase1: Omit<TestCase, "operator"> = {
+      ...atLeastPartialTestCase,
+      positiveSuccessOperands: [10, 10],
+    };
+    const atLeastTestCase2: Omit<TestCase, "operator"> = {
+      ...atLeastPartialTestCase,
+      positiveSuccessOperands: [10, 9],
+    };
+    const atMostPartialTestCase: Omit<
+      TestCase,
+      "operator" | "positiveSuccessOperands"
+    > = {
+      positiveFailureOperands: [11, 10],
+      positiveFailureMessage: "expected 11 to be at most 10",
+      negativeSuccessOperands: [10, 9],
+      negativeFailureOperands: [10, 11],
+      negativeFailureMessage: "expected 10 to be above 11",
+    };
+    const atMostTestCase1: Omit<TestCase, "operator"> = {
+      ...atMostPartialTestCase,
+      positiveSuccessOperands: [10, 10],
+    };
+    const atMostTestCase2: Omit<TestCase, "operator"> = {
+      ...atMostPartialTestCase,
+      positiveSuccessOperands: [10, 11],
+    };
+    const testCases: TestCase[] = [
+      { operator: "equal", ...equalsTestCase },
+      { operator: "eq", ...equalsTestCase },
+      { operator: "above", ...aboveTestCase1 },
+      { operator: "above", ...aboveTestCase2 },
+      { operator: "gt", ...aboveTestCase1 },
+      { operator: "gt", ...aboveTestCase2 },
+      { operator: "below", ...belowTestCase1 },
+      { operator: "below", ...belowTestCase2 },
+      { operator: "lt", ...belowTestCase1 },
+      { operator: "lt", ...belowTestCase2 },
+      { operator: "least", ...atLeastTestCase1 },
+      { operator: "least", ...atLeastTestCase2 },
+      { operator: "gte", ...atLeastTestCase1 },
+      { operator: "gte", ...atLeastTestCase2 },
+      { operator: "most", ...atMostTestCase1 },
+      { operator: "most", ...atMostTestCase2 },
+      { operator: "lte", ...atMostTestCase1 },
+      { operator: "lte", ...atMostTestCase2 },
+    ];
+    for (const {
+      operator,
+      positiveSuccessOperands,
+      positiveFailureOperands,
+      positiveFailureMessage,
+      negativeSuccessOperands,
+      negativeFailureOperands,
+      negativeFailureMessage,
+    } of testCases) {
+      describe(`.to.${operator}`, () => {
         checkAll(
-          positiveFailureOperands[0],
-          positiveFailureOperands[1],
+          positiveSuccessOperands[0],
+          positiveSuccessOperands[1],
           (a, b) => {
-            it(`with ${typestr(a)} and ${typestr(b)}`, function () {
-              expect(() => expect(a).to[operator](b)).to.throw(
-                AssertionError,
-                positiveFailureMessage
-              );
+            it(`should work with ${typestr(a)} and ${typestr(b)}`, function () {
+              expect(a).to[operator](b);
             });
           }
         );
-      });
-      describe("should throw when comparing to a non-integral floating point literal", function () {
-        for (const convert of numberToBigNumberConversions) {
-          const converted = convert(1);
-          it(`with float vs ${typestr(converted)}`, function () {
-            expect(() => expect(1.1).to[operator](converted)).to.throw(
-              RangeError,
-              "The number 1.1 cannot be converted to a BigInt because it is not an integer"
-            );
-          });
-          it(`with ${typestr(converted)} vs float`, function () {
-            expect(() => expect(converted).to[operator](1.1)).to.throw(
-              RangeError,
-              "The number 1.1 cannot be converted to a BigInt because it is not an integer"
-            );
-          });
-        }
-      });
-    });
-    describe(`.not.to.${operator}`, function () {
-      checkAll(
-        negativeSuccessOperands[0],
-        negativeSuccessOperands[1],
-        (a, b) => {
-          it(`should work with ${typestr(a)} and ${typestr(b)}`, function () {
-            expect(a).not.to[operator](b);
-          });
-        }
-      );
-      describe("should throw the proper message on failure", () => {
-        checkAll(
-          negativeFailureOperands[0],
-          negativeFailureOperands[1],
-          (a, b) => {
-            it(`with ${typestr(a)} and ${typestr(b)}`, function () {
-              expect(() => expect(a).not.to[operator](b)).to.throw(
-                AssertionError,
-                negativeFailureMessage
+        describe("should throw the proper message on failure", () => {
+          checkAll(
+            positiveFailureOperands[0],
+            positiveFailureOperands[1],
+            (a, b) => {
+              it(`with ${typestr(a)} and ${typestr(b)}`, function () {
+                expect(() => expect(a).to[operator](b)).to.throw(
+                  AssertionError,
+                  positiveFailureMessage
+                );
+              });
+            }
+          );
+        });
+        describe("should throw when comparing to a non-integral floating point literal", function () {
+          for (const convert of numberToBigNumberConversions) {
+            const converted = convert(1);
+            it(`with float vs ${typestr(converted)}`, function () {
+              expect(() => expect(1.1).to[operator](converted)).to.throw(
+                RangeError,
+                "The number 1.1 cannot be converted to a BigInt because it is not an integer"
+              );
+            });
+            it(`with ${typestr(converted)} vs float`, function () {
+              expect(() => expect(converted).to[operator](1.1)).to.throw(
+                RangeError,
+                "The number 1.1 cannot be converted to a BigInt because it is not an integer"
               );
             });
           }
-        );
+        });
       });
-    });
-  }
+      describe(`.not.to.${operator}`, function () {
+        checkAll(
+          negativeSuccessOperands[0],
+          negativeSuccessOperands[1],
+          (a, b) => {
+            it(`should work with ${typestr(a)} and ${typestr(b)}`, function () {
+              expect(a).not.to[operator](b);
+            });
+          }
+        );
+        describe("should throw the proper message on failure", () => {
+          checkAll(
+            negativeFailureOperands[0],
+            negativeFailureOperands[1],
+            (a, b) => {
+              it(`with ${typestr(a)} and ${typestr(b)}`, function () {
+                expect(() => expect(a).not.to[operator](b)).to.throw(
+                  AssertionError,
+                  negativeFailureMessage
+                );
+              });
+            }
+          );
+        });
+      });
+    }
   });
 
   function checkAllWith3Args(
