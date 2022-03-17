529c69afdabbb1511954cde3725715445cb81535
RavenInd <sam@focusreactive.com>
2022-03-17 11:40:34 +0400

    created header with mobile menu and desktop menu

--- docs/src/components/LandingNavigation.tsx
+++ docs/src/components/LandingNavigation.tsx
@@ -1,9 +1,103 @@
-import React from "react";
+import React, { FC, useEffect, useState } from 'react';
+import { styled } from 'linaria/react';
+import { appTheme, tm } from '../themes';
+import HardhatLogo from '../assets/hardhat-logo';
+import { useRouter } from 'next/router';
+import Hamburger from './ui/Hamburger';
+import MobileMenu from './ui/MobileMenu';
+import Menu from './ui/DesktopMenu';
+import useWindowSize from '../hooks/useWindowSize';
 
-type Props = {};
+const Navigation = styled.nav<{ isMobileMenuOpen: boolean }>`
+  position: sticky;
+  margin-top: 40px;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 96px;
+  box-sizing: border-box;
+  padding: 32px 24px;
+  transition: all ease-in-out 0.5s;
+  background-color: ${tm(({ colors }) => colors.neutral0)};
+  z-index: 10;
+  ${tm(({ media }) => media.lg)} {
+    padding: 240px 24px;
+  }
+  @media screen and (min-width: ${appTheme.breakpoints.lg}px) {
+    padding: 24px 240px;
+  }
+`;
 
-const LandingNavigation = (props: Props) => {
-  return <div>LandingNavigation</div>;
+const ControlsContainer = styled.section`
+  width: 100%;
+  height: 96px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${tm(({ colors }) => colors.neutral0)};
+  padding: 4px 8px;
+  box-sizing: border-box;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const LogoContainer = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 4px 8px;
+  box-sizing: border-box;
+  background-color: ${tm(({ colors }) => colors.neutral0)};
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const LandingNavigation: FC = () => {
+  const router = useRouter();
+  const [isMobileMenuOpen, setMobileMenuState] = useState(false);
+  const windowSize = useWindowSize();
+  const isDesktop = appTheme.breakpoints.lg <= windowSize.width;
+
+  useEffect(() => {
+    const body = document.querySelector('body');
+    if (!body) return;
+
+    if (isMobileMenuOpen) {
+      // Disable scroll
+      body.style.overflow = 'hidden';
+    } else {
+      // Enable scroll
+      body.style.overflow = 'auto';
+    }
+  }, [isMobileMenuOpen]);
+
+  return (
+    <Navigation
+      isMobileMenuOpen={isMobileMenuOpen}
+      onScroll={(e) => {
+        e.stopPropagation();
+      }}
+    >
+      <ControlsContainer>
+        <LogoContainer onClick={() => router.push('/')}>
+          <HardhatLogo />
+        </LogoContainer>
+        {!isDesktop ? (
+          <Hamburger isOpen={isMobileMenuOpen} onClick={() => setMobileMenuState(!isMobileMenuOpen)} />
+        ) : (
+          <Menu />
+        )}
+      </ControlsContainer>
+
+      {!isDesktop && <MobileMenu isOpen={isMobileMenuOpen} />}
+    </Navigation>
+  );
 };
 
 export default LandingNavigation;
---
+++ docs/src/components/ui/Hamburger.tsx
@@ -0,0 +1,69 @@
+import React, { FC } from 'react';
+import { styled } from 'linaria/react';
+import { tm } from '../../themes';
+import { css } from 'linaria';
+
+type HamburgerProps = {
+  isOpen: boolean;
+  onClick: () => void;
+};
+
+type HamburgerLinePosition = 'top' | 'mid' | 'bot';
+
+type HamburgerLineProps = {
+  isOpen: boolean;
+  position: HamburgerLinePosition;
+};
+
+const HamburgerContainer = styled.button`
+  width: 44px;
+  height: 32px;
+  display: flex;
+  padding: 8px;
+  border: none;
+  flex-direction: column;
+  justify-content: space-between;
+  border-radius: 4px;
+  background-color: ${tm(({ colors }) => colors.neutral0)};
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const HamburgerLine = styled.div`
+  background-color: ${tm(({ colors }) => colors.neutral900)};
+  height: 2px;
+  width: 28px;
+  transform-origin: left;
+  user-select: none;
+  transition: all 0.25s ease-in-out;
+  &[data-open='true'] {
+    &[data-position='top'] {
+      transform: rotate(45deg) translate(1px, -5px);
+    }
+    &[data-position='mid'] {
+      opacity: 0;
+    }
+    &[data-position='bot'] {
+      transform: rotate(-45deg) translate(1px, 5px);
+    }
+  }
+  &[data-position='mid'] {
+    width: 19px;
+    transition: all 0.1s ease-out;
+  }
+`;
+
+const Hamburger = (props: HamburgerProps) => {
+  const { onClick, isOpen } = props;
+
+  return (
+    <HamburgerContainer onClick={() => onClick()}>
+      <HamburgerLine data-open={isOpen} data-position="top" />
+      <HamburgerLine data-open={isOpen} data-position="mid" />
+      <HamburgerLine data-open={isOpen} data-position="bot" />
+    </HamburgerContainer>
+  );
+};
+
+export default React.memo(Hamburger);
