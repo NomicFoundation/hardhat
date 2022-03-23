a4f4449d01c450c47cf6c99adb7630d38121387f
RavenInd <sam@focusreactive.com>
2022-03-23 18:23:54 +0400

    created WhyHardhatBlock boilerplate

---
+++ docs/src/components/landingBlocks/WhyHardhatBlock.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Section from '../Section';
+import { styled } from 'linaria/react';
+// import FeatureCard from '../ui/FeatureCard';
+import SolidityImageDesktop from '../../assets/feature-cards/Desktop/SolidityImage.png';
+import SolidityImageMobile from '../../assets/feature-cards/Mobile/SolidityImage.png';
+import { tm, appTheme } from '../../themes';
+const { media } = appTheme;
+
+const content = {
+  title: 'Why hardhat',
+  featureCardOne: {
+    getImgPath: (props: { isDesktop: boolean }) => {
+      return props.isDesktop ? SolidityImageDesktop : SolidityImageMobile;
+    },
+    cta: { url: '/hardhat-network/#console-log', title: 'Get started with Solidity console.log' },
+    articleOne: {
+      title: 'Run Solidity locally',
+      text: 'Easily deploy your contracts, run tests and debug Solidity code without dealing with live environments. Hardhat Network is a local Ethereum network designed for development.',
+    },
+    articleTwo: {
+      title: 'Debugging-first ',
+      text: 'Hardhat is the best choice for Solidity debugging. You get Solidity stack traces, console.log and explicit error messages when transactions fail.',
+    },
+  },
+};
+
+type Props = {
+  content: typeof content;
+};
+
+const Container = styled.section`
+  width: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  border-top: 1px solid ${tm(({ colors }) => colors.neutral400)};
+  border-left: 1px solid ${tm(({ colors }) => colors.neutral400)};
+  padding-top: 52px;
+  ${media.lg} {
+    border-left: unset;
+  }
+`;
+
+const Title = styled.h2`
+  position: absolute;
+  padding: 24px;
+  background-color: ${tm(({ colors }) => colors.neutral0)};
+  left: 0;
+  top: 0;
+  transform: translateY(-50%);
+  text-transform: uppercase;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0.2em;
+  ${media.lg} {
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const WhyHardhatBlock = ({ content }: Props) => {
+  return (
+    <Section>
+      <Container>
+        <Title>{content.title}</Title>
+        {/* <FeatureCard content={content.featureCardOne} isReversed={true} /> */}
+      </Container>
+    </Section>
+  );
+};
+
+export default React.memo(WhyHardhatBlock);
+
+WhyHardhatBlock.defaultProps = { content };
--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -1,18 +1,12 @@
-import type { NextPage } from "next";
-import LandingLayout from "../components/LandingLayout";
-import HeroBlock from "../components/landingBlocks/HeroBlock";
+import type { NextPage } from 'next';
+import LandingLayout from '../components/LandingLayout';
+import HeroBlock from '../components/landingBlocks/HeroBlock';
+import WhyHardhatBlock from '../components/landingBlocks/WhyHardhatBlock';
 
 const Home: NextPage = () => {
   return (
-    <LandingLayout seo={{ title: "Hardhat" }}>
-      <HeroBlock />
-      <HeroBlock />
-      <HeroBlock />
-      {/* <HeroBlock />
-      <HeroBlock />
-      <HeroBlock />
-      <HeroBlock />
-      <HeroBlock /> */}
+    <LandingLayout seo={{ title: 'Hardhat' }}>
+      <WhyHardhatBlock />
     </LandingLayout>
   );
 };
