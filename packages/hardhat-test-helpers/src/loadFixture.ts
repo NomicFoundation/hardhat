d599a631a0af374148e3f39bcd386cf07876ec89
Franco Victorio <victorio.franco@gmail.com>
2022-04-06 19:02:42 -0300

    Add jsdoc for loadFixture

--- packages/hardhat-test-helpers/src/loadFixture.ts
+++ packages/hardhat-test-helpers/src/loadFixture.ts
@@ -10,6 +10,19 @@ interface Snapshot<T> {
 
 const snapshots: Array<Snapshot<any>> = [];
 
+/**
+ * Executes the given function and takes a snapshot of the blockchain. Each
+ * time `loadFixture` is called again with the same function, the blockchain
+ * will be restored to that snapshot instead of executing the function again.
+ *
+ * Useful for `beforeEach` hooks that setup the desired state of the network.
+ *
+ * *Warning*: don't use `loadFixture` with an anonymous function, otherwise the
+ * function will be executed each time instead of using snapshots:
+ *
+ * - Correct usage: `loadFixture(deployTokens)`
+ * - Incorrect usage: `loadFixture(async () => { ... })`
+ */
 export async function loadFixture<T>(fixture: Fixture<T>): Promise<T> {
   const snapshot = snapshots.find((s) => s.fixture === fixture);
 
