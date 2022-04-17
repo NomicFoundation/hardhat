bb2b4ad1f9fcc1de5e3610829b13f9d16760a7ff
Sam Kravtsevich <emico.crow@gmail.com>
2022-04-17 13:41:40 +0300

    feat: unite light and dark theme (#29)
    * feat: unite light and dark theme
    
    * refactor: refactor themes palettes
    
    Co-authored-by: RavenInd <sam@focusreactive.com>

--- docs/src/themes.tsx
+++ docs/src/themes.tsx
@@ -17,41 +17,86 @@ const media = {
   lg: `@media screen and (min-width: ${breakpoints.lg}px)`,
 };
 
+export const lightPalette = {
+  transparent: "transparent",
+  neutral0: "#FFFFFF",
+  neutral100: "#F2F2F2",
+  neutral200: "#FCFCF1",
+  neutral400: "#C4C4C4",
+  neutral500: "#4B4D4D",
+  neutral600: "#6E6F70",
+  neutral700: "#9B9FA8",
+  neutral800: "#16181D",
+  neutral900: "#0A0A0A",
+  accent100: "#FBFCDB",
+  accent600: "#FFF04D",
+  accent700: "#CCB200",
+  accent800: "#FFF100",
+  accent900: "#EDCF00",
+  border: "#4B4D4D",
+  complementary600: "#E9DEFA",
+  cardBoxShadow: "#0A0A0A14",
+  sliderButtonShadow: "rgba(0, 0, 0, 0.102)",
+  sliderButtonHoverShadow: "hsl(0deg 0% 83% / 50%)",
+  toolsBlockBorder: "#d4d4d4",
+  mottoRunnerBackground: "#F8F4CB",
+  mottoNetworkBackground: "#f6edd1",
+  mottoIgnitionBackground: "#f3ecf3",
+  mottoVscodeBackground: "#f0e7fb",
+  getStartedTopBackground:
+    "linear-gradient(180deg, #ffffff 3.12%, rgba(255, 255, 255, 0) 67.71%)",
+  getStartedBottomBackground:
+    "linear-gradient(180deg, #ffffff 0%, rgba(255, 255, 255, 0) 55.73%)",
+  textureBackground:
+    "linear-gradient(254.24deg, #E9DEFA 0%, #FBFCDB 100%, #FBFCDB 100%);",
+  neutralBackground:
+    "linear-gradient(180deg, #FFFFFF 7.96%, rgba(255, 255, 255, 0.484844) 18.71%, rgba(255, 255, 255, 0) 28.83%, rgba(255, 255, 255, 0) 68.82%, #FFFFFF 91.43%);",
+};
+
+export const darkPalette = {
+  transparent: "transparent",
+  neutral0: "#16181D",
+  neutral100: "#F2F2F2",
+  neutral200: "#16181D",
+  neutral400: "#4B4D4D",
+  neutral500: "#4B4D4D",
+  neutral600: "#6E6F70",
+  neutral700: "#9B9FA8",
+  neutral800: "#B0B2B5",
+  neutral900: "#FFFFFF",
+  accent100: "#FBFCDB",
+  accent600: "#FFF04D",
+  accent700: "#CCB200",
+  accent800: "#FFF100",
+  accent900: "#EDCF00",
+  border: "#4B4D4D",
+  complementary600: "#E9DEFA",
+  cardBoxShadow: "#0A0A0A14",
+  sliderButtonShadow: "rgba(0, 0, 0, 0.102)",
+  sliderButtonHoverShadow: "hsl(0deg 0% 83% / 50%)",
+  toolsBlockBorder: "#d4d4d4",
+  mottoRunnerBackground: "#F8F4CB",
+  mottoNetworkBackground: "#f6edd1",
+  mottoIgnitionBackground: "#f3ecf3",
+  mottoVscodeBackground: "#f0e7fb",
+  getStartedTopBackground:
+    "linear-gradient(180deg, #ffffff 3.12%, rgba(255, 255, 255, 0) 67.71%)",
+  getStartedBottomBackground:
+    "linear-gradient(180deg, #ffffff 0%, rgba(255, 255, 255, 0) 55.73%)",
+  textureBackground:
+    "linear-gradient(254.24deg, #E9DEFA 0%, #FBFCDB 100%, #FBFCDB 100%);",
+  neutralBackground:
+    "linear-gradient(180deg, #FFFFFF 7.96%, rgba(255, 255, 255, 0.484844) 18.71%, rgba(255, 255, 255, 0) 28.83%, rgba(255, 255, 255, 0) 68.82%, #FFFFFF 91.43%);",
+};
+
+type Palette = typeof lightPalette;
+
 export const appTheme = {
-  colors: {
-    transparent: "transparent",
-    neutral0: "#FFFFFF",
-    neutral100: "#F2F2F2",
-    neutral200: "#FCFCF1",
-    neutral400: "#C4C4C4",
-    neutral500: "#4B4D4D",
-    neutral600: "#6E6F70",
-    neutral700: "#9B9FA8",
-    neutral800: "#16181D",
-    neutral900: "#0A0A0A",
-    accent100: "#FBFCDB",
-    accent600: "#FFF04D",
-    accent700: "#CCB200",
-    accent800: "#FFF100",
-    accent900: "#EDCF00",
-    complementary600: "#E9DEFA",
-    accentBackground: "linear-gradient(256.6deg, #FFF100 0%, #FFF100 100%)",
-    cardBoxShadow: "#0A0A0A14",
-    sliderButtonShadow: "rgba(0, 0, 0, 0.102)",
-    sliderButtonHoverShadow: "hsl(0deg 0% 83% / 50%)",
-    toolsBlockBorder: "#d4d4d4",
-    mottoRunnerBackground: "#F8F4CB",
-    mottoNetworkBackground: "#f6edd1",
-    mottoIgnitionBackground: "#f3ecf3",
-    mottoVscodeBackground: "#f0e7fb",
-    getStartedTopBackground:
-      "linear-gradient(180deg, #ffffff 3.12%, rgba(255, 255, 255, 0) 67.71%)",
-    getStartedBottomBackground:
-      "linear-gradient(180deg, #ffffff 0%, rgba(255, 255, 255, 0) 55.73%)",
-    textureBackground:
-      "linear-gradient(254.24deg, #E9DEFA 0%, #FBFCDB 100%, #FBFCDB 100%);",
-    neutralBackground:
-      "linear-gradient(180deg, #FFFFFF 7.96%, rgba(255, 255, 255, 0.484844) 18.71%, rgba(255, 255, 255, 0) 28.83%, rgba(255, 255, 255, 0) 68.82%, #FFFFFF 91.43%);",
+  light: {
+    colors: lightPalette,
+  },
+  dark: {
+    colors: darkPalette,
   },
   media,
   breakpoints,
@@ -68,7 +113,10 @@ export const ThemeProvider = ({
   <theming.ThemeProvider theme={themeProp}>{children}</theming.ThemeProvider>
 );
 
-type ThemeSelect = (tm: typeof appTheme) => string;
+type ThemeSelect<T> = (tm: T) => string;
 
-export const tm = (cb: ThemeSelect) => () =>
-  ((fn) => fn(theming.useTheme()))(cb);
+export const tm = (cb: ThemeSelect<{ colors: Palette }>) => () =>
+  ((fn) => fn(theming.useTheme().light))(cb);
+
+export const tmDark = (cb: ThemeSelect<{ colors: Palette }>) => () =>
+  ((fn) => fn(theming.useTheme().dark))(cb);
