308db0ce4a25f28ea38b710a2c45616828c506ea
Franco Victorio <victorio.franco@gmail.com>
2022-04-21 13:05:57 +0200

    Add some comments to hexEqual

--- packages/hardhat-chai-matchers/src/hexEqual.ts
+++ packages/hardhat-chai-matchers/src/hexEqual.ts
@@ -2,16 +2,20 @@ export function supportHexEqual(Assertion: Chai.AssertionStatic) {
   Assertion.addMethod("hexEqual", function (this: any, other: string) {
     const subject = this._obj;
     const isNegated = this.__flags.negate === true;
+
+    // check that both values are proper hex strings
     const isHex = (a: string) => /^0x[0-9a-fA-F]*$/.test(a);
     for (const element of [subject, other]) {
       if (!isHex(element)) {
         this.assert(
-          isNegated,
+          isNegated, // trick to make this assertion always fail
           `Expected "${subject}" to be a hex string equal to "${other}", but "${element}" is not a valid hex string`,
           `Expected "${subject}" not to be a hex string equal to "${other}", but "${element}" is not a valid hex string`
         );
       }
     }
+
+    // compare values
     const extractNumeric = (hex: string) => hex.replace(/^0x0*/, "");
     this.assert(
       extractNumeric(subject.toLowerCase()) ===
