64f478fcd9a77ba2290bc93ea7f605a1eefe0f92
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-04-22 16:39:03 -0500

    advanced-ts: fix eslint plugin breaking ts imports
    fixes https://github.com/NomicFoundation/hardhat/issues/2266
    
    in the typescript starter it is using `plugin:node/recommended` for eslint which breaks every import done across files (see linked issue)
    
    some in the thread suggested removing the rules entirely but i have found that replacing them with the `@typescript-eslint/recommended` is a drop-in replacement.

--- packages/hardhat-core/sample-projects/advanced-ts/.eslintrc.js
+++ packages/hardhat-core/sample-projects/advanced-ts/.eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
   extends: [
     "standard",
     "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
