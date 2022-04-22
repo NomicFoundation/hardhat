b07ff26264bbdec399261bb28f754ada0875bb46
Franco Victorio <victorio.franco@gmail.com>
2022-04-22 16:48:36 +0200

    Fix phrasing in test descriptions

--- packages/hardhat-chai-matchers/test/properPrivateKey.ts
+++ packages/hardhat-chai-matchers/test/properPrivateKey.ts
@@ -4,22 +4,22 @@ import "../src";
 
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 
-describe("Proper private", () => {
-  it("Expect to be proper private", async () => {
+describe("Proper private key", () => {
+  it("Expect to be proper private key", async () => {
     expect("0x706618637b8ca922f6290ce1ecd4c31247e9ab75cf0530a0ac95c0332173d7c5")
       .to.be.properPrivateKey;
     expect("0x03c909455dcef4e1e981a21ffb14c1c51214906ce19e8e7541921b758221b5ae")
       .to.be.properPrivateKey;
   });
 
-  it("Expect not to be proper private", async () => {
+  it("Expect not to be proper private key", async () => {
     expect("0x28FAA621c3348823D6c6548981a19716bcDc740").to.not.be
       .properPrivateKey;
     expect("0x706618637b8ca922f6290ce1ecd4c31247e9ab75cf0530a0ac95c0332173d7cw")
       .to.not.be.properPrivateKey;
   });
 
-  it("Expect to throw if invalid private", async () => {
+  it("Expect to throw if invalid private key", async () => {
     expect(
       () =>
         expect(
@@ -31,7 +31,7 @@ describe("Proper private", () => {
     );
   });
 
-  it("Expect to throw if negation with proper private)", async () => {
+  it("Expect to throw if negation with proper private key)", async () => {
     expect(
       () =>
         expect(
