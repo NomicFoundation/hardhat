0f3dd261605626ccf2c598425d0a5096482d6a1b
Franco Victorio <victorio.franco@gmail.com>
2022-04-26 19:11:05 -0300

    Add .reverted matcher

--- packages/hardhat-chai-matchers/test/fixture-projects/hardhat-project/contracts/Matchers.sol
+++ packages/hardhat-chai-matchers/test/fixture-projects/hardhat-project/contracts/Matchers.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: UNLICENSED
+
 pragma solidity ^0.8.0;
 
 contract Matchers {
@@ -7,7 +9,16 @@ contract Matchers {
     x++;
   }
 
+  function succeedsView() public view returns (uint) {
+    return x;
+  }
+
   function revertsWithoutReasonString() public {
+    x++;
+    require(false);
+  }
+
+  function revertsWithoutReasonStringView() public pure {
     require(false);
   }
 }
--- packages/hardhat-chai-matchers/test/reverted.ts
+++ packages/hardhat-chai-matchers/test/reverted.ts
@@ -1,4 +1,4 @@
-import { expect } from "chai";
+import { AssertionError, expect } from "chai";
 import { useEnvironment, useEnvironmentWithNode } from "./helpers";
 
 import "../src";
@@ -17,36 +17,279 @@ describe("INTEGRATION: Reverted", function () {
   });
 
   function runTests() {
-    const deployMatchers = async (hre: any) => {
-      return (await hre.ethers.getContractFactory("Matchers")).deploy();
+    // deploy Matchers contract before each test
+    let matchers: any;
+    beforeEach("deploy matchers contract", async function () {
+      const Matchers = await this.hre.ethers.getContractFactory("Matchers");
+      matchers = await Matchers.deploy();
+    });
+
+    // helpers
+    const expectAssertionError = async (x: Promise<void>, message: string) => {
+      return expect(x).to.be.eventually.rejectedWith(AssertionError, message);
     };
 
-    it("should pass if transaction reverts", async function () {
-      const matchers = await deployMatchers(this.hre);
+    const mineSuccessfulTransaction = async (hre: any) => {
+      await hre.network.provider.send("evm_setAutomine", [false]);
 
-      await expect(matchers.revertsWithoutReasonString()).to.be.reverted;
+      const [signer] = await hre.ethers.getSigners();
+      const tx = await signer.sendTransaction({ to: signer.address });
+
+      await hre.network.provider.send("hardhat_mine", []);
+      await hre.network.provider.send("evm_setAutomine", [true]);
+
+      return tx;
+    };
+
+    const mineRevertedTransaction = async (hre: any) => {
+      await hre.network.provider.send("evm_setAutomine", [false]);
+
+      const tx = await matchers.revertsWithoutReasonString({
+        gasLimit: 1_000_000,
+      });
+
+      await hre.network.provider.send("hardhat_mine", []);
+      await hre.network.provider.send("evm_setAutomine", [true]);
+
+      return tx;
+    };
+
+    describe("with a string as its subject", function () {
+      it("hash of a successful transaction", async function () {
+        const { hash } = await mineSuccessfulTransaction(this.hre);
+
+        await expectAssertionError(
+          expect(hash).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(hash).to.not.be.reverted;
+      });
+
+      it("hash of a reverted transaction", async function () {
+        const { hash } = await mineRevertedTransaction(this.hre);
+
+        await expect(hash).to.be.reverted;
+        await expectAssertionError(
+          expect(hash).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+
+      it("invalid string", async function () {
+        await expectAssertionError(
+          expect("0x123").to.be.reverted,
+          "Expected a valid transaction hash, but got '0x123'"
+        );
+
+        await expectAssertionError(
+          expect("0x123").to.not.be.reverted,
+          "Expected a valid transaction hash, but got '0x123'"
+        );
+      });
+
+      it("promise of a hash of a successful transaction", async function () {
+        const { hash } = await mineSuccessfulTransaction(this.hre);
+
+        await expectAssertionError(
+          expect(Promise.resolve(hash)).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(Promise.resolve(hash)).to.not.be.reverted;
+      });
+
+      it("promise of a hash of a reverted transaction", async function () {
+        const { hash } = await mineRevertedTransaction(this.hre);
+
+        await expect(Promise.resolve(hash)).to.be.reverted;
+        await expectAssertionError(
+          expect(Promise.resolve(hash)).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+
+      it("promise of an invalid string", async function () {
+        await expectAssertionError(
+          expect(Promise.resolve("0x123")).to.be.reverted,
+          "Expected a valid transaction hash, but got '0x123'"
+        );
+
+        await expectAssertionError(
+          expect(Promise.resolve("0x123")).to.not.be.reverted,
+          "Expected a valid transaction hash, but got '0x123'"
+        );
+      });
     });
 
-    it("should fail if transaction succeeds", async function () {
-      const matchers = await deployMatchers(this.hre);
+    describe("with a TxResponse as its subject", function () {
+      it("TxResponse of a successful transaction", async function () {
+        const tx = await mineSuccessfulTransaction(this.hre);
 
-      await expect(expect(matchers.succeeds()).to.be.reverted).to.be.eventually
-        .rejected;
+        await expectAssertionError(
+          expect(tx).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(tx).to.not.be.reverted;
+      });
+
+      it("TxResponse of a reverted transaction", async function () {
+        const tx = await mineRevertedTransaction(this.hre);
+
+        await expect(tx).to.be.reverted;
+        await expectAssertionError(
+          expect(tx).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+
+      it("promise of a TxResponse of a successful transaction", async function () {
+        const tx = await mineSuccessfulTransaction(this.hre);
+
+        await expectAssertionError(
+          expect(Promise.resolve(tx)).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(Promise.resolve(tx)).to.not.be.reverted;
+      });
+
+      it("promise of a TxResponse of a reverted transaction", async function () {
+        const tx = await mineRevertedTransaction(this.hre);
+
+        await expect(Promise.resolve(tx)).to.be.reverted;
+        await expectAssertionError(
+          expect(Promise.resolve(tx)).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
     });
 
-    describe("negated", function () {
-      it("should pass if transaction succeeds", async function () {
-        const matchers = await deployMatchers(this.hre);
+    describe("with a TxReceipt as its subject", function () {
+      it("TxReceipt of a successful transaction", async function () {
+        const tx = await mineSuccessfulTransaction(this.hre);
+        const receipt = await tx.wait();
 
+        await expectAssertionError(
+          expect(receipt).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(receipt).to.not.be.reverted;
+      });
+
+      it("TxReceipt of a reverted transaction", async function () {
+        const tx = await mineRevertedTransaction(this.hre);
+        const receipt = await this.hre.ethers.provider.waitForTransaction(
+          tx.hash
+        ); // tx.wait rejects, so we use provider.waitForTransaction
+
+        await expect(receipt).to.be.reverted;
+        await expectAssertionError(
+          expect(receipt).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+
+      it("promise of a TxReceipt of a successful transaction", async function () {
+        const tx = await mineSuccessfulTransaction(this.hre);
+        const receipt = await tx.wait();
+
+        await expectAssertionError(
+          expect(Promise.resolve(receipt)).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(Promise.resolve(receipt)).to.not.be.reverted;
+      });
+
+      it("promise of a TxReceipt of a reverted transaction", async function () {
+        const tx = await mineRevertedTransaction(this.hre);
+        const receipt = await this.hre.ethers.provider.waitForTransaction(
+          tx.hash
+        ); // tx.wait rejects, so we use provider.waitForTransaction
+
+        await expect(Promise.resolve(receipt)).to.be.reverted;
+        await expectAssertionError(
+          expect(Promise.resolve(receipt)).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+    });
+
+    describe("calling a contract method that succeeds", function () {
+      it("a write method that succeeds", async function () {
+        await expectAssertionError(
+          expect(matchers.succeeds()).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
         await expect(matchers.succeeds()).to.not.be.reverted;
       });
 
-      it("should fail if transaction reverts", async function () {
-        const matchers = await deployMatchers(this.hre);
+      it("a view method that succeeds", async function () {
+        await expectAssertionError(
+          expect(matchers.succeedsView()).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(matchers.succeedsView()).to.not.be.reverted;
+      });
 
-        await expect(
-          expect(matchers.revertsWithoutReasonString()).to.not.be.reverted
-        ).to.be.eventually.rejected;
+      it("a gas estimation that succeeds", async function () {
+        await expectAssertionError(
+          expect(matchers.estimateGas.succeeds()).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(matchers.estimateGas.succeeds()).to.not.be.reverted;
+      });
+
+      it("a static call of a write method that succeeds", async function () {
+        await expectAssertionError(
+          expect(matchers.callStatic.succeeds()).to.be.reverted,
+          "Expected transaction to be reverted"
+        );
+        await expect(matchers.callStatic.succeeds()).to.not.be.reverted;
+      });
+    });
+
+    describe("calling a contract method that reverts", function () {
+      it("a write method that reverts", async function () {
+        await expect(matchers.revertsWithoutReasonString()).to.be.reverted;
+        await expectAssertionError(
+          expect(matchers.revertsWithoutReasonString()).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+
+      it("a view method that reverts", async function () {
+        await expect(matchers.revertsWithoutReasonStringView()).to.be.reverted;
+        await expectAssertionError(
+          expect(matchers.revertsWithoutReasonStringView()).to.not.be.reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+
+      it("a gas estimation that succeeds", async function () {
+        await expect(matchers.estimateGas.revertsWithoutReasonString()).to.be
+          .reverted;
+        await expectAssertionError(
+          expect(matchers.estimateGas.revertsWithoutReasonString()).to.not.be
+            .reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+
+      it("a static call of a write method that succeeds", async function () {
+        await expect(matchers.callStatic.revertsWithoutReasonString()).to.be
+          .reverted;
+        await expectAssertionError(
+          expect(matchers.callStatic.revertsWithoutReasonString()).to.not.be
+            .reverted,
+          "Expected transaction NOT to be reverted"
+        );
+      });
+    });
+
+    describe("invalid rejection values", function () {
+      it("non-errors", async function () {
+        await expectAssertionError(
+          expect(Promise.reject({})).to.be.reverted,
+          "Expected an Error object"
+        );
       });
     });
   }
