ffac159ff855b28d494b53513002304c3ed010ce
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-04-28 11:58:58 -0600

    Fix lint error: stringify invalid tx b4 printing

--- packages/hardhat-chai-matchers/src/misc/transaction.ts
+++ packages/hardhat-chai-matchers/src/misc/transaction.ts
@@ -18,7 +18,7 @@ export async function waitForPendingTransaction(
     ({ hash } = tx);
   }
   if (hash === undefined) {
-    throw new Error(`${tx} is not a valid transaction`);
+    throw new Error(`${JSON.stringify(tx)} is not a valid transaction`);
   }
   return provider.waitForTransaction(hash);
 }
