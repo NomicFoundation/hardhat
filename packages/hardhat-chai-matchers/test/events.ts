c9bd91c24a49576ccd65435eeab71d9811b62924
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-04-29 11:57:11 -0600

    Add simplified tests of events with arguments

--- packages/hardhat-chai-matchers/test/events.ts
+++ packages/hardhat-chai-matchers/test/events.ts
@@ -73,10 +73,159 @@ describe(".to.emit (contract events)", () => {
       );
     });
 
-    it("Emit two: success", async () => {
-      await expect(contract.emitTwo())
-        .to.emit(contract, "Two")
-        .withArgs(2, "Two");
+    describe(".withArgs", function () {
+      it.skip("Should fail when used with .not.", async function () {
+        await expect(
+          expect(contract.emitUint(1))
+            .not.to.emit(contract, "WithUintArg")
+            .withArgs(1)
+        ).to.be.eventually.rejectedWith(
+          AssertionError,
+          "Do not combine .not. with .withArgs()"
+        );
+      });
+
+      describe("with a uint argument", function () {
+        it("Should match the argument", async function () {
+          await expect(contract.emitUint(1))
+            .to.emit(contract, "WithUintArg")
+            .withArgs(1);
+        });
+
+        it("Should fail when the input argument doesn't match the event argument", async function () {
+          await expect(
+            expect(contract.emitUint(1))
+              .to.emit(contract, "WithUintArg")
+              .withArgs(2)
+          ).to.be.eventually.rejectedWith(
+            AssertionError,
+            "expected 1 to equal 2"
+          );
+        });
+
+        it("Should fail when too many arguments are given", async function () {
+          await expect(
+            expect(contract.emitUint(1))
+              .to.emit(contract, "WithUintArg")
+              .withArgs(1, 3)
+          ).to.be.eventually.rejectedWith(
+            AssertionError,
+            'Expected "WithUintArg" event to have 2 argument(s), but it has 1'
+          );
+        });
+      });
+
+      const string1 = "string1";
+      const string1Bytes = ethers.utils.hexlify(
+        ethers.utils.toUtf8Bytes(string1)
+      );
+      const string2 = "string2";
+      const string2Bytes = ethers.utils.hexlify(
+        ethers.utils.toUtf8Bytes(string2)
+      );
+
+      describe("with a string argument", function () {
+        it("Should match the argument", async function () {
+          await expect(contract.emitString("string"))
+            .to.emit(contract, "WithStringArg")
+            .withArgs("string");
+        });
+
+        it("Should fail when the input argument doesn't match the event argument", async function () {
+          await expect(
+            expect(contract.emitString(string1))
+              .to.emit(contract, "WithStringArg")
+              .withArgs(string2)
+          ).to.be.eventually.rejectedWith(
+            AssertionError,
+            `expected '${string1}' to equal '${string2}'`
+          );
+        });
+      });
+
+      describe("with an indexed string argument", function () {
+        it("Should match the argument", async function () {
+          await expect(contract.emitIndexedString(string1))
+            .to.emit(contract, "WithIndexedStringArg")
+            .withArgs(string1);
+        });
+
+        it.skip("Should fail when the input argument doesn't match the event argument", async function () {
+          await expect(
+            expect(contract.emitIndexedString(string1))
+              .to.emit(contract, "WithIndexedStringArg")
+              .withArgs(string2)
+          ).to.be.eventually.rejectedWith(
+            AssertionError,
+            `expected '${string1}' to equal '${string2}'`
+          );
+        });
+
+        it("Should match the event argument with a hash value", async function () {
+          await expect(contract.emitIndexedString(string1))
+            .to.emit(contract, "WithIndexedStringArg")
+            .withArgs(ethers.utils.keccak256(string1Bytes));
+        });
+
+        it("Should fail when trying to match the event argument with an incorrect hash value", async function () {
+          const expectedHash = ethers.utils.keccak256(string1Bytes);
+          const incorrectHash = ethers.utils.keccak256(string2Bytes);
+          await expect(
+            expect(contract.emitIndexedString(string1))
+              .to.emit(contract, "WithIndexedStringArg")
+              .withArgs(incorrectHash)
+          ).to.be.eventually.rejectedWith(
+            AssertionError,
+            `expected '${expectedHash}' to be one of [ Array(2) ]`
+          );
+        });
+      });
+
+      describe("with a bytes argument", function () {
+        it("Should match the argument", async function () {
+          await expect(contract.emitBytes(string1Bytes))
+            .to.emit(contract, "WithBytesArg")
+            .withArgs(string1Bytes);
+        });
+
+        it("Should fail when the input argument doesn't match the event argument", async function () {
+          await expect(
+            expect(contract.emitBytes(string2Bytes))
+              .to.emit(contract, "WithBytesArg")
+              .withArgs(string1Bytes)
+          ).to.be.eventually.rejectedWith(
+            AssertionError,
+            `expected '${string2Bytes}' to equal '${string1Bytes}'`
+          );
+        });
+      });
+
+      describe("with an indexed bytes argument", function () {
+        it("Should match the argument", async function () {
+          await expect(contract.emitIndexedBytes(string1Bytes))
+            .to.emit(contract, "WithIndexedBytesArg")
+            .withArgs(string1Bytes);
+        });
+
+        it("Should fail when the input argument doesn't match the event argument", async function () {
+          await expect(
+            expect(contract.emitIndexedBytes(string2Bytes))
+              .to.emit(contract, "WithIndexedBytesArg")
+              .withArgs(string1Bytes)
+          ).to.be.eventually.rejectedWith(
+            AssertionError,
+            `expected '${ethers.utils.keccak256(
+              string2Bytes
+            )}' to be one of [ Array(2) ]`
+          );
+        });
+
+        it("Should match the event argument with a hash value", async function () {
+          await expect(contract.emitIndexedBytes(string1Bytes))
+            .to.emit(contract, "WithIndexedBytesArg")
+            .withArgs(ethers.utils.keccak256(string1Bytes));
+        });
+      });
     });
 
     it("Emit index: success", async () => {
@@ -124,16 +273,6 @@ describe(".to.emit (contract events)", () => {
         .and.to.emit(contract, "Two");
     });
 
-    it("Event with proper args", async () => {
-      await expect(contract.emitOne())
-        .to.emit(contract, "One")
-        .withArgs(
-          1,
-          "One",
-          "0x00cfbbaf7ddb3a1476767101c12a0162e241fbad2a0162e2410cfbbaf7162123"
-        );
-    });
-
     it("Event with proper args from nested", async () => {
       await expect(contract.emitNested())
         .to.emit(contract, "One")
@@ -150,58 +289,6 @@ describe(".to.emit (contract events)", () => {
       ).to.be.eventually.rejectedWith(
         AssertionError,
         'Expected "One" event to have 1 argument(s), but it has 3'
-      );
-    });
-
-    it("Event with too many args", async () => {
-      await expect(
-        expect(contract.emitOne()).to.emit(contract, "One").withArgs(1, 2, 3, 4)
-      ).to.be.eventually.rejectedWith(
-        AssertionError,
-        'Expected "One" event to have 4 argument(s), but it has 3'
-      );
-    });
-
-    it("Event with one different arg (integer)", async () => {
-      await expect(
-        expect(contract.emitOne())
-          .to.emit(contract, "One")
-          .withArgs(
-            2,
-            "One",
-            "0x00cfbbaf7ddb3a1476767101c12a0162e241fbad2a0162e2410cfbbaf7162123"
-          )
-      ).to.be.eventually.rejectedWith(AssertionError, "expected 1 to equal 2");
-    });
-
-    it("Event with one different arg (string)", async () => {
-      await expect(
-        expect(contract.emitOne())
-          .to.emit(contract, "One")
-          .withArgs(
-            1,
-            "Two",
-            "0x00cfbbaf7ddb3a1476767101c12a0162e241fbad2a0162e2410cfbbaf7162123"
-          )
-      ).to.be.eventually.rejectedWith(
-        AssertionError,
-        "expected 'One' to equal 'Two'"
-      );
-    });
-
-    it("Event with one different arg (string) #2", async () => {
-      await expect(
-        expect(contract.emitOne())
-          .to.emit(contract, "One")
-          .withArgs(
-            1,
-            "One",
-            "0x00cfbbaf7ddb3a1476767101c12a0162e241fbad2a0162e2410cfbbaf7162124"
-          )
-      ).to.be.eventually.rejectedWith(
-        AssertionError,
-        "expected '0x00cfbbaf7ddb3a1476767101c12a0162e241fbad2a0162e2410cfbbaf7162123' " +
-          "to equal '0x00cfbbaf7ddb3a1476767101c12a0162e241fbad2a0162e2410cfbbaf7162124'"
       );
     });
 
--- packages/hardhat-chai-matchers/test/fixture-projects/hardhat-project/contracts/Events.sol
+++ packages/hardhat-chai-matchers/test/fixture-projects/hardhat-project/contracts/Events.sol
@@ -3,8 +3,12 @@ pragma solidity ^0.8.0;
 
 contract Events {
   event WithoutArgs();
+  event WithUintArg(uint u);
+  event WithStringArg(string s);
+  event WithIndexedStringArg(string indexed s);
+  event WithBytesArg(bytes b);
+  event WithIndexedBytesArg(bytes indexed b);
   event One(uint value, string msg, bytes32 encoded);
-  event Two(uint indexed value, string msg);
   event Index(string indexed msgHashed, string msg, bytes bmsg, bytes indexed bmsgHash, bytes32 indexed encoded);
   event Arrays(uint256[3] value, bytes32[2] encoded);
 
@@ -24,10 +28,6 @@ contract Events {
           bytes("Three"),
           bytes("Three"),
           0x00cFBbaF7DDB3a1476767101c12a0162e241fbAD2a0162e2410cFBbaF7162123);
-  }
-
-  function emitTwo() public {
-    emit Two(2, "Two");
   }
 
   function emitBoth() public {
@@ -62,5 +62,25 @@ contract Events {
 
   function emitWithoutArgs() public {
     emit WithoutArgs();
+  }
+
+  function emitUint(uint u) public {
+    emit WithUintArg(u);
+  }
+
+  function emitString(string memory s) public {
+    emit WithStringArg(s);
+  }
+
+  function emitIndexedString(string memory s) public {
+    emit WithIndexedStringArg(s);
+  }
+
+  function emitBytes(bytes memory b) public {
+    emit WithBytesArg(b);
+  }
+
+  function emitIndexedBytes(bytes memory b) public {
+    emit WithIndexedBytesArg(b);
   }
 }
