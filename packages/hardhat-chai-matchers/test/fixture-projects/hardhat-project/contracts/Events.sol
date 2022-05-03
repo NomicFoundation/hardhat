d456035ddbbfcf25b7a1e648159a339ee3fc29a1
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-05-03 09:33:10 -0600

    Fix support for (and test) withArgs(:bytes32)

--- packages/hardhat-chai-matchers/test/fixture-projects/hardhat-project/contracts/Events.sol
+++ packages/hardhat-chai-matchers/test/fixture-projects/hardhat-project/contracts/Events.sol
@@ -8,9 +8,10 @@ contract Events {
   event WithIndexedStringArg(string indexed s);
   event WithBytesArg(bytes b);
   event WithIndexedBytesArg(bytes indexed b);
+  event WithBytes32Arg(bytes32 b);
+  event WithIndexedBytes32Arg(bytes32 indexed b);
   event One(uint value, string msg, bytes32 encoded);
   event Two(uint indexed value, string msg);
-  event Index(string indexed msgHashed, string msg, bytes bmsg, bytes indexed bmsgHash, bytes32 indexed encoded);
   event Arrays(uint256[3] value, bytes32[2] encoded);
 
   function emitOne() public {
@@ -21,14 +22,6 @@ contract Events {
       emit One(1, "One", 0x00cFBbaF7DDB3a1476767101c12a0162e241fbAD2a0162e2410cFBbaF7162123);
       emit One(1, "One", 0x00cFBbaF7DDB3a1476767101c12a0162e241fbAD2a0162e2410cFBbaF7162123);
       emit One(1, "DifferentKindOfOne", 0x0000000000000000000000000000000000000000000000000000000000000001);
-  }
-
-  function emitIndex() public {
-    emit Index("Three",
-          "Three",
-          bytes("Three"),
-          bytes("Three"),
-          0x00cFBbaF7DDB3a1476767101c12a0162e241fbAD2a0162e2410cFBbaF7162123);
   }
 
   function emitTwo() public {
@@ -85,6 +78,14 @@ contract Events {
 
   function emitIndexedBytes(bytes memory b) public {
     emit WithIndexedBytesArg(b);
+  }
+
+  function emitBytes32(bytes32 b) public {
+    emit WithBytes32Arg(b);
+  }
+
+  function emitIndexedBytes32(bytes32 b) public {
+    emit WithIndexedBytes32Arg(b);
   }
 
   function emitUintAndString(uint u, string memory s) public {
