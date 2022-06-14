244f299f306047d2cb84e7c9999794fc59dd46bb
Patricio Palladino <email@patriciopalladino.com>
2022-06-14 16:02:39 -0300

    Remove TOOLS block

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -8,7 +8,7 @@ import TrustedTeamsBlock from "../components/landingBlocks/TrustedTeamsBlock";
 import BuiltByBlock from "../components/landingBlocks/BuiltByBlock";
 import ReviewsBlock from "../components/landingBlocks/ReviewsBlock";
 // TODO: Re-enable tools section
-import ToolsBlock from "../components/landingBlocks/ToolsBlock";
+//import ToolsBlock from "../components/landingBlocks/ToolsBlock";
 import homepageContent from "../content/home";
 import LandingLayout from "../components/LandingLayout";
 
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
       <HeroBlock content={homepageContent.heroBlockContent} />
 
       {/* // TODO: Re-enable tools section */}
-      <ToolsBlock content={homepageContent.toolsBlockContent} />
+      {/*<ToolsBlock content={homepageContent.toolsBlockContent} />*/}
 
       <WhyHardhatBlock content={homepageContent.whyHardhatContent}>
         <FeatureCard
