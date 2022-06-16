570f0f03a69d57d53ad73576fc94c27535bf09ec
Patricio Palladino <email@patriciopalladino.com>
2022-06-16 10:33:56 -0300

    Fix linter errors

--- docs/src/pages/docs/index.tsx
+++ docs/src/pages/docs/index.tsx
@@ -1,8 +1,6 @@
 import type { GetStaticProps, NextPage } from "next";
-import { styled } from "linaria/react";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 import PluginsLayout from "../../components/PluginsLayout";
-import { media, tm, tmDark, tmSelectors } from "../../themes";
 import { components } from "../../components/DocumentationLayout";
 import {
   prepareMdContent,
@@ -13,27 +11,6 @@ import { DOCS_LANDING_PATH } from "../../config";
 interface IDocsPage {
   mdxSource: MDXRemoteSerializeResult;
 }
-
-const PageTitle = styled.h3`
-  margin-top: 28px;
-  font-size: 42px;
-  font-weight: 700;
-  line-height: 45px;
-  letter-spacing: 0.5px;
-  padding-bottom: 24px;
-  border-bottom: 1px solid ${tm(({ colors }) => colors.border)};
-  ${tmSelectors.dark} {
-    border-bottom-color: ${tmDark(({ colors }) => colors.border)};
-    color: ${tmDark(({ colors }) => colors.neutral800)};
-  }
-
-  ${media.mqDark} {
-    ${tmSelectors.auto} {
-      border-bottom-color: ${tmDark(({ colors }) => colors.border)};
-      color: ${tmDark(({ colors }) => colors.neutral800)};
-    }
-  }
-`;
 
 const Docs: NextPage<IDocsPage> = ({ mdxSource }) => {
   return (
