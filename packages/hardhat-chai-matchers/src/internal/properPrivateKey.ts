b773f083c7164c3607ce84b88349c29a9094cb88
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2022-07-04 18:06:53 -0400

    properPrivateKey msg: use clearly bogus PK
    Addresses review comment https://www.notion.so/Hardhat-Test-Utils-Error-Messages-Copy-Review-a43c4461c9d64861bd5759149a1d6298?d=6aad2a19ea6545e7b21ee343ac991966#5eb12ba0afc54898b2e1622da5f49881

--- packages/hardhat-chai-matchers/src/internal/properPrivateKey.ts
+++ packages/hardhat-chai-matchers/src/internal/properPrivateKey.ts
@@ -5,7 +5,7 @@ export function supportProperPrivateKey(Assertion: Chai.AssertionStatic) {
       /^0x[0-9-a-fA-F]{64}$/.test(subject),
       `Expected "${subject}" to be a proper private key`,
       `Expected "${subject}" not to be a proper private key`,
-      "proper private key (eg.: 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80)",
+      "proper private key (eg.: 0x1010101010101010101010101010101010101010101010101010101010101010)",
       subject
     );
   });
