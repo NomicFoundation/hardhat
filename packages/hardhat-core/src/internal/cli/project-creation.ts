1e247d51da044019a357e0ba4b5e7d7c29becaca
Nathan Bogan <bogannathan@gmail.com>
2022-07-13 17:16:56 -0700

    updated peer dependencies for typescript sample project creation

--- packages/hardhat-core/src/internal/cli/project-creation.ts
+++ packages/hardhat-core/src/internal/cli/project-creation.ts
@@ -43,24 +43,24 @@ const PEER_DEPENDENCIES: Dependencies = {
   hardhat: "^2.9.9",
   "@nomicfoundation/hardhat-network-helpers": "^1.0.0",
   "@nomicfoundation/hardhat-chai-matchers": "^1.0.0",
-  "@nomiclabs/hardhat-ethers": "^2.0.0",
-  "@nomiclabs/hardhat-etherscan": "^3.0.0",
-  chai: "^4.2.0",
-  ethers: "^5.4.7",
+  "@nomiclabs/hardhat-ethers": "^2.1.0",
+  "@nomiclabs/hardhat-etherscan": "^3.1.0",
+  chai: "^4.3.6",
+  ethers: "^5.6.9",
   "hardhat-gas-reporter": "^1.0.8",
   "solidity-coverage": "^0.7.21",
   "@typechain/hardhat": "^6.1.2",
   typechain: "^8.1.0",
   "@typechain/ethers-v5": "^10.1.0",
-  "@ethersproject/abi": "^5.4.7",
-  "@ethersproject/providers": "^5.4.7",
+  "@ethersproject/abi": "^5.6.4",
+  "@ethersproject/providers": "^5.6.8",
 };
 
 const TYPESCRIPT_DEPENDENCIES: Dependencies = {};
 
 const TYPESCRIPT_PEER_DEPENDENCIES: Dependencies = {
-  "@types/chai": "^4.2.0",
-  "@types/mocha": "^9.1.0",
+  "@types/chai": "^4.3.1",
+  "@types/mocha": "^9.1.1",
   "@types/node": ">=12.0.0",
   "ts-node": ">=8.0.0",
   typescript: ">=4.5.0",
