a38f1807686e2192996fc16a6ac9256e9747447b
Franco Victorio <victorio.franco@gmail.com>
2022-07-18 12:23:33 +0200

    Show unlock timestamp in sample project deploy scripts

--- packages/hardhat-core/sample-projects/typescript/scripts/deploy.ts
+++ packages/hardhat-core/sample-projects/typescript/scripts/deploy.ts
@@ -12,7 +12,7 @@ async function main() {
 
   await lock.deployed();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
