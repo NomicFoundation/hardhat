fabc8dd42eb67dd20ae1fe9298e1cb4512ffef35
Patricio Palladino <email@patriciopalladino.com>
2022-07-21 17:47:20 -0300

    Don't import hardhat/console.sol in the sample projects

--- packages/hardhat-core/sample-projects/typescript/contracts/Lock.sol
+++ packages/hardhat-core/sample-projects/typescript/contracts/Lock.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 // Import this file to use console.log
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 contract Lock {
     uint public unlockTime;
@@ -21,7 +21,7 @@ contract Lock {
     }
 
     function withdraw() public {
-        // Uncomment this line to print a log in your terminal
+        // Uncomment this line and the import to "hardhat/console.sol" to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
