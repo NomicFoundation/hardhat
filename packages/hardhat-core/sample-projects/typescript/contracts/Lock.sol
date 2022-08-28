8f48bc42b782cdb14df416e59e3d92b3f2c567a3
Patricio Palladino <email@patriciopalladino.com>
2022-08-28 11:26:38 -0300

    Improve sample project comments

--- packages/hardhat-core/sample-projects/typescript/contracts/Lock.sol
+++ packages/hardhat-core/sample-projects/typescript/contracts/Lock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
+// Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
 contract Lock {
@@ -21,7 +21,7 @@ contract Lock {
     }
 
     function withdraw() public {
-        // Uncomment this line and the import to "hardhat/console.sol" to print a log in your terminal
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
         // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
