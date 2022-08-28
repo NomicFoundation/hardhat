09711980411deffc9c0a8ed3145b96cb9dee4d11
Patricio Palladino <email@patriciopalladino.com>
2022-08-28 11:25:49 -0300

    Update packages/hardhat-core/sample-projects/javascript/contracts/Lock.sol
    Co-authored-by: Franco Victorio <victorio.franco@gmail.com>

--- packages/hardhat-core/sample-projects/javascript/contracts/Lock.sol
+++ packages/hardhat-core/sample-projects/javascript/contracts/Lock.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-// Import this file to use console.log
+// Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
 contract Lock {
