9fd0770a56a11d97e915e47a71705e33be4ab827
Patricio Palladino <email@patriciopalladino.com>
2022-08-28 17:01:45 -0300

    Redo the fs-utils module

--- packages/hardhat-core/src/internal/util/fs-utils.ts
+++ packages/hardhat-core/src/internal/util/fs-utils.ts
@@ -7,23 +7,31 @@ import { CustomError } from "../core/errors";
 // fs apis, as sometimes their errors don't have stack traces.
 export class FileSystemAccessError extends CustomError {}
 
-export class FileNotFoundError extends CustomError {}
-export class InvalidDirectoryError extends CustomError {}
+export class FileNotFoundError extends CustomError {
+  constructor(filePath: string, parent?: Error) {
+    super(`File ${filePath} not found`, parent);
+  }
+}
+export class InvalidDirectoryError extends CustomError {
+  constructor(filePath: string, parent: Error) {
+    super(`Invalid directory ${filePath}`, parent);
+  }
+}
 
 /**
- * Returns the real-case version of an absolute path.
+ * Returns the real path of absolutePath, resolving symlinks.
  *
  * @throws FileNotFoundError if absolutePath doesn't exist.
  */
-export async function getRealCase(absolutePath: string): Promise<string> {
+export async function getRealPath(absolutePath: string): Promise<string> {
   try {
     // This method returns the actual casing.
     // Please read Node.js' docs to learn more.
-    return await fsPromises.realpath(absolutePath);
+    return await fsPromises.realpath(path.normalize(absolutePath));
   } catch (e: any) {
     if (e.code === "ENOENT") {
       // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
-      throw new FileNotFoundError(`File ${absolutePath} not found`, e);
+      throw new FileNotFoundError(absolutePath, e);
     }
 
     // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
@@ -32,38 +40,19 @@ export async function getRealCase(absolutePath: string): Promise<string> {
 }
 
 /**
- * Sync version of getRealCase
+ * Sync version of getRealPath
  *
  * @see getRealCase
  */
-export function getRealCaseSync(absolutePath: string): string {
+export function getRealPathSync(absolutePath: string): string {
   try {
     // This method returns the actual casing.
     // Please read Node.js' docs to learn more.
-    return fs.realpathSync.native(absolutePath);
+    return fs.realpathSync.native(path.normalize(absolutePath));
   } catch (e: any) {
     if (e.code === "ENOENT") {
       // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
-      throw new FileNotFoundError(`File ${absolutePath} not found`, e);
-    }
-
-    // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
-    throw new FileSystemAccessError(e.message, e);
-  }
-}
-
-/**
- * Returns true if absolutePath exists and it has the same casing in the FS.
- */
-export async function fileExistsWithExactCasing(
-  absolutePath: string
-): Promise<boolean> {
-  try {
-    const realpath = await getRealCase(absolutePath);
-    return realpath === absolutePath;
-  } catch (e: any) {
-    if (e instanceof FileNotFoundError) {
-      return false;
+      throw new FileNotFoundError(absolutePath, e);
     }
 
     // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
@@ -76,29 +65,15 @@ export async function fileExistsWithExactCasing(
  *
  * @param absolutePathToDir A directory. If it doesn't exist `[]` is returned.
  * @param matches A function to filter files (not directories)
+ * @returns An An array of absolute paths. Each file has its true case, except
+ *  for the initial absolutePathToDir part, which preserves the given casing.
+ *  No order is guaranteed.
  */
 export async function getAllFilesMatching(
   absolutePathToDir: string,
   matches?: (absolutePathToFile: string) => boolean
 ): Promise<string[]> {
-  let dir;
-  try {
-    dir = await fsPromises.readdir(absolutePathToDir);
-  } catch (e: any) {
-    if (e.code === "ENOENT") {
-      return [];
-    }
-
-    if (e.code === "ENOTDIR") {
-      // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
-      throw new InvalidDirectoryError(
-        `Expected ${absolutePathToDir} to be a directory but it isn't`
-      );
-    }
-
-    // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
-    throw new FileSystemAccessError(e.message, e);
-  }
+  const dir = await readdir(absolutePathToDir);
 
   const results = await Promise.all(
     dir.map(async (file) => {
@@ -127,24 +102,7 @@ export function getAllFilesMatchingSync(
   absolutePathToDir: string,
   matches?: (absolutePathToFile: string) => boolean
 ): string[] {
-  let dir: string[];
-  try {
-    dir = fs.readdirSync(absolutePathToDir);
-  } catch (e: any) {
-    if (e.code === "ENOENT") {
-      return [];
-    }
-
-    if (e.code === "ENOTDIR") {
-      // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
-      throw new InvalidDirectoryError(
-        `Expected ${absolutePathToDir} to be a directory but it isn't`
-      );
-    }
-
-    // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
-    throw new FileSystemAccessError(e.message, e);
-  }
+  const dir = readdirSync(absolutePathToDir);
 
   const results = dir.map((file) => {
     const absolutePathToFile = path.join(absolutePathToDir, file);
@@ -159,4 +117,107 @@ export function getAllFilesMatchingSync(
   });
 
   return results.flat();
+}
+
+/**
+ * Returns the true case relative path of `relativePath` from `from`, without
+ * resolving symlinks.
+ */
+export async function getFileTrueCase(
+  from: string,
+  relativePath: string
+): Promise<string> {
+  const dirEntries = await readdir(from);
+
+  const parts = relativePath.split(path.sep);
+  const nextDirLowerCase = parts[0].toLowerCase();
+
+  for (const dirEntry of dirEntries) {
+    if (dirEntry.toLowerCase() === nextDirLowerCase) {
+      if (parts.length === 1) {
+        return dirEntry;
+      }
+
+      return path.join(
+        dirEntry,
+        await getFileTrueCase(
+          path.join(from, dirEntry),
+          path.relative(parts[0], relativePath)
+        )
+      );
+    }
+  }
+
+  // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
+  throw new FileNotFoundError(path.join(from, relativePath));
+}
+
+/**
+ * Sync version of getFileTrueCase
+ *
+ * @see getFileTrueCase
+ */
+export function getFileTrueCaseSync(
+  from: string,
+  relativePath: string
+): string {
+  const dirEntries = readdirSync(from);
+
+  const parts = relativePath.split(path.sep);
+  const nextDirLowerCase = parts[0].toLowerCase();
+
+  for (const dirEntry of dirEntries) {
+    if (dirEntry.toLowerCase() === nextDirLowerCase) {
+      if (parts.length === 1) {
+        return dirEntry;
+      }
+
+      return path.join(
+        dirEntry,
+        getFileTrueCaseSync(
+          path.join(from, dirEntry),
+          path.relative(parts[0], relativePath)
+        )
+      );
+    }
+  }
+
+  // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
+  throw new FileNotFoundError(path.join(from, relativePath));
+}
+
+async function readdir(absolutePathToDir: string) {
+  try {
+    return await fsPromises.readdir(absolutePathToDir);
+  } catch (e: any) {
+    if (e.code === "ENOENT") {
+      return [];
+    }
+
+    if (e.code === "ENOTDIR") {
+      // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
+      throw new InvalidDirectoryError(absolutePathToDir, e);
+    }
+
+    // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
+    throw new FileSystemAccessError(e.message, e);
+  }
+}
+
+function readdirSync(absolutePathToDir: string) {
+  try {
+    return fs.readdirSync(absolutePathToDir);
+  } catch (e: any) {
+    if (e.code === "ENOENT") {
+      return [];
+    }
+
+    if (e.code === "ENOTDIR") {
+      // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
+      throw new InvalidDirectoryError(absolutePathToDir, e);
+    }
+
+    // eslint-disable-next-line @nomiclabs/hardhat-internal-rules/only-hardhat-error
+    throw new FileSystemAccessError(e.message, e);
+  }
 }
