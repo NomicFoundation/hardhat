da96303b9fefeab5bd2c1e382a6faf44edbb770f
Franco Victorio <victorio.franco@gmail.com>
2022-08-31 00:46:04 +0200

    Use glob if getAllFilesMatching is undefined

--- packages/hardhat-truffle4/src/glob.ts
+++ packages/hardhat-truffle4/src/glob.ts
@@ -6,14 +6,24 @@ export async function getSolidityFiles(directory: string): Promise<string[]> {
       "hardhat/internal/util/fs-utils"
     );
 
+    if (getAllFilesMatching === undefined) {
+      // we don't want to catch errors from this function
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return getSolidityFilesUsingGlob(directory);
+    }
+
     return await getAllFilesMatching(directory, (f) => f.endsWith(".sol"));
   } catch (e: any) {
     if (e.code === "MODULE_NOT_FOUND") {
-      const { glob } = await import("hardhat/internal/util/glob");
-
-      return glob(join(directory, "**", "*.sol"));
+      return getSolidityFilesUsingGlob(directory);
     }
 
     throw e;
   }
+}
+
+async function getSolidityFilesUsingGlob(directory: string): Promise<string[]> {
+  const { glob } = await import("hardhat/internal/util/glob");
+
+  return glob(join(directory, "**", "*.sol"));
 }
--- packages/hardhat-vyper/src/glob.ts
+++ packages/hardhat-vyper/src/glob.ts
@@ -9,16 +9,29 @@ export async function getFilesWithExtension(
       "hardhat/internal/util/fs-utils"
     );
 
+    if (getAllFilesMatching === undefined) {
+      // we don't want to catch errors from this function
+      // eslint-disable-next-line @typescript-eslint/return-await
+      return getFilesWithExtensionUsingGlob(directory, extension);
+    }
+
     return await getAllFilesMatching(directory, (f) =>
       f.endsWith(`.${extension}`)
     );
   } catch (e: any) {
     if (e.code === "MODULE_NOT_FOUND") {
-      const { glob } = await import("hardhat/internal/util/glob");
-
-      return glob(join(directory, "**", `*.${extension}`));
+      return getFilesWithExtensionUsingGlob(directory, extension);
     }
 
     throw e;
   }
+}
+
+async function getFilesWithExtensionUsingGlob(
+  directory: string,
+  extension: string
+): Promise<string[]> {
+  const { glob } = await import("hardhat/internal/util/glob");
+
+  return glob(join(directory, "**", `*.${extension}`));
 }
