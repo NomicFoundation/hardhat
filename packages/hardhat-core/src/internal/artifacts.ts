3db50d5bf50cd251261227e1ba7ee0c1020ce894
Patricio Palladino <email@patriciopalladino.com>
2022-08-30 09:58:26 -0300

    Run internal clearCache calls in finally clauses

--- packages/hardhat-core/src/internal/artifacts.ts
+++ packages/hardhat-core/src/internal/artifacts.ts
@@ -181,37 +181,42 @@ export class Artifacts implements IArtifacts {
     artifact: Artifact,
     pathToBuildInfo?: string
   ) {
-    // artifact
-    const fullyQualifiedName = getFullyQualifiedName(
-      artifact.sourceName,
-      artifact.contractName
-    );
+    try {
+      // artifact
+      const fullyQualifiedName = getFullyQualifiedName(
+        artifact.sourceName,
+        artifact.contractName
+      );
 
-    const artifactPath =
-      this.formArtifactPathFromFullyQualifiedName(fullyQualifiedName);
+      const artifactPath =
+        this.formArtifactPathFromFullyQualifiedName(fullyQualifiedName);
 
-    await fsExtra.ensureDir(path.dirname(artifactPath));
+      await fsExtra.ensureDir(path.dirname(artifactPath));
 
-    await Promise.all([
-      fsExtra.writeJSON(artifactPath, artifact, {
-        spaces: 2,
-      }),
-      (async () => {
-        if (pathToBuildInfo === undefined) {
-          return;
-        }
-
-        // save debug file
-        const debugFilePath = this._getDebugFilePath(artifactPath);
-        const debugFile = this._createDebugFile(artifactPath, pathToBuildInfo);
-
-        await fsExtra.writeJSON(debugFilePath, debugFile, {
+      await Promise.all([
+        fsExtra.writeJSON(artifactPath, artifact, {
           spaces: 2,
-        });
-      })(),
-    ]);
+        }),
+        (async () => {
+          if (pathToBuildInfo === undefined) {
+            return;
+          }
 
-    this.clearCache();
+          // save debug file
+          const debugFilePath = this._getDebugFilePath(artifactPath);
+          const debugFile = this._createDebugFile(
+            artifactPath,
+            pathToBuildInfo
+          );
+
+          await fsExtra.writeJSON(debugFilePath, debugFile, {
+            spaces: 2,
+          });
+        })(),
+      ]);
+    } finally {
+      this.clearCache();
+    }
   }
 
   public async saveBuildInfo(
@@ -220,112 +225,114 @@ export class Artifacts implements IArtifacts {
     input: CompilerInput,
     output: CompilerOutput
   ): Promise<string> {
-    const buildInfoDir = path.join(this._artifactsPath, BUILD_INFO_DIR_NAME);
-    await fsExtra.ensureDir(buildInfoDir);
-
-    const buildInfoName = this._getBuildInfoName(
-      solcVersion,
-      solcLongVersion,
-      input
-    );
-
-    const buildInfo = this._createBuildInfo(
-      buildInfoName,
-      solcVersion,
-      solcLongVersion,
-      input,
-      output
-    );
-
-    const buildInfoPath = path.join(buildInfoDir, `${buildInfoName}.json`);
-
-    // JSON.stringify of the entire build info can be really slow
-    // in larger projects, so we stringify per part and incrementally create
-    // the JSON in the file.
-    //
-    // We split this code into different curly-brace-enclosed scopes so that
-    // partial JSON strings get out of scope sooner and hence can be reclaimed
-    // by the GC if needed.
-    const file = await fsPromises.open(buildInfoPath, "w");
     try {
-      {
-        const withoutOutput = JSON.stringify({
-          ...buildInfo,
-          output: undefined,
-        });
+      const buildInfoDir = path.join(this._artifactsPath, BUILD_INFO_DIR_NAME);
+      await fsExtra.ensureDir(buildInfoDir);
 
-        // We write the JSON (without output) except the last }
-        await file.write(withoutOutput.slice(0, -1));
-      }
+      const buildInfoName = this._getBuildInfoName(
+        solcVersion,
+        solcLongVersion,
+        input
+      );
 
-      {
-        const outputWithoutSourcesAndContracts = JSON.stringify({
-          ...buildInfo.output,
-          sources: undefined,
-          contracts: undefined,
-        });
+      const buildInfo = this._createBuildInfo(
+        buildInfoName,
+        solcVersion,
+        solcLongVersion,
+        input,
+        output
+      );
 
-        // We start writing the output
-        await file.write(',"output":');
+      const buildInfoPath = path.join(buildInfoDir, `${buildInfoName}.json`);
 
-        // Write the output object except for the last }
-        await file.write(outputWithoutSourcesAndContracts.slice(0, -1));
+      // JSON.stringify of the entire build info can be really slow
+      // in larger projects, so we stringify per part and incrementally create
+      // the JSON in the file.
+      //
+      // We split this code into different curly-brace-enclosed scopes so that
+      // partial JSON strings get out of scope sooner and hence can be reclaimed
+      // by the GC if needed.
+      const file = await fsPromises.open(buildInfoPath, "w");
+      try {
+        {
+          const withoutOutput = JSON.stringify({
+            ...buildInfo,
+            output: undefined,
+          });
 
-        // If there were other field apart from sources and contracts we need
-        // a comma
-        if (outputWithoutSourcesAndContracts.length > 2) {
-          await file.write(",");
-        }
-      }
-
-      // Writing the sources
-      await file.write('"sources":{');
-
-      let isFirst = true;
-      for (const [name, value] of Object.entries(
-        buildInfo.output.sources ?? {}
-      )) {
-        if (isFirst) {
-          isFirst = false;
-        } else {
-          await file.write(",");
+          // We write the JSON (without output) except the last }
+          await file.write(withoutOutput.slice(0, -1));
         }
 
-        await file.write(`${JSON.stringify(name)}:${JSON.stringify(value)}`);
-      }
+        {
+          const outputWithoutSourcesAndContracts = JSON.stringify({
+            ...buildInfo.output,
+            sources: undefined,
+            contracts: undefined,
+          });
 
-      // Close sources object
-      await file.write("}");
+          // We start writing the output
+          await file.write(',"output":');
 
-      // Writing the contracts
-      await file.write(',"contracts":{');
+          // Write the output object except for the last }
+          await file.write(outputWithoutSourcesAndContracts.slice(0, -1));
 
-      isFirst = true;
-      for (const [name, value] of Object.entries(
-        buildInfo.output.contracts ?? {}
-      )) {
-        if (isFirst) {
-          isFirst = false;
-        } else {
-          await file.write(",");
+          // If there were other field apart from sources and contracts we need
+          // a comma
+          if (outputWithoutSourcesAndContracts.length > 2) {
+            await file.write(",");
+          }
         }
 
-        await file.write(`${JSON.stringify(name)}:${JSON.stringify(value)}`);
+        // Writing the sources
+        await file.write('"sources":{');
+
+        let isFirst = true;
+        for (const [name, value] of Object.entries(
+          buildInfo.output.sources ?? {}
+        )) {
+          if (isFirst) {
+            isFirst = false;
+          } else {
+            await file.write(",");
+          }
+
+          await file.write(`${JSON.stringify(name)}:${JSON.stringify(value)}`);
+        }
+
+        // Close sources object
+        await file.write("}");
+
+        // Writing the contracts
+        await file.write(',"contracts":{');
+
+        isFirst = true;
+        for (const [name, value] of Object.entries(
+          buildInfo.output.contracts ?? {}
+        )) {
+          if (isFirst) {
+            isFirst = false;
+          } else {
+            await file.write(",");
+          }
+
+          await file.write(`${JSON.stringify(name)}:${JSON.stringify(value)}`);
+        }
+
+        // close contracts object
+        await file.write("}");
+        // close output object
+        await file.write("}");
+        // close build info object
+        await file.write("}");
+      } finally {
+        await file.close();
       }
 
-      // close contracts object
-      await file.write("}");
-      // close output object
-      await file.write("}");
-      // close build info object
-      await file.write("}");
+      return buildInfoPath;
     } finally {
-      await file.close();
+      this.clearCache();
     }
-
-    this.clearCache();
-
-    return buildInfoPath;
   }
 
   /**
