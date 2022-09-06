ee745ebeb53a8eb4d585cd572df274f13ed2f36c
Simon Emanuel Schmid <simon@edgeandnode.com>
2022-09-06 13:42:52 +0200

    Fix build

--- docs/src/content/hardhat-runner/plugins/plugins.ts
+++ docs/src/content/hardhat-runner/plugins/plugins.ts
@@ -514,7 +514,7 @@ const communityPlugins: IPlugin[] = [
     authorUrl: "https://thegraph.com",
     description:
       "Develop your subgraph side by side with your contracts to save gas and increase productivity.",
-    tags: ["graphprotocol",·"subgraph",·"graphql",·"development",·"deployment"],
+    tags: ["graphprotocol", "subgraph", "graphql", "development", "deployment"],
   },
 ];
 
