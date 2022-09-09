NomicFoundation/hardhat

commit bd2bbc0ef318e66852f80136fb5d554c86edb018

Make `httpHeaders` non-optional in resolved config

packages/hardhat-core/src/types/config.ts
@@ -177,7 +177,7 @@ export interface HardhatNetworkForkingConfig {
   enabled: boolean;
   url: string;
   blockNumber?: number;
-  httpHeaders?: { [name: string]: string };
+  httpHeaders: { [name: string]: string };
 }
 
 export interface HttpNetworkConfig {

packages/hardhat-core/src/internal/core/config/config-resolution.ts
@@ -150,6 +150,7 @@ function resolveHardhatNetworkConfig(
       ? {
           url: hardhatNetworkConfig.forking.url,
           enabled: hardhatNetworkConfig.forking.enabled ?? true,
+          httpHeaders: {},
         }
       : undefined;
 
@@ -159,9 +160,11 @@ function resolveHardhatNetworkConfig(
       forking.blockNumber = hardhatNetworkConfig?.forking?.blockNumber;
     }
   }
-  const httpHeaders = hardhatNetworkConfig?.forking?.httpHeaders;
-  if (httpHeaders !== undefined && forking !== undefined) {
-    forking.httpHeaders = hardhatNetworkConfig?.forking?.httpHeaders;
+  if (
+    forking !== undefined &&
+    hardhatNetworkConfig.forking?.httpHeaders !== undefined
+  ) {
+    forking.httpHeaders = hardhatNetworkConfig.forking.httpHeaders;
   }
 
   const mining = resolveMiningConfig(hardhatNetworkConfig.mining);

packages/hardhat-core/src/internal/core/jsonrpc/types/input/hardhat-network.ts
@@ -8,6 +8,7 @@ export const rpcForkConfig = optional(
     {
       jsonRpcUrl: t.string,
       blockNumber: optional(t.number),
+      httpHeaders: optional(t.record(t.string, t.string, "httpHeaders")),
     },
     "RpcForkConfig"
   )

packages/hardhat-core/src/internal/core/providers/construction.ts
@@ -73,6 +73,7 @@ export function createProvider(
       forkConfig = {
         jsonRpcUrl: hardhatNetConfig.forking?.url,
         blockNumber: hardhatNetConfig.forking?.blockNumber,
+        httpHeaders: hardhatNetConfig.forking.httpHeaders,
       };
     }
 

packages/hardhat-core/test/internal/core/config/config-resolution.ts
@@ -395,6 +395,7 @@ describe("Config resolution", () => {
           assert.deepEqual(config.networks.hardhat.forking, {
             url: "asd",
             enabled: true,
+            httpHeaders: {},
           });
         });
 
@@ -413,6 +414,7 @@ describe("Config resolution", () => {
           assert.deepEqual(config.networks.hardhat.forking, {
             url: "asd",
             enabled: false,
+            httpHeaders: {},
           });
         });
 
@@ -432,6 +434,7 @@ describe("Config resolution", () => {
             url: "asd",
             enabled: true,
             blockNumber: 123,
+            httpHeaders: {},
           });
         });
       });

commit 025f429d1afa651de50b38dc915265f316319b6c

Fix breaking change introduced in bd2bbc0ef3

packages/hardhat-core/src/types/config.ts
@@ -175,7 +175,7 @@ export interface HardhatNetworkForkingConfig {
   enabled: boolean;
   url: string;
   blockNumber?: number;
-  httpHeaders: { [name: string]: string };
+  httpHeaders?: { [name: string]: string };
 }
 
 export interface HttpNetworkConfig {