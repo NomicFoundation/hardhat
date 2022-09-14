1365d403c89fdd10a49f15bc2ddd15dbb4c4bf93
Franco Victorio <victorio.franco@gmail.com>
2022-09-14 16:32:34 +0200

    Test that empty config uses latest solc version

--- packages/hardhat-core/test/sample-projects/solidity-version.ts
+++ packages/hardhat-core/test/sample-projects/solidity-version.ts
@@ -3,9 +3,40 @@ import fs from "fs";
 import path from "path";
 import semver from "semver";
 
+import { EMPTY_HARDHAT_CONFIG } from "../../src/internal/cli/project-creation";
 import { SUPPORTED_SOLIDITY_VERSION_RANGE } from "../../src/internal/hardhat-network/stack-traces/constants";
 
 describe("sample projects solidity versions", function () {
+  describe("empty hardhat config", function () {
+    it("should use the latest supported version", async function () {
+      const configContent = EMPTY_HARDHAT_CONFIG;
+
+      const solidityVersion = configContent.match(/solidity: "(.*)"/)![1];
+
+      if (typeof solidityVersion !== "string") {
+        assert.fail("solidity version should ve a string");
+      }
+
+      const nextPatch = semver.inc(solidityVersion, "patch");
+      const nextMinor = semver.inc(solidityVersion, "minor");
+      const nextMajor = semver.inc(solidityVersion, "major");
+
+      if (nextPatch === null || nextMinor === null || nextMajor === null) {
+        assert.fail("shouldn't happen");
+      }
+
+      assert.isFalse(
+        semver.satisfies(nextPatch, SUPPORTED_SOLIDITY_VERSION_RANGE)
+      );
+      assert.isFalse(
+        semver.satisfies(nextMinor, SUPPORTED_SOLIDITY_VERSION_RANGE)
+      );
+      assert.isFalse(
+        semver.satisfies(nextMajor, SUPPORTED_SOLIDITY_VERSION_RANGE)
+      );
+    });
+  });
+
   describe("javascript sample project", function () {
     it("should use the latest supported version", async function () {
       const pathToConfig = path.resolve(
