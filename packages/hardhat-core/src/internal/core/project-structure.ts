8db31929b1c8182eb6a2bbb3583461eb476f8e14
Franco Victorio <victorio.franco@gmail.com>
2022-09-27 11:24:06 +0200

    Detect .cts config files

--- packages/hardhat-core/src/internal/core/project-structure.ts
+++ packages/hardhat-core/src/internal/core/project-structure.ts
@@ -6,6 +6,7 @@ import { getPackageRoot } from "../util/packageInfo";
 
 import { HardhatError } from "./errors";
 import { ERRORS } from "./errors-list";
+
 const JS_CONFIG_FILENAME = "hardhat.config.js";
 const CJS_CONFIG_FILENAME = "hardhat.config.cjs";
 const TS_CONFIG_FILENAME = "hardhat.config.ts";
@@ -15,7 +16,8 @@ export function isCwdInsideProject() {
   return (
     findUp.sync(TS_CONFIG_FILENAME) !== null ||
     findUp.sync(CJS_CONFIG_FILENAME) !== null ||
-    findUp.sync(JS_CONFIG_FILENAME) !== null
+    findUp.sync(JS_CONFIG_FILENAME) !== null ||
+    findUp.sync(CTS_CONFIG_FILENAME) !== null
   );
 }
 
