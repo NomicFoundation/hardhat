4691806707d760b81a8d6bd86b7fa9a6657dfec7
Franco Victorio <victorio.franco@gmail.com>
2022-10-26 09:13:56 +0200

    Fix Goerli Optimism URLs

--- packages/hardhat-etherscan/src/ChainConfig.ts
+++ packages/hardhat-etherscan/src/ChainConfig.ts
@@ -96,8 +96,8 @@ export const chainConfig: ChainConfig = {
   optimisticGoerli: {
     chainId: 420,
     urls: {
-      apiURL: "https://api-goerli-optimistic.etherscan.io/api",
-      browserURL: "https://goerli-optimistic.etherscan.io/",
+      apiURL: "https://api-goerli-optimism.etherscan.io/api",
+      browserURL: "https://goerli-optimism.etherscan.io/",
     },
   },
   polygon: {
