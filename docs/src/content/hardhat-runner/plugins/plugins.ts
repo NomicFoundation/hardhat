ddc011c0ad6824ee251a34ed26a75489d3c627b6
Franco Victorio <victorio.franco@gmail.com>
2022-11-02 14:13:26 +0100

    Add hardhat-foundry to the list of plugins

--- docs/src/content/hardhat-runner/plugins/plugins.ts
+++ docs/src/content/hardhat-runner/plugins/plugins.ts
@@ -648,6 +648,13 @@ const officialPlugins: IPlugin[] = [
     tags: ["Etherscan", "Verification"],
   },
   {
+    name: "@nomicfoundation/hardhat-foundry",
+    author: "Nomic Foundation",
+    authorUrl: "https://twitter.com/NomicFoundation",
+    description: "Makes it easier to use Hardhat and Foundry in the same project",
+    tags: ["Foundry"],
+  },
+  {
     name: "@nomiclabs/hardhat-vyper",
     author: "Nomic Foundation",
     authorUrl: "https://twitter.com/NomicFoundation",
