d870880b7875aa003d17b41cdcaa8d1cfcc7489d
morgansliman <morgansliman@gmail.com>
2022-11-06 19:31:36 -0500

    added user validation for buildModule

---
+++ packages/core/test/buildModule.ts
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-unused-modules */
+import { assert } from "chai";
+
+import { buildModule } from "dsl/buildModule";
+
+describe("buildModule", () => {
+  const values = [
+    [42, "number"],
+    ["thanks for all the fish", "string"],
+    [{}, "object"],
+    [[], "object"],
+    [() => {}, "function"],
+    [undefined, "undefined"],
+    [null, "null"],
+  ];
+
+  describe("param validation", () => {
+    it("should only allow a string to be passed as `moduleName`", () => {
+      for (const [value, type] of values) {
+        if (type === "string") {
+          // @ts-ignore
+          assert.doesNotThrow(() => buildModule(value, () => {}));
+        } else {
+          assert.throws(
+            // @ts-ignore
+            () => buildModule(value, () => {}),
+            /buildModule: `moduleName` must be a string/
+          );
+        }
+      }
+    });
+
+    it("should only allow a function to be passed as `moduleAction`", () => {
+      for (const [value, type] of values) {
+        if (type === "function") {
+          // @ts-ignore
+          assert.doesNotThrow(() => buildModule("", value));
+        } else {
+          assert.throws(
+            // @ts-ignore
+            () => buildModule("", value),
+            /buildModule: `moduleAction` must be a function/
+          );
+        }
+      }
+    });
+  });
+});
