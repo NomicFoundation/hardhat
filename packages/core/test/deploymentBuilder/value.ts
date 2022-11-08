85a93ddb6585d600b1bbb462386b335c6b3053d4
morgansliman <morgansliman@gmail.com>
2022-11-08 12:45:48 -0500

    cleanup to address comments

--- packages/core/test/deploymentBuilder/value.ts
+++ packages/core/test/deploymentBuilder/value.ts
@@ -7,29 +7,11 @@ import { generateDeploymentGraphFrom } from "process/generateDeploymentGraphFrom
 import type {
   IDeploymentGraph,
   IDeploymentBuilder,
-  HardhatContractDeploymentVertex,
-  CallDeploymentVertex,
-  ArtifactContractDeploymentVertex,
 } from "types/deploymentGraph";
 import type { Artifact } from "types/hardhat";
+import { isArtifactContract, isHardhatContract, isCall } from "utils/guards";
 
 import { getDeploymentVertexByLabel } from "./helpers";
-
-function isHardhatContract(d: {
-  type: string;
-}): d is HardhatContractDeploymentVertex {
-  return d.type === "HardhatContract";
-}
-
-function isHardhatCall(d: { type: string }): d is CallDeploymentVertex {
-  return d.type === "Call";
-}
-
-function isHardhatArtifact(d: {
-  type: string;
-}): d is ArtifactContractDeploymentVertex {
-  return d.type === "ArtifactContract";
-}
 
 describe("deployment builder - value", () => {
   let deploymentGraph: IDeploymentGraph;
@@ -77,103 +59,110 @@ describe("deployment builder - value", () => {
     );
   });
 
-  it("should deploy with a given value - contract", () => {
-    const depNode = getDeploymentVertexByLabel(deploymentGraph, "Exchange");
+  describe("contract", () => {
+    it("should deploy with a given value", () => {
+      const depNode = getDeploymentVertexByLabel(deploymentGraph, "Exchange");
 
-    assert.isDefined(depNode);
-    if (!isHardhatContract(depNode)) {
-      return assert.fail("unknown error");
-    }
+      assert.isDefined(depNode);
+      if (!isHardhatContract(depNode)) {
+        return assert.fail("unknown error");
+      }
 
-    assert.equal(
-      depNode.value.toString(),
-      ethers.utils.parseUnits("42").toString()
-    );
-  });
-
-  it("should deploy with a given value - call", () => {
-    const depNode = getDeploymentVertexByLabel(
-      deploymentGraph,
-      "Exchange/addToken"
-    );
-
-    assert.isDefined(depNode);
-    if (!isHardhatCall(depNode)) {
-      return assert.fail("unknown error");
-    }
-
-    assert.equal(
-      depNode.value.toString(),
-      ethers.utils.parseUnits("10").toString()
-    );
-  });
-
-  it("should deploy with a given value - artifact", () => {
-    const depNode = getDeploymentVertexByLabel(deploymentGraph, "Foo");
-
-    assert.isDefined(depNode);
-    if (!isHardhatArtifact(depNode)) {
-      return assert.fail("unknown error");
-    }
-
-    assert.equal(
-      depNode.value.toString(),
-      ethers.utils.parseUnits("3").toString()
-    );
-  });
-
-  it("should throw if given a value that is not a BigNumber - contract", () => {
-    const callModule = buildModule("call", (m: IDeploymentBuilder) => {
-      // @ts-ignore
-      const token = m.contract("Token", { value: "42" });
-
-      return { token };
+      assert.equal(
+        depNode.value.toString(),
+        ethers.utils.parseUnits("42").toString()
+      );
     });
 
-    assert.throws(
-      () => generateDeploymentGraphFrom(callModule, { chainId: 31337 }),
-      /`value` must be a BigNumber/
-    );
-  });
-
-  it("should throw if given a value that is not a BigNumber - call", () => {
-    const callModule = buildModule("call", (m: IDeploymentBuilder) => {
-      const token = m.contract("Token");
-      const exchange = m.contract("Exchange");
-
-      m.call(exchange, "addToken", {
-        args: [token],
+    it("should throw if given a value that is not a BigNumber", () => {
+      const callModule = buildModule("call", (m: IDeploymentBuilder) => {
         // @ts-ignore
-        value: 10,
+        const token = m.contract("Token", { value: "42" });
+
+        return { token };
       });
 
-      return {};
+      assert.throws(
+        () => generateDeploymentGraphFrom(callModule, { chainId: 31337 }),
+        /`value` must be a BigNumber/
+      );
     });
-
-    assert.throws(
-      () => generateDeploymentGraphFrom(callModule, { chainId: 31337 }),
-      /`value` must be a BigNumber/
-    );
   });
 
-  it("should throw if given a value that is not a BigNumber - call", () => {
-    const artifact = { abi: [], bytecode: "xxx" } as any as Artifact;
+  describe("call", () => {
+    it("should deploy with a given value", () => {
+      const depNode = getDeploymentVertexByLabel(
+        deploymentGraph,
+        "Exchange/addToken"
+      );
 
-    const fromArtifactModule = buildModule(
-      "FromArtifact",
-      (m: IDeploymentBuilder) => {
-        const foo = m.contract("Foo", artifact, {
-          args: [0],
-          value: null,
+      assert.isDefined(depNode);
+      if (!isCall(depNode)) {
+        return assert.fail("unknown error");
+      }
+
+      assert.equal(
+        depNode.value.toString(),
+        ethers.utils.parseUnits("10").toString()
+      );
+    });
+
+    it("should throw if given a value that is not a BigNumber", () => {
+      const callModule = buildModule("call", (m: IDeploymentBuilder) => {
+        const token = m.contract("Token");
+        const exchange = m.contract("Exchange");
+
+        m.call(exchange, "addToken", {
+          args: [token],
+          // @ts-ignore
+          value: 10,
         });
 
-        return { foo };
-      }
-    );
+        return {};
+      });
 
-    assert.throws(
-      () => generateDeploymentGraphFrom(fromArtifactModule, { chainId: 31337 }),
-      /`value` must be a BigNumber/
-    );
+      assert.throws(
+        () => generateDeploymentGraphFrom(callModule, { chainId: 31337 }),
+        /`value` must be a BigNumber/
+      );
+    });
+  });
+
+  describe("artifact", () => {
+    it("should deploy with a given value", () => {
+      const depNode = getDeploymentVertexByLabel(deploymentGraph, "Foo");
+
+      assert.isDefined(depNode);
+      if (!isArtifactContract(depNode)) {
+        return assert.fail("unknown error");
+      }
+
+      assert.equal(
+        depNode.value.toString(),
+        ethers.utils.parseUnits("3").toString()
+      );
+    });
+
+    it("should throw if given a value that is not a BigNumber", () => {
+      const artifact = { abi: [], bytecode: "xxx" } as any as Artifact;
+
+      const fromArtifactModule = buildModule(
+        "FromArtifact",
+        (m: IDeploymentBuilder) => {
+          const foo = m.contract("Foo", artifact, {
+            args: [0],
+            value: null,
+          });
+
+          return { foo };
+        }
+      );
+
+      assert.throws(
+        () =>
+          generateDeploymentGraphFrom(fromArtifactModule, { chainId: 31337 }),
+        /`value` must be a BigNumber/
+      );
+    });
   });
 });
