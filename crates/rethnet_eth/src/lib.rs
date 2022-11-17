0ae706bcfd9a515e29c0c17c2a2a51adb8d68fe0
Wodann <Wodann@users.noreply.github.com>
2022-11-16 22:03:54 -0600

    feat: foundational Eth types

---
+++ crates/rethnet_eth/src/lib.rs
@@ -0,0 +1,32 @@
+// use ethers_core::{
+//     abi::ethereum_types::H64,
+//     types::{
+//         transaction::eip712::TypedData, Address, BlockId, BlockNumber, Bytes, Filter,
+//         GethDebugTracingOptions, TxHash, H256, U256,
+//     },
+// };
+
+pub mod access_list;
+pub mod block;
+// pub mod proof;
+pub mod receipt;
+pub mod signature;
+// pub mod state;
+// pub mod subscription;
+pub mod transaction;
+pub mod trie;
+pub mod utils;
+
+// #[cfg(feature = "serde")]
+// pub mod serde_helpers;
+
+pub use bytes::Bytes;
+pub use ethbloom::Bloom;
+pub use primitive_types::{H256, H512};
+pub use ruint::aliases::{B64, U256, U64};
+
+use primitive_types::H160;
+
+pub type Address = H160;
+pub type Secret = H256;
+pub type Public = H512;
