947a7a20a17c82f30bfc4a1af2c9d49aa8a125d2
delaaxe <1091900+delaaxe@users.noreply.github.com>
2022-11-22 20:35:08 +0200

    Use proper async/await syntax in sample code

--- packages/hardhat-core/sample-projects/typescript/scripts/deploy.ts
+++ packages/hardhat-core/sample-projects/typescript/scripts/deploy.ts
@@ -17,7 +17,11 @@ async function main() {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+(async () => {
+  try {
+    await main();
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1;
+  }
+}());
