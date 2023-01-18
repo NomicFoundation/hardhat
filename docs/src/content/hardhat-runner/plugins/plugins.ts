7bcdb6dbdca90f4609aa536a25315ce27649881e
Saeed Dadkhah <saeed@zilliqa.com>
2023-01-18 18:33:53 +0330

    Fix previous commit

--- docs/src/content/hardhat-runner/plugins/plugins.ts
+++ docs/src/content/hardhat-runner/plugins/plugins.ts
@@ -672,6 +672,14 @@ const communityPlugins: IPlugin[] = [
     tags: ["Tooling", "Testing", "Storage"],
   },
   {
+    name: "hardhat-insight",
+    author: "Andres Adjimann",
+    authorUrl: "https://www.npmjs.com/package/hardhat-insight",
+    description:
+      "Hardhat plugin to get contract storage, gas and code size insights using the compiler ast output",
+    tags: ["Tooling", "Storage", "Gas", "Size", "Bytecode"],
+  },
+  {
     name: "hardhat-scilla-plugin",
     author: "Saeed Dadkhah",
     authorUrl: "https://www.github.com/its-saeed",
