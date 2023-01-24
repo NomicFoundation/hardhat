c887a4492d80cb0307f8b3e476c0779aea3018fe
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2023-01-24 16:33:18 -0500

    ForkDatabase

--- crates/rethnet_evm/src/db.rs
+++ crates/rethnet_evm/src/db.rs
@@ -1,3 +1,4 @@
+mod fork;
 mod layered_db;
 mod remote;
 mod request;
@@ -5,6 +6,7 @@ mod sync;
 
 pub use sync::{AsyncDatabase, SyncDatabase};
 
+pub use fork::{ForkDatabase, ForkDatabaseError};
 pub use layered_db::{LayeredDatabase, RethnetLayer};
 
 pub use remote::RemoteDatabase;
