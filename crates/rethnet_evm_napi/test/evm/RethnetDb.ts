aab9032e660efbfbaf48370a4726fe1cb8fe1d27
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2023-01-25 17:07:39 -0500

    fix indentation (only whitespace changes)

--- crates/rethnet_evm_napi/test/evm/RethnetDb.ts
+++ crates/rethnet_evm_napi/test/evm/RethnetDb.ts
@@ -41,76 +41,76 @@ describe("Rethnet", () => {
 
   for (const {name, getStateManager} of stateManagers) {
     describe(`With the ${name} StateManager`, () => {
-  beforeEach(async function () {
-    blockchain = new Blockchain(async function (
-      _blockNumber: bigint
-    ): Promise<Buffer> {
-      return Buffer.allocUnsafe(0);
-    });
+      beforeEach(async function () {
+        blockchain = new Blockchain(async function (
+          _blockNumber: bigint
+        ): Promise<Buffer> {
+          return Buffer.allocUnsafe(0);
+        });
 
-    stateManager = getStateManager();
+        stateManager = getStateManager();
 
-    const cfg: Config = {
-      chainId: BigInt(0),
-      specId: SpecId.GrayGlacier,
-      limitContractCodeSize: BigInt(2n) ** BigInt(32n),
-      disableEip3607: true,
-    };
-    rethnet = new Rethnet(blockchain, stateManager, cfg);
-  });
+        const cfg: Config = {
+          chainId: BigInt(0),
+          specId: SpecId.GrayGlacier,
+          limitContractCodeSize: BigInt(2n) ** BigInt(32n),
+          disableEip3607: true,
+        };
+        rethnet = new Rethnet(blockchain, stateManager, cfg);
+      });
 
-  it("call", async () => {
-    // Add funds to caller
-    await stateManager.insertAccount(caller.buf, {
-      nonce: 0n,
-      balance: BigInt("0xffffffff"),
-    });
+      it("call", async () => {
+        // Add funds to caller
+        await stateManager.insertAccount(caller.buf, {
+          nonce: 0n,
+          balance: BigInt("0xffffffff"),
+        });
 
-    // send some value
-    const sendValue: Transaction = {
-      from: caller.buf,
-      to: receiver.buf,
-      gasLimit: BigInt(1000000),
-      value: 100n,
-    };
+        // send some value
+        const sendValue: Transaction = {
+          from: caller.buf,
+          to: receiver.buf,
+          gasLimit: BigInt(1000000),
+          value: 100n,
+        };
 
-    const block: BlockConfig = {
-      number: BigInt(1),
-      timestamp: BigInt(Math.ceil(new Date().getTime() / 1000)),
-    };
-    let sendValueChanges = await rethnet.dryRun(sendValue, block);
+        const block: BlockConfig = {
+          number: BigInt(1),
+          timestamp: BigInt(Math.ceil(new Date().getTime() / 1000)),
+        };
+        let sendValueChanges = await rethnet.dryRun(sendValue, block);
 
-    // receiver should have 100 (0x64) wei
-    expect(
-      BigInt(
-        sendValueChanges.state["0x70997970c51812dc3a010c7d01b50e0d17dc79c8"]
-          .info.balance
-      )
-    ).to.equal(BigInt("0x64"));
+        // receiver should have 100 (0x64) wei
+        expect(
+          BigInt(
+            sendValueChanges.state["0x70997970c51812dc3a010c7d01b50e0d17dc79c8"]
+              .info.balance
+          )
+        ).to.equal(BigInt("0x64"));
 
-    // create a contract
-    const createContract: Transaction = {
-      from: caller.buf,
+        // create a contract
+        const createContract: Transaction = {
+          from: caller.buf,
 
-      gasLimit: BigInt(1000000),
+          gasLimit: BigInt(1000000),
 
-      // minimal creation bytecode
-      input: Buffer.from("3859818153F3", "hex"),
-    };
+          // minimal creation bytecode
+          input: Buffer.from("3859818153F3", "hex"),
+        };
 
-    let createContractChanges = await rethnet.dryRun(createContract, block);
+        let createContractChanges = await rethnet.dryRun(createContract, block);
 
-    expect(
-      createContractChanges.state["0x5fbdb2315678afecb367f032d93f642f64180aa3"]
-    ).to.exist;
-    // check that the code hash is not the null hash (i.e., the address has code)
-    expect(
-      createContractChanges.state["0x5fbdb2315678afecb367f032d93f642f64180aa3"]
-        .info.code_hash
-    ).to.not.equal(
-      "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
-    );
-  });
+        expect(
+          createContractChanges.state["0x5fbdb2315678afecb367f032d93f642f64180aa3"]
+        ).to.exist;
+        // check that the code hash is not the null hash (i.e., the address has code)
+        expect(
+          createContractChanges.state["0x5fbdb2315678afecb367f032d93f642f64180aa3"]
+            .info.code_hash
+        ).to.not.equal(
+          "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"
+        );
+      });
     });
   }
 });
--- crates/rethnet_evm_napi/test/evm/StateManager.ts
+++ crates/rethnet_evm_napi/test/evm/StateManager.ts
@@ -24,77 +24,77 @@ describe("State Manager", () => {
 
   for (const {name, getStateManager} of stateManagers) {
     describe(`With the ${name} StateManager`, () => {
-  let stateManager: StateManager;
+      let stateManager: StateManager;
 
-  beforeEach(function () {
-    stateManager = getStateManager();
-  });
+      beforeEach(function () {
+        stateManager = getStateManager();
+      });
 
-  // TODO: insertBlock, setAccountCode, setAccountStorageSlot
-  it("getAccountByAddress", async () => {
-    await stateManager.insertAccount(caller.buf, {
-      nonce: 0n,
-      balance: 0n,
-    });
-    let account = await stateManager.getAccountByAddress(caller.buf);
+      // TODO: insertBlock, setAccountCode, setAccountStorageSlot
+      it("getAccountByAddress", async () => {
+        await stateManager.insertAccount(caller.buf, {
+          nonce: 0n,
+          balance: 0n,
+        });
+        let account = await stateManager.getAccountByAddress(caller.buf);
 
-    expect(account?.balance).to.equal(0n);
-    expect(account?.nonce).to.equal(0n);
-  });
+        expect(account?.balance).to.equal(0n);
+        expect(account?.nonce).to.equal(0n);
+      });
 
-  it("setAccountBalance", async () => {
-    await stateManager.insertAccount(caller.buf, {
-      nonce: 0n,
-      balance: 0n,
-    });
+      it("setAccountBalance", async () => {
+        await stateManager.insertAccount(caller.buf, {
+          nonce: 0n,
+          balance: 0n,
+        });
 
-    await stateManager.modifyAccount(
-      caller.buf,
-      async function (
-        _balance: bigint,
-        nonce: bigint,
+        await stateManager.modifyAccount(
+          caller.buf,
+          async function (
+            _balance: bigint,
+            nonce: bigint,
         code: Bytecode | undefined
       ): Promise<Account> {
-        return {
-          balance: 100n,
-          nonce,
-          code,
-        };
-      }
-    );
+            return {
+              balance: 100n,
+              nonce,
+              code,
+            };
+          }
+        );
 
-    let account = await stateManager.getAccountByAddress(caller.buf);
+        let account = await stateManager.getAccountByAddress(caller.buf);
 
-    expect(account?.balance).to.equal(100n);
-    expect(account?.nonce).to.equal(0n);
-  });
+        expect(account?.balance).to.equal(100n);
+        expect(account?.nonce).to.equal(0n);
+      });
 
-  it("setAccountNonce", async () => {
-    await stateManager.insertAccount(caller.buf, {
-      nonce: 0n,
-      balance: 0n,
-    });
+      it("setAccountNonce", async () => {
+        await stateManager.insertAccount(caller.buf, {
+          nonce: 0n,
+          balance: 0n,
+        });
 
-    await stateManager.modifyAccount(
-      caller.buf,
-      async function (
-        balance: bigint,
-        nonce: bigint,
+        await stateManager.modifyAccount(
+          caller.buf,
+          async function (
+            balance: bigint,
+            nonce: bigint,
         code: Bytecode | undefined
       ): Promise<Account> {
-        return {
-          balance,
-          nonce: 5n,
-          code,
-        };
-      }
-    );
+            return {
+              balance,
+              nonce: 5n,
+              code,
+            };
+          }
+        );
 
-    let account = await stateManager.getAccountByAddress(caller.buf);
+        let account = await stateManager.getAccountByAddress(caller.buf);
 
-    expect(account?.balance).to.equal(0n);
-    expect(account?.nonce).to.equal(5n);
-  });
+        expect(account?.balance).to.equal(0n);
+        expect(account?.nonce).to.equal(5n);
+      });
     });
   }
 });
