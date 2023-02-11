1c42237296c816fa70a5856abfb2dc0d1d51cdce
Patricio Palladino <email@patriciopalladino.com>
2023-02-11 14:45:34 -0300

    WIP

--- packages/hardhat-core/src/internal/cli/bootstrap.ts
+++ packages/hardhat-core/src/internal/cli/bootstrap.ts
@@ -1,38 +1,19 @@
-import { fork } from "child_process";
+#!/usr/bin/env node
+import semver from "semver";
+import chalk from "chalk";
 
-import { getEnvHardhatArguments } from "../core/params/env-variables";
-import { HARDHAT_PARAM_DEFINITIONS } from "../core/params/hardhat-params";
+const SUPPORTED_NODE_VERSIONS = ["^14.0.0", "^16.0.0", "^18.0.0"];
 
-import { ArgumentsParser } from "./ArgumentsParser";
+if (!semver.satisfies(process.version, SUPPORTED_NODE_VERSIONS.join(" || "))) {
+  console.warn(
+    chalk.yellow(
+      `You are using a version of Node.js that is not supported, and it may work incorrectly, or not work at all.
 
-const nodeArgs = [...process.execArgv];
+Please, make sure you are using a supported version of Node.js.
 
-if (process.env.DISABLE_HARDHAT_NETWORK_OPTIMIZATIONS === undefined) {
-  nodeArgs.push("--max-semi-space-size=100");
+To learn more about which versions of Node.js are supported go to https://hardhat.org/nodejs-versions`
+    )
+  );
 }
 
-const envVariableArguments = getEnvHardhatArguments(
-  HARDHAT_PARAM_DEFINITIONS,
-  process.env
-);
-
-const argumentsParser = new ArgumentsParser();
-
-const { hardhatArguments } = argumentsParser.parseHardhatArguments(
-  HARDHAT_PARAM_DEFINITIONS,
-  envVariableArguments,
-  process.argv.slice(2)
-);
-
-if (hardhatArguments.maxMemory !== undefined) {
-  nodeArgs.push(`--max-old-space-size=${hardhatArguments.maxMemory}`);
-}
-
-const childProcess = fork(`${__dirname}/cli`, process.argv.slice(2), {
-  stdio: "inherit" as any, // There's an error in the TS definition of ForkOptions
-  execArgv: nodeArgs,
-});
-
-childProcess.once("close", (status) => {
-  process.exit(status as number);
-});
+require("./cli");
