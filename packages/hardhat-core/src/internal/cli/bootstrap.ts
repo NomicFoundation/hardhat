ce2a03c6c4ae8f47348b0e988e3f6eab449ec2b2
Patricio Palladino <email@patriciopalladino.com>
2023-02-11 16:12:07 -0300

    Improve the warning message

--- packages/hardhat-core/src/internal/cli/bootstrap.ts
+++ packages/hardhat-core/src/internal/cli/bootstrap.ts
@@ -6,14 +6,11 @@ const SUPPORTED_NODE_VERSIONS = ["^14.0.0", "^16.0.0", "^18.0.0"];
 
 if (!semver.satisfies(process.version, SUPPORTED_NODE_VERSIONS.join(" || "))) {
   console.warn(
-    chalk.yellow(
-      `You are using a version of Node.js that is not supported, and it may work incorrectly, or not work at all.
-
-Please, make sure you are using a supported version of Node.js.
-
-To learn more about which versions of Node.js are supported go to https://hardhat.org/nodejs-versions`
-    )
+    chalk.yellow.bold(`WARNING:`),
+    `You are using a version of Node.js that is not supported, and it may work incorrectly, or not work at all. See https://hardhat.org/nodejs-versions`
   );
+  console.log();
+  console.log();
 }
 
 require("./cli");
