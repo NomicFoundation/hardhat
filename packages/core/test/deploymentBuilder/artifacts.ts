04bc5181cd570ca8aa9476b10b1383366cd2a07e
zoeyTM <morgansliman@gmail.com>
2023-03-22 00:59:56 -0400

    added another test

--- packages/core/test/deploymentBuilder/artifacts.ts
+++ packages/core/test/deploymentBuilder/artifacts.ts
@@ -1,14 +1,14 @@
 /* eslint-disable import/no-unused-modules */
 import type {
-  IDeploymentGraph,
   IDeploymentBuilder,
-} from "../../src/types/deploymentGraph";
+  IDeploymentGraph,
+} from "../../src/internal/types/deploymentGraph";
 
 import { assert } from "chai";
 
 import { buildModule } from "../../src/dsl/buildModule";
-import { generateDeploymentGraphFrom } from "../../src/process/generateDeploymentGraphFrom";
-import { isArtifactContract } from "../../src/utils/guards";
+import { generateDeploymentGraphFrom } from "../../src/internal/process/generateDeploymentGraphFrom";
+import { isArtifactContract } from "../../src/internal/utils/guards";
 
 import { getDeploymentVertexByLabel } from "./helpers";
 
@@ -58,5 +58,38 @@ describe("deployment builder - artifacts", () => {
     }
 
     assert.equal(depNode.artifact.bytecode, "test");
+  });
+
+  it("should throw when trying to retrieve an invalid artifact", () => {
+    const artifactsModule = buildModule(
+      "artifacts",
+      (m: IDeploymentBuilder) => {
+        const artifact = m.getArtifact("None");
+
+        const token = m.contract("Token", artifact);
+
+        return { token };
+      }
+    );
+
+    assert.throws(
+      () =>
+        generateDeploymentGraphFrom(artifactsModule, {
+          chainId: 31337,
+          accounts: [
+            "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+            "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+          ],
+          artifacts: [
+            {
+              contractName: "Token",
+              bytecode: "test",
+              abi: [],
+              linkReferences: {},
+            },
+          ],
+        }),
+      /Artifact None does not exist/
+    );
   });
 });
