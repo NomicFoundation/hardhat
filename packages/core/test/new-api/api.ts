e848efe45d3574831d3a657988867a687c7bf025
John Kane <john@kanej.me>
2023-04-27 16:44:49 +0100

    test(new-api): add todo tests to cover `after` and id throw

--- packages/core/test/new-api/api.ts
+++ packages/core/test/new-api/api.ts
@@ -61,5 +61,80 @@ describe("new api", () => {
       assert.equal(anotherFuture.dependencies.size, 1);
       assert(anotherFuture.dependencies.has(exampleFuture!));
     });
+
+    // TODO: confirm after syntax
+    it.skip("should be able to pass one contract as an after dependency of another", () => {
+      const moduleWithDependentContracts = buildModule("Module1", (m) => {
+        const example = m.contract("Example");
+        const another = m.contract(
+          "Another",
+          []
+          // proposed after syntax
+          // { after: [example] }
+        );
+
+        return { example, another };
+      });
+
+      assert.isDefined(moduleWithDependentContracts);
+
+      const exampleFuture = [...moduleWithDependentContracts.futures].find(
+        ({ id }) => id === "Module1:Example"
+      );
+
+      const anotherFuture = [...moduleWithDependentContracts.futures].find(
+        ({ id }) => id === "Module1:Another"
+      );
+
+      if (
+        !(anotherFuture instanceof NamedContractDeploymentFutureImplementation)
+      ) {
+        assert.fail("Not a named contract deployment");
+      }
+
+      assert.equal(anotherFuture.dependencies.size, 1);
+      assert(anotherFuture.dependencies.has(exampleFuture!));
+    });
+
+    // TODO: determine whether this should throw due to id duplication
+    // on the contract calls or whether we should auto-infer
+    // the id as something like:
+    //   Module1:SameContract:1
+    //   Module1:SameContract:2
+    it.skip("should be able to deploy the same contract twice", () => {
+      const moduleWithSameContractTwice = buildModule("Module1", (m) => {
+        const sameContract1 = m.contract("SameContract");
+        const sameContract2 = m.contract("SameContract");
+
+        return { sameContract1, sameContract2 };
+      });
+
+      assert.isDefined(moduleWithSameContractTwice);
+
+      // Sets ids based on module id and contract name
+      assert.equal(moduleWithSameContractTwice.id, "Module1");
+      assert.equal(
+        moduleWithSameContractTwice.results.sameContract1.id,
+        "Module1:SameContract" // TODO: what should the id be here?
+      );
+      assert.equal(
+        moduleWithSameContractTwice.results.sameContract2.id,
+        "Module1:SameContract"
+      );
+
+      // 2 contract futures
+      assert.equal(moduleWithSameContractTwice.futures.size, 2);
+      assert.equal(
+        [...moduleWithSameContractTwice.futures][0].type,
+        FutureType.NAMED_CONTRACT_DEPLOYMENT
+      );
+      assert.equal(
+        [...moduleWithSameContractTwice.futures][1].type,
+        FutureType.NAMED_CONTRACT_DEPLOYMENT
+      );
+
+      // No submodules
+      assert.equal(moduleWithSameContractTwice.submodules.size, 0);
+    });
   });
 });
