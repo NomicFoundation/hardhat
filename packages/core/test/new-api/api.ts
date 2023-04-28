8465d6c3b514fcd27632963e5ced7ff7420fae19
John Kane <john@kanej.me>
2023-04-28 15:49:55 +0100

    tests(new-api): add `useModule` tests

--- packages/core/test/new-api/api.ts
+++ packages/core/test/new-api/api.ts
@@ -140,4 +140,47 @@ describe("new api", () => {
       });
     });
   });
+
+  describe("useModule", () => {
+    it("should be able to use a submodule", () => {
+      const submodule = buildModule("Submodule1", (m) => {
+        const contract1 = m.contract("Contract1");
+
+        return { contract1 };
+      });
+
+      const moduleWithSubmodule = buildModule("Module1", (m) => {
+        const { contract1 } = m.useModule(submodule);
+
+        return { contract1 };
+      });
+
+      // the submodule is linked
+      assert.equal(moduleWithSubmodule.submodules.size, 1);
+      assert(moduleWithSubmodule.submodules.has(submodule));
+    });
+
+    it("should return cached version on second run", () => {
+      const submodule = buildModule("Submodule1", (m) => {
+        const contract1 = m.contract("Contract1");
+
+        return { contract1 };
+      });
+
+      const moduleWithSubmodule = buildModule("Module1", (m) => {
+        const { contract1: first } = m.useModule(submodule);
+        const { contract1: second } = m.useModule(submodule);
+
+        return { first, second };
+      });
+
+      assert.equal(
+        moduleWithSubmodule.results.first,
+        moduleWithSubmodule.results.second
+      );
+
+      assert.equal(moduleWithSubmodule.submodules.size, 1);
+      assert(moduleWithSubmodule.submodules.has(submodule));
+    });
+  });
 });
