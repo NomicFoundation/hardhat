3e4b413ff7855bd1b8d61d8344da85d92af0018f
Nicolás Santángelo <santangelonicolas@gmail.com>
2023-05-18 20:57:14 -0300

    Provider wrapper tests

--- packages/hardhat-core/src/internal/core/providers/wrapper.ts
+++ packages/hardhat-core/src/internal/core/providers/wrapper.ts
@@ -4,11 +4,10 @@ import { EventEmitterWrapper } from "../../util/event-emitter";
 import { InvalidInputError } from "./errors";
 
 /**
- * A wrapper class that makes it easy to implement EIP1193 (Javascript Ethereum Provider) standard.
- * It comes baked in with all event emitter methods needed,
+ * A wrapper class that makes it easy to implement the EIP1193 (Javascript Ethereum Provider) standard.
+ * It comes baked in with all EventEmitter methods needed,
  * which will be added to the provider supplied in the constructor.
- * It also provides the interface for the standard .request() method.
- * Lastly it adds a helper to parse RequestArguments as an (optionally typed) array.
+ * It also provides the interface for the standard .request() method as an abstract method.
  */
 export abstract class ProviderWrapper
   extends EventEmitterWrapper
@@ -20,6 +19,10 @@ export abstract class ProviderWrapper
 
   public abstract request(args: RequestArguments): Promise<unknown>;
 
+  /**
+   * Extract the params from RequestArguments and optionally type them.
+   * It defaults to an empty array if no params are found.
+   */
   protected _getParams<ParamsT extends any[] = any[]>(
     args: RequestArguments
   ): ParamsT | [] {
