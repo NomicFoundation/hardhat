d1147ab415cf4cda7f63dcb7ed5de76f4fe405bf
Patricio Palladino <email@patriciopalladino.com>
2023-05-24 11:53:12 -0300

    Improve type-level tests of Future

--- packages/core/test/new-api/types/module.ts
+++ packages/core/test/new-api/types/module.ts
@@ -4,6 +4,12 @@ import {
   IgnitionModule,
 } from "../../../src/new-api/types/module";
 
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
 interface BaseFuture {
   id: string;
   type: FutureType;
@@ -11,6 +17,16 @@ interface BaseFuture {
   dependencies: Set<Future>;
 }
 
-const _testThatEveryFutureIsBaseFuture: Future extends BaseFuture
-  ? true
-  : never = true;
+function _testThatEveryFutureIsBaseFuture(f: Future): BaseFuture {
+  return f;
+}
+
+function _testThatBaseFutureIncludesAllSharedFieldsExceptType(
+  f: Omit<BaseFuture, "type">
+): UnionToIntersection<Omit<Future, "type">> {
+  return f;
+}
+
+function _testThatEveryFutureTypeIsUsed(type: FutureType): Future["type"] {
+  return type;
+}
