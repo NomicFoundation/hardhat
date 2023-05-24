d2868ba0150b73d9b155383ee065a2ad54a8ba68
zoeyTM <morgansliman@gmail.com>
2023-05-24 00:38:59 -0400

    add tests for send

---
+++ packages/core/test/new-api/send.ts
@@ -0,0 +1,227 @@
+import { assert } from "chai";
+
+import { defineModule } from "../../src/new-api/define-module";
+import { SendDataFutureImplementation } from "../../src/new-api/internal/module";
+import { ModuleConstructor } from "../../src/new-api/internal/module-builder";
+import { FutureType } from "../../src/new-api/types/module";
+
+describe("send", () => {
+  it("should be able to setup a send", () => {
+    const moduleWithASingleContractDefinition = defineModule("Module1", (m) => {
+      m.send("0xtest", "test-data");
+
+      return {};
+    });
+
+    const constructor = new ModuleConstructor(0, []);
+    const moduleWithASingleContract = constructor.construct(
+      moduleWithASingleContractDefinition
+    );
+
+    assert.isDefined(moduleWithASingleContract);
+
+    // 1 contract future & 1 call future
+    assert.equal(moduleWithASingleContract.futures.size, 1);
+    assert.equal(
+      [...moduleWithASingleContract.futures][0].type,
+      FutureType.SEND_DATA
+    );
+
+    // No submodules
+    assert.equal(moduleWithASingleContract.submodules.size, 0);
+
+    const sendFuture = [...moduleWithASingleContract.futures].find(
+      ({ id }) => id === "Module1:0xtest"
+    );
+
+    if (!(sendFuture instanceof SendDataFutureImplementation)) {
+      assert.fail("Not a send data future");
+    }
+
+    assert.equal(sendFuture.data, "test-data");
+  });
+
+  it("should be able to pass one contract as the 'to' arg for a send", () => {
+    const moduleWithDependentContractsDefinition = defineModule(
+      "Module1",
+      (m) => {
+        const example = m.contract("Example");
+        m.send(example, "");
+
+        return { example };
+      }
+    );
+
+    const constructor = new ModuleConstructor(0, []);
+    const moduleWithDependentContracts = constructor.construct(
+      moduleWithDependentContractsDefinition
+    );
+
+    assert.isDefined(moduleWithDependentContracts);
+
+    const exampleFuture = [...moduleWithDependentContracts.futures].find(
+      ({ id }) => id === "Module1:Example"
+    );
+
+    const sendFuture = [...moduleWithDependentContracts.futures].find(
+      ({ id }) => id === "Module1:Module1:Example"
+    );
+
+    if (!(sendFuture instanceof SendDataFutureImplementation)) {
+      assert.fail("Not a send data future");
+    }
+
+    assert.equal(sendFuture.dependencies.size, 1);
+    assert(sendFuture.dependencies.has(exampleFuture!));
+  });
+
+  it("should be able to pass one contract as an after dependency of a send", () => {
+    const moduleWithDependentContractsDefinition = defineModule(
+      "Module1",
+      (m) => {
+        const example = m.contract("Example");
+        m.send("0xtest", "", { after: [example] });
+
+        return { example };
+      }
+    );
+
+    const constructor = new ModuleConstructor(0, []);
+    const moduleWithDependentContracts = constructor.construct(
+      moduleWithDependentContractsDefinition
+    );
+
+    assert.isDefined(moduleWithDependentContracts);
+
+    const exampleFuture = [...moduleWithDependentContracts.futures].find(
+      ({ id }) => id === "Module1:Example"
+    );
+
+    const sendFuture = [...moduleWithDependentContracts.futures].find(
+      ({ id }) => id === "Module1:0xtest"
+    );
+
+    if (!(sendFuture instanceof SendDataFutureImplementation)) {
+      assert.fail("Not a send data future");
+    }
+
+    assert.equal(sendFuture.dependencies.size, 1);
+    assert(sendFuture.dependencies.has(exampleFuture!));
+  });
+
+  it("should be able to pass value as an option", () => {
+    const moduleWithDependentContractsDefinition = defineModule(
+      "Module1",
+      (m) => {
+        m.send("0xtest", "", { value: BigInt(42) });
+
+        return {};
+      }
+    );
+
+    const constructor = new ModuleConstructor(0, []);
+    const moduleWithDependentContracts = constructor.construct(
+      moduleWithDependentContractsDefinition
+    );
+
+    assert.isDefined(moduleWithDependentContracts);
+
+    const sendFuture = [...moduleWithDependentContracts.futures].find(
+      ({ id }) => id === "Module1:0xtest"
+    );
+
+    if (!(sendFuture instanceof SendDataFutureImplementation)) {
+      assert.fail("Not a send data future");
+    }
+
+    assert.equal(sendFuture.value, BigInt(42));
+  });
+
+  it("should be able to pass from as an option", () => {
+    const moduleWithDependentContractsDefinition = defineModule(
+      "Module1",
+      (m) => {
+        m.send("0xtest", "", { from: m.accounts[1] });
+
+        return {};
+      }
+    );
+
+    const constructor = new ModuleConstructor(0, ["0x1", "0x2"]);
+    const moduleWithDependentContracts = constructor.construct(
+      moduleWithDependentContractsDefinition
+    );
+
+    assert.isDefined(moduleWithDependentContracts);
+
+    const sendFuture = [...moduleWithDependentContracts.futures].find(
+      ({ id }) => id === "Module1:0xtest"
+    );
+
+    if (!(sendFuture instanceof SendDataFutureImplementation)) {
+      assert.fail("Not a send data future");
+    }
+
+    assert.equal(sendFuture.from, "0x2");
+  });
+
+  describe("passing id", () => {
+    it("should be able to call the same function twice by passing an id", () => {
+      const moduleWithSameCallTwiceDefinition = defineModule("Module1", (m) => {
+        m.send("0xtest", "test", { id: "first" });
+        m.send("0xtest", "test", { id: "second" });
+
+        return {};
+      });
+
+      const constructor = new ModuleConstructor(0, []);
+      const moduleWithSameCallTwice = constructor.construct(
+        moduleWithSameCallTwiceDefinition
+      );
+
+      assert.equal(moduleWithSameCallTwice.id, "Module1");
+
+      const sendFuture = [...moduleWithSameCallTwice.futures].find(
+        ({ id }) => id === "Module1:first"
+      );
+
+      const sendFuture2 = [...moduleWithSameCallTwice.futures].find(
+        ({ id }) => id === "Module1:second"
+      );
+
+      assert.isDefined(sendFuture);
+      assert.isDefined(sendFuture2);
+    });
+
+    it("should throw if the same function is called twice without differentiating ids", () => {
+      const moduleDefinition = defineModule("Module1", (m) => {
+        m.send("0xtest", "test");
+        m.send("0xtest", "test");
+
+        return {};
+      });
+
+      const constructor = new ModuleConstructor(0, []);
+
+      assert.throws(
+        () => constructor.construct(moduleDefinition),
+        /Duplicated id Module1:0xtest found in module Module1/
+      );
+    });
+
+    it("should throw if a call tries to pass the same id twice", () => {
+      const moduleDefinition = defineModule("Module1", (m) => {
+        m.send("0xtest", "test", { id: "first" });
+        m.send("0xtest", "test", { id: "first" });
+        return {};
+      });
+
+      const constructor = new ModuleConstructor(0, []);
+
+      assert.throws(
+        () => constructor.construct(moduleDefinition),
+        /Duplicated id Module1:first found in module Module1/
+      );
+    });
+  });
+});
