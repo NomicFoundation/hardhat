273538f88a7da8160775ca7c260cec2fff7d4373
Patricio Palladino <email@patriciopalladino.com>
2023-05-25 14:50:44 -0300

    Define function to get all the futures in an array of arguments

--- packages/core/src/new-api/internal/utils.ts
+++ packages/core/src/new-api/internal/utils.ts
@@ -1,4 +1,4 @@
-import { Future, FutureType } from "../types/module";
+import { ArgumentType, Future, FutureType } from "../types/module";
 
 export function isFuture(potential: unknown): potential is Future {
   return (
@@ -8,4 +8,24 @@ export function isFuture(potential: unknown): potential is Future {
     typeof potential.type === "number" &&
     FutureType[potential.type] !== undefined
   );
+}
+
+export function getFutures(args: ArgumentType[]): Future[] {
+  return args.flatMap(_getFutures);
+}
+
+function _getFutures(argument: ArgumentType): Future[] {
+  if (isFuture(argument)) {
+    return [argument];
+  }
+
+  if (Array.isArray(argument)) {
+    return getFutures(argument);
+  }
+
+  if (typeof argument === "object" && argument !== null) {
+    return getFutures(Object.values(argument));
+  }
+
+  return [];
 }
