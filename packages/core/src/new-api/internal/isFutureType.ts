95040ee107d7259b890d60cf04b01f5a63782705
Patricio Palladino <email@patriciopalladino.com>
2023-05-26 18:52:25 -0300

    Delete unused files

--- packages/core/src/new-api/internal/isFutureType.ts
+++
@@ -1,8 +0,0 @@
-import { FutureType } from "../types/module";
-
-export function isFutureType(potential: unknown): potential is FutureType {
-  return (
-    typeof potential === "string" &&
-    (FutureType as any)[potential] !== undefined
-  );
-}
