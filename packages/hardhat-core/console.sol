85763bb30a9703f8c069ef24dfe2b5fb19afbbc3
Mikhail Melnik <by.zumzoom@gmail.com>
2023-05-31 13:28:43 +0300

    add memory-safe annotation

--- packages/hardhat-core/console.sol
+++ packages/hardhat-core/console.sol
@@ -7,6 +7,7 @@ library console {
 	function _sendLogPayload(bytes memory payload) private view {
 		uint256 payloadLength = payload.length;
 		address consoleAddress = CONSOLE_ADDRESS;
+    /// @solidity memory-safe-assembly
 		assembly {
 			let payloadStart := add(payload, 32)
 			let r := staticcall(gas(), consoleAddress, payloadStart, payloadLength, 0, 0)
