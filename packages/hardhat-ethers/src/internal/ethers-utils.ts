26f240ebff27a67be4c8711bd8f6b67420ce1884
Franco Victorio <victorio.franco@gmail.com>
2023-06-06 12:35:34 +0200

    Remove commented out code in vendored utils

--- packages/hardhat-ethers/src/internal/ethers-utils.ts
+++ packages/hardhat-ethers/src/internal/ethers-utils.ts
@@ -273,41 +273,6 @@ export function formatTransactionResponse(
     }
   }
 
-  // @TODO: check chainID
-  /*
-    if (value.chainId != null) {
-        let chainId = value.chainId;
-
-        if (isHexString(chainId)) {
-            chainId = BigNumber.from(chainId).toNumber();
-        }
-
-        result.chainId = chainId;
-
-    } else {
-        let chainId = value.networkId;
-
-        // geth-etc returns chainId
-        if (chainId == null && result.v == null) {
-            chainId = value.chainId;
-        }
-
-        if (isHexString(chainId)) {
-            chainId = BigNumber.from(chainId).toNumber();
-        }
-
-        if (typeof(chainId) !== "number" && result.v != null) {
-            chainId = (result.v - 35) / 2;
-            if (chainId < 0) { chainId = 0; }
-            chainId = parseInt(chainId);
-        }
-
-        if (typeof(chainId) !== "number") { chainId = 0; }
-
-        result.chainId = chainId;
-    }
-    */
-
   // 0x0000... should actually be null
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (result.blockHash && getBigInt(result.blockHash) === 0n) {
