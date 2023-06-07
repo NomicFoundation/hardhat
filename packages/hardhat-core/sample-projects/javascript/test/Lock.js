9d900ce0e191a9c8fa1b22903ea2a948f34b8a65
Franco Victorio <victorio.franco@gmail.com>
2023-06-07 16:40:06 +0200

    Use toolbox/network-helpers in docs

--- packages/hardhat-core/sample-projects/javascript/test/Lock.js
+++ packages/hardhat-core/sample-projects/javascript/test/Lock.js
@@ -1,7 +1,7 @@
 const {
   time,
   loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
 
