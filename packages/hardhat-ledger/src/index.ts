b1fe73bb6bc48d8ff93d58c1f2f068afc762a5b6
Nicolás Santángelo <santangelonicolas@gmail.com>
2023-06-12 16:16:48 -0300

    Export errors for a possible use by the importer

--- packages/hardhat-ledger/src/index.ts
+++ packages/hardhat-ledger/src/index.ts
@@ -4,6 +4,8 @@ import { createLedgerProvider } from "./internal/create-ledger-provider";
 
 import "./type-extensions";
 
+export * from "./errors";
+
 extendProvider(async (provider, config, network) => {
   const networkConfig = config.networks[network] as HardhatNetworkUserConfig;
   return createLedgerProvider(provider, networkConfig);
