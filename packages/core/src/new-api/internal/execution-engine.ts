f9face72d40fa15758c24bf75f70ce3ec1cd7e42
John Kane <john@kanej.me>
2023-06-19 12:57:01 +0100

    refactor: remove old execution-engine file

--- packages/core/src/new-api/internal/execution-engine.ts
+++
@@ -1,7 +0,0 @@
-import { ExecutionEngineState } from "./types/execution-engine";
-
-export class ExecutionEngine {
-  public execute(_preparedExecution: ExecutionEngineState) {
-    throw new Error("Method not implemented.");
-  }
-}
