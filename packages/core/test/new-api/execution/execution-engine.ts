60d57ee5f003545a7b1eaea2512e0dbe07ccf746
zoeyTM <probablyzoey+github@gmail.com>
2023-06-18 21:14:19 -0400

    add test for named library execution

--- packages/core/test/new-api/execution/execution-engine.ts
+++ packages/core/test/new-api/execution/execution-engine.ts
@@ -86,6 +86,78 @@ describe("execution engine", () => {
       },
     ]);
   });
+
+  it("should execute a library deploy", async () => {
+    const moduleDefinition = defineModule("Module1", (m) => {
+      const library1 = m.library("Library1");
+
+      return { library1 };
+    });
+
+    const constructor = new ModuleConstructor();
+    const module = constructor.construct(moduleDefinition);
+
+    assert.isDefined(module);
+
+    const executionStateMap = {};
+
+    const batches = Batcher.batch(module, {});
+
+    const executionEngine = new ExecutionEngine();
+    const journal = new MemoryJournal();
+    const accounts: string[] = exampleAccounts;
+    const mockTransactionService = setupMockTransactionService();
+
+    const result = await executionEngine.execute({
+      batches,
+      module,
+      executionStateMap,
+      accounts,
+      strategy: new BasicExecutionStrategy(),
+      journal,
+      transactionService: mockTransactionService,
+    });
+
+    assert.isDefined(result);
+    const journalMessages = await accumulateMessages(journal);
+
+    assert.deepStrictEqual(journalMessages, [
+      {
+        futureId: "Module1:Library1",
+        type: "execution-start",
+        futureType: FutureType.NAMED_LIBRARY_DEPLOYMENT,
+        strategy: "basic",
+        dependencies: [],
+        storedArtifactPath: "./artifact.json",
+        storedBuildInfoPath: "./build-info.json",
+        contractName: "Library1",
+        value: BigInt(0).toString(),
+        constructorArgs: [],
+        libraries: {},
+        from: accounts[0],
+      },
+      {
+        type: "onchain-action",
+        subtype: "deploy-contract",
+        contractName: "Library1",
+        args: [],
+        value: BigInt(0).toString(),
+        from: exampleAccounts[0],
+      },
+      {
+        type: "onchain-result",
+        subtype: "deploy-contract",
+        contractAddress: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+      },
+      {
+        type: "execution-success",
+        subtype: "deploy-contract",
+        futureId: "Module1:Library1",
+        contractName: "Library1",
+        contractAddress: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+      },
+    ]);
+  });
 });
 
 function setupMockTransactionService(): TransactionService {
