5a7a9cfd7af3a9cde1d8d972388dc0f86ea19021
Mikhail Melnik <by.zumzoom@gmail.com>
2023-06-19 12:52:27 +0200

    add test for stack-too-deep with console.log

---
+++ packages/hardhat-core/test/internal/hardhat-network/stack-traces/test-files/0_8/console-logs/stack-too-deep/c.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+
+import "./../../../../../../../../console.sol";
+
+contract C {
+    function logSum(uint256 arg0, uint256 arg1, uint256 arg2, uint256 arg3, uint256 arg4, uint256 arg5, uint256 arg6, uint256 arg7, uint256 arg8, uint256 arg9, uint256 arg10, uint256 arg11, uint256 arg12, uint256 arg13, uint256 arg14, uint256 arg15) public view {
+        console.log(arg0 + arg1 + arg2 + arg3 + arg4 + arg5 + arg6 + arg7 + arg8 + arg9 + arg10 + arg11 + arg12 + arg13 + arg14 + arg15);
+    }
+}
