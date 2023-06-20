ba917e5a45c03dc5520dfc308419e89f9434bd5c
Nicolás Santángelo <santangelonicolas@gmail.com>
2023-06-20 00:43:42 -0300

    Prefer passing explicit values instead of default arguments

--- packages/hardhat-ledger/src/internal/cache.ts
+++ packages/hardhat-ledger/src/internal/cache.ts
@@ -5,22 +5,22 @@ import path from "path";
 type CacheableJson = Record<string, string>;
 
 export const CACHE_FILE_NAME = "accounts.json";
+const PACKAGE_NAME = "hardhat";
 
 export async function write<T extends CacheableJson>(json: T) {
-  const ledgerCacheFile = await getLedgerCacheFile();
+  const ledgerCacheFile = await getLedgerCacheFile(CACHE_FILE_NAME);
   await fs.writeJSON(ledgerCacheFile, json);
 }
 
 export async function read<T>(): Promise<T | undefined> {
-  const ledgerCacheFile = await getLedgerCacheFile();
+  const ledgerCacheFile = await getLedgerCacheFile(CACHE_FILE_NAME);
   try {
-    await fs.access(ledgerCacheFile, fs.constants.F_OK);
     const file: T = await fs.readJSON(ledgerCacheFile);
     return file;
   } catch (error) {}
 }
 
-async function getLedgerCacheFile(fileName = CACHE_FILE_NAME): Promise<string> {
+async function getLedgerCacheFile(fileName: string): Promise<string> {
   const ledgerCacheDir = await getLedgerCacheDir();
   return path.join(ledgerCacheDir, fileName);
 }
@@ -33,12 +33,12 @@ async function getLedgerCacheDir(): Promise<string> {
 }
 
 async function getCacheDir(): Promise<string> {
-  const { cache } = await generatePaths();
+  const { cache } = await generatePaths(PACKAGE_NAME);
   await fs.ensureDir(cache);
   return cache;
 }
 
-async function generatePaths(packageName = "hardhat"): Promise<Paths> {
+async function generatePaths(packageName: string): Promise<Paths> {
   const { default: envPaths } = await import("env-paths");
   return envPaths(packageName);
 }
