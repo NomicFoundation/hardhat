3149473993e90099e8bfee9c5902beab2ba458ff
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2023-06-20 17:52:53 -0400

    misc: Rethnet state benchmark tweaks (#4037)

--- crates/rethnet_evm/benches/state/database_commit.rs
+++ crates/rethnet_evm/benches/state/database_commit.rs
@@ -1,49 +1,10 @@
-use std::str::FromStr;
-
 use criterion::{criterion_group, criterion_main, Criterion};
 use revm::{db::StateRef, primitives::Bytecode};
 
 use rethnet_eth::{Address, Bytes, U256};
 
 mod util;
-use util::{bench_sync_state_method, state_prep_no_op, Permutations};
-
-fn bench_basic(c: &mut Criterion) {
-    bench_sync_state_method(
-        c,
-        "StateRef:basic",
-        state_prep_no_op,
-        |state, number_of_accounts, _, _| {
-            for i in (1..=number_of_accounts).rev() {
-                let result = state.basic(Address::from_str(&format!("0x{:0>40x}", i)).unwrap());
-                debug_assert!(result.is_ok());
-            }
-        },
-        &[0],
-        &[1],
-    );
-}
-
-fn bench_code_by_hash(c: &mut Criterion) {
-    bench_sync_state_method(
-        c,
-        "StateRef:code_by_hash",
-        state_prep_no_op,
-        |state, number_of_accounts, _, _| {
-            for i in (1..=number_of_accounts).rev() {
-                let result = state.code_by_hash(
-                    Bytecode::new_raw(Bytes::copy_from_slice(
-                        Address::from_low_u64_ne(i).as_bytes(),
-                    ))
-                    .hash(),
-                );
-                debug_assert!(result.is_ok());
-            }
-        },
-        &[0],
-        &[1],
-    );
-}
+use util::{bench_sync_state_method, state_prep_no_op};
 
 fn bench_database_commit(c: &mut Criterion) {
     use hashbrown::HashMap;
@@ -66,7 +27,7 @@ fn bench_database_commit(c: &mut Criterion) {
     let mut accounts_to_commit: HashMap<Address, Account> = HashMap::new();
     let json_accounts: HashMap<Address, AccountState> = serde_json::from_str(
         &std::fs::read_to_string(
-            "benches/fixtures/accounts_changed_in_mainnet_block_17295357.json",
+            "benches/state/fixtures/accounts_changed_in_mainnet_block_17295357.json",
         )
         .unwrap(),
         /* a fresh set of account updates can be retrieved via, eg:
@@ -118,7 +79,8 @@ fn bench_database_commit(c: &mut Criterion) {
         |state, _number_of_accounts, _, _| {
             state.commit(accounts_to_commit.clone());
 
-            debug_assert!(json_accounts.iter().all(|(address, json)| {
+            #[cfg(debug_assertions)]
+            json_accounts.iter().for_each(|(address, json)| {
                 if let Some(committed) = state.basic(*address).unwrap() {
                     debug_assert!(committed.balance == json.balance.unwrap());
                     debug_assert!(committed.nonce == json.nonce.unwrap());
@@ -132,35 +94,12 @@ fn bench_database_commit(c: &mut Criterion) {
                 } else {
                     debug_assert!(false);
                 }
-                true
-            }));
+            });
         },
         &[0],
         &[1],
     );
 }
 
-fn bench_storage(c: &mut Criterion) {
-    bench_sync_state_method(
-        c,
-        "StateRef:storage",
-        state_prep_no_op,
-        |state, number_of_accounts, _, _| {
-            for i in (1..=number_of_accounts).rev() {
-                let result = state.storage(Address::from_low_u64_ne(i), U256::from(i));
-                debug_assert!(result.is_ok());
-            }
-        },
-        &Permutations::storage_scales(),
-        &[1],
-    );
-}
-
-criterion_group!(
-    benches,
-    bench_basic,
-    bench_code_by_hash,
-    bench_storage,
-    bench_database_commit
-);
+criterion_group!(benches, bench_database_commit);
 criterion_main!(benches);
---
+++ crates/rethnet_evm/benches/state/state_ref.rs
@@ -0,0 +1,65 @@
+use std::str::FromStr;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+use revm::{db::StateRef, primitives::Bytecode};
+
+use rethnet_eth::{Address, Bytes, U256};
+
+mod util;
+use util::{bench_sync_state_method, state_prep_no_op, Permutations};
+
+fn bench_basic(c: &mut Criterion) {
+    bench_sync_state_method(
+        c,
+        "StateRef:basic",
+        state_prep_no_op,
+        |state, number_of_accounts, _, _| {
+            for i in (1..=number_of_accounts).rev() {
+                let result = state.basic(Address::from_str(&format!("0x{:0>40x}", i)).unwrap());
+                debug_assert!(result.is_ok());
+            }
+        },
+        &[0],
+        &[1],
+    );
+}
+
+fn bench_code_by_hash(c: &mut Criterion) {
+    bench_sync_state_method(
+        c,
+        "StateRef:code_by_hash",
+        state_prep_no_op,
+        |state, number_of_accounts, _, _| {
+            for i in (1..=number_of_accounts).rev() {
+                let result = state.code_by_hash(
+                    Bytecode::new_raw(Bytes::copy_from_slice(
+                        Address::from_low_u64_ne(i).as_bytes(),
+                    ))
+                    .hash(),
+                );
+                debug_assert!(result.is_ok());
+            }
+        },
+        &[0],
+        &[1],
+    );
+}
+
+fn bench_storage(c: &mut Criterion) {
+    bench_sync_state_method(
+        c,
+        "StateRef:storage",
+        state_prep_no_op,
+        |state, number_of_accounts, _, _| {
+            for i in (1..=number_of_accounts).rev() {
+                let result = state.storage(Address::from_low_u64_ne(i), U256::from(i));
+                debug_assert!(result.is_ok());
+            }
+        },
+        &Permutations::storage_scales(),
+        &[1],
+    );
+}
+
+criterion_group!(benches, bench_basic, bench_code_by_hash, bench_storage,);
+criterion_main!(benches);
