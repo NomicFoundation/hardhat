640375fa5c90bed329abcf1734eae549db96c963
John Kane <john@kanej.me>
2023-06-21 09:29:46 +0100

    fix: use default param is no parameters provided

--- packages/core/src/new-api/internal/utils/resolve-module-parameter.ts
+++ packages/core/src/new-api/internal/utils/resolve-module-parameter.ts
@@ -10,6 +10,15 @@ export function resolveModuleParameter(
   moduleParamRuntimeValue: ModuleParameterRuntimeValue<ModuleParameterType>,
   context: { deploymentParameters: { [key: string]: ModuleParameters } }
 ): ModuleParameterType {
+  if (context.deploymentParameters === undefined) {
+    assertIgnitionInvariant(
+      moduleParamRuntimeValue.defaultValue !== undefined,
+      `No default value provided for module parameter ${moduleParamRuntimeValue.moduleId}/${moduleParamRuntimeValue.name}`
+    );
+
+    return moduleParamRuntimeValue.defaultValue;
+  }
+
   const moduleParameters =
     context.deploymentParameters[moduleParamRuntimeValue.moduleId];
 
