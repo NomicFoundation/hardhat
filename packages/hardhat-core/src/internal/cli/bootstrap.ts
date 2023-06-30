63c1ab7462c1cd7a554d585b9874162f603f036e
Franco Victorio <victorio.franco@gmail.com>
2023-06-30 12:43:54 +0200

    Show current node.js version in warning

--- packages/hardhat-core/src/internal/cli/bootstrap.ts
+++ packages/hardhat-core/src/internal/cli/bootstrap.ts
@@ -7,7 +7,7 @@ import { SUPPORTED_NODE_VERSIONS } from "./constants";
 if (!semver.satisfies(process.version, SUPPORTED_NODE_VERSIONS.join(" || "))) {
   console.warn(
     chalk.yellow.bold(`WARNING:`),
-    `You are using a version of Node.js that is not supported, and it may work incorrectly, or not work at all. See https://hardhat.org/nodejs-versions`
+    `You are using a version of Node.js (${process.version}) that is not supported, and it may work incorrectly, or not work at all. See https://hardhat.org/nodejs-versions`
   );
   console.log();
   console.log();
