c37262153ee0a9cc9d03ca9c08cc3c81af6e9689
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2023-06-30 13:16:45 -0400

    Propagate error using `?`
    Co-authored-by: Wodann <Wodann@users.noreply.github.com>

--- crates/rethnet/src/main.rs
+++ crates/rethnet/src/main.rs
@@ -3,7 +3,7 @@ use rethnet::{run_with_args, ExitStatus};
 /// Main entry point for the `rethnet` executable.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let status = run_with_args(std::env::args_os()).await.unwrap();
+    let status = run_with_args(std::env::args_os()).await?;
     match status {
         ExitStatus::Success => (),
         ExitStatus::Error => std::process::exit(1),
