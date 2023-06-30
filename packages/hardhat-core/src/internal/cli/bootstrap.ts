3c674dd1111e6f5a90e6557d340f54221e1ce293
Franco Victorio <victorio.franco@gmail.com>
2023-06-30 12:39:41 +0200

    Update SUPPORTED_NODE_VERSIONS constant

--- packages/hardhat-core/src/internal/cli/bootstrap.ts
+++ packages/hardhat-core/src/internal/cli/bootstrap.ts
@@ -2,7 +2,7 @@
 import semver from "semver";
 import chalk from "chalk";
 
-const SUPPORTED_NODE_VERSIONS = ["^14.0.0", "^16.0.0", "^18.0.0"];
+import { SUPPORTED_NODE_VERSIONS } from "./constants";
 
 if (!semver.satisfies(process.version, SUPPORTED_NODE_VERSIONS.join(" || "))) {
   console.warn(
