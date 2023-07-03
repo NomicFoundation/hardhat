b2b5a01b554895003c3a28359a6c97940c68402b
Franco Victorio <victorio.franco@gmail.com>
2023-07-03 16:56:21 +0200

    Improve unsupported node version message

--- packages/hardhat-core/src/internal/cli/bootstrap.ts
+++ packages/hardhat-core/src/internal/cli/bootstrap.ts
@@ -7,7 +7,7 @@ import { SUPPORTED_NODE_VERSIONS } from "./constants";
 if (!semver.satisfies(process.version, SUPPORTED_NODE_VERSIONS.join(" || "))) {
   console.warn(
     chalk.yellow.bold(`WARNING:`),
-    `You are using a version of Node.js (${process.version}) that is not supported, and it may work incorrectly, or not work at all. See https://hardhat.org/nodejs-versions`
+    `You are currently using Node.js ${process.version}, which is not supported by Hardhat. This can lead to unexpected behavior. See https://hardhat.org/nodejs-versions`
   );
   console.log();
   console.log();
