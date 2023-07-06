2ed96dbb8b84a30bba1844a55505b30e29ea27d8
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-07-06 19:23:28 +0200

    restore tests to original version

--- packages/hardhat-core/test/internal/hardhat-network/stack-traces/test-files/0_8/console-logs/uint/uint/c.sol
+++ packages/hardhat-core/test/internal/hardhat-network/stack-traces/test-files/0_8/console-logs/uint/uint/c.sol
@@ -5,7 +5,7 @@ import "./../../../../../../../../console.sol";
 contract C {
 
 	function log(
-		uint p0, uint p1, uint p2, string memory p4, bool p8, address p12, uint p3, string memory p5, bool p9, address p13, string memory p6
+		uint p0, uint p1, uint p2, string memory p4, bool p8, address p12, uint p3, string memory p5, bool p9, address p13
 	) public {
 		console.log(p0, p1);
 		console.log(p0, p1, p2);
@@ -28,8 +28,5 @@ contract C {
 		console.log(p0, p1, p12, p4);
 		console.log(p0, p1, p12, p8);
 		console.log(p0, p1, p12, p13);
-		// In the variable p6, the occurrences of %d and %i will be converted to %s when the count of % characters preceding the d or i is odd.
-		// If the count is even, the % character is considered escaped and should not be replaced.
-		console.log(p6);
 	}
 }
