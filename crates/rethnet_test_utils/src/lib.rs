4a8cbaef09315e15c58c61fc03c47646704679af
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2023-07-18 16:29:58 -0400

    simplify serde test helper
    addresses review comment https://github.com/NomicFoundation/hardhat/pull/4132#discussion_r1267091171

--- crates/rethnet_test_utils/src/lib.rs
+++ crates/rethnet_test_utils/src/lib.rs
@@ -1,7 +1,15 @@
+pub fn help_test_method_invocation_serde<MethodInvocation>(call: MethodInvocation)
+where
+    MethodInvocation:
+        PartialEq + Clone + std::fmt::Debug + serde::de::DeserializeOwned + serde::Serialize,
+{
+    help_test_method_invocation_serde_with_expected(call.clone(), call);
+}
+
 #[allow(clippy::type_complexity)]
-pub fn help_test_method_invocation_serde<MethodInvocation>(
+pub fn help_test_method_invocation_serde_with_expected<MethodInvocation>(
     call: MethodInvocation,
-    default_block_tag_resolver: Option<Box<fn(MethodInvocation) -> MethodInvocation>>,
+    expected: MethodInvocation,
 ) where
     MethodInvocation: PartialEq + std::fmt::Debug + serde::de::DeserializeOwned + serde::Serialize,
 {
@@ -26,13 +34,7 @@ pub fn help_test_method_invocation_serde<MethodInvocation>(
         panic!("should have successfully deserialized, with params as a Vec<String>, json {json}")
     });
 
-    let call = if let Some(resolver) = default_block_tag_resolver {
-        resolver(call)
-    } else {
-        call
-    };
-
     let call_decoded: MethodInvocation = serde_json::from_str(&json)
         .unwrap_or_else(|_| panic!("should have successfully deserialized json {json}"));
-    assert_eq!(call, call_decoded);
+    assert_eq!(expected, call_decoded);
 }
