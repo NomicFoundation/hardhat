571755c086442cc61bd481edf99d29a26ba7e285
Franco Victorio <victorio.franco@gmail.com>
2023-07-20 16:29:51 +0200

    Plugin initial setup

---
+++ packages/hardhat-viem/test/clients.ts
@@ -0,0 +1,17 @@
+describe("clients", () => {
+  describe("getPublicClient", () => {
+    it("should return a public client", () => {
+      // TODO
+    });
+  });
+  describe("getWalletClients", () => {
+    it("should return a wallet client", () => {
+      // TODO
+    });
+  });
+  describe("getTestClient", () => {
+    it("should return a test client", () => {
+      // TODO
+    });
+  });
+});
