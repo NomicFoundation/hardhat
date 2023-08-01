257f7c8c1f10cb969a2db5355ecf5aef6c72261e
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2023-07-31 20:52:50 -0400

    use external crate cargo_toml
    addresses review comment https://github.com/NomicFoundation/hardhat/pull/4218#discussion_r1279955563

--- crates/rethnet_rpc_server/build.rs
+++ crates/rethnet_rpc_server/build.rs
@@ -1,28 +1,16 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CargoToml {
-    dependencies: HashMap<String, DependencyValue>,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-enum DependencyValue {
-    String(String),
-    Object {
-        version: String,
-        git: Option<String>,
-        rev: Option<String>,
-    },
-}
+use cargo_toml::{Dependency, DependencyDetail, Manifest};
 
 fn main() {
-    let cargo_toml: CargoToml = toml::from_str(include_str!("../rethnet_evm/Cargo.toml"))
+    let cargo_toml: Manifest = toml::from_str(include_str!("../rethnet_evm/Cargo.toml"))
         .expect("should deserialize Cargo.toml");
     let revm_version = match cargo_toml.dependencies.get("revm") {
-        Some(DependencyValue::String(s)) => s.clone(),
-        Some(DependencyValue::Object { version, git, rev }) => {
+        Some(Dependency::Simple(s)) => s.clone(),
+        Some(Dependency::Detailed(DependencyDetail {
+            version: Some(version),
+            git,
+            rev,
+            ..
+        })) => {
             let rev = rev.clone().map_or(String::new(), |rev| format!("@{rev}"));
             let git = git
                 .clone()
@@ -30,6 +18,7 @@ fn main() {
             format!("{git}{version}")
         }
         None => panic!("revm dependency not found"),
+        _ => panic!("Unrecognized revm dependency format"),
     };
     println!("cargo:rustc-env=REVM_VERSION={revm_version}");
 }
