f196c7746b46b6444f664285f685199864695320
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2023-08-02 15:16:13 +0200

    Update docs/src/content/hardhat-runner/plugins/plugins.ts
    Co-authored-by: Franco Victorio <victorio.franco@gmail.com>

--- docs/src/content/hardhat-runner/plugins/plugins.ts
+++ docs/src/content/hardhat-runner/plugins/plugins.ts
@@ -812,7 +812,7 @@ const communityPlugins: IPlugin[] = [
     author: "Ahmed Ali",
     authorUrl: "https://twitter.com/0xAhmedAli",
     npmPackage: "https://www.npmjs.com/package/hardhat-gasless-deployer",
-    description: "Deploy contracts with HardHat using Gas Station Network",
+    description: "Deploy contracts with Hardhat using Gas Station Network",
     tags: ["GSN", "GASLESS", "Deployment"],
   },
 ];
