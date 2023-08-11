3e3bfec0bf2625c962ab1cb3a9d12f9882df4396
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-08-11 17:58:18 +0200

    modify comments

--- packages/hardhat-core/test/fixture-projects/compilation-import-folder-from-module/contracts/A.sol
+++ packages/hardhat-core/test/fixture-projects/compilation-import-folder-from-module/contracts/A.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-// The file has to not exist for test purposes
-import "some-lib"
+// For test purposes this module must not exist
+import "some-lib";
 
 contract A {}
--- packages/hardhat-core/test/fixture-projects/compilation-import-folder-relative-path/contracts/A.sol
+++ packages/hardhat-core/test/fixture-projects/compilation-import-folder-relative-path/contracts/A.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-// The file has to not exist for test purposes
+// For test purposes this import must point to a folder
 import "../contracts/dependency";
 
 contract A {}
