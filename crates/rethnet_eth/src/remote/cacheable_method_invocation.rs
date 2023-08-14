52b83e8029b7197cec9cee8007cff6cb02d7e324
Agost Biro <agostbiro@gmail.com>
2023-08-14 17:24:23 +0200

    review: use named fiels in

--- crates/rethnet_eth/src/remote/cacheable_method_invocation.rs
+++ crates/rethnet_eth/src/remote/cacheable_method_invocation.rs
@@ -10,62 +10,83 @@ use revm_primitives::{Address, B256};
 #[repr(u8)]
 pub(super) enum CacheableMethodInvocation<'a> {
     /// eth_call
-    Call(&'a TransactionInput, &'a Option<BlockSpec>) = 0,
+    Call {
+        transaction_input: &'a TransactionInput,
+        block_spec: &'a Option<BlockSpec>,
+    } = 0,
     /// eth_chainId
-    ChainId() = 1,
+    ChainId = 1,
     /// eth_estimateGas
-    EstimateGas(&'a TransactionInput, &'a Option<BlockSpec>) = 2,
+    EstimateGas {
+        transaction_input: &'a TransactionInput,
+        block_spec: &'a Option<BlockSpec>,
+    } = 2,
     /// eth_feeHistory
-    FeeHistory(
+    FeeHistory {
         /// block count
-        &'a U256,
+        block_count: &'a U256,
         /// newest block
-        &'a BlockSpec,
+        block_spec: &'a BlockSpec,
         /// reward percentiles
-        &'a Percentiles,
-    ) = 3,
+        percentiles: &'a Percentiles,
+    } = 3,
     /// eth_getBalance
-    GetBalance(&'a Address, &'a Option<BlockSpec>) = 4,
+    GetBalance {
+        address: &'a Address,
+        block_spec: &'a Option<BlockSpec>,
+    } = 4,
     /// eth_getBlockByNumber
-    GetBlockByNumber(
-        &'a BlockSpec,
+    GetBlockByNumber {
+        block_spec: &'a BlockSpec,
+
         /// include transaction data
-        bool,
-    ) = 5,
+        include_tx_data: bool,
+    } = 5,
     /// eth_getBlockByHash
-    GetBlockByHash(
+    GetBlockByHash {
         /// hash
-        &'a B256,
+        block_hash: &'a B256,
         /// include transaction data
-        bool,
-    ) = 6,
+        include_tx_data: bool,
+    } = 6,
     /// eth_getBlockTransactionCountByHash
-    GetBlockTransactionCountByHash(&'a B256) = 7,
+    GetBlockTransactionCountByHash { block_hash: &'a B256 } = 7,
     /// eth_getBlockTransactionCountByNumber
-    GetBlockTransactionCountByNumber(&'a BlockSpec) = 8,
+    GetBlockTransactionCountByNumber { block_spec: &'a BlockSpec } = 8,
     /// eth_getCode
-    GetCode(&'a Address, &'a Option<BlockSpec>) = 9,
+    GetCode {
+        address: &'a Address,
+        block_spec: &'a Option<BlockSpec>,
+    } = 9,
     /// eth_getLogs
-    GetLogs(&'a GetLogsInput) = 11,
+    GetLogs { params: &'a GetLogsInput } = 11,
     /// eth_getStorageAt
-    GetStorageAt(
-        &'a Address,
-        /// position
-        &'a U256,
-        &'a Option<BlockSpec>,
-    ) = 12,
+    GetStorageAt {
+        address: &'a Address,
+        position: &'a U256,
+        block_spec: &'a Option<BlockSpec>,
+    } = 12,
     /// eth_getTransactionByBlockHashAndIndex
-    GetTransactionByBlockHashAndIndex(&'a B256, &'a U256) = 13,
+    GetTransactionByBlockHashAndIndex {
+        block_hash: &'a B256,
+        index: &'a U256,
+    } = 13,
     /// eth_getTransactionByBlockNumberAndIndex
-    GetTransactionByBlockNumberAndIndex(&'a U256, &'a U256) = 14,
+    GetTransactionByBlockNumberAndIndex {
+        block_number: &'a U256,
+        index: &'a U256,
+    } = 14,
     /// eth_getTransactionByHash
-    GetTransactionByHash(&'a B256) = 15,
+    GetTransactionByHash { transaction_hash: &'a B256 } = 15,
     /// eth_getTransactionCount
-    GetTransactionCount(&'a Address, &'a Option<BlockSpec>) = 16,
+    GetTransactionCount {
+        address: &'a Address,
+        block_spec: &'a Option<BlockSpec>,
+    } = 16,
     /// eth_getTransactionReceipt
-    GetTransactionReceipt(&'a B256) = 17,
+    GetTransactionReceipt { transaction_hash: &'a B256 } = 17,
     /// net_version
-    NetVersion() = 18,
+    NetVersion = 18,
 }
 
 /// Error type for [`CacheableMethodInvocation::try_from`].
@@ -78,54 +99,86 @@ impl<'a> TryFrom<&'a MethodInvocation> for CacheableMethodInvocation<'a> {
 
     fn try_from(value: &'a MethodInvocation) -> Result<Self, Self::Error> {
         match value {
-            MethodInvocation::Call(transaction_input, block_spec) => Ok(
-                CacheableMethodInvocation::Call(transaction_input, block_spec),
-            ),
-            MethodInvocation::ChainId() => Ok(CacheableMethodInvocation::ChainId()),
-            MethodInvocation::EstimateGas(transaction_input, block_spec) => Ok(
-                CacheableMethodInvocation::EstimateGas(transaction_input, block_spec),
-            ),
-            MethodInvocation::FeeHistory(block_count, newest_block, percentiles) => Ok(
-                CacheableMethodInvocation::FeeHistory(block_count, newest_block, percentiles),
-            ),
+            MethodInvocation::Call(transaction_input, block_spec) => {
+                Ok(CacheableMethodInvocation::Call {
+                    transaction_input,
+                    block_spec,
+                })
+            }
+            MethodInvocation::ChainId() => Ok(CacheableMethodInvocation::ChainId),
+            MethodInvocation::EstimateGas(transaction_input, block_spec) => {
+                Ok(CacheableMethodInvocation::EstimateGas {
+                    transaction_input,
+                    block_spec,
+                })
+            }
+            MethodInvocation::FeeHistory(block_count, block_spec, percentiles) => {
+                Ok(CacheableMethodInvocation::FeeHistory {
+                    block_count,
+                    block_spec,
+                    percentiles,
+                })
+            }
             MethodInvocation::GetBalance(address, block_spec) => {
-                Ok(CacheableMethodInvocation::GetBalance(address, block_spec))
+                Ok(CacheableMethodInvocation::GetBalance {
+                    address,
+                    block_spec,
+                })
             }
-            MethodInvocation::GetBlockByNumber(block_spec, include_tx_data) => Ok(
-                CacheableMethodInvocation::GetBlockByNumber(block_spec, *include_tx_data),
-            ),
-            MethodInvocation::GetBlockByHash(hash, include_tx_data) => Ok(
-                CacheableMethodInvocation::GetBlockByHash(hash, *include_tx_data),
-            ),
-            MethodInvocation::GetBlockTransactionCountByHash(hash) => Ok(
-                CacheableMethodInvocation::GetBlockTransactionCountByHash(hash),
-            ),
-            MethodInvocation::GetBlockTransactionCountByNumber(block_spec) => Ok(
-                CacheableMethodInvocation::GetBlockTransactionCountByNumber(block_spec),
-            ),
+            MethodInvocation::GetBlockByNumber(block_spec, include_tx_data) => {
+                Ok(CacheableMethodInvocation::GetBlockByNumber {
+                    block_spec,
+                    include_tx_data: *include_tx_data,
+                })
+            }
+            MethodInvocation::GetBlockByHash(block_hash, include_tx_data) => {
+                Ok(CacheableMethodInvocation::GetBlockByHash {
+                    block_hash,
+                    include_tx_data: *include_tx_data,
+                })
+            }
+            MethodInvocation::GetBlockTransactionCountByHash(block_hash) => {
+                Ok(CacheableMethodInvocation::GetBlockTransactionCountByHash { block_hash })
+            }
+            MethodInvocation::GetBlockTransactionCountByNumber(block_spec) => {
+                Ok(CacheableMethodInvocation::GetBlockTransactionCountByNumber { block_spec })
+            }
             MethodInvocation::GetCode(address, block_spec) => {
-                Ok(CacheableMethodInvocation::GetCode(address, block_spec))
+                Ok(CacheableMethodInvocation::GetCode {
+                    address,
+                    block_spec,
+                })
             }
-            MethodInvocation::GetLogs(params) => Ok(CacheableMethodInvocation::GetLogs(params)),
-            MethodInvocation::GetStorageAt(address, position, block_spec) => Ok(
-                CacheableMethodInvocation::GetStorageAt(address, position, block_spec),
+            MethodInvocation::GetLogs(params) => Ok(CacheableMethodInvocation::GetLogs { params }),
+            MethodInvocation::GetStorageAt(address, position, block_spec) => {
+                Ok(CacheableMethodInvocation::GetStorageAt {
+                    address,
+                    position,
+                    block_spec,
+                })
+            }
+            MethodInvocation::GetTransactionByBlockHashAndIndex(block_hash, index) => Ok(
+                CacheableMethodInvocation::GetTransactionByBlockHashAndIndex { block_hash, index },
             ),
-            MethodInvocation::GetTransactionByBlockHashAndIndex(block_hash, index) => {
-                Ok(CacheableMethodInvocation::GetTransactionByBlockHashAndIndex(block_hash, index))
-            }
             MethodInvocation::GetTransactionByBlockNumberAndIndex(block_number, index) => Ok(
-                CacheableMethodInvocation::GetTransactionByBlockNumberAndIndex(block_number, index),
+                CacheableMethodInvocation::GetTransactionByBlockNumberAndIndex {
+                    block_number,
+                    index,
+                },
             ),
-            MethodInvocation::GetTransactionByHash(hash) => {
-                Ok(CacheableMethodInvocation::GetTransactionByHash(hash))
+            MethodInvocation::GetTransactionByHash(transaction_hash) => {
+                Ok(CacheableMethodInvocation::GetTransactionByHash { transaction_hash })
             }
-            MethodInvocation::GetTransactionCount(address, block_spec) => Ok(
-                CacheableMethodInvocation::GetTransactionCount(address, block_spec),
-            ),
-            MethodInvocation::GetTransactionReceipt(hash) => {
-                Ok(CacheableMethodInvocation::GetTransactionReceipt(hash))
+            MethodInvocation::GetTransactionCount(address, block_spec) => {
+                Ok(CacheableMethodInvocation::GetTransactionCount {
+                    address,
+                    block_spec,
+                })
             }
-            MethodInvocation::NetVersion() => Ok(CacheableMethodInvocation::NetVersion()),
+            MethodInvocation::GetTransactionReceipt(transaction_hash) => {
+                Ok(CacheableMethodInvocation::GetTransactionReceipt { transaction_hash })
+            }
+            MethodInvocation::NetVersion() => Ok(CacheableMethodInvocation::NetVersion),
 
             // Explicit to make sure if a new method is added, it is not forgotten here.
             MethodInvocation::Accounts()
