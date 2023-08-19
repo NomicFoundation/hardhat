e1a2f22c42f0b6db9db1697f20f5aee5d176a02b
Luis Schaab <schaable@gmail.com>
2023-08-19 12:00:30 -0300

    Add more tests to client getters

--- packages/hardhat-viem/test/clients.ts
+++ packages/hardhat-viem/test/clients.ts
@@ -3,7 +3,11 @@ import type { EthereumProvider } from "hardhat/types";
 import { expect, assert } from "chai";
 import * as chains from "viem/chains";
 
-import { _getPublicClient, _getWalletClients } from "../src/internal/clients";
+import {
+  _getPublicClient,
+  _getTestClient,
+  _getWalletClients,
+} from "../src/internal/clients";
 import { EthereumMockedProvider } from "./mocks/provider";
 
 describe("clients", () => {
@@ -16,6 +20,27 @@ describe("clients", () => {
       assert.isDefined(client);
       expect(client.type).to.equal("publicClient");
       expect(client.chain!.id).to.equal(chains.mainnet.id);
+    });
+
+    it("should return a public client with custom parameters", async () => {
+      const provider: EthereumProvider = new EthereumMockedProvider();
+
+      const client = await _getPublicClient(provider, chains.mainnet, {
+        pollingInterval: 1000,
+        cacheTime: 2000,
+      });
+
+      expect(client.pollingInterval).to.equal(1000);
+      expect(client.cacheTime).to.equal(2000);
+    });
+
+    it("should return a public client with default parameters for development networks", async () => {
+      const provider: EthereumProvider = new EthereumMockedProvider();
+
+      const client = await _getPublicClient(provider, chains.hardhat);
+
+      expect(client.pollingInterval).to.equal(50);
+      expect(client.cacheTime).to.equal(0);
     });
   });
 
@@ -33,7 +58,13 @@ describe("clients", () => {
 
   describe("_getTestClient", () => {
     it("should return a test client", async () => {
-      // TODO
+      const provider: EthereumProvider = new EthereumMockedProvider();
+
+      const client = await _getTestClient(provider, chains.mainnet);
+
+      assert.isDefined(client);
+      expect(client.type).to.equal("testClient");
+      expect(client.chain!.id).to.equal(chains.mainnet.id);
     });
   });
 });
