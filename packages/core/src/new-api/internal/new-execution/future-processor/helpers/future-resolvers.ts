4840eab44248675c8b1c0c8dd9b02811bffc5de2
John Kane <john@kanej.me>
2023-08-21 20:10:05 +0100

    chore: remove answered todo comment

--- packages/core/src/new-api/internal/new-execution/future-processor/helpers/future-resolvers.ts
+++ packages/core/src/new-api/internal/new-execution/future-processor/helpers/future-resolvers.ts
@@ -108,8 +108,6 @@ export function resolveLibraries(
   return Object.fromEntries(
     Object.entries(libraries).map(([key, lib]) => [
       key,
-      // TODO: should this be expanded to static call and other future types?
-      // we could swap to findResult and assert the result is an address?
       findAddressForContractFuture(deploymentState, lib.id),
     ])
   );
