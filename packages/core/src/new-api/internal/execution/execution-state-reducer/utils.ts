76cf056c4a2340e0465b1e20aead49d6c0784186
Patricio Palladino <email@patriciopalladino.com>
2023-08-23 00:03:39 +0000

    Delete the old execution except for its tests

--- packages/core/src/new-api/internal/execution/execution-state-reducer/utils.ts
+++
@@ -1,5 +0,0 @@
-import { IgnitionError } from "../../../../errors";
-
-export function assertUnknownMessageType(message: never): any {
-  throw new IgnitionError(`Unknown message type ${JSON.stringify(message)}`);
-}
