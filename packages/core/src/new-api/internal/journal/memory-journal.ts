b563bc7456943e080bed09e9668bfc93747dfaec
Patricio Palladino <email@patriciopalladino.com>
2023-08-23 20:57:08 +0000

    Simplify memory journal

--- packages/core/src/new-api/internal/journal/memory-journal.ts
+++ packages/core/src/new-api/internal/journal/memory-journal.ts
@@ -1,9 +1,7 @@
 import { JournalMessage } from "../new-execution/types/messages";
 
 import { Journal } from "./types";
-import { deserializeReplacer } from "./utils/deserialize-replacer";
 import { logJournalableMessage } from "./utils/log";
-import { serializeReplacer } from "./utils/serialize-replacer";
 
 /**
  * An in-memory journal.
@@ -11,23 +9,18 @@ import { serializeReplacer } from "./utils/serialize-replacer";
  * @beta
  */
 export class MemoryJournal implements Journal {
-  private messages: string[] = [];
+  private messages: JournalMessage[] = [];
 
   constructor(private _verbose: boolean = false) {}
 
   public record(message: JournalMessage): void {
     this._log(message);
 
-    this.messages.push(JSON.stringify(message, serializeReplacer.bind(this)));
+    this.messages.push(message);
   }
 
   public async *read(): AsyncGenerator<JournalMessage> {
-    for (const entry of this.messages) {
-      const message: JournalMessage = JSON.parse(
-        entry,
-        deserializeReplacer.bind(this)
-      );
-
+    for (const message of this.messages) {
       yield message;
     }
   }
