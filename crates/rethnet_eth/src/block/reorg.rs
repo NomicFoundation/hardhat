c0c973e7d3489a723c02cbe9a9419c307a3731da
Agost Biro <agostbiro@gmail.com>
2023-08-25 16:52:09 +0200

    Change safe block threshold to 32

--- crates/rethnet_eth/src/block/reorg.rs
+++ crates/rethnet_eth/src/block/reorg.rs
@@ -10,24 +10,17 @@ pub fn largest_safe_block_number(chain_id: &U256, latest_block_number: &U256) ->
 ///
 /// # Source
 ///
-/// These numbers were taken from:
+/// The custom numbers were taken from:
 /// <https://github.com/NomicFoundation/hardhat/blob/caa504fe0e53c183578f42d66f4740b8ec147051/packages/hardhat-core/src/internal/hardhat-network/provider/utils/reorgs-protection.ts>
 pub fn largest_possible_reorg(chain_id: &U256) -> U256 {
     let chain_id: u64 = chain_id.try_into().expect("invalid chain id");
     let threshold: u64 = match chain_id {
-        // mainnet
-        1 => 5,
         // Ropsten
         3 => 100,
-        // Rinkeby
-        4 => 5,
-        // Goerli
-        5 => 5,
-        // Kovan
-        42 => 5,
         // xDai
         100 => 38,
-        _ => 30,
+        // One epoch on Ethereum mainnet
+        _ => 32,
     };
     U256::from(threshold)
 }
