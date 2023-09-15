8388720eac382d053c450199ee5f2bebdb843948
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-09-15 19:42:08 +0200

    Add "debug_traceCall" JSON-RPC method (#4353)
    Added support for the RPC method debug_traceCall.

--- docs/src/content/hardhat-network/docs/reference/index.md
+++ docs/src/content/hardhat-network/docs/reference/index.md
@@ -253,6 +253,50 @@ To customise it, take a look at [the configuration section](/config/index.md#har
 
 ### Standard methods
 
+#### `debug_traceCall`
+
+Traces the execution of an `eth_call` within the context of a specific block's execution. See the [Geth's documentation](https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-debug#debugtracecall) for more info.
+
+Arguments:
+
+- transaction object
+- blockTag: optional, default value is "latest"
+- traceConfig: optional object with the following properties:
+  - disableMemory: optional boolean, default value is false
+  - disableStack: optional boolean, default value is false
+  - disableStorage: optional boolean, default value is false
+
+Example without traceConfig:
+
+```js
+const result = await network.provider.send("debug_traceCall", [
+  {
+    from: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    to: "0x4b23ad35Da73fEe8154CDc8b291c814028A4E743",
+    data: "0xc0129d43",
+  },
+  "latest",
+]);
+```
+
+Example with traceConfig:
+
+```js
+const trace = await network.provider.send("debug_traceCall", [
+  {
+    from: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    to: "0x4b23ad35Da73fEe8154CDc8b291c814028A4E743",
+    data: "0xc0129d43",
+  },
+  "latest",
+  {
+    disableMemory: true,
+    disableStack: true,
+    disableStorage: true,
+  },
+]);
+```
+
 #### `debug_traceTransaction`
 
 Get debug traces of already-mined transactions.
--- packages/hardhat-core/src/internal/hardhat-network/provider/modules/debug.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/modules/debug.ts
@@ -1,5 +1,13 @@
 import { rpcHash } from "../../../core/jsonrpc/types/base-types";
 import {
+  OptionalRpcNewBlockTag,
+  optionalRpcNewBlockTag,
+} from "../../../core/jsonrpc/types/input/blockTag";
+import {
+  RpcCallRequest,
+  rpcCallRequest,
+} from "../../../core/jsonrpc/types/input/callRequest";
+import {
   rpcDebugTracingConfig,
   RpcDebugTracingConfig,
 } from "../../../core/jsonrpc/types/input/debugTraceTransaction";
@@ -10,17 +18,22 @@ import {
 } from "../../../core/providers/errors";
 import { HardhatNode } from "../node";
 import { RpcDebugTraceOutput } from "../output";
+import { Base } from "./base";
 
 /* eslint-disable @nomicfoundation/hardhat-internal-rules/only-hardhat-error */
 
-export class DebugModule {
-  constructor(private readonly _node: HardhatNode) {}
+export class DebugModule extends Base {
+  constructor(_node: HardhatNode) {
+    super(_node);
+  }
 
   public async processRequest(
     method: string,
     params: any[] = []
   ): Promise<any> {
     switch (method) {
+      case "debug_traceCall":
+        return this._traceCallAction(...this._traceCallParams(params));
       case "debug_traceTransaction":
         return this._traceTransactionAction(
           ...this._traceTransactionParams(params)
@@ -28,6 +41,34 @@ export class DebugModule {
     }
 
     throw new MethodNotFoundError(`Method ${method} not found`);
+  }
+
+  // debug_traceCall
+
+  private _traceCallParams(
+    params: any[]
+  ): [RpcCallRequest, OptionalRpcNewBlockTag, RpcDebugTracingConfig] {
+    const validatedParams = validateParams(
+      params,
+      rpcCallRequest,
+      optionalRpcNewBlockTag,
+      rpcDebugTracingConfig
+    );
+
+    this._validateTracerParam(validatedParams[2]);
+
+    return validatedParams;
+  }
+
+  private async _traceCallAction(
+    callConfig: RpcCallRequest,
+    block: OptionalRpcNewBlockTag,
+    traceConfig: RpcDebugTracingConfig
+  ): Promise<RpcDebugTraceOutput> {
+    const callParams = await this.rpcCallRequestToNodeCallParams(callConfig);
+    const blockNumber = await this.resolveNewBlockTag(block);
+
+    return this._node.traceCall(callParams, blockNumber, traceConfig);
   }
 
   // debug_traceTransaction
@@ -46,18 +87,18 @@ export class DebugModule {
     return validatedParams;
   }
 
+  private async _traceTransactionAction(
+    hash: Buffer,
+    config: RpcDebugTracingConfig
+  ): Promise<RpcDebugTraceOutput> {
+    return this._node.traceTransaction(hash, config);
+  }
+
   private _validateTracerParam(config: RpcDebugTracingConfig) {
     if (config?.tracer !== undefined) {
       throw new InvalidArgumentsError(
         "Hardhat currently only supports the default tracer, so no tracer parameter should be passed."
       );
     }
-  }
-
-  private async _traceTransactionAction(
-    hash: Buffer,
-    config: RpcDebugTracingConfig
-  ): Promise<RpcDebugTraceOutput> {
-    return this._node.traceTransaction(hash, config);
   }
 }
