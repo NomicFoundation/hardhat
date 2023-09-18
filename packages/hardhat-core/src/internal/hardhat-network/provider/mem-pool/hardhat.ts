2ce355509f5a64f4429db86515b1780dcef2f1e0
Wodann <Wodann@users.noreply.github.com>
2023-09-18 23:31:24 +0000

    fix: remove transaction from mempool in Hardhat

--- packages/hardhat-core/src/internal/hardhat-network/provider/mem-pool/hardhat.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/mem-pool/hardhat.ts
@@ -87,7 +87,7 @@ export class HardhatMemPool implements MemPoolAdapter {
 
     if (tx === undefined) {
       // transaction doesn't exist in the mempool
-      return true;
+      return false;
     }
 
     this._deleteTransactionByHash(hash);
