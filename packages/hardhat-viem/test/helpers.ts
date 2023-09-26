5f5c0751947162d853bdfce51b24fa2ec75e607f
Luis Schaab <schaable@gmail.com>
2023-09-26 13:57:13 -0300

    Snapshot testing

--- packages/hardhat-viem/test/helpers.ts
+++ packages/hardhat-viem/test/helpers.ts
@@ -1,6 +1,9 @@
 import type { HardhatRuntimeEnvironment } from "hardhat/types";
 
 import path from "path";
+import fs from "fs/promises";
+import { assert } from "chai";
+import { diffLinesUnified } from "jest-diff";
 import { resetHardhatContext } from "hardhat/plugins-testing";
 
 // Import this plugin type extensions for the HardhatRuntimeEnvironment
@@ -25,4 +28,31 @@ export const useEnvironment = (fixtureProjectName: string): void => {
     resetHardhatContext();
     delete process.env.HARDHAT_NETWORK;
   });
+};
+
+export const assertSnapshotMatch = async (
+  snapshotPath: string,
+  generatedFilePath: string
+) => {
+  const expectedSnapshotContent = await fs.readFile(snapshotPath, "utf-8");
+  const generatedFileContent = await fs.readFile(generatedFilePath, "utf-8");
+
+  if (expectedSnapshotContent !== generatedFileContent) {
+    assert.fail(`
+Generated file differs from the expected snapshot:
+
+${generatedFilePath} should match ${snapshotPath}
+
+To update the snapshot, run:
+yarn snapshots:update
+
+${diffLinesUnified(
+  expectedSnapshotContent.split("\n"),
+  generatedFileContent.split("\n"),
+  {
+    contextLines: 3,
+    expand: false,
+  }
+)}`);
+  }
 };
