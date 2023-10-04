769966b2f4897df46edf8fff33dd545a9937d4d1
Luis Schaab <schaable@gmail.com>
2023-10-04 20:52:19 -0300

    Copy the toolbox package and update it to reference viem

---
+++ packages/hardhat-toolbox-viem/test/test.ts
@@ -0,0 +1,34 @@
+import { assert } from "chai";
+
+import { useEnvironment } from "./helpers";
+
+describe("hardhat-toolbox", function () {
+  describe("only-toolbox", function () {
+    useEnvironment("only-toolbox");
+
+    it("has all the expected things in the HRE", async function () {
+      await this.env.run("run", {
+        noCompile: true,
+        script: "script.js",
+      });
+
+      assert.equal(process.exitCode, 0);
+    });
+  });
+
+  describe("hardhat-gas-reporter-config", function () {
+    useEnvironment("with-gas-reporter-config");
+
+    it("Should not crash while loading the HRE", async function () {
+      assert.isDefined(this.env);
+    });
+  });
+
+  describe("typechain config", function () {
+    useEnvironment("typechain-config");
+
+    it("should disable typechain overrides for js projects", async function () {
+      assert.isTrue(this.env.config.typechain.dontOverrideCompile);
+    });
+  });
+});
