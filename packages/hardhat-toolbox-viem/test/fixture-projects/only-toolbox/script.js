4a045f25720e2c62c1d8fe68bdc920c00d01b807
Luis Schaab <schaable@gmail.com>
2023-10-05 09:51:16 -0300

    Add viem

--- packages/hardhat-toolbox-viem/test/fixture-projects/only-toolbox/script.js
+++ packages/hardhat-toolbox-viem/test/fixture-projects/only-toolbox/script.js
@@ -1,6 +1,9 @@
 const assert = require("assert");
 
 async function main() {
+  // check that viem exists
+  assert(viem !== undefined);
+
   // check that the expected tasks are there
   const taskNames = Object.keys(tasks);
   assert(taskNames.includes("verify"));
