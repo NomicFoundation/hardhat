a82d5ec3728d92ca387c8ff111d530d70a91baee
Luis Schaab <schaable@gmail.com>
2023-10-07 16:19:06 -0300

    Update hardhat init to include a viem project

---
+++ packages/hardhat-core/sample-projects/typescript-viem/scripts/deploy.ts
@@ -0,0 +1,26 @@
+import viem from "viem";
+import hre from "hardhat";
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = BigInt(currentTimestampInSeconds + 60);
+
+  const lockedAmount = viem.parseEther("0.001");
+
+  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  console.log(
+    `Lock with ${viem.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
