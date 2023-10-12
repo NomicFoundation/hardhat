5fc8124ff608878283af06e76de568a2b9e473b2
John Kane <john@kanej.me>
2023-10-12 11:26:29 +0100

    improvement: include error 602 in whitelist
    This is the error if during a wipe, the future can't be cleared because
    it has dependents that have state.

--- packages/hardhat-plugin/src/utils/shouldBeHardhatPluginError.ts
+++ packages/hardhat-plugin/src/utils/shouldBeHardhatPluginError.ts
@@ -1,9 +1,9 @@
 import { IgnitionError } from "@nomicfoundation/ignition-core";
 
-/* 
+/*
   This is a whitelist of error codes that should be rethrown as NomicLabsHardhatPluginError.
  */
-const whitelist = ["600", "601", "800"];
+const whitelist = ["600", "601", "602", "800"];
 
 export function shouldBeHardhatPluginError(error: IgnitionError): boolean {
   const code = error.message.match(/IGN([0-9]+):/)![1];
