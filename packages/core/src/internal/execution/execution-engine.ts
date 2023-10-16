80464602018834d4211396c57de353c9bfbbde0d
Patricio Palladino <email@patriciopalladino.com>
2023-10-16 20:22:31 +0000

    Update error messages and delete an unnecessary error

--- packages/core/src/internal/execution/execution-engine.ts
+++ packages/core/src/internal/execution/execution-engine.ts
@@ -12,6 +12,7 @@ import {
   IgnitionModuleResult,
 } from "../../types/module";
 import { DeploymentLoader } from "../deployment-loader/types";
+import { assertIgnitionInvariant } from "../utils/assertions";
 import { getFuturesFromModule } from "../utils/get-futures-from-module";
 import { getPendingNonceAndSender } from "../views/execution-state/get-pending-nonce-and-sender";
 import { hasExecutionSucceeded } from "../views/has-execution-succeeded";
@@ -238,9 +239,10 @@ export class ExecutionEngine {
   private _lookupFuture(futures: Future[], futureId: string): Future {
     const future = futures.find((f) => f.id === futureId);
 
-    if (future === undefined) {
-      throw new IgnitionError(ERRORS.EXECUTION.FUTURE_NOT_FOUND);
-    }
+    assertIgnitionInvariant(
+      future !== undefined,
+      `Future ${futureId} not found`
+    );
 
     return future;
   }
