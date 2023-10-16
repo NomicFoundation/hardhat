306c7413fa098ff85b6779dc117af503452e5930
Agost Biro <agostbiro@gmail.com>
2023-10-16 15:13:35 +0200

    Improve reporting

--- crates/tools/src/benchmark.rs
+++ crates/tools/src/benchmark.rs
@@ -1,15 +1,15 @@
+use std::io::Write;
 use std::path::PathBuf;
-use std::time::Duration;
 
 use anyhow::Result;
 
 /// Run a command iterations times and print the time it takes to run each time.
 pub(crate) fn run(working_directory: PathBuf, test_command: &str, iterations: usize) -> Result<()> {
-    let mut times = Vec::new();
+    let mut deltas = Vec::new();
 
     // Run test command in working directory iterations times and capture the time it takes to run
-    for i in 0..iterations {
-        println!("Running {}/{iterations} `{test_command}`", i + 1);
+    for i in 1..(iterations + 1) {
+        println!("Running {i}/{iterations} `{test_command}`");
         let start = std::time::Instant::now();
         let output = if cfg!(target_os = "windows") {
             std::process::Command::new("cmd")
@@ -25,18 +25,32 @@ pub(crate) fn run(working_directory: PathBuf, test_command: &str, iterations: us
         };
         let end = std::time::Instant::now();
 
+        let delta = (end - start).as_secs();
         if !output.status.success() {
+            let mut stdout = std::io::stdout();
+            stdout.write_all(&output.stdout)?;
+            stdout.flush()?;
+            let mut stderr = std::io::stderr();
+            stderr.write_all(&output.stderr)?;
+            stderr.flush()?;
+
             anyhow::bail!(
-                "Benchmark failed with exit code: {}",
-                output.status.code().unwrap_or(-1)
+                "Command `{}` failed in {} seconds with exit code {}",
+                test_command,
+                delta,
+                output
+                    .status
+                    .code()
+                    .map_or_else(|| "N/A".to_string(), |c| c.to_string())
             );
         }
 
-        times.push(end - start);
+        println!("Command `{test_command}` succeeded in {delta} seconds");
+
+        deltas.push(delta);
     }
 
-    let seconds = times.iter().map(Duration::as_secs).collect::<Vec<_>>();
-    println!("Benchmark results (seconds): {seconds:?}");
+    println!("Benchmark results (seconds): {deltas:?}");
 
     Ok(())
 }
