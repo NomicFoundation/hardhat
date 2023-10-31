736b2d14706ba12c4435b9c209121dbd7a59ce56
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-10-31 11:59:32 +0100

    throw error in test

--- packages/hardhat-core/test/internal/hardhat-network/stack-traces/test-files/0_8/file-level-event/c.sol
+++ packages/hardhat-core/test/internal/hardhat-network/stack-traces/test-files/0_8/file-level-event/c.sol
@@ -3,13 +3,14 @@
 // LogFileEvent is available ONLY from versions >= 0.8.22.
 // Smoke test to check that 0.8.22 is setup correctly.
 
-pragma solidity 0.8.22;
+pragma solidity ^0.8.22;
 
 // File-level event definition
 event LogFileEvent();
 
 contract C {
-    function emitEvent() public {
+    function f() public {
         emit LogFileEvent();
+        require(false, "error reason");
     }
 }
