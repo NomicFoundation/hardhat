4bee0e17ee4597c2530cdb19b1ce4bcf1a2f1eca
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-11-27 19:48:31 +0100

    fix flatten test to replace hardhat version automatically

--- e2e/fixture-projects/flatten/test.sh
+++ e2e/fixture-projects/flatten/test.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env sh
 
+# replace the hardhat version in the files used to check that the output is correct because it changes in every release
+sed -i "s/hardhat v[0-9]*\.[0-9]*\.[0-9]*/hardhat v$1/g" "flatten_all_files.txt"
+sed -i "s/hardhat v[0-9]*\.[0-9]*\.[0-9]*/hardhat v$1/g" "flatten_lock_file.txt"
+
 # it should flatten all the files in the folder because no file names are passed
 if ! npx hardhat flatten | diff - flatten_all_files.txt; then
     echo "The flatten file is different from the flatten_all_files.txt"
@@ -11,9 +15,3 @@ if ! npx hardhat flatten contracts/Lock.sol | diff - flatten_lock_file.txt; then
     echo "The flatten file is different from the flatten_lock_file.txt"
     exit 1
 fi
-
-
-
-
-
-
