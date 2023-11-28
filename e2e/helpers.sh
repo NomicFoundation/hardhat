5f7e05a3935472a33ebf14bbb90746f78616ad6d
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-11-28 15:27:27 +0100

    add helper function to log errors

---
+++ e2e/helpers.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env sh
+
+print_error_msg() {
+  echo "\033[31mERROR: $1\033[0m"
+}
--- e2e/run-fixture-projects.sh
+++ e2e/run-fixture-projects.sh
@@ -11,14 +11,15 @@ HARDHAT_TGZ_FILE=$(pnpm pack)
 echo "[e2e] Built $HARDHAT_TGZ_FILE"
 cd - >/dev/null
 
-HARDHAT_VERSION=$(echo $HARDHAT_TGZ_FILE | grep -oP 'hardhat-\K[0-9]+\.[0-9]+\.[0-9]+')
+HARDHAT_VERSION=$(echo $HARDHAT_TGZ_FILE | grep -o -P 'hardhat-\K[0-9]+\.[0-9]+\.[0-9]+')
 
 # create a temporary directory to run the tests
 FIXTURE_PROJECTS_DIR=fixture-projects-run-$(date +%Y-%m-%d-%H-%M-%S)
 cp -r fixture-projects $FIXTURE_PROJECTS_DIR
 
 # run all the e2e tests in the temporary directory
-echo "[e2e] Running tests in $FIXTURE_PROJECTS_DIR"
+echo "[e2e] Running tests in $FIXTURE_PROJECTS_DIR\n\n"
+
 for dir in ${FIXTURE_PROJECTS_DIR}/*; do
   if [ -d "$dir" ]; then
     echo "[e2e] Running tests in $dir"
@@ -33,6 +34,8 @@ for dir in ${FIXTURE_PROJECTS_DIR}/*; do
     # E.g.: in the flatten task
     ./test.sh $HARDHAT_VERSION
     cd -
+
+    echo "[e2e] Finished test in $dir\n\n"
   fi
 done
 
