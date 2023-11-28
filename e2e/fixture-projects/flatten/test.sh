3d41ff6fd74e4456da48dd06d3fcb0334251ab6f
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2023-11-28 15:31:19 +0100

    add error functions in tests scripts and print test names

--- e2e/fixture-projects/flatten/test.sh
+++ e2e/fixture-projects/flatten/test.sh
@@ -1,17 +1,22 @@
 #! /usr/bin/env sh
 
+# import helpers functions
+. ../../helpers.sh
+
+echo "Running tests: $(basename "$(pwd)")"
+
 # replace the hardhat version in the files used to check that the output is correct because it changes in every release
 sed -i "s/hardhat v[0-9]*\.[0-9]*\.[0-9]*/hardhat v$1/g" "flatten_all_files.txt"
 sed -i "s/hardhat v[0-9]*\.[0-9]*\.[0-9]*/hardhat v$1/g" "flatten_lock_file.txt"
 
-# it should flatten all the files in the folder because no file names are passed
-if ! npx hardhat flatten | diff - flatten_all_files.txt; then
-    echo "The flatten file is different from the flatten_all_files.txt"
-    exit 1
+echo "it should flatten all the files in the folder because no file names are passed"
+if ! npx hardhat flatten 2>stderr | diff - flatten_all_files.txt; then
+  print_error_msg "The flatten file is different from the flatten_all_files.txt"
+  exit 1
 fi
 
-# it should flatten only the 'Lock.sol' file because it is passed as an argument
-if ! npx hardhat flatten contracts/Lock.sol | diff - flatten_lock_file.txt; then
-    echo "The flatten file is different from the flatten_lock_file.txt"
-    exit 1
+echo "it should flatten only the 'Lock.sol' file because it is passed as an argument"
+if ! npx hardhat flatten contracts/Lock.sol 2>stderr | diff - flatten_lock_file.txt; then
+  print_error_msg "The flatten file is different from the flatten_lock_file.txt"
+  exit 1
 fi
--- e2e/fixture-projects/minimal-project/test.sh
+++ e2e/fixture-projects/minimal-project/test.sh
@@ -2,5 +2,8 @@
 # fail if any commands fails
 set -e
 
-npx hardhat compile
-npx hardhat test
+echo "Running tests: $(basename "$(pwd)")"
+
+echo "it should compile and run the tests in the project folder"
+npx hardhat compile >stdout 2>stderr
+npx hardhat test >stdout 2>stderr
