a444a93a35960269e7955940a8b0432a49afa1c2
4rgon4ut <egorlaptev98@gmail.com>
2023-11-28 21:02:44 +0300

    chore(test): fix chain-config test

--- packages/hardhat-verify/test/unit/chain-config.ts
+++ packages/hardhat-verify/test/unit/chain-config.ts
@@ -4,21 +4,12 @@ import { builtinChains } from "../../src/internal/chain-config";
 describe("Chain Config", () => {
   describe("builtinChains", () => {
     it("should have no duplicate chain ids", () => {
-      // check that xdai/gnosis is the only duplicate
-      const xdaiGnosisChains = builtinChains.filter(
-        ({ chainId }) => chainId === 100
-      );
-      assert.lengthOf(xdaiGnosisChains, 2);
-
-      // check that there are no duplicates in the rest of the list
-      const filteredChainIds = builtinChains.filter(
-        ({ chainId }) => chainId !== 100
+      const chainIds = builtinChains.map(({ chainId }) => chainId);
+      const duplicatedIds = chainIds.filter(
+        (id, index) => chainIds.indexOf(id) !== index
       );
 
-      const uniqueIds = [...new Set(filteredChainIds)];
-
-      assert.notEqual(0, uniqueIds.length);
-      assert.equal(uniqueIds.length, filteredChainIds.length);
+      assert.isEmpty(duplicatedIds, `Duplicate chainIds found: ${duplicatedIds.join(', ')}`);
     });
 
     it("should be sorted by chainId in ascending order", () => {
