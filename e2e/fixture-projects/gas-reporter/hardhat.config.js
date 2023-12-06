03a84454d2dd1cf07c60652522a8a469351a290d
Franco Victorio <victorio.franco@gmail.com>
2023-12-06 13:55:07 +0100

    Use toolbox in e2e fixture projects

--- e2e/fixture-projects/gas-reporter/hardhat.config.js
+++ e2e/fixture-projects/gas-reporter/hardhat.config.js
@@ -1,8 +1,5 @@
-require("hardhat-gas-reporter");
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.20",
-  gasReporter: {
-    enabled: process.env.REPORT_GAS ? true : false,
-  },
 };
--- e2e/fixture-projects/solidity-coverage/hardhat.config.js
+++ e2e/fixture-projects/solidity-coverage/hardhat.config.js
@@ -1,4 +1,4 @@
-require("solidity-coverage");
+require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
   solidity: "0.8.20",
