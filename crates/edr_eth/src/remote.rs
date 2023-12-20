10a0d1b3ceb42a0f5e10e21c284e3cffda94496d
Agost Biro <agostbiro@gmail.com>
2023-12-20 16:44:26 +0100

    ci: fix and enable block tag integration tests

--- crates/edr_eth/src/remote.rs
+++ crates/edr_eth/src/remote.rs
@@ -11,34 +11,137 @@ mod r#override;
 mod request_methods;
 
 use std::{
+    borrow::Cow,
     fmt,
     fmt::{Display, Formatter},
 };
+
+use serde::de;
 
 pub use self::{
     client::{RpcClient, RpcClientError},
     r#override::*,
 };
-use crate::B256;
+use crate::{B256, U64};
+
+const BLOCK_HASH_FIELD: &str = "blockHash";
+const REQUIRE_CANONICAL_FIELD: &str = "requireCanonical";
+const BLOCK_NUMBER_FIELD: &str = "blockNumber";
 
 /// for representing block specifications per EIP-1898
-#[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+#[derive(Clone, Debug, PartialEq, serde::Serialize)]
 #[serde(untagged)]
 pub enum Eip1898BlockSpec {
     /// to represent the Object { blockHash, requireCanonical } in EIP-1898
+    #[serde(rename_all = "camelCase")]
     Hash {
         /// the block hash
         block_hash: B256,
         /// whether the server should additionally raise a JSON-RPC error if the
         /// block is not in the canonical chain
+        #[serde(skip_serializing_if = "Option::is_none")]
         require_canonical: Option<bool>,
     },
     /// to represent the Object { blockNumber } in EIP-1898
+    #[serde(rename_all = "camelCase")]
     Number {
         /// the block number
-        #[serde(with = "crate::serde::u64")]
+        #[serde(serialize_with = "crate::serde::u64::serialize")]
         block_number: u64,
     },
+}
+
+// We are not using a derived implementation for untagged enums, because we want
+// to be able to error on invalid combinations of fields
+impl<'de> serde::Deserialize<'de> for Eip1898BlockSpec {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(serde::Deserialize)]
+        #[serde(field_identifier, rename_all = "camelCase")]
+        enum Field {
+            BlockHash,
+            RequireCanonical,
+            BlockNumber,
+        }
+
+        // https://serde.rs/deserialize-struct.html
+        struct Eip1898BlockSpecVisitor;
+        impl<'de> de::Visitor<'de> for Eip1898BlockSpecVisitor {
+            type Value = Eip1898BlockSpec;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                formatter.write_str("EIP-1898 block spec")
+            }
+
+            fn visit_map<M>(self, mut map: M) -> Result<Eip1898BlockSpec, M::Error>
+            where
+                M: de::MapAccess<'de>,
+            {
+                let mut block_hash = None;
+                let mut block_number: Option<Cow<'de, str>> = None;
+                let mut require_canonical = None;
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        Field::BlockHash => {
+                            if block_hash.is_some() {
+                                return Err(de::Error::duplicate_field(BLOCK_HASH_FIELD));
+                            }
+                            block_hash = Some(map.next_value()?);
+                        }
+                        Field::RequireCanonical => {
+                            if require_canonical.is_some() {
+                                return Err(de::Error::duplicate_field(REQUIRE_CANONICAL_FIELD));
+                            }
+                            require_canonical = Some(map.next_value()?);
+                        }
+                        Field::BlockNumber => {
+                            if block_number.is_some() {
+                                return Err(de::Error::duplicate_field(BLOCK_NUMBER_FIELD));
+                            }
+                            block_number = Some(map.next_value()?);
+                        }
+                    }
+                }
+
+                if block_hash.is_some() && block_number.is_some() {
+                    return Err(de::Error::custom(format!(
+                        "EIP-1898 block spec cannot have both `{BLOCK_HASH_FIELD}` and `{BLOCK_NUMBER_FIELD}`",
+                    )));
+                }
+
+                if block_number.is_some() && require_canonical.is_some() {
+                    return Err(de::Error::custom(format!(
+                        "EIP-1898 block spec cannot have both `{BLOCK_NUMBER_FIELD}` and `{REQUIRE_CANONICAL_FIELD}`",
+                    )));
+                }
+
+                if let Some(block_hash) = block_hash {
+                    Ok(Eip1898BlockSpec::Hash {
+                        block_hash,
+                        require_canonical,
+                    })
+                } else if let Some(block_number) = block_number {
+                    let block_number: U64 = de::Deserialize::deserialize(
+                        de::value::CowStrDeserializer::new(block_number),
+                    )?;
+                    Ok(Eip1898BlockSpec::Number {
+                        block_number: block_number.try_into().expect("U64 fits into u64"),
+                    })
+                } else {
+                    Err(de::Error::custom("Invalid EIP-1898 block spec"))
+                }
+            }
+        }
+
+        const FIELDS: &[&str] = &[
+            BLOCK_HASH_FIELD,
+            REQUIRE_CANONICAL_FIELD,
+            BLOCK_NUMBER_FIELD,
+        ];
+        deserializer.deserialize_struct("Eip1898BlockSpec", FIELDS, Eip1898BlockSpecVisitor)
+    }
 }
 
 impl Display for Eip1898BlockSpec {
@@ -83,15 +186,63 @@ impl Display for BlockTag {
 }
 
 /// For specifying a block
-#[derive(Clone, Debug, PartialEq, serde::Deserialize, serde::Serialize)]
+#[derive(Clone, Debug, PartialEq, serde::Serialize)]
 #[serde(untagged)]
 pub enum BlockSpec {
     /// as a block number
-    Number(#[serde(with = "crate::serde::u64")] u64),
+    Number(#[serde(serialize_with = "crate::serde::u64::serialize")] u64),
     /// as a block tag (eg "latest")
     Tag(BlockTag),
     /// as an EIP-1898-compliant block specifier
     Eip1898(Eip1898BlockSpec),
+}
+
+// We are not using a derived implementation for untagged enums, because we want
+// to propagate custom error messages from the `Eip1898BlockSpec` deserializer.
+impl<'de> de::Deserialize<'de> for BlockSpec {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: de::Deserializer<'de>,
+    {
+        struct BlockSpecVisitor;
+
+        impl<'de> de::Visitor<'de> for BlockSpecVisitor {
+            type Value = BlockSpec;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
+                formatter.write_str("a block specifier")
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                let result = if v.starts_with("0x") {
+                    let number: U64 =
+                        de::Deserialize::deserialize(de::value::StrDeserializer::new(v))?;
+                    BlockSpec::Number(number.try_into().expect("U64 fits into u64"))
+                } else {
+                    // Forward to deserializer of `BlockTag`
+                    BlockSpec::Tag(de::Deserialize::deserialize(
+                        de::value::StrDeserializer::new(v),
+                    )?)
+                };
+                Ok(result)
+            }
+
+            fn visit_map<M>(self, map: M) -> Result<BlockSpec, M::Error>
+            where
+                M: de::MapAccess<'de>,
+            {
+                // Forward to deserializer of `Eip1898BlockSpec`
+                Ok(BlockSpec::Eip1898(de::Deserialize::deserialize(
+                    de::value::MapAccessDeserializer::new(map),
+                )?))
+            }
+        }
+
+        deserializer.deserialize_any(BlockSpecVisitor)
+    }
 }
 
 macro_rules! impl_block_tags {
@@ -165,6 +316,8 @@ impl_block_tags!(PreEip1898BlockSpec);
 
 #[cfg(test)]
 mod tests {
+    use serde_json::json;
+
     use super::*;
 
     fn help_test_block_spec_serde(block_spec: BlockSpec) {
@@ -186,8 +339,115 @@ mod tests {
             block_hash: B256::from_low_u64_ne(1),
             require_canonical: Some(true),
         }));
+        help_test_block_spec_serde(BlockSpec::Eip1898(Eip1898BlockSpec::Hash {
+            block_hash: B256::from_low_u64_ne(1),
+            require_canonical: None,
+        }));
         help_test_block_spec_serde(BlockSpec::Eip1898(Eip1898BlockSpec::Number {
             block_number: 1,
         }));
+    }
+
+    #[test]
+    fn test_eip_1898_block_number_serialization() {
+        let block_spec = Eip1898BlockSpec::Number { block_number: 1 };
+        let serialize = serde_json::to_string(&block_spec).unwrap();
+        assert!(serialize.contains("0x1"));
+    }
+
+    #[test]
+    fn test_eip_1898_block_hash_serialization_skips_canonical_none() {
+        let block_spec = Eip1898BlockSpec::Hash {
+            block_hash: B256::from_low_u64_ne(1),
+            require_canonical: None,
+        };
+        let serialize = serde_json::to_string(&block_spec).unwrap();
+        assert!(!serialize.contains("requireCanonical"));
+    }
+
+    #[test]
+    fn test_block_spec_eip_1898_block_number_deserialization() {
+        // Owned value
+        let value = json!({"blockNumber": "0x1"});
+        let block_spec: BlockSpec = serde_json::from_value(value).unwrap();
+        assert!(matches!(
+            block_spec,
+            BlockSpec::Eip1898(Eip1898BlockSpec::Number { block_number: 1 })
+        ));
+    }
+
+    #[test]
+    fn test_block_spec_eip_1898_block_number_from_str_deserialization() {
+        let value = r#"{"blockNumber": "0x1"}"#;
+        // Check that the deserializer works for references as well (serde_json::Value
+        // is owned).
+        let block_spec: BlockSpec = serde_json::from_str(value).unwrap();
+        assert!(matches!(
+            block_spec,
+            BlockSpec::Eip1898(Eip1898BlockSpec::Number { block_number: 1 })
+        ));
+    }
+
+    #[test]
+    fn test_block_spec_eip_1898_block_hash_deserialization() {
+        let value = json!({
+            "blockHash": "0xa3294400ca7d8dc2c1a9120b2178718700fd2a0f58d59c518cc25767524fe0f2"
+        });
+        let block_spec: BlockSpec = serde_json::from_value(value).unwrap();
+        assert!(matches!(
+            block_spec,
+            BlockSpec::Eip1898(Eip1898BlockSpec::Hash {
+                block_hash: _,
+                require_canonical: None
+            })
+        ));
+    }
+
+    #[test]
+    fn test_block_spec_eip_1898_block_hash_canonical_deserialization() {
+        let value = json!({
+            "blockHash": "0xa3294400ca7d8dc2c1a9120b2178718700fd2a0f58d59c518cc25767524fe0f2",
+            "requireCanonical": true
+        });
+        let block_spec: BlockSpec = serde_json::from_value(value).unwrap();
+        assert!(matches!(
+            block_spec,
+            BlockSpec::Eip1898(Eip1898BlockSpec::Hash {
+                block_hash: _,
+                require_canonical: Some(true)
+            })
+        ));
+    }
+
+    #[test]
+    fn test_block_spec_eip_1898_block_number_and_hash_deserialization_error() {
+        let value = json!({
+            "blockNumber": "0x0",
+            "blockHash": "0xa3294400ca7d8dc2c1a9120b2178718700fd2a0f58d59c518cc25767524fe0f2"
+        });
+        let error_message = serde_json::from_value::<BlockSpec>(value)
+            .err()
+            .unwrap()
+            .to_string();
+        assert_eq!(
+            error_message,
+            "EIP-1898 block spec cannot have both `blockHash` and `blockNumber`"
+        );
+    }
+
+    #[test]
+    fn test_block_spec_eip_1898_block_number_and_canonical_deserialization_error() {
+        let value = json!({
+            "blockNumber": "0x0",
+            "requireCanonical": true
+        });
+        let error_message = serde_json::from_value::<BlockSpec>(value)
+            .err()
+            .unwrap()
+            .to_string();
+        assert_eq!(
+            error_message,
+            "EIP-1898 block spec cannot have both `blockNumber` and `requireCanonical`"
+        );
     }
 }
