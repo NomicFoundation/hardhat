c0278d5d3698255ea143250babe44740d3dcbceb
Franco Victorio <victorio.franco@gmail.com>
2023-12-22 15:36:43 +0100

    Add comment in telemetry consent hit script

--- packages/hardhat-core/src/internal/util/report-telemetry-consent.ts
+++ packages/hardhat-core/src/internal/util/report-telemetry-consent.ts
@@ -3,6 +3,8 @@ import { Analytics } from "../cli/analytics";
 async function main() {
   const [telemetryConsent] = process.argv.slice(2);
 
+  // we pass undefined as the telemetryConsent value because
+  // this hit is done before the consent is saved
   const analytics = await Analytics.getInstance(undefined);
 
   const [_, consentHitPromise] = await analytics.sendTelemetryConsentHit(
