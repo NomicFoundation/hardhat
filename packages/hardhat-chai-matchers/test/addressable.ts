2998193fed901dcb5b1ead4a28e5a569471861ae
RenanSouza2 <renan.rodrigues.souza1@gmail.com>
2023-12-26 12:25:58 -0300

    expand tests

--- packages/hardhat-chai-matchers/test/addressable.ts
+++ packages/hardhat-chai-matchers/test/addressable.ts
@@ -7,18 +7,30 @@ describe("Addressable matcher", () => {
   const signer = ethers.Wallet.createRandom();
   const address = signer.address;
   const contract = new ethers.Contract(address, []);
+  const typedAddress = ethers.Typed.address(address);
+  const typedSigner = ethers.Typed.address(signer);
+  const typedContract = ethers.Typed.address(contract);
 
   const otherSigner = ethers.Wallet.createRandom();
   const otherAddress = otherSigner.address;
   const otherContract = new ethers.Contract(otherAddress, []);
+  const otherTypedAddress = ethers.Typed.address(address);
+  const otherTypedSigner = ethers.Typed.address(signer);
+  const otherTypedContract = ethers.Typed.address(contract);
 
   const elements = [
     { name: "address", object: address, class: address },
     { name: "signer", object: signer, class: address },
     { name: "contract", object: contract, class: address },
+    { name: "typed contract", object: typedAddress, class: address },
+    { name: "typed contract", object: typedSigner, class: address },
+    { name: "typed contract", object: typedContract, class: address },
     { name: "other address", object: otherAddress, class: otherAddress },
     { name: "other signer", object: otherSigner, class: otherAddress },
     { name: "other contract", object: otherContract, class: otherAddress },
+    { name: "other typed address", object: otherTypedAddress, class: otherAddress },
+    { name: "other typed signer", object: otherTypedSigner, class: otherAddress },
+    { name: "other typed contract", object: otherTypedContract, class: otherAddress },
   ];
 
   for (const el1 of elements)
