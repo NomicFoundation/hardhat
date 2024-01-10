9ea61d60da668fd40d18b132889ce5ab76acf6f4
John Kane <john@kanej.me>
2024-01-10 09:45:03 +0000

    docs: update signature doc

--- packages/core/src/status.ts
+++ packages/core/src/status.ts
@@ -11,6 +11,8 @@ import { StatusResult } from "./types/status";
  * Show the status of a deployment.
  *
  * @param deploymentDir - the directory of the deployment to get the status of
+ * @param artifactResolver - the artifact resolver to use when loading artifacts
+ * for a future
  *
  * @beta
  */
