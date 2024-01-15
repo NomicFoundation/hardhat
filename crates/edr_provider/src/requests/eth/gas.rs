8760993eff58de6e7f295e27e7623e415240d2b1
Agost Biro <agostbiro@gmail.com>
2024-01-15 20:33:25 +0100

    feat: add `eth_estimateGas` support

--- crates/edr_provider/src/requests/eth/gas.rs
+++ crates/edr_provider/src/requests/eth/gas.rs
@@ -2,10 +2,14 @@ use edr_eth::{
     remote::{eth::CallRequest, BlockSpec},
     SpecId, U256,
 };
+use edr_evm::state::StateOverrides;
 
 use crate::{
     data::ProviderData,
-    requests::validation::{validate_call_request, validate_post_merge_block_tags},
+    requests::{
+        eth::resolve_call_request,
+        validation::{validate_call_request, validate_post_merge_block_tags},
+    },
     ProviderError,
 };
 
@@ -16,9 +20,20 @@ pub fn handle_estimate_gas(
 ) -> Result<U256, ProviderError> {
     validate_call_request(data.spec_id(), &call_request, &block_spec)?;
 
-    // TODO implement logic
-    // https://github.com/NomicFoundation/edr/issues/227
-    Ok(U256::ZERO)
+    // Matching Hardhat behavior in defaulting to "pending" instead of "latest" for
+    // estimate gas.
+    let block_spec = block_spec.unwrap_or_else(BlockSpec::pending);
+
+    let transaction = resolve_call_request(
+        data,
+        call_request,
+        Some(&block_spec),
+        &StateOverrides::default(),
+    )?;
+
+    let gas_limit = data.estimate_gas(transaction, &block_spec)?;
+
+    Ok(U256::from(gas_limit))
 }
 
 pub fn handle_fee_history(
