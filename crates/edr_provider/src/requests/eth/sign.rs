a0865be23b6253c833832bbd475d51b0053505f9
Agost Biro <5764438+agostbiro@users.noreply.github.com>
2024-01-19 16:55:59 +0100

    feat: add `eth_signTypedData_v4` support (#4770)

--- crates/edr_provider/src/requests/eth/sign.rs
+++ crates/edr_provider/src/requests/eth/sign.rs
@@ -1,4 +1,5 @@
 use edr_eth::{Address, Bytes};
+use ethers_core::types::transaction::eip712::TypedData;
 
 use crate::{data::ProviderData, ProviderError};
 
@@ -8,4 +9,12 @@ pub fn handle_sign_request(
     address: Address,
 ) -> Result<Bytes, ProviderError> {
     Ok((&data.sign(&address, message)?).into())
+}
+
+pub fn handle_sign_typed_data_v4(
+    data: &ProviderData,
+    address: Address,
+    message: TypedData,
+) -> Result<Bytes, ProviderError> {
+    Ok((&data.sign_typed_data_v4(&address, &message)?).into())
 }
