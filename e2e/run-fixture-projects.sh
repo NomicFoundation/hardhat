ed1f906518e51a7f0be92d51f8cd926151175e03
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-01-23 18:43:10 +0100

    create separate tmp dir for fixture tests

--- e2e/run-fixture-projects.sh
+++ e2e/run-fixture-projects.sh
@@ -18,8 +18,14 @@ echo "[e2e] Built $HARDHAT_TGZ_FILE"
 cd - >/dev/null
 
 # create a temporary directory to run the tests
-FIXTURE_PROJECTS_DIR=fixture-projects-run-$(date +%Y-%m-%d-%H-%M-%S)
+TMP_DIR=$(mktemp -d)
+FIXTURE_PROJECTS_DIR="${TMP_DIR}/fixture-projects-run-$(date +%Y-%m-%d-%H-%M-%S)"
 cp -r fixture-projects $FIXTURE_PROJECTS_DIR
+# also copy the helper script
+cp helpers.sh $FIXTURE_PROJECTS_DIR/helpers.sh
+
+# store the path to hardhat-core so it can be used in the tmp folder
+HARDHAT_CORE_FOLDER_PATH="$(pwd)/../packages/hardhat-core"
 
 # run all the e2e tests in the temporary directory
 echo "[e2e] Running tests in $FIXTURE_PROJECTS_DIR\n\n"
@@ -39,7 +45,7 @@ for dir in ${FIXTURE_PROJECTS_DIR}/*; do
     cd "$dir"
 
     echo "[e2e] Installing modules in $dir"
-    npm add ../../../packages/hardhat-core/$HARDHAT_TGZ_FILE >/dev/null 2>&1
+    npm add $HARDHAT_CORE_FOLDER_PATH/$HARDHAT_TGZ_FILE >/dev/null 2>&1
     npm install >/dev/null 2>&1 # install moduled specified in the package.json
     echo "[e2e] All modules have been installed in $dir"
 
