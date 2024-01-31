16ae15642951ac324ef7093a3342f7cf3a2a49a4
Franco Victorio <victorio.franco@gmail.com>
2024-01-31 12:09:30 +0100

    Use dynamic import

--- packages/hardhat-chai-matchers/src/internal/typed.ts
+++ packages/hardhat-chai-matchers/src/internal/typed.ts
@@ -1,6 +1,5 @@
-import { Typed } from "ethers";
-
 export function tryDereference(value: any, type: string) {
+  const { Typed } = require("ethers") as typeof import("ethers");
   try {
     return Typed.dereference(value, type);
   } catch {
