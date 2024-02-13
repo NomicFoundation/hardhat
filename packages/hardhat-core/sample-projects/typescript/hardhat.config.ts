812dd56956eb66d8b57b5e7d95c77f8b677d0968
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-02-13 12:29:42 +0100

    increase version in sample projects

--- packages/hardhat-core/sample-projects/typescript/hardhat.config.ts
+++ packages/hardhat-core/sample-projects/typescript/hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity: "0.8.24",
 };
 
 export default config;
