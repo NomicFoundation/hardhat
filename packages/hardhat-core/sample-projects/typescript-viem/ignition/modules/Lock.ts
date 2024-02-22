4e0d856330d90f4d55b5b8304b5156233df78108
John Kane <john@kanej.me>
2024-02-22 22:55:00 +0000

    add default unlockTime to sample projects
    The unlock is arbitrary (1st Jan 2030), but it a set point in time. This
    means we will not have a problem with reconciliation if there is a rerun
    without a module parameter setting the unlock time.

--- packages/hardhat-core/sample-projects/typescript-viem/ignition/modules/Lock.ts
+++ packages/hardhat-core/sample-projects/typescript-viem/ignition/modules/Lock.ts
@@ -1,10 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { parseEther } from "viem";
 
+const JAN_1ST_2030 = 1893456000;
 const ONE_GWEI: bigint = parseEther("0.001");
 
 const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime");
+  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
   const lock = m.contract("Lock", [unlockTime], {
