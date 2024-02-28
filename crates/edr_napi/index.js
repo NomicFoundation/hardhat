1f5a9c0e0bcf58e6d7264b642c4acc7bde7c1f2e
Wodann <Wodann@users.noreply.github.com>
2024-02-28 20:00:57 +0000

    build: add updated N-API bindings

--- crates/edr_napi/index.js
+++ crates/edr_napi/index.js
@@ -237,6 +237,49 @@ switch (platform) {
           loadError = e
         }
         break
+      case 'riscv64':
+        if (isMusl()) {
+          localFileExisted = existsSync(
+            join(__dirname, 'edr.linux-riscv64-musl.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./edr.linux-riscv64-musl.node')
+            } else {
+              nativeBinding = require('@nomicfoundation/edr-linux-riscv64-musl')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        } else {
+          localFileExisted = existsSync(
+            join(__dirname, 'edr.linux-riscv64-gnu.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./edr.linux-riscv64-gnu.node')
+            } else {
+              nativeBinding = require('@nomicfoundation/edr-linux-riscv64-gnu')
+            }
+          } catch (e) {
+            loadError = e
+          }
+        }
+        break
+      case 's390x':
+        localFileExisted = existsSync(
+          join(__dirname, 'edr.linux-s390x-gnu.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./edr.linux-s390x-gnu.node')
+          } else {
+            nativeBinding = require('@nomicfoundation/edr-linux-s390x-gnu')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
       default:
         throw new Error(`Unsupported architecture on Linux: ${arch}`)
     }
