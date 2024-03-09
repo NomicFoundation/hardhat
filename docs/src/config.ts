174d4bb3924d0be1edc17033e101173b2701e936
Patricio Palladino <email@patriciopalladino.com>
2024-03-09 14:52:49 +0000

    [Website] Remove the solidity survey banner

--- docs/src/config.ts
+++ docs/src/config.ts
@@ -22,14 +22,13 @@ export const SOCIALS_LINKS = {
   [SocialsEnum.DISCORD]: "/discord",
 };
 
-export const BANNER_LINK =
-  "https://cryptpad.fr/form/#/2/form/view/pV-DdryeJoYUWvW+gXsFaMNynEY7t5mUsgeD1urgwSE";
+export const BANNER_LINK = "https://nomic.foundation/hiring";
 
 export const PRIVACY_POLICY_PATH = "/privacy-policy.html";
 
 export const bannerContent = {
-  text: "Please take a few minutes to complete the 2023 Solidity Survey",
-  shortText: "Complete the 2023 Solidity Survey here",
+  text: "Join the Hardhat team! Nomic Foundation is hiring",
+  shortText: "Join the Hardhat team! We are hiring",
   href: BANNER_LINK,
 };
 
