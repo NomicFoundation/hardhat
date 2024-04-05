02dbc825f71309a499884a8e6aa3e10e9bc57d92
Patricio Palladino <email@patriciopalladino.com>
2024-04-05 20:23:31 +0000

    Setup the slow imports rule.

--- packages/template-package/.eslintrc.cjs
+++ packages/template-package/.eslintrc.cjs
@@ -1,3 +1,6 @@
 const { createConfig } = require("../../config/v-next/eslint.cjs");
 
-module.exports = createConfig(__filename);
+module.exports = createConfig(__filename, [
+  "src/index.ts",
+  "src/other-entry-point.ts",
+]);
