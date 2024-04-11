e0edb221f4a7b050bd7ca7699c984326af3e2b47
Wodann <Wodann@users.noreply.github.com>
2024-04-11 13:52:49 +0000

    build: remove support for i686 & arm windows builds

--- crates/edr_napi/index.js
+++ crates/edr_napi/index.js
@@ -224,17 +224,32 @@ switch (platform) {
         }
         break
       case 'arm':
-        localFileExisted = existsSync(
-          join(__dirname, 'edr.linux-arm-gnueabihf.node')
-        )
-        try {
-          if (localFileExisted) {
-            nativeBinding = require('./edr.linux-arm-gnueabihf.node')
-          } else {
-            nativeBinding = require('@nomicfoundation/edr-linux-arm-gnueabihf')
+        if (isMusl()) {
+          localFileExisted = existsSync(
+            join(__dirname, 'edr.linux-arm-musleabihf.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./edr.linux-arm-musleabihf.node')
+            } else {
+              nativeBinding = require('@nomicfoundation/edr-linux-arm-musleabihf')
+            }
+          } catch (e) {
+            loadError = e
           }
-        } catch (e) {
-          loadError = e
+        } else {
+          localFileExisted = existsSync(
+            join(__dirname, 'edr.linux-arm-gnueabihf.node')
+          )
+          try {
+            if (localFileExisted) {
+              nativeBinding = require('./edr.linux-arm-gnueabihf.node')
+            } else {
+              nativeBinding = require('@nomicfoundation/edr-linux-arm-gnueabihf')
+            }
+          } catch (e) {
+            loadError = e
+          }
         }
         break
       case 'riscv64':
