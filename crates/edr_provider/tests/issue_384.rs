3f2390478c1004095673e9e2e658cee486f0e64b
Wodann <Wodann@users.noreply.github.com>
2024-04-16 11:30:26 +0000

    misc: apply review suggestion

--- crates/edr_provider/tests/issue_384.rs
+++ crates/edr_provider/tests/issue_384.rs
@@ -4,17 +4,18 @@ use edr_provider::{
     hardhat_rpc_types::ForkConfig, test_utils::create_test_config_with_fork, MethodInvocation,
     NoopLogger, Provider, ProviderRequest,
 };
+use edr_test_utils::env::get_infura_url;
 use tokio::runtime;
 
 #[tokio::test(flavor = "multi_thread")]
-async fn flare_network_mine_local_block() -> anyhow::Result<()> {
+async fn avalanche_chain_mine_local_block() -> anyhow::Result<()> {
     const BLOCK_NUMBER: u64 = 22_587_773;
 
     let logger = Box::new(NoopLogger);
     let subscriber = Box::new(|_event| {});
 
     let config = create_test_config_with_fork(Some(ForkConfig {
-        json_rpc_url: "https://flare-api.flare.network/ext/bc/C/rpc".to_string(),
+        json_rpc_url: get_infura_url().replace("mainnet", "avalanche-mainnet"),
         block_number: Some(BLOCK_NUMBER),
         http_headers: None,
     }));
