e2cc3d77574ebfd03716ee6bd94f3414d3efec85
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-04-23 16:53:09 +0200

    move code into separate package

---
+++ packages/hardhat-build-system/README.md
@@ -0,0 +1,11 @@
+# Template package
+
+This is a template package with a base configuration that should be shared by other packages.
+
+It sets up the following things:
+
+- Typescript
+- eslint
+- prettier
+- npm scripts
+- package.json' export field
---
+++ packages/hardhat-build-system/src/util/package-info.ts
@@ -0,0 +1,38 @@
+import findup from "find-up";
+import fsExtra from "fs-extra";
+import path from "path";
+import { assertHardhatInvariant } from "./errors";
+
+export interface PackageJson {
+  name: string;
+  version: string;
+  type?: "commonjs" | "module";
+  engines: {
+    node: string;
+  };
+}
+
+export function getHardhatVersion(): string {
+  const packageJsonPath = findClosestPackageJson(__filename);
+
+  assertHardhatInvariant(
+    packageJsonPath !== null,
+    "There should be a package.json in hardhat-core's root directory"
+  );
+
+  const packageJson = fsExtra.readJsonSync(packageJsonPath);
+  return packageJson.version;
+}
+
+export function findClosestPackageJson(file: string): string | null {
+  return findup.sync("package.json", { cwd: path.dirname(file) });
+}
+
+export async function getPackageName(file: string): Promise<string> {
+  const packageJsonPath = findClosestPackageJson(file);
+  if (packageJsonPath !== null && packageJsonPath !== "") {
+    const packageJson: PackageJson = await fsExtra.readJSON(packageJsonPath);
+    return packageJson.name;
+  }
+  return "";
+}
