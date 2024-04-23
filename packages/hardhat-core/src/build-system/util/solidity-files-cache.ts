96de61b32ae0a03b9ad689fa8fb080a3e17a8ff3
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-04-23 16:53:58 +0200

    delete build-system from hh-core

--- packages/hardhat-core/src/build-system/util/solidity-files-cache.ts
+++
@@ -1,8 +0,0 @@
-import * as path from "path";
-import { ProjectPathsConfig } from "./types";
-
-export const SOLIDITY_FILES_CACHE_FILENAME = "solidity-files-cache.json";
-
-export function getSolidityFilesCachePath(paths: ProjectPathsConfig): string {
-  return path.join(paths.cache, SOLIDITY_FILES_CACHE_FILENAME);
-}
