cc36e7d379d870929f79642af0925bbedb2353e8
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-04-24 16:30:54 +0200

    hh-core tests moved into build-system package (missing 2 tests)

--- packages/hardhat-build-system/src/util/types/config.ts
+++ packages/hardhat-build-system/src/util/types/config.ts
@@ -1,32 +1,18 @@
 import { SolcConfig } from "./builtin-tasks/compile";
 
-export interface HardhatConfig {
-  // defaultNetwork: string;
+export interface BuildConfig {
   paths: ProjectPathsConfig;
-  // networks: NetworksConfig;
   solidity: SolidityConfig;
-  // mocha: Mocha.MochaOptions;
 }
 
 export interface ProjectPathsConfig {
   root: string;
-  configFile: string;
   cache: string;
   artifacts: string;
   sources: string;
-  tests: string;
 }
 
 export interface SolidityConfig {
   compilers: SolcConfig[];
   overrides: Record<string, SolcConfig>;
-}
-
-export interface ProjectPathsConfig {
-  root: string;
-  configFile: string;
-  cache: string;
-  artifacts: string;
-  sources: string;
-  tests: string;
 }
---
+++ packages/hardhat-build-system/test/fixture-projects/compilation-ambiguous-remappings/contracts/A.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "foo/Foo.sol";
+import "bar/Foo.sol";
+
+contract A {}
