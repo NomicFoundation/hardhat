1a916b46ab3f96b87bbd291a2ccd0592dd7cdfe7
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-04-26 10:48:32 +0200

    FLATTEN task tests, hh-core is now passing all tests + smaller adjustments

--- packages/hardhat-build-system/src/internal/build-system.ts
+++ packages/hardhat-build-system/src/internal/build-system.ts
@@ -9,12 +9,15 @@ import {
   taskCompileRemoveObsoleteArtifacts,
   taskCompileSolidity,
   taskCompileSolidityGetCompilationJobsFailureReasons,
+  taskCompileSolidityGetDependencyGraph,
+  taskCompileSolidityGetSourceNames,
+  taskCompileSolidityGetSourcePaths,
   taskCompileSolidityReadFile,
 } from "./tasks";
 
 export interface BuildRequest {
-  // TODO: should profile be defined in the hh-config and have a default value? Then it can be overridden when calling 'build'?
-  // TODO: files are by default the ones in the folder 'contracts', but can be overridden when calling 'build'
+  // TODO: should build profiles be defined in the hh-config and have a default value? Then it can be overridden when calling 'build'?
+  // TODO: files are by default the ones in the folder 'contracts'? But can be overridden when calling 'build'?
   profile?: string;
   type?: string;
   files?: string[];
@@ -35,20 +38,22 @@ export class BuildSystem {
   private _config: BuildConfig;
 
   constructor(config: BuildConfig) {
-    this._config = config; // TODO: can be optional? E.g.: not needed for method 'solidityReadFile'
+    this._config = config; // TODO: can be optional? E.g.: it's not needed for method 'solidityReadFile'
+    // TODO: clone the config?
   }
 
   public async build(buildRequest?: BuildRequest): Promise<BuildResult> {
+    // TODO: how to handle artifacts? Should they be passed as a parameter or should they be created here? Or both?
     const artifacts =
       buildRequest?.artifacts ??
       new ArtifactsImpl(this._config.paths.artifacts);
 
     await taskCompileSolidity(
-      this._config,
+      this._config, // TODO: should it be passed as a parameter and override default values?
       artifacts,
       buildRequest?.force ?? false,
       buildRequest?.quiet ?? false,
-      buildRequest?.concurrency ?? 1,
+      buildRequest?.concurrency ?? 1, // TODO: default value?
       buildRequest?.tasksOverrides ?? undefined,
     );
 
@@ -62,10 +67,38 @@ export class BuildSystem {
     return taskCompileSolidityReadFile(absolutePath);
   }
 
-  // TODO: TASK_COMPILE_SOLIDITY_READ_FILE - naming? Do we want to keep it?
+  // TODO: TASK_COMPILE_SOLIDITY_GET_COMPILATION_JOBS_FAILURE_REASONS - naming? Do we want to keep it?
   public async solidityGetCompilationJobsFailureReasons(
     errors: CompilationJobCreationError[],
   ) {
     return taskCompileSolidityGetCompilationJobsFailureReasons(errors);
+  }
+
+  // TODO: the following methods are used to make the hh-core tests for the FLATTEN task pass
+  // TODO: TASK_COMPILE_SOLIDITY_GET_DEPENDENCY_GRAPH
+  public async solidityGetDependencyGraph(
+    sourceNames: string[],
+    config: BuildConfig,
+  ) {
+    return taskCompileSolidityGetDependencyGraph(sourceNames, config);
+  }
+
+  // TODO: TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS
+  public async solidityGetSourcePaths(
+    sourcePath: string | undefined,
+  ): Promise<string[]> {
+    return taskCompileSolidityGetSourcePaths(this._config, sourcePath);
+  }
+
+  // TODO: TASK_COMPILE_SOLIDITY_GET_SOURCE_NAMES
+  public async solidityGetSourceNames(
+    sourcePaths: string[],
+    rootPath?: string | undefined,
+  ): Promise<string[]> {
+    return taskCompileSolidityGetSourceNames(
+      this._config,
+      sourcePaths,
+      rootPath,
+    );
   }
 }
