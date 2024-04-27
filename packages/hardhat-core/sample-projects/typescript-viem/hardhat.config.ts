12d10f8185be72fcafcbcfa77ce86444ba2cfcac
sam bacha <sam@manifoldfinance.com>
2024-04-27 05:32:42 -0700

    fix(ts): import type adjustments
    these are only used as types

--- packages/hardhat-core/sample-projects/typescript-viem/hardhat.config.ts
+++ packages/hardhat-core/sample-projects/typescript-viem/hardhat.config.ts
@@ -1,4 +1,5 @@
-import { HardhatUserConfig } from "hardhat/config";
+// @ts-check
+import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
