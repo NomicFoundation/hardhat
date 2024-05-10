d45a6333653954538b6f17e1eedc4a3940ddff90
John Kane <john@kanej.me>
2024-05-10 10:30:55 +0100

    refactor: replace keccak256 dep with utils version

--- v-next/hardhat-build-system/src/internal/utils/keccak.ts
+++
@@ -1,7 +0,0 @@
-import { Keccak } from "keccak";
-
-export function keccak256(data: Uint8Array): Uint8Array {
-  const hash = new Keccak(256, 0, null, 256, {});
-  hash.update(Buffer.from(data));
-  return hash.digest();
-}
