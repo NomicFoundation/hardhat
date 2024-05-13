4245d846b24388e54981e7e1622dc3d52d1fd04e
Luis Schaab <schaable@gmail.com>
2024-05-13 10:45:28 -0300

    Fix test

--- packages/hardhat-verify/test/integration/index.ts
+++ packages/hardhat-verify/test/integration/index.ts
@@ -92,7 +92,8 @@ describe("verify task integration tests", () => {
 
       expect(logStub).to.be.calledOnceWith(
         `The contract ${address} has already been verified on the block explorer. If you're trying to verify a partially verified contract, please use the --force flag.
-https://hardhat.etherscan.io/address/${address}#code`
+https://hardhat.etherscan.io/address/${address}#code
+`
       );
       logStub.restore();
       assert.isUndefined(taskResponse);
