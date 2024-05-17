2a90488f1710d0177f311f3342948b510d3e3e6f
johnpm-12 <39016062+johnpm-12@users.noreply.github.com>
2024-05-17 15:15:17 -0700

    fork from hardhat-chai-matchers for ethers

---
+++ packages/hardhat-chai-matchers-viem/test/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [`${__dirname}/../.eslintrc.js`],
+  parserOptions: {
+    project: `${__dirname}/../tsconfig.json`,
+    sourceType: "module",
+  },
+  rules: {
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        devDependencies: true,
+      },
+    ],
+  },
+};
