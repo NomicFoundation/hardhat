7c5b1856aef998929cf4e98a508b9d3145968aa2
John Kane <john@kanej.me>
2024-05-24 10:25:16 +0100

    feat: plugin install validation (#5261)
    Validates the plugins provided through the initial config object.
    
    If the plugin comes from an npm package check that the package has been
    installed.
    
    If the plugin is present further confirm that peer dependencies have
    been installed. It then checks that the installed version falls within
    the range of the originating package.
    
    This leverages the node require mechanism based on `process.cwd()` as
    the starting directory.
    
    It supports peer dependencies installed not in the top level `node_modules`
    but a sub-`node_modules` of the package of the plugin.
    
    Resolves #5256
    
    ---------
    
    Co-authored-by: Luis Schaab <schaable@gmail.com>

--- v-next/core/src/internal/plugins/plugin-validation.ts
+++ v-next/core/src/internal/plugins/plugin-validation.ts
@@ -1,6 +1,107 @@
+import { createRequire } from "node:module";
+import path from "node:path";
+import process from "node:process";
+
+import { HardhatError } from "@nomicfoundation/hardhat-errors";
+import { PackageJson } from "@nomicfoundation/hardhat-utils/package";
+import semver from "semver";
+
 import { HardhatPlugin } from "../../types/plugins.js";
 
-export async function validatePluginNpmDependencies(_plugin: HardhatPlugin) {
-  // TODO: If it has an npm package, validate their peer dependencies
-  // If any is missing, throw
+/**
+ * Validate that a plugin is installed and that its peer dependencies are installed and satisfy the version constraints.
+ *
+ * @param plugin - the plugin to be validated
+ * @param basePathForNpmResolution - the directory path to use for node module resolution, defaulting to `process.cwd()`
+ * @throws {HardhatError} with descriptor:
+ * - {@link ERRORS.ARTIFACTS.PLUGIN_NOT_INSTALLED} if the plugin is not installed as an npm package
+ * - {@link ERRORS.ARTIFACTS.PLUGIN_MISSING_DEPENDENCY} if the plugin's package peer dependency is not installed
+ * - {@link ERRORS.ARTIFACTS.DEPENDENCY_VERSION_MISMATCH} if the plugin's package peer dependency is installed but has the wrong version
+ */
+export async function validatePluginNpmDependencies(
+  plugin: HardhatPlugin,
+  // TODO: When all loads where based on a hardhat config file
+  // on disk we used the config file's directory as the base path
+  // We need to decide if the current working directory is the right
+  // option for module resolution now
+  basePathForNpmResolution?: string,
+): Promise<void> {
+  if (plugin.npmPackage === undefined) {
+    return;
+  }
+
+  const pluginPackageResult = readPackageJsonViaNodeRequire(
+    plugin.npmPackage,
+    basePathForNpmResolution,
+  );
+
+  if (pluginPackageResult === undefined) {
+    throw new HardhatError(HardhatError.ERRORS.PLUGINS.PLUGIN_NOT_INSTALLED, {
+      pluginId: plugin.id,
+    });
+  }
+
+  const { packageJson: pluginPackageJson, packagePath } = pluginPackageResult;
+
+  if (pluginPackageJson.peerDependencies === undefined) {
+    return;
+  }
+
+  for (const [dependencyName, versionSpec] of Object.entries(
+    pluginPackageJson.peerDependencies,
+  )) {
+    const dependencyPackageJsonResult = readPackageJsonViaNodeRequire(
+      dependencyName,
+      packagePath,
+    );
+
+    if (dependencyPackageJsonResult === undefined) {
+      throw new HardhatError(
+        HardhatError.ERRORS.PLUGINS.PLUGIN_MISSING_DEPENDENCY,
+        {
+          pluginId: plugin.id,
+          peerDependencyName: dependencyName,
+        },
+      );
+    }
+
+    const installedVersion = dependencyPackageJsonResult.packageJson.version;
+
+    if (!semver.satisfies(installedVersion, versionSpec)) {
+      throw new HardhatError(
+        HardhatError.ERRORS.PLUGINS.DEPENDENCY_VERSION_MISMATCH,
+        {
+          pluginId: plugin.id,
+          peerDependencyName: dependencyName,
+          installedVersion,
+          expectedVersion: versionSpec,
+        },
+      );
+    }
+  }
+}
+
+/**
+ * Read the package.json of a named package resolved through the node
+ * require system.
+ *
+ * @param packageName - the package name i.e. "@nomiclabs/hardhat-waffle"
+ * @param baseRequirePath - the directory path to use for resolution, defaults to `process.cwd()`
+ * @returns the package.json object or undefined if the package is not found
+ */
+function readPackageJsonViaNodeRequire(
+  packageName: string,
+  baseRequirePath?: string,
+): { packageJson: PackageJson; packagePath: string } | undefined {
+  try {
+    const require = createRequire(baseRequirePath ?? process.cwd());
+
+    const packagePath = require.resolve(path.join(packageName, "package.json"));
+
+    const packageJson = require(packagePath);
+
+    return { packageJson, packagePath };
+  } catch (error) {
+    return undefined;
+  }
 }
---
+++ v-next/core/test/plugins/fixture-projects/installed-peer-deps/README.md
@@ -0,0 +1,3 @@
+# Installed Peer Deps
+
+This is part of the plugin validation tests. We have checked in a `node_modules` folder to simulate an installed `example` package with an installed set of peer dependencies: `peer1` and `peer2`.
