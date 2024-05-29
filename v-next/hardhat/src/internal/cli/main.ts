c9bbcd892171ca521db0014151c1a2c8174bca8b
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-05-29 11:35:52 +0200

    fix merge error: unused code was copied

--- v-next/hardhat/src/internal/cli/main.ts
+++ v-next/hardhat/src/internal/cli/main.ts
@@ -466,29 +466,3 @@ function formatParameterValue(
       return value === "true";
   }
 }
-
-async function importUserConfig(configPath: string) {
-  const normalizedPath = isAbsolute(configPath)
-    ? configPath
-    : resolve(process.cwd(), configPath);
-
-  const { exists } = await import("@nomicfoundation/hardhat-utils/fs");
-
-  if (!(await exists(normalizedPath))) {
-    throw new Error(`Config file ${configPath} not found`);
-  }
-
-  const imported = await import(normalizedPath);
-
-  if (!("default" in imported)) {
-    throw new Error(`No config exported in ${configPath}`);
-  }
-
-  const config = imported.default;
-
-  if (typeof config !== "object" || config === null) {
-    throw new Error(`Invalid config exported in ${configPath}`);
-  }
-
-  return config;
-}
