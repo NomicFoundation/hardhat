d5ce13f963d5f7450bb946cb3c0933e48f895730
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-06-06 14:17:21 +0200

    simplify condition based on comment suggestion

--- v-next/hardhat-utils/src/ci.ts
+++ v-next/hardhat-utils/src/ci.ts
@@ -6,15 +6,14 @@
 export function isCi() {
   const env = process.env;
 
-  return !!(
+  return (
     isGithubActions() ||
     isNow() ||
     isAwsCodeBuild() ||
     env.CI !== undefined || // Travis CI, CircleCI, Cirrus CI, GitLab CI, Appveyor, CodeShip, dsari
     env.CONTINUOUS_INTEGRATION !== undefined || // Travis CI, Cirrus CI
     env.BUILD_NUMBER !== undefined || // Jenkins, TeamCity
-    env.RUN_ID !== undefined || // TaskCluster, dsari
-    false
+    env.RUN_ID !== undefined // TaskCluster, dsari
   );
 }
 
