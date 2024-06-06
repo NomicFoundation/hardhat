a343f9281313b8516ffbc82b6340254c7c489f5f
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-06-06 14:04:30 +0200

    combine the pnpm logic to check for CI and the old hardhat logic + tests

--- v-next/hardhat-utils/src/ci.ts
+++ v-next/hardhat-utils/src/ci.ts
@@ -1,6 +1,33 @@
 /**
- * Returns 'true' if the current process is running in the GitHub CI environment.
+ * Checks whether the current process is running in a CI environment.
+ *
+ * @returns True if the current process is running in a CI environment.
  */
-export function isCI() {
-  return process.env.CI === "true";
+export function isCi() {
+  const env = process.env;
+
+  return !!(
+    isGithubActions() ||
+    isNow() ||
+    isAwsCodeBuild() ||
+    env.CI !== undefined || // Travis CI, CircleCI, Cirrus CI, GitLab CI, Appveyor, CodeShip, dsari
+    env.CONTINUOUS_INTEGRATION !== undefined || // Travis CI, Cirrus CI
+    env.BUILD_NUMBER !== undefined || // Jenkins, TeamCity
+    env.RUN_ID !== undefined || // TaskCluster, dsari
+    false
+  );
+}
+
+function isGithubActions(): boolean {
+  return process.env.GITHUB_ACTIONS !== undefined;
+}
+
+function isNow() {
+  return (
+    process.env.NOW !== undefined || process.env.DEPLOYMENT_ID !== undefined
+  );
+}
+
+function isAwsCodeBuild() {
+  return process.env.CODEBUILD_BUILD_NUMBER !== undefined;
 }
--- v-next/hardhat-utils/test/ci.ts
+++ v-next/hardhat-utils/test/ci.ts
@@ -1,20 +1,64 @@
 import assert from "node:assert/strict";
-import { describe, it } from "node:test";
+import { after, beforeEach, describe, it } from "node:test";
 
-import { isCI } from "../src/ci.js";
+import { isCi } from "../src/ci.js";
+
+// Get the original ENV variables so they can be restored at the end of teh tests
+const ORIGINAL_ENV_VARS = process.env;
 
 describe("ci", () => {
-  it("should be true because the ENV variable CI is true", async function () {
-    process.env.CI = "true";
-    assert.equal(isCI(), true);
-  });
+  describe("isCi", () => {
+    beforeEach(function () {
+      process.env = {};
+    });
 
-  it("should be false because the ENV variable CI is false", async function () {
-    process.env.CI = "false";
-    assert.equal(isCI(), false);
-  });
+    after(function () {
+      // Restore original ENV variables
+      process.env = ORIGINAL_ENV_VARS;
+    });
 
-  it("should be false because the ENV variable CI is undefined", async function () {
-    assert.equal(isCI(), false);
+    it("should be false because all the ENV variables are undefined", async function () {
+      assert.equal(isCi(), false);
+    });
+
+    it("should be true because the ENV variable CI is true", async function () {
+      process.env.CI = "true";
+      assert.equal(isCi(), true);
+    });
+
+    it("should be true because the ENV variable CONTINUOUS_INTEGRATION is true", async function () {
+      process.env.CONTINUOUS_INTEGRATION = "true";
+      assert.equal(isCi(), true);
+    });
+
+    it("should be true because the ENV variable BUILD_NUMBER is true", async function () {
+      process.env.BUILD_NUMBER = "true";
+      assert.equal(isCi(), true);
+    });
+
+    it("should be true because the ENV variable RUN_ID is true", async function () {
+      process.env.RUN_ID = "true";
+      assert.equal(isCi(), true);
+    });
+
+    it("should be true because the ENV variable GITHUB_ACTIONS is true", async function () {
+      process.env.GITHUB_ACTIONS = "true";
+      assert.equal(isCi(), true);
+    });
+
+    it("should be true because the ENV variable NOW is true", async function () {
+      process.env.NOW = "true";
+      assert.equal(isCi(), true);
+    });
+
+    it("should be true because the ENV variable DEPLOYMENT_ID is true", async function () {
+      process.env.DEPLOYMENT_ID = "true";
+      assert.equal(isCi(), true);
+    });
+
+    it("should be true because the ENV variable CODEBUILD_BUILD_NUMBER is true", async function () {
+      process.env.CODEBUILD_BUILD_NUMBER = "true";
+      assert.equal(isCi(), true);
+    });
   });
 });
