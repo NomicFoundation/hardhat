32e3f76e0da6b1b8a26759382ecfee661cd6cb4e
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-06-18 00:37:33 +0200

    modify getHardhatVersion to use readClosestPackageJson

--- v-next/hardhat/src/internal/utils/package.ts
+++ v-next/hardhat/src/internal/utils/package.ts
@@ -1,22 +1,11 @@
 import type { PackageJson } from "@nomicfoundation/hardhat-utils/package";
 
-import { fileURLToPath } from "node:url";
-
-import { assertHardhatInvariant } from "@nomicfoundation/hardhat-errors";
-import { readJsonFile } from "@nomicfoundation/hardhat-utils/fs";
-import { findClosestPackageJson } from "@nomicfoundation/hardhat-utils/package";
+import { readClosestPackageJson } from "@nomicfoundation/hardhat-utils/package";
 
 export async function getHardhatVersion(): Promise<string> {
-  const packageJsonPath = await findClosestPackageJson(
-    fileURLToPath(import.meta.url),
+  const packageJson: PackageJson = await readClosestPackageJson(
+    import.meta.url,
   );
-
-  assertHardhatInvariant(
-    packageJsonPath !== null,
-    "There should be a package.json in hardhat's root directory",
-  );
-
-  const packageJson: PackageJson = await readJsonFile(packageJsonPath);
 
   return packageJson.version;
 }
