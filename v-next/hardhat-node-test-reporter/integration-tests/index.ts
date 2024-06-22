48f086278b80bd9858219f006226d95cc09fee83
Patricio Palladino <email@patriciopalladino.com>
2024-06-22 19:32:08 +0000

    Remove the node internal frames from stack traces

--- v-next/hardhat-node-test-reporter/integration-tests/index.ts
+++ v-next/hardhat-node-test-reporter/integration-tests/index.ts
@@ -71,5 +71,9 @@ function normalizeOutputs(output: string): string {
       .replaceAll(/\(.*?:\d+:\d+\)/g, (match) => {
         return match.replaceAll(path.sep, "/");
       })
+      // Remove lines like `at TestHook.run (node:internal/test_runner/test:1107:18)`
+      .replaceAll(/at .*? \(node\:.*?:\d+:\d+\)/g, "")
+      // Remove lines like `at node:internal/test_runner/test:776:20`
+      .replaceAll(/at node\:.*?:\d+:\d+/g, "")
   );
 }
