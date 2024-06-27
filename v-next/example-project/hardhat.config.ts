6336cd07522eeb0740ae46a703898d3c9765ab5e
Luis Schaab <schaable@gmail.com>
2024-06-27 13:09:32 -0300

    Add HardhatPluginError

--- v-next/example-project/hardhat.config.ts
+++ v-next/example-project/hardhat.config.ts
@@ -1,4 +1,5 @@
 import { task, HardhatUserConfig } from "@ignored/hardhat-vnext/config";
+import { HardhatPluginError } from "@ignored/hardhat-vnext/plugins";
 
 export default {
   tasks: [
@@ -10,6 +11,13 @@ export default {
       })
       .setAction(async ({ greeting }, _) => {
         console.log(greeting);
+
+        if (greeting === "") {
+          throw new HardhatPluginError(
+            "example-plugin",
+            "Greeting cannot be empty",
+          );
+        }
       })
       .build(),
   ],
---
+++ v-next/hardhat/src/plugins.ts
@@ -0,0 +1,1 @@
+export { HardhatPluginError } from "@nomicfoundation/hardhat-errors";
