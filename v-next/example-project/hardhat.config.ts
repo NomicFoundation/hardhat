d5740922861d2147f8d8574e9160f0d4d05c8275
Luis Schaab <schaable@gmail.com>
2024-06-27 14:37:15 -0300

    Throw HardhatPluginError from an actual plugin task

--- v-next/example-project/hardhat.config.ts
+++ v-next/example-project/hardhat.config.ts
@@ -11,14 +11,31 @@ export default {
       })
       .setAction(async ({ greeting }, _) => {
         console.log(greeting);
-
-        if (greeting === "") {
-          throw new HardhatPluginError(
-            "example-plugin",
-            "Greeting cannot be empty",
-          );
-        }
       })
       .build(),
+  ],
+  plugins: [
+    {
+      id: "plugin-example",
+      tasks: [
+        task("plugin1-hello", "Prints a greeting from plugin1")
+          .addOption({
+            name: "greeting",
+            description: "The greeting to print",
+            defaultValue: "Hello, World from plugin1!",
+          })
+          .setAction(async ({ greeting }, _) => {
+            console.log(greeting);
+
+            if (greeting === "") {
+              throw new HardhatPluginError(
+                "plugin-example",
+                "Greeting cannot be empty",
+              );
+            }
+          })
+          .build(),
+      ],
+    },
   ],
 } satisfies HardhatUserConfig;
