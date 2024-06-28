2d80ced757fddb51986b8f4e6071a477312bfd1e
Luis Schaab <schaable@gmail.com>
2024-06-28 17:52:22 -0300

    Fix lint

--- v-next/hardhat/src/cli.ts
+++ v-next/hardhat/src/cli.ts
@@ -2,9 +2,10 @@
 // small file is loaded with sourcemaps enabled.
 process.setSourceMapsEnabled(true);
 
-// eslint-disable-next-line no-restricted-syntax -- Allow top-level await here
+/* eslint-disable no-restricted-syntax -- Allow top-level await here */
 const { main } = await import("./internal/cli/main.js");
 const { printErrorMessages } = await import("./internal/cli/error-handler.js");
+/* eslint-enable no-restricted-syntax */
 
 main(process.argv.slice(2)).catch((error: unknown) => {
   printErrorMessages(error);
