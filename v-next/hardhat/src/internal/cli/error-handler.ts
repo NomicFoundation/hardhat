fca90009231c0f89fac16edb90e2c7570401503c
Luis Schaab <schaable@gmail.com>
2024-07-01 14:13:39 -0300

    Add missing parameter in jsdoc

--- v-next/hardhat/src/internal/cli/error-handler.ts
+++ v-next/hardhat/src/internal/cli/error-handler.ts
@@ -60,6 +60,9 @@ interface ErrorMessages {
  *
  * @param error the error to handle. Supported categories are defined in
  * {@link ErrorCategory}.
+ * @param shouldShowStackTraces whether to show stack traces or not. If true,
+ * the stack trace is always shown. If false, the stack trace is only shown for
+ * errors of category {@link ErrorCategory.OTHER}.
  * @param print the function used to print the error message, defaults to
  * `console.error`. Useful for testing to capture error messages.
  */
