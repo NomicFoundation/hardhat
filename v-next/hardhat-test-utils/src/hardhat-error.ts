0834fbaba40328ab592617435ac2b25f73cd7946
Patricio Palladino <email@patriciopalladino.com>
2024-07-05 20:54:14 +0000

    Introduce the hardhat-test-utils package

---
+++ v-next/hardhat-test-utils/src/hardhat-error.ts
@@ -0,0 +1,68 @@
+import type { ErrorDescriptor } from "@ignored/hardhat-vnext-errors";
+
+import assert from "node:assert/strict";
+
+import { HardhatError } from "@ignored/hardhat-vnext-errors";
+import { ensureError } from "@ignored/hardhat-vnext-utils/error";
+
+export function assertIsHardhatError<ErrorDescriptorT extends ErrorDescriptor>(
+  error: unknown,
+  descritor: ErrorDescriptorT,
+  messageArguments: HardhatError<ErrorDescriptorT>["messageArguments"],
+): asserts error is HardhatError<ErrorDescriptorT> {
+  assert.ok(HardhatError.isHardhatError(error), "Error is not a HardhatError");
+
+  // We first compare the number individually to throw a better error message
+  // in case the descriptors are different.
+  assert.equal(
+    error.descriptor.number,
+    descritor.number,
+    `Expected error number ${descritor.number}, but got ${error.descriptor.number}`,
+  );
+
+  assert.deepEqual(error.descriptor, descritor);
+
+  assert.deepEqual(error.messageArguments, messageArguments);
+}
+
+export function assertThrowsHardhatError<
+  ErrorDescriptorT extends ErrorDescriptor,
+>(
+  f: () => any,
+  descritor: ErrorDescriptorT,
+  messageArguments: HardhatError<ErrorDescriptorT>["messageArguments"],
+): void {
+  try {
+    f();
+  } catch (error) {
+    ensureError(error);
+    assertIsHardhatError(error, descritor, messageArguments);
+
+    return;
+  }
+
+  assert.fail("Function did not throw any error");
+}
+
+export async function assertThrowsHardhatErrorAsync<
+  ErrorDescriptorT extends ErrorDescriptor,
+>(
+  f: (() => Promise<any>) | Promise<any>,
+  descritor: ErrorDescriptorT,
+  messageArguments: HardhatError<ErrorDescriptorT>["messageArguments"],
+): Promise<void> {
+  try {
+    if (f instanceof Promise) {
+      await f;
+    } else {
+      await f();
+    }
+  } catch (error) {
+    ensureError(error);
+    assertIsHardhatError(error, descritor, messageArguments);
+
+    return;
+  }
+
+  assert.fail("Function did not throw any error");
+}
---
+++ v-next/hardhat-test-utils/test/hardhat-error.ts
@@ -0,0 +1,168 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+
+import { HardhatError } from "@ignored/hardhat-vnext-errors";
+
+import {
+  assertIsHardhatError,
+  assertThrowsHardhatError,
+  assertThrowsHardhatErrorAsync,
+} from "../src/hardhat-error.js";
+
+describe("HardhatError helpers", () => {
+  describe("assertIsHardhatError", () => {
+    it("should throw if the error is not a HardhatError", () => {
+      assert.throws(() => {
+        assertIsHardhatError(
+          new Error("Not a HardhatError"),
+          HardhatError.ERRORS.TASK_DEFINITIONS.INVALID_ACTION,
+          { action: "foo", task: "bar" },
+        );
+      });
+
+      assert.throws(() => {
+        assertIsHardhatError(
+          123,
+          HardhatError.ERRORS.TASK_DEFINITIONS.INVALID_ACTION,
+          { action: "foo", task: "bar" },
+        );
+      });
+    });
+
+    it("should throw if the error has a different descriptor", () => {
+      assert.throws(() => {
+        assertIsHardhatError(
+          new HardhatError(
+            HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+            { option: "foo", task: "bar" },
+          ),
+          HardhatError.ERRORS.TASK_DEFINITIONS.INVALID_ACTION,
+          { action: "foo", task: "bar" },
+        );
+      });
+    });
+
+    it("should throw if the error has different message arguments", () => {
+      assert.throws(() => {
+        assertIsHardhatError(
+          new HardhatError(
+            HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+            { option: "foo", task: "bar" },
+          ),
+          HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+          { option: "foo2", task: "bar" },
+        );
+      });
+    });
+
+    it("Should not throw if the error is a HardhatError with the same descriptor and message arguments", () => {
+      assertIsHardhatError(
+        new HardhatError(
+          HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+          { option: "foo", task: "bar" },
+        ),
+        HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+        { option: "foo", task: "bar" },
+      );
+    });
+  });
+
+  describe("assertThrowsHardhatError", () => {
+    it("should throw if the function does not throw", () => {
+      assert.throws(() => {
+        assertThrowsHardhatError(
+          () => {},
+          HardhatError.ERRORS.TASK_DEFINITIONS.INVALID_ACTION,
+          { action: "foo", task: "bar" },
+        );
+      });
+    });
+
+    it("asserts the error that was thrown", () => {
+      assert.throws(() =>
+        assertThrowsHardhatError(
+          () => {
+            throw new HardhatError(
+              HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+              { option: "foo", task: "bar" },
+            );
+          },
+          HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+          { option: "foo2", task: "bar" },
+        ),
+      );
+
+      assertThrowsHardhatError(
+        () => {
+          throw new HardhatError(
+            HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+            { option: "foo", task: "bar" },
+          );
+        },
+        HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+        { option: "foo", task: "bar" },
+      );
+    });
+  });
+
+  describe("assertThrowsHardhatErrorAsync", () => {
+    it("should throw if the function doesn't return a promise that rejects", async () => {
+      await assert.rejects(async () =>
+        assertThrowsHardhatErrorAsync(
+          async () => {},
+          HardhatError.ERRORS.TASK_DEFINITIONS.INVALID_ACTION,
+          { action: "foo", task: "bar" },
+        ),
+      );
+    });
+
+    it("should throw the promise that rejects", async () => {
+      await assert.rejects(async () =>
+        assertThrowsHardhatErrorAsync(
+          Promise.resolve(1),
+          HardhatError.ERRORS.TASK_DEFINITIONS.INVALID_ACTION,
+          { action: "foo", task: "bar" },
+        ),
+      );
+    });
+
+    it("asserts the error of the rejection", async () => {
+      await assert.rejects(() =>
+        assertThrowsHardhatErrorAsync(
+          async () => {
+            throw new HardhatError(
+              HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+              { option: "foo", task: "bar" },
+            );
+          },
+          HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+          { option: "foo2", task: "bar" },
+        ),
+      );
+
+      await assert.rejects(() =>
+        assertThrowsHardhatErrorAsync(
+          Promise.reject(
+            new HardhatError(
+              HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+              { option: "foo", task: "bar" },
+            ),
+          ),
+          HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+          { option: "foo2", task: "bar" },
+        ),
+      );
+
+      await assertThrowsHardhatErrorAsync(
+        async () => {
+          throw new HardhatError(
+            HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+            { option: "foo", task: "bar" },
+          );
+        },
+        HardhatError.ERRORS.TASK_DEFINITIONS.UNRECOGNIZED_TASK_OPTION,
+        { option: "foo", task: "bar" },
+      );
+    });
+  });
+});
