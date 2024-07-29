b4b96ed9a5709d0db4ba36c7ef7462ca5668959a
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-07-29 18:15:07 +0200

    use test keys fro google analytics

--- v-next/hardhat/src/internal/cli/telemetry/analytics/analytics-subprocess.ts
+++ v-next/hardhat/src/internal/cli/telemetry/analytics/analytics-subprocess.ts
@@ -2,8 +2,8 @@ import { postJsonRequest } from "@ignored/hardhat-vnext-utils/request";
 
 // These keys are expected to be public
 const ANALYTICS_URL = "https://www.google-analytics.com/mp/collect";
-const API_SECRET = "fQ5joCsDRTOp55wX8a2cVw";
-const MEASUREMENT_ID = "G-8LQ007N2QJ";
+const API_SECRET = "iXzTRik5RhahYpgiatSv1w";
+const MEASUREMENT_ID = "G-ZFZWHGZ64H";
 
 const payload = JSON.parse(process.argv[2]);
 
