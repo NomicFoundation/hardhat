0a488a0ff891aff6457d03d8f464e7b3951c7191
Franco Victorio <victorio.franco@gmail.com>
2024-08-21 19:02:24 +0200

    Add await in provider creation

--- packages/hardhat-core/src/internal/hardhat-network/provider/provider.ts
+++ packages/hardhat-core/src/internal/hardhat-network/provider/provider.ts
@@ -628,7 +628,7 @@ export async function createHardhatNetworkProvider(
   log("Making tracing config");
   const tracingConfig = await makeTracingConfig(artifacts);
   log("Creating EDR provider");
-  const provider = EdrProviderWrapper.create(
+  const provider = await EdrProviderWrapper.create(
     hardhatNetworkProviderConfig,
     loggerConfig,
     tracingConfig
