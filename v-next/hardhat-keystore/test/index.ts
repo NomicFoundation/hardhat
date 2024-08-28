3ce796b71940d22ba8a9c4769383e2bc4bce763a
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-08-28 15:11:52 +0200

    simplify the tests by removing unnecessary mock tests

--- v-next/hardhat-keystore/test/index.ts
+++ v-next/hardhat-keystore/test/index.ts
@@ -12,6 +12,7 @@ import { createHardhatRuntimeEnvironment } from "@ignored/hardhat-vnext/hre";
 import { HardhatError } from "@ignored/hardhat-vnext-errors";
 import {
   ensureDir,
+  exists,
   readJsonFile,
   remove,
   writeJsonFile,
@@ -61,6 +62,15 @@ async function getConfigDir(): Promise<string> {
   return config;
 }
 
+function getFullOutput(mockF: Mock<any>, totCalls: number): string {
+  const list = [];
+  for (let i = 0; i < totCalls; i++) {
+    list.push(mockF.mock.calls[i].arguments[0]);
+  }
+
+  return list.join("\n");
+}
+
 describe("tasks", () => {
   let hre: HardhatRuntimeEnvironment;
   let task: Task;
@@ -98,6 +108,8 @@ describe("tasks", () => {
     mockError.mock.resetCalls();
 
     mockRequestSecretInput.mock.resetCalls();
+
+    await deleteKeystore();
   });
 
   describe("hook", () => {
@@ -175,7 +187,7 @@ describe("tasks", () => {
         );
       });
 
-      it("should successfully set the keystore on the first usage", async () => {
+      it("should successfully configure the keystore on the first usage", async () => {
         mockRequestSecretInput = mock.method(
           io,
           "requestSecretInput",
@@ -184,26 +196,30 @@ describe("tasks", () => {
 
         await task.run({ key: "key" });
 
-        assert.equal(mockRequestSecretInput.mock.calls.length, 3);
-        // Although 5 messages are asserted, only 4 are checked because a new key is set during the process.
-        // We ignore this for now and will test it in the next test.
-        assert.equal(mockInfo.mock.calls.length, 5);
-        assert.equal(
-          mockInfo.mock.calls[0].arguments[0],
-          "\n👷🔐 Hardhat-Keystore 🔐👷\n",
-        );
-        assert.equal(
-          mockInfo.mock.calls[1].arguments[0],
-          "This is the first time you are using the keystore, please set a password.",
-        );
-        assert.equal(
-          mockInfo.mock.calls[2].arguments[0],
-          "The password must have at least 8 characters, one uppercase letter, one lowercase letter, and one special character.",
-        );
-        assert.equal(mockInfo.mock.calls[3].arguments[0], "");
+        assert.equal(await exists(await getKeystoreFilePath()), true);
       });
 
-      it("should successfully set the keystore on the first usage but show password warnings", async () => {
+      it("should successfully show the right configuration message when configuring the keystore on the first usage", async () => {
+        mockRequestSecretInput = mock.method(
+          io,
+          "requestSecretInput",
+          (_msg: string) => TEST_PASSWORD,
+        );
+
+        await task.run({ key: "key" });
+
+        assert.equal(
+          getFullOutput(mockInfo, 4),
+          `
+👷🔐 Hardhat-Keystore 🔐👷
+
+This is the first time you are using the keystore, please set a password.
+The password must have at least 8 characters, one uppercase letter, one lowercase letter, and one special character.
+`,
+        );
+      });
+
+      it("should successfully configure the keystore on the first usage but also show password warnings", async () => {
         // Simulate a scenario where a password does not meet the criteria, and another where passwords do not match.
         // The user will eventually correct the password values.
         let count = 0;
@@ -214,16 +230,16 @@ describe("tasks", () => {
             let msg = "";
 
             if (count === 0) {
-              // Invalid password
+              // Step 1: invalid password
               msg = "invalid";
             } else if (count === 1) {
-              // Valid password
+              // Step 2: valid password
               msg = TEST_PASSWORD;
             } else if (count === 2) {
-              // Passwords do not match
+              // Step 3: passwords do not match
               msg = "not-matching-password";
             } else {
-              // Passwords match
+              // Step 4: passwords match
               msg = TEST_PASSWORD;
             }
 
@@ -234,27 +250,13 @@ describe("tasks", () => {
 
         await task.run({ key: "key" });
 
-        assert.equal(mockRequestSecretInput.mock.calls.length, 5);
-        assert.equal(mockInfo.mock.calls.length, 5);
-        assert.equal(mockError.mock.calls.length, 2);
-        assert.equal(
-          mockInfo.mock.calls[0].arguments[0],
-          "\n👷🔐 Hardhat-Keystore 🔐👷\n",
-        );
-        assert.equal(
-          mockInfo.mock.calls[1].arguments[0],
-          "This is the first time you are using the keystore, please set a password.",
-        );
-        assert.equal(
-          mockInfo.mock.calls[2].arguments[0],
-          "The password must have at least 8 characters, one uppercase letter, one lowercase letter, and one special character.",
-        );
-        assert.equal(mockInfo.mock.calls[3].arguments[0], "");
         assert.equal(mockError.mock.calls[0].arguments[0], "Invalid password!");
         assert.equal(
           mockError.mock.calls[1].arguments[0],
           "Passwords do not match!",
         );
+
+        assert.equal(await exists(await getKeystoreFilePath()), true);
       });
 
       it("should indicate that the key is not valid", async () => {
@@ -262,7 +264,6 @@ describe("tasks", () => {
 
         await task.run({ key: "1key" });
 
-        assert.equal(mockError.mock.calls.length, 1);
         assert.equal(
           mockError.mock.calls[0].arguments[0],
           `Invalid value for key: "1key". Keys can only have alphanumeric characters and underscores, and they cannot start with a number.`,
@@ -280,7 +281,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key2" });
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(mockInfo.mock.calls[0].arguments[0], `Key "key2" set`);
         assert.deepEqual(await getKeystore(), {
           version: "",
@@ -302,7 +302,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key" });
 
-        assert.equal(mockWarn.mock.calls.length, 1);
         assert.equal(
           mockWarn.mock.calls[0].arguments[0],
           `The key "key" already exists. Use the ${chalk.blue.italic("--force")} flag to overwrite it.`,
@@ -327,7 +326,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key", force: true });
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(mockInfo.mock.calls[0].arguments[0], `Key "key" set`);
         assert.deepEqual(await getKeystore(), {
           version: "",
@@ -348,7 +346,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key2" });
 
-        assert.equal(mockError.mock.calls.length, 1);
         assert.equal(
           mockError.mock.calls[0].arguments[0],
           "The secret cannot be empty.",
@@ -382,7 +379,6 @@ describe("tasks", () => {
       it("should indicate that the keystore is not set", async () => {
         await task.run({ key: "key" });
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(mockInfo.mock.calls[0].arguments[0], NO_KEYSTORE_SET);
       });
 
@@ -391,7 +387,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key" });
 
-        assert.equal(mockError.mock.calls.length, 1);
         assert.equal(
           mockError.mock.calls[0].arguments[0],
           `Key "key" not found`,
@@ -403,7 +398,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key" });
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(mockInfo.mock.calls[0].arguments[0], "value");
       });
     });
@@ -416,7 +410,6 @@ describe("tasks", () => {
       it("should indicate that the keystore is not set", async () => {
         await task.run({});
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(mockInfo.mock.calls[0].arguments[0], NO_KEYSTORE_SET);
       });
 
@@ -425,7 +418,6 @@ describe("tasks", () => {
 
         await task.run({});
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(
           mockInfo.mock.calls[0].arguments[0],
           "The keystore does not contain any keys.",
@@ -440,10 +432,12 @@ describe("tasks", () => {
 
         await task.run({});
 
-        assert.equal(mockInfo.mock.calls.length, 3);
-        assert.equal(mockInfo.mock.calls[0].arguments[0], "Keys:");
-        assert.equal(mockInfo.mock.calls[1].arguments[0], "key");
-        assert.equal(mockInfo.mock.calls[2].arguments[0], "key2");
+        assert.equal(
+          getFullOutput(mockInfo, 3),
+          `Keys:
+key
+key2`,
+        );
       });
     });
 
@@ -466,7 +460,6 @@ describe("tasks", () => {
       it("should indicate that the keystore is not set", async () => {
         await task.run({ key: "key" });
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(mockInfo.mock.calls[0].arguments[0], NO_KEYSTORE_SET);
       });
 
@@ -475,7 +468,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key" });
 
-        assert.equal(mockError.mock.calls.length, 1);
         assert.equal(
           mockError.mock.calls[0].arguments[0],
           `Key "key" not found`,
@@ -487,7 +479,6 @@ describe("tasks", () => {
 
         await task.run({ key: "key" });
 
-        assert.equal(mockInfo.mock.calls.length, 1);
         assert.equal(mockInfo.mock.calls[0].arguments[0], `Key "key" removed`);
         assert.deepEqual(await getKeystore(), { version: "", keys: {} });
       });
