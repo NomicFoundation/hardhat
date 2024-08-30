97f7d56f21a0cdb3d72aed81c87bd11fb7b1001d
galargh <piotr.galar@gmail.com>
2024-08-30 11:41:11 +0100

    chore: add ignored cases to test only it with the flag

--- v-next/hardhat-node-test-reporter/integration-tests/fixture-tests/only-test-with-the-flag/test.ts
+++ v-next/hardhat-node-test-reporter/integration-tests/fixture-tests/only-test-with-the-flag/test.ts
@@ -1,6 +1,10 @@
 import assert from "node:assert/strict";
 import { describe, it, test } from "node:test";
 
+test("test", async () => {
+  assert.equal(1, 1);
+});
+
 /* eslint-disable-next-line no-only-tests/no-only-tests -- testing test.only explicitly */
 test.only("test only", async () => {
   assert.equal(1, 2);
@@ -17,5 +21,9 @@ describe("describe", async () => {
   // eslint-disable-next-line no-only-tests/no-only-tests -- testing it.only explicitly
   it.only("it only", async () => {
     assert.equal(1, 2);
+  });
+
+  it("it", async () => {
+    assert.equal(1, 1);
   });
 });
