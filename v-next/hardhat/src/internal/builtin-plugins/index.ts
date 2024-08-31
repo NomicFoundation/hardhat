2b8b97ad417cb7aada2039777668c2d27818faf4
Patricio Palladino <email@patriciopalladino.com>
2024-08-31 21:54:27 +0000

    Reorder the builtin plugins

--- v-next/hardhat/src/internal/builtin-plugins/index.ts
+++ v-next/hardhat/src/internal/builtin-plugins/index.ts
@@ -7,14 +7,15 @@ import run from "./run/index.js";
 
 // Note: When importing a plugin, you have to export its types, so that its
 // type extensions, if any, also get loaded.
+export type * from "./network-manager/index.js";
 export type * from "./clean/index.js";
 export type * from "./console/index.js";
 export type * from "./network-manager/index.js";
 export type * from "./run/index.js";
 
 export const builtinPlugins: HardhatPlugin[] = [
+  networkManager,
   clean,
   console,
-  networkManager,
   run,
 ];
