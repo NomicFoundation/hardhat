e77c5af6485346d47b5126c8c2cc7f3a7fc806d8
John Kane <john@kanej.me>
2024-09-02 12:27:37 +0100

    test: include set in integration tests

--- v-next/hardhat-keystore/test/integration/tasks.ts
+++ v-next/hardhat-keystore/test/integration/tasks.ts
@@ -3,6 +3,7 @@ import type { HardhatRuntimeEnvironment } from "@ignored/hardhat-vnext/types/hre
 
 import assert from "node:assert/strict";
 import path from "node:path";
+import { Readable } from "node:stream";
 import { afterEach, beforeEach, describe, it } from "node:test";
 import { fileURLToPath } from "node:url";
 
@@ -10,9 +11,7 @@ import { createHardhatRuntimeEnvironment } from "@ignored/hardhat-vnext/hre";
 import { remove, writeJsonFile } from "@ignored/hardhat-vnext-utils/fs";
 
 import hardhatKeystorePlugin from "../../src/index.js";
-import { MockUserInterruptionManager } from "../helpers/mock-user-interruption-manager.js";
 import { setupKeystoreFileLocationOverrideAt } from "../helpers/setup-keystore-file-location-override-at.js";
-import { setupMockUserInterruptionPlugin } from "../helpers/setup-mock-user-interruptions-plugin.js";
 
 const keystoreFilePath = path.join(
   fileURLToPath(import.meta.url),
@@ -21,11 +20,8 @@ const keystoreFilePath = path.join(
 
 describe("integration tests for the keystore tasks", () => {
   let hre: HardhatRuntimeEnvironment;
-  let mockUserInterruptions: MockUserInterruptionManager;
 
   beforeEach(async () => {
-    await remove(keystoreFilePath);
-
     const keystoreFile: KeystoreFile = {
       version: "",
       keys: {
@@ -34,15 +30,12 @@ describe("integration tests for the keystore tasks", () => {
       },
     };
 
-    await writeJsonFile(keystoreFilePath, keystoreFile);
-
-    mockUserInterruptions = new MockUserInterruptionManager();
+    await _overwriteKeystoreFileWith(keystoreFilePath, keystoreFile);
 
     hre = await createHardhatRuntimeEnvironment({
       plugins: [
         hardhatKeystorePlugin,
         setupKeystoreFileLocationOverrideAt(keystoreFilePath),
-        setupMockUserInterruptionPlugin(mockUserInterruptions),
       ],
     });
   });
@@ -52,46 +45,84 @@ describe("integration tests for the keystore tasks", () => {
   });
 
   it("should display the value on a `npx hardhat keystore get`", async () => {
-    await assertConsoleOutputMatchesFor(
+    await _assertConsoleOutputMatchesFor(
       () => hre.tasks.getTask(["keystore", "get"]).run({ key: "myKey1" }),
       "myValue1\n",
     );
   });
 
   it("should display the list of keys on `npx hardhat keystore list`", async () => {
-    await assertConsoleOutputMatchesFor(
+    await _assertConsoleOutputMatchesFor(
       () => hre.tasks.getTask(["keystore", "list"]).run({}),
       "Keys:\nmyKey1\nmyKey2\n",
     );
   });
 
   it("should display the delete the key on `npx hardhat keystore delete myKey1`", async () => {
-    await assertConsoleOutputMatchesFor(
+    await _assertConsoleOutputMatchesFor(
       () => hre.tasks.getTask(["keystore", "delete"]).run({ key: "myKey1" }),
       'Key "myKey1" removed\n',
     );
   });
 
-  it.skip("should display the setting of the key on `npx hardhat keystore set myNewKey`", async () => {
-    const task = hre.tasks.getTask(["keystore", "set"]);
-
-    await task.run({ key: "myNewKey" });
+  it("should display the setting of the key on `npx hardhat keystore set myNewKey`", async () => {
+    await _assertConsoleOutputMatchesFor(
+      () => hre.tasks.getTask(["keystore", "set"]).run({ key: "myNewKey" }),
+      "Enter secret to store: " + 'Key "myNewKey" set\n',
+      ["myNewValue\n"],
+    );
   });
 });
 
-// This is a ... hack
-async function assertConsoleOutputMatchesFor(
+async function _overwriteKeystoreFileWith(
+  filePath: string,
+  keystoreFile: KeystoreFile,
+) {
+  await remove(filePath);
+
+  await writeJsonFile(filePath, keystoreFile);
+}
+
+/**
+ * This is a helper function that mocks out `process.stdin`'s read and `process.stdout`'s write
+ * to allow for crude integration testing.
+ *
+ * This is obviously brittle, however the tasks intentionally don't go through the Hook
+ * User Interruption system, hence we can't just mock `hre.interruptions` to provide inputs
+ * and assert against outputs.
+ *
+ * @param action the action to run in the scope of monkey patched stdin and stdout
+ * @param expectedOutput the expected stdout output as a string
+ * @param inputs optional inputs to be provided to stdin
+ */
+async function _assertConsoleOutputMatchesFor(
   action: () => Promise<void>,
   expectedOutput: string,
+  inputs?: string[],
 ): Promise<void> {
   let output = "";
-  const originalWrite = process.stdout.write;
 
+  const originalWrite = process.stdout.write;
   process.stdout.write = (chunk: string): boolean => {
     output += chunk.toString();
 
     return true;
   };
+
+  const originalRead = process.stdin.read;
+  if (inputs !== undefined) {
+    const mockStream = new Readable({
+      read() {
+        for (const input of inputs) {
+          this.push(input);
+        }
+
+        this.push(null);
+      },
+    });
+
+    Object.assign(process.stdin, mockStream);
+  }
 
   try {
     await action();
@@ -99,5 +130,6 @@ async function assertConsoleOutputMatchesFor(
     assert.equal(output, expectedOutput);
   } finally {
     process.stdout.write = originalWrite;
+    process.stdin.read = originalRead;
   }
 }
