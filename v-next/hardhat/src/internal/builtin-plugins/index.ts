2c9c85925b9af8cd64626e96b1d88c58bbcbcde9
John Kane <john@kanej.me>
2024-09-02 16:52:27 +0100

    refactor: remove the entire foo plugin

--- v-next/hardhat/src/internal/builtin-plugins/index.ts
+++ v-next/hardhat/src/internal/builtin-plugins/index.ts
@@ -8,7 +8,6 @@ import run from "./run/index.js";
 // type extensions, if any, also get loaded.
 export type * from "./clean/index.js";
 export type * from "./console/index.js";
-export type * from "./hardhat-foo/index.js";
 export type * from "./run/index.js";
 
 export const builtinPlugins: HardhatPlugin[] = [clean, console, run];
