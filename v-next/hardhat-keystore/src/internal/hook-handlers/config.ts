356d40682232b06431ddc903ba1298859d579144
John Kane <john@kanej.me>
2024-09-03 12:43:28 +0100

    refactor: remove unused user type
    The only config we are changing is `HardhatConfig`, there was a copy and
    paste error that included user validation.

--- v-next/hardhat-keystore/src/internal/hook-handlers/config.ts
+++ v-next/hardhat-keystore/src/internal/hook-handlers/config.ts
@@ -1,23 +1,9 @@
 import type { ConfigHooks } from "@ignored/hardhat-vnext/types/hooks";
 
-import { validateUserConfigZodType } from "@ignored/hardhat-vnext-zod-utils";
-import { z } from "zod";
-
 import { getKeystoreFilePath } from "../utils/get-keystore-file-path.js";
-
-const keystoreConfigType = z.object({
-  filePath: z.string().optional(),
-});
-
-const userConfigType = z.object({
-  keystore: z.optional(keystoreConfigType),
-});
 
 export default async (): Promise<Partial<ConfigHooks>> => {
   const handlers: Partial<ConfigHooks> = {
-    validateUserConfig: async (userConfig) => {
-      return validateUserConfigZodType(userConfig, userConfigType);
-    },
     resolveUserConfig: async (
       userConfig,
       resolveConfigurationVariable,
