5c20b1a3f03dd32a28b1d7cdc0c1040c259dca9b
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-09-04 11:04:13 +0200

    tmp - re-add keystore dependency in example-project

--- v-next/example-project/hardhat.config.ts
+++ v-next/example-project/hardhat.config.ts
@@ -10,6 +10,7 @@ import {
 } from "@ignored/hardhat-vnext/config";
 import HardhatNodeTestRunner from "@ignored/hardhat-vnext-node-test-runner";
 import HardhatMochaTestRunner from "@ignored/hardhat-vnext-mocha-test-runner";
+import HardhatKeystore from "@ignored/hardhat-vnext-keystore";
 import { viemScketchPlugin } from "./viem-scketch-plugin.js";
 
 const exampleEmptyTask = emptyTask("empty", "An example empty task").build();
@@ -122,6 +123,7 @@ const config: HardhatUserConfig = {
   ],
   plugins: [
     pluginExample,
+    HardhatKeystore,
     HardhatMochaTestRunner,
     // if testing node plugin, use the following line instead
     // HardhatNodeTestRunner,
