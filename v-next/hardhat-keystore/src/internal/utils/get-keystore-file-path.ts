d843e7a6f69c13eaa05a89c935487bbe7ce07285
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-09-09 16:59:47 +0200

    use getConfigDir from hh-utils

--- v-next/hardhat-keystore/src/internal/utils/get-keystore-file-path.ts
+++ v-next/hardhat-keystore/src/internal/utils/get-keystore-file-path.ts
@@ -1,7 +1,6 @@
 import path from "node:path";
 
-import { ensureDir } from "@ignored/hardhat-vnext-utils/fs";
-import envPaths from "env-paths";
+import { getConfigDir } from "@ignored/hardhat-vnext-utils/global-dir";
 
 /**
  * Get the path for the keystore in the Hardhat global config directory.
@@ -9,10 +8,4 @@ import envPaths from "env-paths";
 export async function getKeystoreFilePath(): Promise<string> {
   const configDirPath = await getConfigDir();
   return path.join(configDirPath, "keystore.json");
-}
-
-async function getConfigDir(): Promise<string> {
-  const { config } = envPaths("hardhat");
-  await ensureDir(config);
-  return config;
 }
