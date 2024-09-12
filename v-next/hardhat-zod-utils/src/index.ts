4fcaf04c521cc6cdc8b118ccd776883df11d3667
Patricio Palladino <email@patriciopalladino.com>
2024-09-12 20:07:36 +0000

    Add conditionalUnionType

--- v-next/hardhat-zod-utils/src/index.ts
+++ v-next/hardhat-zod-utils/src/index.ts
@@ -32,6 +32,88 @@ export const unionType = (
   });
 
 /**
+ * A Zod union type that allows you to provide hints to Zod about which of the
+ * type variant it should use.
+ *
+ * It receives an array of tuples, where each tuple contains a predicate
+ * function and a ZodType. The predicate function takes the data to be parsed
+ * and returns a boolean. If the predicate function returns true, the ZodType
+ * is used to parse the data.
+ *
+ * If none of the predicates returns true, an error is added to the context
+ * with the noMatchMessage message.
+ *
+ * For example, you can use this to conditionally validate a union type based
+ * on the values `typeof` and its fields:
+ *
+ * @example
+ * ```ts
+ * const fooType = conditionalUnionType(
+ *   [
+ *     [(data) => typeof data === "string", z.string()],
+ *     [(data) => Array.isArray(data), z.array(z.string()).nonempty()],
+ *     [(data) => isObject(data), z.object({foo: z.string().optional()})]
+ *   ],
+ *   "Expected a string, an array of strings, or an object with an optional 'foo' property",
+ * );
+ * ```
+ *
+ * @param cases An array of tuples of a predicate function and a ZodType.
+ * @param noMatchMessage THe error message to return if none of the predicates
+ * returns true.
+ * @returns The conditional union ZodType.
+ */
+export const conditionalUnionType = (
+  cases: Array<[predicate: (data: unknown) => boolean, zodType: ZodType<any>]>,
+  noMatchMessage: string,
+) =>
+  z.any().superRefine((data, ctx) => {
+    const matchingCase = cases.find(([predicate]) => predicate(data));
+    if (matchingCase === undefined) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: noMatchMessage,
+      });
+      return;
+    }
+
+    const zodeType = matchingCase[1];
+
+    const parsedData = zodeType.safeParse(data);
+    if (parsedData.error !== undefined) {
+      for (const issue of parsedData.error.issues) {
+        ctx.addIssue(issue);
+      }
+    }
+  });
+
+/**
+ * Creates a Zod type to validate that a field of an object doesn't exist.
+ *
+ * This is useful when you have a {@link conditionalUnionType} that represents
+ * a union of object types with incompatible fields between each other.
+ *
+ * @example
+ * ```ts
+ * const type = z.object({
+ *   foo: z.string(),
+ *   bar: unexpectedFieldType("This field is incompatible with `foo`"),
+ * });
+ * ```
+ *
+ * @param errorMessage The error message to display if the field is present.
+ * @returns A Zod type that validates that a field of an object doesn't exist.
+ */
+export const incompatibleFieldType = (errorMessage = "Unexpected field") =>
+  z
+    .never({
+      errorMap: () => ({
+        message: errorMessage,
+      }),
+    })
+    .optional();
+
+/**
  * A Zod type to validate Hardhat's ConfigurationVariable objects.
  */
 export const configurationVariableType = z.object({
@@ -42,16 +124,22 @@ export const configurationVariableType = z.object({
 /**
  * A Zod type to validate Hardhat's SensitiveString values.
  */
-export const sensitiveStringType = unionType(
-  [z.string(), configurationVariableType],
+export const sensitiveStringType = conditionalUnionType(
+  [
+    [(data) => typeof data === "string", z.string()],
+    [(data) => typeof data === "object", configurationVariableType],
+  ],
   "Expected a string or a Configuration Variable",
 );
 
 /**
  * A Zod type to validate Hardhat's SensitiveString values that expect a URL.
  */
-export const sensitiveUrlType = unionType(
-  [z.string().url(), configurationVariableType],
+export const sensitiveUrlType = conditionalUnionType(
+  [
+    [(data) => typeof data === "string", z.string().url()],
+    [(data) => typeof data === "object", configurationVariableType],
+  ],
   "Expected a URL or a Configuration Variable",
 );
 
@@ -64,15 +152,15 @@ export const sensitiveUrlType = unionType(
  * from the root of the config object, so that they are correctly reported to
  * the user.
  */
-export async function validateUserConfigZodType<
+export function validateUserConfigZodType<
   Output,
   Def extends ZodTypeDef = ZodTypeDef,
   Input = Output,
 >(
   hardhatUserConfig: HardhatUserConfigToValidate,
   configType: ZodType<Output, Def, Input>,
-): Promise<HardhatUserConfigValidationError[]> {
-  const result = await configType.safeParseAsync(hardhatUserConfig);
+): HardhatUserConfigValidationError[] {
+  const result = configType.safeParse(hardhatUserConfig);
 
   if (result.success) {
     return [];
--- v-next/hardhat-zod-utils/test/index.ts
+++ v-next/hardhat-zod-utils/test/index.ts
@@ -3,14 +3,19 @@ import { describe, it } from "node:test";
 
 import { z } from "zod";
 
-import { unionType } from "../src/index.js";
+import { unionType, conditionalUnionType } from "../src/index.js";
 
 function assertParseResult(
   result: z.SafeParseReturnType<any, any>,
   expectedMessage: string,
+  path?: Array<string | number>,
 ) {
   assert.equal(result.error?.errors.length, 1);
   assert.equal(result.error?.errors[0].message, expectedMessage);
+
+  if (path !== undefined) {
+    assert.deepEqual(result.error?.errors[0].path, path);
+  }
 }
 
 describe("unionType", () => {
@@ -30,5 +35,124 @@ describe("unionType", () => {
     assertParseResult(union.safeParse({}), "Expected error message");
 
     assertParseResult(union.safeParse(undefined), "Expected error message");
+  });
+});
+
+describe("conditionalUnionType", () => {
+  describe("Conditions evaluation", () => {
+    it("should return the first type that matches", async () => {
+      const shouldUseString = conditionalUnionType(
+        [
+          [(data) => typeof data === "string", z.string()],
+          [(data) => typeof data === "string", z.string().url()],
+        ],
+        "No match",
+      );
+
+      // Both conditions match, but we only use the first one
+      assert.equal(shouldUseString.safeParse("asd").success, true);
+
+      const shouldUseUrl = conditionalUnionType(
+        [
+          [(data) => typeof data === "string", z.string().url()],
+          [(data) => typeof data === "string", z.string()],
+        ],
+        "No match",
+      );
+
+      assertParseResult(shouldUseUrl.safeParse("asd"), "Invalid url");
+    });
+
+    it("should return the provided error message if no condition matches", async () => {
+      const shouldUseString = conditionalUnionType(
+        [
+          [(data) => typeof data === "string", z.string()],
+          [(data) => typeof data === "string", z.string().url()],
+        ],
+        "No match",
+      );
+
+      // Both conditions match, but we only use the first one
+      assertParseResult(shouldUseString.safeParse(123), "No match");
+    });
+  });
+
+  describe("Zod issues paths", () => {
+    it("should have an empty path when nothing matches in as a top-level type", async () => {
+      const shouldUseString = conditionalUnionType(
+        [
+          [(data) => typeof data === "string", z.string()],
+          [(data) => typeof data === "string", z.string().url()],
+        ],
+        "No match",
+      );
+
+      assertParseResult(shouldUseString.safeParse(123), "No match", []);
+    });
+
+    it("Should have the path to the nested error if a condition matches", () => {
+      const shouldUseString = conditionalUnionType(
+        [
+          [(data) => typeof data === "object", z.object({ foo: z.string() })],
+          [(data) => typeof data === "string", z.string().url()],
+        ],
+        "No match",
+      );
+
+      assertParseResult(
+        shouldUseString.safeParse({ foo: 123 }),
+        "Expected string, received number",
+        ["foo"],
+      );
+    });
+
+    it("Should have the path to the nested error, even if it's also a conditional union type", () => {
+      const shouldUseString = conditionalUnionType(
+        [
+          [(data) => typeof data === "string", z.string()],
+          [
+            (data) => typeof data === "object",
+            conditionalUnionType(
+              [
+                [
+                  (data) =>
+                    typeof data === "object" && data !== null && "foo" in data,
+                  z.object({ foo: z.string().url() }),
+                ],
+                [
+                  (data) =>
+                    typeof data === "object" && data !== null && "bar" in data,
+                  z.object({ bar: z.array(z.number()) }),
+                ],
+              ],
+              "No internal match",
+            ),
+          ],
+        ],
+        "No outer match",
+      );
+
+      assertParseResult(shouldUseString.safeParse(123), "No outer match", []);
+
+      assertParseResult(shouldUseString.safeParse({}), "No internal match", []);
+
+      assertParseResult(
+        shouldUseString.safeParse({ foo: "asd" }),
+        "Invalid url",
+        ["foo"],
+      );
+
+      assertParseResult(
+        shouldUseString.safeParse({ bar: "asd" }),
+        "Expected array, received string",
+        ["bar"],
+      );
+
+      assertParseResult(
+        shouldUseString.safeParse({ bar: ["asd"] }),
+        "Expected number, received string",
+        ["bar", 0],
+      );
+    });
   });
 });
