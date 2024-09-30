07d15ed572b2136e15e17c830ca9dfc9f8d1515c
Christopher Dedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2024-09-30 11:52:48 +0200

    update how ChainType is imported

--- v-next/hardhat-ethers/src/type-extensions.ts
+++ v-next/hardhat-ethers/src/type-extensions.ts
@@ -1,6 +1,5 @@
 import "@ignored/hardhat-vnext/types/network";
 import type { HardhatEthers } from "./types.js";
-import type { ChainType } from "@ignored/hardhat-vnext/types/config";
 
 declare module "@ignored/hardhat-vnext/types/network" {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars -- the ChainTypeT must be declared in the interface but in this scenario it's not used
