46ba80154463de717fc0d11057d19d364b8cc728
Luis Schaab <schaable@gmail.com>
2024-10-03 10:34:27 -0300

    feat: add skeleton for hardhat-viem package

---
+++ v-next/hardhat-viem/src/index.ts
@@ -0,0 +1,8 @@
+import type { HardhatPlugin } from "@ignored/hardhat-vnext/types/plugins";
+
+const hardhatPlugin: HardhatPlugin = {
+  id: "hardhat-viem",
+  hookHandlers: {},
+};
+
+export default hardhatPlugin;
