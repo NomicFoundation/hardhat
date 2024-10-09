a800b31fe08ce2fd90465f8a56cec48b9e98ee7d
John Kane <john@kanej.me>
2024-10-09 15:40:38 +0100

    refactor: replace `ansi-escapes` usage

--- v-next/hardhat/src/internal/builtin-plugins/network-manager/edr/utils/logger.ts
+++ v-next/hardhat/src/internal/builtin-plugins/network-manager/edr/utils/logger.ts
@@ -1,4 +1,8 @@
-import ansiEscapes from "ansi-escapes";
+const ANSI_ESCAPES_ESC = "\u001B[";
+const ANSI_ESCAPES_CURSOR_HIDE = `${ANSI_ESCAPES_ESC}?25l`;
+const ANSI_ESCAPES_CURSOR_PREV_LINE = `${ANSI_ESCAPES_ESC}F`;
+const ANSI_ESCAPES_ERASE_END_LINE = `${ANSI_ESCAPES_ESC}K`;
+const ANSI_ESCAPES_CURSOR_SHOW = `${ANSI_ESCAPES_ESC}?25h`;
 
 export function printLine(line: string): void {
   console.log(line);
@@ -7,12 +11,12 @@ export function printLine(line: string): void {
 export function replaceLastLine(newLine: string): void {
   if (process.stdout.isTTY === true) {
     process.stdout.write(
-      ansiEscapes.cursorHide +
-        ansiEscapes.cursorPrevLine +
+      ANSI_ESCAPES_CURSOR_HIDE +
+        ANSI_ESCAPES_CURSOR_PREV_LINE +
         newLine +
-        ansiEscapes.eraseEndLine +
+        ANSI_ESCAPES_ERASE_END_LINE +
         "\n" +
-        ansiEscapes.cursorShow,
+        ANSI_ESCAPES_CURSOR_SHOW,
     );
   } else {
     process.stdout.write(`${newLine}\n`);
