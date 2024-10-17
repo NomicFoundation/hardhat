e25727260d07e52ffbfa3412549f586f71a46be2
Luis Schaab <schaable@gmail.com>
2024-10-17 10:25:39 -0300

    Remove unneeded import

--- v-next/hardhat-viem/src/index.ts
+++ v-next/hardhat-viem/src/index.ts
@@ -1,10 +1,5 @@
 import type { HardhatPlugin } from "@ignored/hardhat-vnext/types/plugins";
 
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars
--- TODO: if we remove this line, the types are not augmented
-with the network types */
-import { createHardhatRuntimeEnvironment } from "@ignored/hardhat-vnext/hre";
-
 import "./type-extensions.js";
 
 const hardhatPlugin: HardhatPlugin = {
