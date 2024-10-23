a516d97a42b2a8eac5940a93e0fc091c9963673a
Luis Schaab <schaable@gmail.com>
2024-10-23 12:18:16 -0300

    refactor: format the libraries within the error classes

--- v-next/hardhat-utils/src/errors/bytecode.ts
+++ v-next/hardhat-utils/src/errors/bytecode.ts
@@ -1,7 +1,13 @@
+import type { LibraryAddresses, LibraryLink } from "../internal/bytecode.js";
+
 import { CustomError } from "../error.js";
 
 export class InvalidLibraryAddressError extends CustomError {
-  constructor(formattedLibraries: string) {
+  constructor(libraries: LibraryAddresses) {
+    const formattedLibraries = Object.entries(libraries)
+      .map(([libraryName, address]) => `\t* "${libraryName}": "${address}"`)
+      .join("\n");
+
     super(`The following libraries have invalid addresses:
 ${formattedLibraries}
 
@@ -10,7 +16,16 @@ Please provide valid Ethereum addresses for these libraries.`);
 }
 
 export class AmbiguousLibraryNameError extends CustomError {
-  constructor(formattedLibraries: string) {
+  constructor(libraries: Record<string, LibraryLink[]>) {
+    const formattedLibraries = Object.entries(libraries)
+      .map(
+        ([providedLibraryName, matchingLibraries]) =>
+          `\t* "${providedLibraryName}":\n${matchingLibraries
+            .map(({ libraryFqn }) => `\t\t* "${libraryFqn}"`)
+            .join("\n")}`,
+      )
+      .join("\n");
+
     super(`The following libraries may resolve to multiple libraries:
 ${formattedLibraries}
 
@@ -19,7 +34,11 @@ Please provide the fully qualified name for these libraries.`);
 }
 
 export class UnnecessaryLibraryError extends CustomError {
-  constructor(formattedLibraries: string) {
+  constructor(libraries: string[]) {
+    const formattedLibraries = libraries
+      .map((libraryName) => `\t* "${libraryName}"`)
+      .join("\n");
+
     super(`The following libraries are not referenced by the contract:
 ${formattedLibraries}
 
@@ -28,7 +47,11 @@ Please provide only the libraries that are needed.`);
 }
 
 export class OverlappingLibrariesError extends CustomError {
-  constructor(formattedLibraries: string) {
+  constructor(libraries: string[]) {
+    const formattedLibraries = libraries
+      .map((libraryFqn) => `\t* "${libraryFqn}"`)
+      .join("\n");
+
     super(`The following libraries are provided more than once:
 ${formattedLibraries}
 
@@ -37,7 +60,11 @@ Please ensure that each library is provided only once, either by its name or its
 }
 
 export class MissingLibrariesError extends CustomError {
-  constructor(formattedLibraries: string) {
+  constructor(libraries: string[]) {
+    const formattedLibraries = libraries
+      .map((libraryFqn) => `\t* "${libraryFqn}"`)
+      .join("\n");
+
     super(
       `The following libraries are missing:
 ${formattedLibraries}
