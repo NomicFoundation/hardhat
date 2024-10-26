1c893f4c69d5f7ba125dafa1c4f6df25b64a212f
Patricio Palladino <email@patriciopalladino.com>
2024-10-26 23:53:24 +0000

    Normalize the builtin plugin names

--- v-next/hardhat/src/internal/builtin-plugins/clean/index.ts
+++ v-next/hardhat/src/internal/builtin-plugins/clean/index.ts
@@ -3,7 +3,7 @@ import type { HardhatPlugin } from "../../../types/plugins.js";
 import { task } from "../../core/config.js";
 
 const hardhatPlugin: HardhatPlugin = {
-  id: "clean",
+  id: "builtin:clean",
   tasks: [
     task("clean", "Clears the cache and deletes all artifacts")
       .addFlag({
