fbef5084cc54962e7d0c96d214dd2bd8109c029b
Patricio Palladino <email@patriciopalladino.com>
2024-10-27 17:48:14 +0000

    Add an example with forge-std

--- v-next/example-project/hardhat.config.ts
+++ v-next/example-project/hardhat.config.ts
@@ -154,7 +154,11 @@ const config: HardhatUserConfig = {
       },
     },
     dependenciesToCompile: ["@openzeppelin/contracts/token/ERC20/ERC20.sol"],
-    remappings: ["remapped/=npm/@openzeppelin/contracts@5.1.0/access/"],
+    remappings: [
+      "remapped/=npm/@openzeppelin/contracts@5.1.0/access/",
+      // This is necessary because most people import forge-std/Test.sol, and not forge-std/src/Test.sol
+      "forge-std/=npm/forge-std@1.9.4/src/",
+    ],
   },
 };
 
