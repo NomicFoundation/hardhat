7c38be3ab53b30c4a580075a8a18e4e5d32ceedd
Patricio Palladino <email@patriciopalladino.com>
2024-10-28 21:06:02 +0000

    Load the builtin plugin types in /types/config

--- v-next/hardhat/src/types/config.ts
+++ v-next/hardhat/src/types/config.ts
@@ -1,3 +1,7 @@
+// NOTE: We import the builtin plugins in this module, so that their
+// type-extensions are loaded when the user imports `hardhat/types/config`.
+import "../internal/builtin-plugins/index.js";
+
 /**
  * A configuration variable to be fetched at runtime from
  * different sources, depending on the user's setup.
