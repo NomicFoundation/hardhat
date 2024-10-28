76b584ff0630c32e0a44fce7e0c87b39ae6240c8
Patricio Palladino <email@patriciopalladino.com>
2024-10-28 19:45:39 +0000

    [hardhat-utils] Add a stream utilities

---
+++ v-next/hardhat-utils/src/stream.ts
@@ -0,0 +1,16 @@
+import { Writable } from "node:stream";
+
+/**
+ * Creates a Tansform that writes everything to actualWritable, without closing it
+ * when finished.
+ *
+ * This is useful to pipe things to stdout, without closing it, while being
+ * able to await for the result of the pipe to finish.
+ */
+export function createNonClosingWriter(actualWritable: Writable): Writable {
+  return new Writable({
+    write(chunk, encoding, callback) {
+      actualWritable.write(chunk, encoding, callback);
+    },
+  });
+}
