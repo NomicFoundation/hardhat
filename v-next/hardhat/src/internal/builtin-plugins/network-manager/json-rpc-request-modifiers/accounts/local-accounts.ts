efb5c31c3d020c5e42cfe15154981ea072dbd993
Patricio Palladino <email@patriciopalladino.com>
2024-11-01 17:37:47 +0000

    Explicitly error about known limitation

--- v-next/hardhat/src/internal/builtin-plugins/network-manager/json-rpc-request-modifiers/accounts/local-accounts.ts
+++ v-next/hardhat/src/internal/builtin-plugins/network-manager/json-rpc-request-modifiers/accounts/local-accounts.ts
@@ -260,10 +260,14 @@ export class LocalAccounts extends ChainId {
       };
     });
 
+    // TODO: Fix after the alpha release
+    assertHardhatInvariant(
+      txData.to !== undefined,
+      "The alpha version doesn't support deploying contracts with local accounts yet",
+    );
+
     const checksummedAddress = addr.addChecksum(
-      txData.to !== undefined
-        ? bytesToHexString(txData.to).toLowerCase()
-        : "0x0",
+      bytesToHexString(txData.to).toLowerCase(),
     );
 
     assertHardhatInvariant(
