7f43da27b349d51700f95d2220abf0e37729ccb5
Patricio Palladino <email@patriciopalladino.com>
2024-11-01 17:34:16 +0000

    Small documentation fix

--- v-next/hardhat/src/types/network.ts
+++ v-next/hardhat/src/types/network.ts
@@ -21,7 +21,7 @@ export type GenericChainType = "generic";
  * `ChainTypeConfig` interface with a valid `ChainType` value.
  * For example:
  * ```ts
- * declare module "@ignored/hardhat-vnext/types/config" {
+ * declare module "@ignored/hardhat-vnext/types/network" {
  *   export interface ChainTypeConfig {
  *     defaultChainType: "l1";
  *   }
