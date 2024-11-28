9ad82f53684abd6ce7e89a297233d08a8228298f
Simone Di Cola <admin@b-side.dev>
2024-11-28 11:07:52 +0000

    feat: adds --output to flatten (#5947)
    * feat: adds --output to flatten
    
    * fix: adds temporary file
    
    * chore: clean up after failed flatten test run
    
    * docs: add a changeset for the new --output param
    
    ---------
    
    Co-authored-by: John Kane <john@kanej.me>
    Co-authored-by: Piotr Galar <piotr.galar@gmail.com>

---
+++ .changeset/thick-lizards-yell.md
@@ -0,0 +1,5 @@
+---
+"hardhat": patch
+---
+
+Added an optional `--output` param to the flatten task
--- packages/hardhat-core/test/builtin-tasks/flatten.ts
+++ packages/hardhat-core/test/builtin-tasks/flatten.ts
@@ -1,8 +1,10 @@
 import { assert } from "chai";
-import fs from "fs";
+import fs, { readFileSync } from "fs";
 
 import sinon, { SinonSpy } from "sinon";
 import picocolors from "picocolors";
+import { removeSync } from "fs-extra";
+import { tmpdir } from "os";
 import {
   TASK_FLATTEN,
   TASK_FLATTEN_GET_FLATTENED_SOURCE,
@@ -410,6 +412,21 @@ describe("Flatten task", () => {
       });
 
       assert(!spyFunctionConsoleWarn.called);
+    });
+
+    it("should write to an output file when the parameter output is specified", async function () {
+      const outputFile = `${tmpdir()}/flatten.sol`;
+      try {
+        await this.env.run(TASK_FLATTEN, {
+          files: ["contracts/A.sol", "contracts/D.sol"],
+          output: outputFile,
+        });
+        const expected = await getExpectedSol();
+        const actual = readFileSync(outputFile, "utf8");
+        assert.equal(actual, expected);
+      } finally {
+        removeSync(outputFile);
+      }
     });
 
     describe("No contracts to flatten", () => {
