237ddc4ba08311b968565a391449687d34496eac
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2024-12-18 11:40:22 +0100

    allow top level await

--- v-next/hardhat-chai-matchers/test/helpers/pretest.ts
+++ v-next/hardhat-chai-matchers/test/helpers/pretest.ts
@@ -1,28 +1,29 @@
+/* eslint-disable no-restricted-syntax -- top-level await should be allowed in ESM */
+
 import path, { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import { createHardhatRuntimeEnvironment } from "@ignored/hardhat-vnext/hre";
 
 // Compile the contracts before executing the tests to bypass the compilation step during testing.
-(async () => {
-  const currentDir = dirname(fileURLToPath(import.meta.url));
 
-  const fixtureProjectDir = path.join(
-    currentDir,
-    "..",
-    "fixture-projects",
-    "hardhat-project",
-  );
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
-  const hre = await createHardhatRuntimeEnvironment({
-    solidity: {
-      version: "0.8.24", // Same version as the one in the contracts in the "hardhat-project" fixture project
-    },
-    paths: {
-      artifacts: path.join(fixtureProjectDir, "artifacts"),
-      sources: path.join(fixtureProjectDir, "contracts"),
-    },
-  });
+const fixtureProjectDir = path.join(
+  currentDir,
+  "..",
+  "fixture-projects",
+  "hardhat-project",
+);
 
-  await hre.tasks.getTask("compile").run({});
-})();
+const hre = await createHardhatRuntimeEnvironment({
+  solidity: {
+    version: "0.8.24", // Same version as the one in the contracts in the "hardhat-project" fixture project
+  },
+  paths: {
+    artifacts: path.join(fixtureProjectDir, "artifacts"),
+    sources: path.join(fixtureProjectDir, "contracts"),
+  },
+});
+
+await hre.tasks.getTask("compile").run({});
