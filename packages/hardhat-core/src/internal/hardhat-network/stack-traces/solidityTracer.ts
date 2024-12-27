51bdf26f267e8973ca33d1672e0008a5243e0b6d
Franco Victorio <victorio.franco@gmail.com>
2024-12-27 12:26:12 +0100

    Get solidity stack traces from EDR's response

--- packages/hardhat-core/src/internal/hardhat-network/stack-traces/solidityTracer.ts
+++
@@ -1,7 +0,0 @@
-import { requireNapiRsModule } from "../../../common/napi-rs";
-
-const { SolidityTracer } = requireNapiRsModule(
-  "@nomicfoundation/edr"
-) as typeof import("@nomicfoundation/edr");
-
-export { SolidityTracer };
