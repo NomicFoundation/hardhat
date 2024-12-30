17e8a2eace06acfa0ddd5c4d81911791fdd85565
Luis Schaab <schaable@gmail.com>
2024-12-30 17:03:55 +0000

    doc: move jsdoc above the buildAssert fn

--- packages/hardhat-chai-matchers/src/utils.ts
+++ packages/hardhat-chai-matchers/src/utils.ts
@@ -3,23 +3,6 @@ import { AssertionError } from "chai";
 // just a generic function type to avoid errors from the ban-types eslint rule
 export type Ssfi = (...args: any[]) => any;
 
-/**
- * This function is used by the matchers to obtain an `assert` function, which
- * should be used instead of `this.assert`.
- *
- * The first parameter is the value of the `negated` flag. Keep in mind that
- * this value should be captured at the beginning of the matcher's
- * implementation, before any async code is executed. Otherwise things like
- * `.to.emit().and.not.to.emit()` won't work, because by the time the async part
- * of the first emit is executd, the `.not` (executed synchronously) has already
- * modified the flag.
- *
- * The second parameter is what Chai calls the "start stack function indicator",
- * a function that is used to build the stack trace. It's unclear to us what's
- * the best way to use this value, so this needs some trial-and-error. Use the
- * existing matchers for a reference of something that works well enough.
- */
-
 type Message = string | (() => string);
 
 function evalMessage(message?: Message): string {
@@ -58,6 +41,22 @@ function buildNormal(ssfi: Ssfi) {
   };
 }
 
+/**
+ * This function is used by the matchers to obtain an `assert` function, which
+ * should be used instead of `this.assert`.
+ *
+ * The first parameter is the value of the `negated` flag. Keep in mind that
+ * this value should be captured at the beginning of the matcher's
+ * implementation, before any async code is executed. Otherwise things like
+ * `.to.emit().and.not.to.emit()` won't work, because by the time the async part
+ * of the first emit is executd, the `.not` (executed synchronously) has already
+ * modified the flag.
+ *
+ * The second parameter is what Chai calls the "start stack function indicator",
+ * a function that is used to build the stack trace. It's unclear to us what's
+ * the best way to use this value, so this needs some trial-and-error. Use the
+ * existing matchers for a reference of something that works well enough.
+ */
 export function buildAssert(negated: boolean, ssfi: Ssfi) {
   return negated ? buildNegated(ssfi) : buildNormal(ssfi);
 }
