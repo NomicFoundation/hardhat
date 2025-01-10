dcaee305dcaa7041beea5103eb052a6dfee3623c
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-01-10 17:46:15 +0100

    typechain packages from npm

--- v-next/hardhat/src/internal/builtin-plugins/solidity/tasks/compile.ts
+++ v-next/hardhat/src/internal/builtin-plugins/solidity/tasks/compile.ts
@@ -1,4 +1,6 @@
+import type { SuccessfulFileBuildResult } from "../../../../types/solidity.js";
 import type { NewTaskActionFunction } from "../../../../types/tasks.js";
+import type { PublicConfig as RunTypeChainConfig } from "typechain";
 
 import { HardhatError } from "@ignored/hardhat-vnext-errors";
 import { resolveFromRoot } from "@ignored/hardhat-vnext-utils/path";
@@ -28,6 +30,11 @@ const compileAction: NewTaskActionFunction<CompileActionArguments> = async (
           return resolveFromRoot(process.cwd(), file);
         });
 
+  // ----
+  // solidity compilation here
+  // const compileSolOutput = await runSuper(taskArgs)
+  // await run(TASK_TYPECHAIN_GENERATE_TYPES, { compileSolOutput, quiet: taskArgs.quiet })
+  // ----
   const results = await solidity.build(rootPaths, {
     force,
     buildProfile: globalOptions.buildProfile,
@@ -62,6 +69,91 @@ const compileAction: NewTaskActionFunction<CompileActionArguments> = async (
   if (files.length === 0) {
     await solidity.cleanupArtifacts(rootPaths);
   }
+
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
+
+  const artifactsPaths = extractArtifactsPaths(
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- TMP
+    results as Map<string, SuccessfulFileBuildResult>,
+  );
+
+  // console.log(artifactsPaths);
+
+  console.log("=========================================== BEFORE");
+
+  const { runTypeChain } = await import("typechain");
+
+  const cwd = process.cwd();
+
+  // console.log(cwd);
+
+  console.log(artifactsPaths);
+
+  const typechainOptions: Omit<RunTypeChainConfig, "filesToProcess"> = {
+    cwd,
+    allFiles: artifactsPaths,
+    // outDir: `${cwd}/typechain/types`,
+    target: "ethers-v6",
+    flags: {
+      alwaysGenerateOverloads: false,
+      discriminateTypes: false, // typechainCfg.discriminateTypes,
+
+      tsNocheck: false, // typechainCfg.tsNocheck,
+      environment: "hardhat",
+      node16Modules: true, // typechainCfg.node16Modules,
+    },
+  };
+
+  const result = await runTypeChain({
+    ...typechainOptions,
+    filesToProcess: artifactsPaths,
+  });
+
+  console.log(`Successfully generated ${result.filesGenerated} typings!`);
+
+  console.log("=========================================== AFTER");
+
+  // ---------------------------------------------------------
+  // ---------------------------------------------------------
 };
+
+function extractArtifactsPaths(
+  results: Map<string, SuccessfulFileBuildResult>,
+): string[] {
+  const artifactSet = new Set<string>();
+
+  results.forEach((r) => {
+    r.contractArtifactsGenerated.forEach((artifactPath) =>
+      artifactSet.add(artifactPath),
+    );
+  });
+
+  return Array.from(artifactSet);
+}
+
+// function getFQNamesFromCompilationOutput(compileSolOutput: any): string[] {
+//   const allFQNNamesNested = compileSolOutput.artifactsEmittedPerJob.map(
+//     (a: any) => {
+//       return a.artifactsEmittedPerFile.map((artifactPerFile: any) => {
+//         return artifactPerFile.artifactsEmitted.map((artifactName: any) => {
+//           return getFullyQualifiedName(
+//             artifactPerFile.file.sourceName,
+//             artifactName,
+//           );
+//         });
+//       });
+//     },
+//   );
+
+//   return allFQNNamesNested.flat(2);
+// }
+
+// function getFullyQualifiedName(
+//   sourceName: string,
+//   contractName: string,
+// ): string {
+//   return `${sourceName}:${contractName}`;
+// }
 
 export default compileAction;
