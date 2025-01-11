94faf15d59d09dc2e9489debc775aa16b3baa6f0
Wodann <Wodann@users.noreply.github.com>
2025-01-11 00:58:23 +0000

    fix: setup.sh for Debian

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,4 +7,7 @@ sudo apt update
 sudo apt install -y libudev-dev
 
 # aha and wkhtmltoimage are used in the integration tests of the node:test reporter
-sudo apt install -y aha wkhtmltoimage
+# We install wkhmtltopdf instead of wkhtmltoimage because:
+# 1. Debian does not have the wkhtmltoimage package
+# 2. wkhtmltopdf is a superset of wkhtmltoimage
+sudo apt install -y aha wkhtmltopdf
