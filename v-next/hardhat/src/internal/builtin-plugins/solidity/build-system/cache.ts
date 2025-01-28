1a755bcaf51f8936819a5b4e90eb0875de9475da
galargh <piotr.galar@gmail.com>
2025-01-28 14:54:39 +0100

    chore: remove the setFiles/getFiles methods from the cache interface

--- v-next/hardhat/src/internal/builtin-plugins/solidity/build-system/cache.ts
+++ v-next/hardhat/src/internal/builtin-plugins/solidity/build-system/cache.ts
@@ -1,12 +1,10 @@
 import path from "node:path";
 
-import { assertHardhatInvariant } from "@ignored/hardhat-vnext-errors";
 import {
   exists,
   readJsonFile,
   writeJsonFile,
 } from "@ignored/hardhat-vnext-utils/fs";
-import AdmZip from "adm-zip";
 
 export class Cache {
   readonly #basePath: string;
@@ -35,43 +33,5 @@ export class Cache {
   public async getJson<T>(key: string): Promise<T | undefined> {
     const filePath = this.#getPath(key);
     return (await this.has(key)) ? readJsonFile<T>(filePath) : undefined;
-  }
-
-  public async setFiles(
-    key: string,
-    rootPath: string,
-    filePaths: string[],
-  ): Promise<void> {
-    const zipFilePath = this.#getPath(key);
-    const zip = new AdmZip();
-    for (const filePath of filePaths) {
-      zip.addLocalFile(
-        filePath,
-        path.dirname(path.relative(rootPath, filePath)),
-      );
-    }
-    const zipFileCreated = await zip.writeZipPromise(zipFilePath, {
-      overwrite: true,
-    });
-    assertHardhatInvariant(
-      zipFileCreated,
-      `Failed to create zip file ${zipFilePath}`,
-    );
-  }
-
-  public async getFiles(
-    key: string,
-    rootPath: string,
-  ): Promise<string[] | undefined> {
-    if (await this.has(key)) {
-      const zipFilePath = this.#getPath(key);
-      const zip = new AdmZip(zipFilePath);
-      zip.extractAllTo(rootPath, true);
-      const filePaths = zip
-        .getEntries()
-        .map((entry) => path.join(rootPath, entry.entryName));
-      return filePaths;
-    }
-    return undefined;
   }
 }
