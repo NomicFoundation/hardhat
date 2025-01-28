ecf6ffc0d4e4634d29543d67f659bcb2b579e13c
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-01-28 14:10:31 +0100

    add 'outDir' in default config

--- v-next/hardhat-typechain/src/internal/config/default.ts
+++ v-next/hardhat-typechain/src/internal/config/default.ts
@@ -1,6 +1,7 @@
 import type { TypechainConfig } from "../../types.js";
 
 export const DEFAULT_CONFIG: TypechainConfig = {
+  outDir: undefined,
   alwaysGenerateOverloads: false,
   dontOverrideCompile: false,
   discriminateTypes: false,
