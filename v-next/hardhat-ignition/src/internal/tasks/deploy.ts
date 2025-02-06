7766d6bdfcdfadd2f67980851a75b5a4ac36d494
John Kane <john@kanej.me>
2025-02-06 18:03:05 +0000

    chore: add a message indicating --verify is not yet available

--- v-next/hardhat-ignition/src/internal/tasks/deploy.ts
+++ v-next/hardhat-ignition/src/internal/tasks/deploy.ts
@@ -1,6 +1,8 @@
 import type { HardhatRuntimeEnvironment } from "@ignored/hardhat-vnext/types/hre";
 import type { NewTaskActionFunction } from "@ignored/hardhat-vnext/types/tasks";
 
+import { HardhatError } from "@ignored/hardhat-vnext-errors";
+
 interface TaskDeployArguments {
   modulePath: string;
   parameters: string;
@@ -13,10 +15,15 @@ interface TaskDeployArguments {
 }
 
 const taskDeploy: NewTaskActionFunction<TaskDeployArguments> = async (
-  _deployArgs,
+  deployArgs,
   _hre: HardhatRuntimeEnvironment,
 ): Promise<void> => {
-  console.log("Deploying contract with Ignition ...");
+  if (deployArgs.verify) {
+    throw new HardhatError(HardhatError.ERRORS.INTERNAL.NOT_IMPLEMENTED_ERROR, {
+      message:
+        "Verifying deployments is not available yet. It will be available in a future version of the Harhdat 3 Alpha",
+    });
+  }
 };
 
 export default taskDeploy;
