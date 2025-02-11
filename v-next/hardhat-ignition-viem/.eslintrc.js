4ae877c42ed9e4f980af18b7c3b6add7daea693b
John Kane <john@kanej.me>
2025-02-11 23:32:42 +0000

    chore: update to the new eslint rules

--- v-next/hardhat-ignition-viem/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: [`${__dirname}/../../config/eslint/eslintrc.js`],
-  parserOptions: {
-    project: `${__dirname}/tsconfig.json`,
-    sourceType: "module",
-  },
-  ignorePatterns: [".eslintrc.js", "**/fixture-projects/**/*"],
-};
