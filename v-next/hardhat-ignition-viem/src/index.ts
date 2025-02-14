9a00f176dbd311d149031d79951995cc81f300cc
John Kane <john@kanej.me>
2025-02-14 11:25:40 +0000

    refactor: remove commented out code

--- v-next/hardhat-ignition-viem/src/index.ts
+++ v-next/hardhat-ignition-viem/src/index.ts
@@ -20,25 +20,3 @@ const hardhatIgnitionViemPlugin: HardhatPlugin = {
 };
 
 export default hardhatIgnitionViemPlugin;
-
-/**
- * Add an `ignition` object to the HRE.
- */
-// extendEnvironment((hre) => {
-//   if (
-//     hre.ignition !== undefined &&
-//     hre.ignition.type !== "stub" &&
-//     hre.ignition.type !== "viem"
-//   ) {
-//     throw new HardhatPluginError(
-//       "hardhat-ignition-viem",
-//       `Found ${hre.ignition.type} and viem, but only one Hardhat Ignition extension plugin can be used at a time.`
-//     );
-//   }
-
-//   hre.ignition = lazyObject(() => {
-//     const { ViemIgnitionHelper } = require("./viem-ignition-helper");
-
-//     return new ViemIgnitionHelper(hre);
-//   });
-// });
