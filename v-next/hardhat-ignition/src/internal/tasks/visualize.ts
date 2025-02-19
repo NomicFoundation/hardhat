97f05597384fb40df5bc4c6b0fdd75e8f9fb696a
John Kane <john@kanej.me>
2025-02-19 15:39:56 +0000

    chore: set visualize as not implemented
    We will bring this back with the bug fix.

--- v-next/hardhat-ignition/src/internal/tasks/visualize.ts
+++ v-next/hardhat-ignition/src/internal/tasks/visualize.ts
@@ -1,19 +1,20 @@
 import type { HardhatRuntimeEnvironment } from "@ignored/hardhat-vnext/types/hre";
 import type { NewTaskActionFunction } from "@ignored/hardhat-vnext/types/tasks";
 
-import path from "node:path";
+// import path from "node:path";
 
-import { HardhatError } from "@ignored/hardhat-vnext-errors";
-import {
-  batches,
-  IgnitionError,
-  IgnitionModuleSerializer,
-} from "@ignored/hardhat-vnext-ignition-core";
+// import { HardhatError } from "@ignored/hardhat-vnext-errors";
+// import {
+//   batches,
+//   IgnitionError,
+//   IgnitionModuleSerializer,
+// } from "@ignored/hardhat-vnext-ignition-core";
 
-import { loadModule } from "../utils/load-module.js";
-import { open } from "../utils/open.js";
-import { shouldBeHardhatPluginError } from "../utils/shouldBeHardhatPluginError.js";
-import { writeVisualization } from "../visualization/write-visualization.js";
+// import { loadModule } from "../utils/load-module.js";
+// import { open } from "../utils/open.js";
+// import { shouldBeHardhatPluginError } from "../utils/shouldBeHardhatPluginError.js";
+// import { writeVisualization } from "../visualization/write-visualization.js";
+import chalk from "chalk";
 
 interface TaskVisualizeArguments {
   modulePath: string;
@@ -21,48 +22,56 @@ interface TaskVisualizeArguments {
 }
 
 const visualizeTask: NewTaskActionFunction<TaskVisualizeArguments> = async (
-  { noOpen, modulePath }: { noOpen: boolean; modulePath: string },
-  hre: HardhatRuntimeEnvironment,
+  _args: { noOpen: boolean; modulePath: string },
+  _hre: HardhatRuntimeEnvironment,
 ) => {
-  await hre.tasks.getTask("compile").run({ quiet: true });
+  console.log(
+    chalk.yellow(
+      "This task will be implemented soon. Check back soon for more updates.",
+    ),
+  );
 
-  const userModule = await loadModule(hre.config.paths.ignition, modulePath);
+  return;
 
-  if (userModule === undefined) {
-    throw new HardhatError(HardhatError.ERRORS.IGNITION.NO_MODULES_FOUND);
-  } else {
-    try {
-      const serializedIgnitionModule =
-        IgnitionModuleSerializer.serialize(userModule);
+  // await hre.tasks.getTask("compile").run({ quiet: true });
 
-      const batchInfo = batches(userModule);
+  // const userModule = await loadModule(hre.config.paths.ignition, modulePath);
 
-      await writeVisualization(
-        { module: serializedIgnitionModule, batches: batchInfo },
-        {
-          cacheDir: hre.config.paths.cache,
-        },
-      );
-    } catch (e) {
-      if (e instanceof IgnitionError && shouldBeHardhatPluginError(e)) {
-        throw new HardhatError(HardhatError.ERRORS.IGNITION.INTERNAL_ERROR, e);
-      }
+  // if (userModule === undefined) {
+  //   throw new HardhatError(HardhatError.ERRORS.IGNITION.NO_MODULES_FOUND);
+  // } else {
+  //   try {
+  //     const serializedIgnitionModule =
+  //       IgnitionModuleSerializer.serialize(userModule);
 
-      throw e;
-    }
-  }
+  //     const batchInfo = batches(userModule);
 
-  if (!noOpen) {
-    const indexFile = path.join(
-      hre.config.paths.cache,
-      "visualization",
-      "index.html",
-    );
+  //     await writeVisualization(
+  //       { module: serializedIgnitionModule, batches: batchInfo },
+  //       {
+  //         cacheDir: hre.config.paths.cache,
+  //       },
+  //     );
+  //   } catch (e) {
+  //     if (e instanceof IgnitionError && shouldBeHardhatPluginError(e)) {
+  //       throw new HardhatError(HardhatError.ERRORS.IGNITION.INTERNAL_ERROR, e);
+  //     }
 
-    console.log(`Deployment visualization written to ${indexFile}`);
+  //     throw e;
+  //   }
+  // }
 
-    open(indexFile);
-  }
+  // if (!noOpen) {
+  //   const indexFile = path.join(
+  //     hre.config.paths.cache,
+  //     "visualization",
+  //     "index.html",
+  //   );
+
+  //   console.log(`Deployment visualization written to ${indexFile}`);
+
+  //   open(indexFile);
+  // }
 };
 
 export default visualizeTask;
--- v-next/hardhat-ignition/test/plan/index.ts
+++ v-next/hardhat-ignition/test/plan/index.ts
@@ -9,7 +9,8 @@ import { useEphemeralIgnitionProject } from "../test-helpers/use-ignition-projec
 describe("visualize", () => {
   useEphemeralIgnitionProject("minimal");
 
-  it("should create a visualization", async function () {
+  // TODO: HH3 bring back visualization
+  it.skip("should create a visualization", async function () {
     const visualizationPath = path.resolve("../minimal/cache/visualization");
     await emptyDir(visualizationPath);
 
