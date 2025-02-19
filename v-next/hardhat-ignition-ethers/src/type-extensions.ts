eb0394588528a867712f3aac37241de22aac6ecd
zoeyTM <probablyzoey+github@gmail.com>
2025-02-19 13:13:21 -0500

    import type extensions from base plugin

--- v-next/hardhat-ignition-ethers/src/type-extensions.ts
+++ v-next/hardhat-ignition-ethers/src/type-extensions.ts
@@ -1,5 +1,7 @@
 import type { EthersIgnitionHelper } from "./types.js";
 
+import "@ignored/hardhat-vnext-ignition";
+
 declare module "@ignored/hardhat-vnext/types/network" {
   interface NetworkConnection<
     // eslint-disable-next-line @typescript-eslint/no-unused-vars -- the ChainTypeT must be declared in the interface but in this scenario it's not used
