f85d90522020e3b05c206fe0d3d5369153aeedc6
Patricio Palladino <email@patriciopalladino.com>
2025-03-24 19:31:48 +0000

    [Unrelated] Fix devcontainer setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,5 +6,5 @@ sudo apt update
 # libudev-dev is required by hardhat-ledger
 sudo apt install -y libudev-dev
 
-# aha and wkhtmltoimage are used in the integration tests of the node:test reporter
-sudo apt install -y aha wkhtmltoimage
+# aha and wkhtmltoimage (included in the wkhtmltopdf package) are used in the integration tests of the node:test reporter
+sudo apt install -y aha wkhtmltopdf
