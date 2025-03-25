17c9fd25fb7806e1b9a60f4e687277aa0f72acb1
zoeyTM <probablyzoey+github@gmail.com>
2025-03-25 19:03:47 -0400

    add all submodule dependencies recursively for both submodule and future dependencies

--- packages/hardhat-ignition-core/src/internal/utils/adjacency-list-converter.ts
+++ packages/hardhat-ignition-core/src/internal/utils/adjacency-list-converter.ts
@@ -1,5 +1,9 @@
 import { isFuture } from "../../type-guards";
-import { Future } from "../../types/module";
+import {
+  Future,
+  IgnitionModule,
+  IgnitionModuleResult,
+} from "../../types/module";
 
 import { AdjacencyList } from "./adjacency-list";
 import { getFuturesFromModule } from "./get-futures-from-module";
@@ -14,22 +18,11 @@ export class AdjacencyListConverter {
       for (const dependency of future.dependencies) {
         dependencyGraph.addDependency({ from: future.id, to: dependency.id });
 
-        if (isFuture(dependency)) {
-          this._optionallyAddDependenciesSubmoduleSiblings(
-            dependencyGraph,
-            future,
-            dependency
-          );
-        } else {
-          const futures = getFuturesFromModule(dependency);
-
-          for (const moduleDep of futures) {
-            dependencyGraph.addDependency({
-              from: future.id,
-              to: moduleDep.id,
-            });
-          }
-        }
+        this._optionallyAddDependenciesFromSubmodules(
+          dependencyGraph,
+          future,
+          dependency
+        );
       }
     }
 
@@ -39,18 +32,25 @@ export class AdjacencyListConverter {
   /**
    * The famed Malaga rule, if a future's dependency is in a submodule,
    * then that future should not be executed until all futures in the
-   * submodule have been run.
+   * submodule and its submodules (recursive) have been run.
    */
-  private static _optionallyAddDependenciesSubmoduleSiblings(
+  private static _optionallyAddDependenciesFromSubmodules(
     dependencyGraph: AdjacencyList,
     future: Future,
-    dependency: Future
+    dependency:
+      | Future
+      | IgnitionModule<string, string, IgnitionModuleResult<string>>
   ): void {
-    if (future.module === dependency.module) {
+    // we only need to worry about this case if the dependency is a future
+    if (isFuture(dependency) && future.module === dependency.module) {
       return;
     }
 
-    for (const moduleDep of dependency.module.futures) {
+    const futures = getFuturesFromModule(
+      isFuture(dependency) ? dependency.module : dependency
+    );
+
+    for (const moduleDep of futures) {
       dependencyGraph.addDependency({
         from: future.id,
         to: moduleDep.id,
