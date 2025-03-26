594dbe8337b1937f8397bad2e112a03b521b97eb
Eugene Boruhov <eugene@focusreactive.com>
2025-03-26 11:50:21 +0100

    Use cjs ScrollTrigger

--- docs/src/hooks/useImageAnimation.ts
+++ docs/src/hooks/useImageAnimation.ts
@@ -2,7 +2,7 @@
 
 import { useState, useEffect, useRef } from "react";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import getImage from "../utils";
 
 gsap.registerPlugin(ScrollTrigger);
