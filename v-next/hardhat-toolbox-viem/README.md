8f296f5efc9985bb8d925e38cb79ce7232d92f72
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-03-31 14:23:35 +0200

    create new toolbox package

---
+++ v-next/hardhat-toolbox-viem/README.md
@@ -0,0 +1,13 @@
+# Hardhat Toolbox Viem
+
+This package is a recommended bundle of Hardhat plugins.
+
+## Plugins included
+
+- @nomicfoundation/hardhat-ignition
+- @nomicfoundation/hardhat-keystore
+- @nomicfoundation/hardhat-network-helpers
+- @nomicfoundation/hardhat-node-test-runner
+- @nomicfoundation/hardhat-verify (not available yet)
+- @nomicfoundation/hardhat-viem
+- @nomicfoundation/hardhat-viem-assertion-library (not available yet)
---
+++ v-next/hardhat-toolbox-viem/src/index.ts
@@ -0,0 +1,13 @@
+import hardhatIgnitionViemPlugin from "@nomicfoundation/hardhat-ignition-viem";
+import hardhatKeystorePlugin from "@nomicfoundation/hardhat-keystore";
+import hardhatNetworkHelpersPlugin from "@nomicfoundation/hardhat-network-helpers";
+import hardhatNodeTestRunnerPlugin from "@nomicfoundation/hardhat-node-test-runner";
+import hardhatViemPlugin from "@nomicfoundation/hardhat-viem";
+
+export {
+  hardhatIgnitionViemPlugin,
+  hardhatKeystorePlugin,
+  hardhatNetworkHelpersPlugin,
+  hardhatNodeTestRunnerPlugin,
+  hardhatViemPlugin,
+};
