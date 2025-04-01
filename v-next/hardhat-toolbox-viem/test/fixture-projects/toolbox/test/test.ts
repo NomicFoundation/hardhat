75a06f53013ee2afa27ad609b27540c643d9d436
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-04-01 16:45:17 +0200

    remove unused file in fixture

--- v-next/hardhat-toolbox-viem/test/fixture-projects/toolbox/test/test.ts
+++
@@ -1,8 +0,0 @@
-import assert from "node:assert/strict";
-import { describe, it } from "node:test";
-
-describe("NodeJs builtin test runner", function () {
-  it("should pass the test", () => {
-    assert.equal(1, 1);
-  });
-});
