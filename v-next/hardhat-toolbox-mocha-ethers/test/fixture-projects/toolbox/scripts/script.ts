c7ea8f02436e96065862a59491b8080c41eb5af5
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-04-02 16:24:07 +0200

    add missing test for chai

--- v-next/hardhat-toolbox-mocha-ethers/test/fixture-projects/toolbox/scripts/script.ts
+++ v-next/hardhat-toolbox-mocha-ethers/test/fixture-projects/toolbox/scripts/script.ts
@@ -1,11 +1,11 @@
-// import { expect } from "chai";
+import { expect } from "chai";
 import { network } from "hardhat";
 
 async function main() {
   const { ethers, networkHelpers } = await network.connect();
 
   // chai matchers should be available
-  // expect("0x0000010AB").to.not.hexEqual("0x0010abc");
+  expect("0x0000010AB").to.not.hexEqual("0x0010abc");
 
   // ethers should be available
   await ethers.getSigners();
