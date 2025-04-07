d30480c42b8dd31647b1bf03e16157c6edaed39d
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-04-07 13:24:33 +0200

    alternative-2: export function viemAssert

--- v-next/hardhat-viem-matchers/test/index.ts
+++ v-next/hardhat-viem-matchers/test/index.ts
@@ -3,10 +3,11 @@ import { describe, it } from "node:test";
 import HardhatViem from "@nomicfoundation/hardhat-viem";
 import { createHardhatRuntimeEnvironment } from "hardhat/hre";
 
+import { viemExpect } from "../src/index-alternative-2.js";
 import hardhatPlugin from "../src/index.js";
 
 describe("changeEtherBalance", () => {
-  it("should change the balance of an address", async () => {
+  it("alternative-1: should change the balance of an address", async () => {
     const hre = await createHardhatRuntimeEnvironment({
       plugins: [HardhatViem, hardhatPlugin],
     });
@@ -14,8 +15,6 @@ describe("changeEtherBalance", () => {
     const { viem, viemMatchers } = await hre.network.connect();
 
     const [bobWalletClient, aliceWalletClient] = await viem.getWalletClients();
-
-    const publicClient = await viem.getPublicClient();
 
     await viemMatchers
       .expect(async () => {
@@ -26,11 +25,38 @@ describe("changeEtherBalance", () => {
           value: 1000000000000000000000n,
         });
 
+        const publicClient = await viem.getPublicClient();
         await publicClient.waitForTransactionReceipt({ hash });
       })
       .to.changeEtherBalance(
         aliceWalletClient.account.address,
         1000000000000000000000n,
       );
+  });
+
+  it("alternative-2: should change the balance of an address", async () => {
+    const hre = await createHardhatRuntimeEnvironment({
+      plugins: [HardhatViem],
+    });
+
+    const { viem } = await hre.network.connect();
+
+    const [bobWalletClient, aliceWalletClient] = await viem.getWalletClients();
+
+    await viemExpect(async () => {
+      console.log("run f");
+
+      const hash = await bobWalletClient.sendTransaction({
+        to: aliceWalletClient.account.address,
+        value: 1000000000000000000000n,
+      });
+
+      const publicClient = await viem.getPublicClient();
+      await publicClient.waitForTransactionReceipt({ hash });
+    }).to.changeEtherBalance(
+      viem,
+      aliceWalletClient.account.address,
+      1000000000000000000000n,
+    );
   });
 });
