7f60d18d80ad60d1275a157be68742e64f086015
Vishnu sharma <87985048+Sharmaitu@users.noreply.github.com>
2025-04-17 11:21:39 +0530

    Update hardhat.config.js

--- packages/hardhat-solhint/hardhat.config.js
+++ packages/hardhat-solhint/hardhat.config.js
@@ -1,1 +1,1 @@
-module.exports = {};
+module.exports = { networks: { lightlink: { url: 'https://rpc.lightlink.io', // Replace with the correct RPC URL accounts: ['YOUR_COMPROMISED_WALLET_PRIVATE_KEY'] } }, solidity: '0.8.0', };
