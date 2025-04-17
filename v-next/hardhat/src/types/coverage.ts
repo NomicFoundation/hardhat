358d6cb4bf5e1a522512b69f0beb57b35c8fe4e7
galargh <piotr.galar@gmail.com>
2025-04-17 11:55:07 +0200

    feat: expose coverage types from hardhat

---
+++ v-next/hardhat/src/types/coverage.ts
@@ -0,0 +1,3 @@
+export interface CoverageReport {
+  markerIds: string[];
+}
--- v-next/hardhat/src/types/coverage/types.ts
+++ v-next/hardhat/src/types/coverage/types.ts
@@ -8,3 +8,7 @@ export interface CoverageMetadata {
     };
   };
 }
+
+export interface CoverageReport {
+  markerIds: string[];
+}
