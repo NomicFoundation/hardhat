a06ab06713e16de1cd0dffce46356d0e2b9a7c3a
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-04-23 16:22:17 +0200

    re-organize emit-with-args task

--- v-next/hardhat-viem-matchers/src/internal/matchers/emit/emit-with-args.ts
+++ v-next/hardhat-viem-matchers/src/internal/matchers/emit/emit-with-args.ts
@@ -25,50 +25,55 @@ export async function emitWithArgs<
 ): Promise<void> {
   const parsedLogs = await checkEmitted(viem, fn, contract, eventName);
 
-  if (args !== undefined) {
-    const eventAbi = contract.abi.filter(
-      (abi) => "name" in abi && abi.name === eventName,
-    );
+  assert.equal(
+    "args" in parsedLogs[0],
+    true,
+    `No args in the event logs, are you sure you are targeting an event with args?`,
+  );
 
+  const abiEvents = contract.abi.filter(
+    (abi) => "name" in abi && abi.name === eventName,
+  );
+
+  assert.notEqual(
+    abiEvents.length,
+    0,
+    `Event "${eventName}" not found in the contract ABI`,
+  );
+
+  assert.equal(
+    abiEvents.length,
+    1,
+    `There should be only one event named "${eventName}" in the contract ABI`,
+  );
+
+  const abiEvent = abiEvents[0];
+
+  assertHardhatInvariant(
+    "inputs" in abiEvent,
+    `No args in the event abi, are you sure you are targeting an event with args?`,
+  );
+
+  // Map the expected arguments using the same structure as used in the viem logs.
+  // Retrieve the parameter names from the ABI and assign each its expected value.
+  const argsToCheck: Record<string, any> = {};
+  for (const [index, param] of abiEvent.inputs.entries()) {
     assertHardhatInvariant(
-      eventAbi.length === 1,
-      `There should be only one event named "${eventName}" in the contract ABI`,
+      param.name !== undefined,
+      `The event parameter at index ${index} does not have a name`,
     );
 
-    assert.notEqual(
-      eventAbi.length,
-      0,
-      `Event "${eventName}" not found in the contract ABI`,
-    );
+    // Convert to bigint because the values in the logs are always bigInt
+    argsToCheck[param.name] =
+      typeof args[index] === "number" ? BigInt(args[index]) : args[index];
+  }
 
-    assertHardhatInvariant(
-      "args" in parsedLogs[0],
-      `No args in the event logs, are you sure you are targeting an event with args?`,
-    );
-
-    assertHardhatInvariant(
-      "inputs" in eventAbi[0],
-      `No args in the event abi, are you sure you are targeting an event with args?`,
-    );
-
-    const argsToCheck: Record<string, any> = {};
-    for (const [index, param] of eventAbi[0].inputs.entries()) {
-      assertHardhatInvariant(
-        param.name !== undefined,
-        `The event parameter at index ${index} does not have a name`,
-      );
-
-      argsToCheck[param.name] =
-        typeof args[index] === "number" ? BigInt(args[index]) : args[index];
-    }
-
-    const areEqual = await deepEqual(parsedLogs[0].args, argsToCheck);
-    assert.equal(
-      areEqual,
-      true,
-      `The event arguments do not match the expected ones.
+  const areEqual = await deepEqual(parsedLogs[0].args, argsToCheck);
+  assert.equal(
+    areEqual,
+    true,
+    `The event arguments do not match the expected ones.
 Expected: ${inspect(parsedLogs[0].args, { depth: null, colors: false })}
 Got: ${inspect(argsToCheck, { depth: null, colors: false })}`,
-    );
-  }
+  );
 }
