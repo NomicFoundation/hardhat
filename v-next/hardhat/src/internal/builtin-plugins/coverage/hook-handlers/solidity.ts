857b8845c28d8684029e453b0f4555abdb42dfd9
galargh <piotr.galar@gmail.com>
2025-05-02 12:04:09 +0200

    test: fix compilation job implementation tests

--- v-next/hardhat/src/internal/builtin-plugins/coverage/hook-handlers/solidity.ts
+++ v-next/hardhat/src/internal/builtin-plugins/coverage/hook-handlers/solidity.ts
@@ -3,7 +3,10 @@ import type { SolidityHooks } from "../../../../types/hooks.js";
 import path from "node:path";
 
 import { addStatementCoverageInstrumentation } from "@ignored/edr-optimism";
-import { HardhatError } from "@nomicfoundation/hardhat-errors";
+import {
+  assertHardhatInvariant,
+  HardhatError,
+} from "@nomicfoundation/hardhat-errors";
 import { ensureError } from "@nomicfoundation/hardhat-utils/error";
 import { readUtf8File } from "@nomicfoundation/hardhat-utils/fs";
 
@@ -55,6 +58,11 @@ export default async (): Promise<Partial<SolidityHooks>> => ({
   },
   preprocessSolcInputSourcesBeforeBuilding: async (context, sources, next) => {
     if (context.globalOptions.coverage) {
+      assertHardhatInvariant(
+        sources["hardhat/coverage.sol"] === undefined,
+        'The "hardhat/coverage.sol" entry should not exist in the sources.',
+      );
+
       const content = await readUtf8File(
         path.join(import.meta.dirname, "../../../../../../coverage.sol"),
       );
--- v-next/hardhat/test/internal/builtin-plugins/solidity/build-system/compilation-job.ts
+++ v-next/hardhat/test/internal/builtin-plugins/solidity/build-system/compilation-job.ts
@@ -1,6 +1,6 @@
 import type { Remapping } from "../../../../../src/internal/builtin-plugins/solidity/build-system/resolver/types.js";
 import type { SolcConfig } from "../../../../../src/types/config.js";
-import type { HookManager } from "../../../../../src/types/hooks.js";
+import type { HookContext } from "../../../../../src/types/hooks.js";
 import type {
   NpmPackageResolvedFile,
   ProjectResolvedFile,
@@ -25,7 +25,7 @@ describe("CompilationJobImplementation", () => {
   let solcConfig: SolcConfig;
   let solcLongVersion: string;
   let remappings: Remapping[];
-  let hooks: HookManager;
+  let hooks: HookManagerImplementation;
   let compilationJob: CompilationJobImplementation;
 
   beforeEach(() => {
@@ -73,6 +73,8 @@ describe("CompilationJobImplementation", () => {
     solcLongVersion = "0.8.0-c7dfd78";
     remappings = [];
     hooks = new HookManagerImplementation(process.cwd(), []);
+    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- We don't care about hooks in this context
+    hooks.setContext({} as HookContext);
     compilationJob = new CompilationJobImplementation(
       dependencyGraph,
       solcConfig,
