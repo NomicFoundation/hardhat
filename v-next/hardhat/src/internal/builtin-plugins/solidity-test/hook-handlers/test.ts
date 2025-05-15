67c3a2fabbdb44b2f3133f2d739edb055e90e5b9
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-05-15 11:33:25 +0200

    solidity test runner: file should end in .t.sol

--- v-next/hardhat/src/internal/builtin-plugins/solidity-test/hook-handlers/test.ts
+++ v-next/hardhat/src/internal/builtin-plugins/solidity-test/hook-handlers/test.ts
@@ -7,7 +7,10 @@ export default async (): Promise<Partial<TestHooks>> => {
     registerFileForTestRunner: async (context, filePath, next) => {
       const absoluteFilePath = resolveFromRoot(process.cwd(), filePath);
 
-      if (absoluteFilePath.includes(context.config.paths.tests.solidity)) {
+      if (
+        absoluteFilePath.includes(context.config.paths.tests.solidity) &&
+        filePath.endsWith(".t.sol")
+      ) {
         return "solidity";
       }
 
