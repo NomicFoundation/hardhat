5af85da75c9f468dd472718920cc9bfd12b9971a
Armando Andini <armando.andini@gmail.com>
2025-05-22 15:30:37 +0700

    Support Etherscan API v2

--- packages/hardhat-verify/test/unit/etherscan.ts
+++ packages/hardhat-verify/test/unit/etherscan.ts
@@ -2,7 +2,9 @@ import type { EthereumProvider } from "hardhat/types";
 import type { ChainConfig } from "../../src/types";
 
 import { assert, expect } from "chai";
+import sinon, { SinonStub } from "sinon";
 import { Etherscan } from "../../src/internal/etherscan";
+import * as undici from "../../src/internal/undici";
 
 describe("Etherscan", () => {
   const chainConfig = {
@@ -13,6 +15,22 @@ describe("Etherscan", () => {
       browserURL: "https://goerli.etherscan.io",
     },
   };
+
+  let warnStub: SinonStub;
+  let sendGetRequestStub: SinonStub;
+  let sendPostRequestStub: SinonStub;
+
+  beforeEach(() => {
+    warnStub = sinon.stub(console, "warn");
+    sendGetRequestStub = sinon.stub(undici, "sendGetRequest");
+    sendPostRequestStub = sinon.stub(undici, "sendPostRequest");
+  });
+
+  afterEach(() => {
+    warnStub.restore();
+    sendGetRequestStub.restore();
+    sendPostRequestStub.restore();
+  });
 
   describe("constructor", () => {
     it("should throw if the apiKey is undefined or empty", () => {
@@ -33,6 +51,49 @@ describe("Etherscan", () => {
       ).to.throw(
         /You are trying to verify a contract in 'goerli', but no API token was found for this network./
       );
+    });
+
+    it("resolves apiUrl to etherscan v2 if chain id is specified", async () => {
+      expect(
+        new Etherscan("api_key", "api_url", "browser_url", 5).apiUrl
+      ).to.equal("https://api.etherscan.io/v2/api");
+    });
+
+    it("uses apiUrl parameter if chain id is not specified", async () => {
+      expect(
+        new Etherscan("api_key", "api_url", "browser_url", undefined).apiUrl
+      ).to.equal("api_url");
+    });
+  });
+
+  describe("fromChainConfig", () => {
+    it("warns if apiKey config var is an object", async () => {
+      Etherscan.fromChainConfig({ goerli: "<api-key>" }, chainConfig);
+
+      expect(warnStub).to.be.calledOnceWith(
+        sinon.match(
+          /Network and explorer-specific api keys are deprecated in favour of the new Etherscan v2 api/
+        )
+      );
+    });
+
+    it("doesnt warn if apiKey config var is a string", async () => {
+      Etherscan.fromChainConfig("<api-key>", chainConfig);
+
+      expect(warnStub).to.be.callCount(0);
+    });
+
+    it("passes chain id to Etherscan constructor if apiKey is a string (treated as v2 api)", async () => {
+      const etherscan = Etherscan.fromChainConfig("<api-key>", chainConfig);
+      expect(etherscan.chainId).to.equal(5);
+    });
+
+    it("doesnt pass chain id to Etherscan constructor if apiKey is an object (treated as v1 api)", async () => {
+      const etherscan = Etherscan.fromChainConfig(
+        { goerli: "<api-key>" },
+        chainConfig
+      );
+      expect(etherscan.chainId).to.equal(undefined);
     });
   });
 
@@ -186,6 +247,144 @@ describe("Etherscan", () => {
       contractUrl = etherscan.getContractUrl("someAddress");
 
       assert.equal(contractUrl, expectedContractAddress);
+    });
+  });
+
+  describe("isVerified", function () {
+    it("calls the api with a chainid parameter if present", async () => {
+      const etherscan = new Etherscan(
+        "api_key",
+        "https://api.etherscan.io/api",
+        "https://etherscan.io",
+        5
+      );
+
+      try {
+        await etherscan.isVerified("0x123abc");
+      } catch (error) {}
+
+      expect(sendGetRequestStub).to.be.calledOnceWithExactly(
+        sinon.match.has(
+          "search",
+          sinon.match(sinon.match((value) => /chainid=5/.test(value)))
+        )
+      );
+    });
+
+    it("doesnt pass chainid if its not present in the instance", async () => {
+      const etherscan = new Etherscan(
+        "api_key",
+        "https://api.etherscan.io/api",
+        "https://etherscan.io",
+        undefined
+      );
+
+      try {
+        await etherscan.isVerified("0x123abc");
+      } catch (error) {}
+
+      expect(sendGetRequestStub).to.be.calledOnceWithExactly(
+        sinon.match.has(
+          "search",
+          sinon.match((value) => !/chainid/.test(value))
+        )
+      );
+    });
+  });
+
+  describe("verify", function () {
+    it("calls the api with a chainid parameter if present", async () => {
+      const etherscan = new Etherscan(
+        "api_key",
+        "https://api.etherscan.io/api",
+        "https://etherscan.io",
+        5
+      );
+
+      try {
+        await etherscan.verify(
+          "0x123abc",
+          "sourceCode",
+          "contractName",
+          "v0.8.19",
+          "constructorArgs"
+        );
+      } catch (error) {}
+
+      expect(sendPostRequestStub).to.be.calledOnceWith(
+        sinon.match.has(
+          "search",
+          sinon.match(sinon.match((value) => /chainid=5/.test(value)))
+        )
+      );
+    });
+
+    it("doesnt pass chainid if its not present in the instance", async () => {
+      const etherscan = new Etherscan(
+        "api_key",
+        "https://api.etherscan.io/api",
+        "https://etherscan.io",
+        undefined
+      );
+
+      try {
+        await etherscan.verify(
+          "0x123abc",
+          "sourceCode",
+          "contractName",
+          "v0.8.19",
+          "constructorArgs"
+        );
+      } catch (error) {}
+
+      expect(sendPostRequestStub).to.be.calledOnceWith(
+        sinon.match.has(
+          "search",
+          sinon.match((value) => !/chainid/.test(value))
+        )
+      );
+    });
+  });
+
+  describe("getVerificationStatus", function () {
+    it("calls the api with a chainid parameter if present", async () => {
+      const etherscan = new Etherscan(
+        "api_key",
+        "https://api.etherscan.io/api",
+        "https://etherscan.io",
+        5
+      );
+
+      try {
+        await etherscan.getVerificationStatus("0x123abc");
+      } catch (error) {}
+
+      expect(sendGetRequestStub).to.be.calledOnceWithExactly(
+        sinon.match.has(
+          "search",
+          sinon.match(sinon.match((value) => /chainid=5/.test(value)))
+        )
+      );
+    });
+
+    it("doesnt pass chainid if its not present in the instance", async () => {
+      const etherscan = new Etherscan(
+        "api_key",
+        "https://api.etherscan.io/api",
+        "https://etherscan.io",
+        undefined
+      );
+
+      try {
+        await etherscan.getVerificationStatus("0x123abc");
+      } catch (error) {}
+
+      expect(sendGetRequestStub).to.be.calledOnceWithExactly(
+        sinon.match.has(
+          "search",
+          sinon.match((value) => !/chainid/.test(value))
+        )
+      );
     });
   });
 });
