0392a6364b4466cea9c91b5f85976f1ec76a581e
John Kane <john@kanej.me>
2025-05-29 15:50:34 +0100

    refactor: unhook still to be implemented subtasks
    This is to hide the options in the help menu.

--- v-next/hardhat-verify/src/index.ts
+++ v-next/hardhat-verify/src/index.ts
@@ -1,22 +1,15 @@
 import type { HardhatPlugin } from "hardhat/types/plugins";
 
 import "./type-extensions.js";
-import verifyBlockscoutTask from "./internal/tasks/verify/blockscout/index.js";
 import verifyEtherscanTask from "./internal/tasks/verify/etherscan/index.js";
 import verifyTask from "./internal/tasks/verify/index.js";
-import verifySourcifyTask from "./internal/tasks/verify/sourcify/index.js";
 
 const hardhatPlugin: HardhatPlugin = {
   id: "hardhat-verify",
   hookHandlers: {
     config: import.meta.resolve("./internal/hook-handlers/config.js"),
   },
-  tasks: [
-    verifyTask,
-    verifyEtherscanTask,
-    verifySourcifyTask,
-    verifyBlockscoutTask,
-  ],
+  tasks: [verifyTask, verifyEtherscanTask],
   npmPackage: "@nomicfoundation/hardhat-verify",
 };
 
