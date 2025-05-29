1323a38a1c667a5a100f04faf8872fe23befa6b2
Patricio Palladino <email@patriciopalladino.com>
2025-05-29 16:36:25 +0000

    Run template's tests in the CI

--- v-next/hardhat/test/internal/cli/init/template.ts
+++ v-next/hardhat/test/internal/cli/init/template.ts
@@ -1,7 +1,9 @@
+import type { Template } from "../../../../src/internal/cli/init/template.js";
+
 import assert from "node:assert/strict";
 import { spawnSync } from "node:child_process";
 import path from "node:path";
-import { describe, it } from "node:test";
+import { after, before, describe, it } from "node:test";
 
 import { exists, remove } from "@nomicfoundation/hardhat-utils/fs";
 
@@ -34,47 +36,91 @@ describe("getTemplates", () => {
   });
 });
 
-describe("template contents", () => {
-  describe("templates should have valid typescript", async () => {
-    const templates = await getTemplates();
+describe("template contents", async () => {
+  async function useTemplateHardhatProject(
+    template: Template,
+  ): Promise<() => Promise<void>> {
+    const previousCwd = process.cwd();
+    process.chdir(template.path);
 
-    for (const template of templates) {
-      it(`template ${template.name} should compile with tsc`, async () => {
-        const previousCwd = process.cwd();
-        process.chdir(template.path);
+    async function cleanup() {
+      process.chdir(previousCwd);
 
-        try {
-          const resultHardhatBuild = spawnSync("pnpm hardhat compile", {
-            stdio: "inherit",
-            shell: true,
-          });
-
-          assert.equal(
-            resultHardhatBuild.status,
-            0,
-            "Template failed to compile contracts",
-          );
-
-          // We run tsc --noEmit in the template, which will throw it it fails
-          const resultTscBuild = spawnSync("pnpm tsc --noEmit", {
-            stdio: "inherit",
-            shell: true,
-          });
-
-          assert.equal(
-            resultTscBuild.status,
-            0,
-            "Template failed to compile its TypeScript",
-          );
-        } finally {
-          // Remove all the files that `hardhat compile` may have created
-          await remove(path.join(template.path, "artifacts"));
-          await remove(path.join(template.path, "cache"));
-          await remove(path.join(template.path, "types"));
-
-          process.chdir(previousCwd);
+      // Remove all the files that `hardhat compile` may have created
+      const toRemove = ["artifacts", "cache", "types"];
+      for (const file of toRemove) {
+        const filePath = path.join(template.path, file);
+        if (await exists(filePath)) {
+          await remove(filePath);
         }
-      });
+      }
     }
-  });
+
+    try {
+      const resultHardhatBuild = spawnSync("pnpm hardhat compile", {
+        stdio: "inherit",
+        shell: true,
+      });
+
+      assert.equal(
+        resultHardhatBuild.status,
+        0,
+        `Template ${template.name}: hardhat compile failed`,
+      );
+    } catch (error) {
+      await cleanup();
+      throw error;
+    }
+
+    return cleanup;
+  }
+
+  const templates = await getTemplates();
+
+  for (const template of templates) {
+    describe(`template ${template.name}`, async () => {
+      let cleanup: () => Promise<void> | undefined;
+      before(async () => {
+        cleanup = await useTemplateHardhatProject(template);
+      });
+
+      it(`should compile with tsc`, async () => {
+        // We run tsc --noEmit in the template
+        const resultTscBuild = spawnSync("pnpm tsc --noEmit", {
+          stdio: "inherit",
+          shell: true,
+        });
+
+        assert.equal(
+          resultTscBuild.status,
+          0,
+          `Template ${template.name}: tsc --noEmit failed`,
+        );
+      });
+
+      it(`should pass the hardhat tests`, async () => {
+        const hardhatTest = spawnSync("pnpm hardhat test", {
+          stdio: "inherit",
+          shell: true,
+          env: {
+            ...process.env,
+            // We override the NODE_TEST_CONTEXT environment variable to
+            // ensure that `node:test` runs in the subprocess, otherwise it
+            // will be disabled automatically
+            NODE_TEST_CONTEXT: undefined,
+          },
+        });
+
+        assert.equal(
+          hardhatTest.status,
+          0,
+          `Template ${template.name}: hardhat test failed`,
+        );
+      });
+
+      after(async () => {
+        await cleanup?.();
+      });
+    });
+  }
 });
