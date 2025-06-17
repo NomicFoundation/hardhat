32df5c58052a4b3a2c42a9cc298d632eebb564b8
Patricio Palladino <email@patriciopalladino.com>
2025-06-17 22:13:18 +0000

    [hardhat-utils] Make shortenPath always use ./ or ../ for relative paths

--- v-next/hardhat-utils/test/path.ts
+++ v-next/hardhat-utils/test/path.ts
@@ -1,7 +1,8 @@
 import assert from "node:assert/strict";
 import path from "node:path";
-import { describe, it } from "node:test";
+import { after, before, describe, it } from "node:test";
 
+import { ensureDir } from "../src/fs.js";
 import { resolveFromRoot, shortenPath } from "../src/path.js";
 
 describe("path", () => {
@@ -35,104 +36,65 @@ describe("path", () => {
   });
 
   describe("shortenPath", () => {
+    let originalCwd: string;
+    before(() => {
+      originalCwd = process.cwd();
+    });
+
+    after(() => {
+      process.chdir(originalCwd);
+    });
+
     it("Should shorten a path that's inside the folder", () => {
+      process.chdir(import.meta.dirname);
+
+      assert.equal(shortenPath(import.meta.dirname), "." + path.sep);
+
       assert.equal(
-        shortenPath(import.meta.filename, import.meta.dirname),
-        path.basename(import.meta.filename),
+        shortenPath(path.join(import.meta.dirname, "A.sol")),
+        "." + path.sep + "A.sol",
       );
 
       assert.equal(
-        shortenPath(import.meta.filename, path.dirname(import.meta.dirname)),
-        path.join(
-          path.basename(import.meta.dirname),
-          path.basename(import.meta.filename),
-        ),
+        shortenPath(path.join(import.meta.dirname, "a", "b", "A.sol")),
+        "." + path.sep + path.join("a", "b", "A.sol"),
       );
 
       // Test that it works with a path.sep at the end
       assert.equal(
-        shortenPath(
-          import.meta.filename,
-          path.dirname(import.meta.dirname) + path.sep,
-        ),
-        path.join(
-          path.basename(import.meta.dirname),
-          path.basename(import.meta.filename),
-        ),
+        shortenPath(path.join(import.meta.dirname, "a") + path.sep),
+        "." + path.sep + "a",
       );
     });
 
     it("Should shorten a path that's not inside the folder", () => {
+      process.chdir(path.join(import.meta.dirname, "fixture-projects"));
+
       assert.equal(
-        shortenPath(import.meta.filename, import.meta.dirname + "nope"),
-        path.join("..", "test", "path.ts"),
+        shortenPath(path.join(import.meta.dirname, "nope")),
+        path.join("..", "nope"),
+      );
+
+      assert.equal(
+        shortenPath(path.join(import.meta.dirname)),
+        ".." + path.sep,
       );
     });
 
-    it("Should shorten a path that's not inside the folder whose path relative path would be longer", () => {
-      assert.equal(
-        shortenPath(
-          import.meta.dirname,
-          // We define a folder so nested that the relative path will be longer
-          // than the absolute one, due to too many ".." in the path
-          path.join(
-            import.meta.dirname,
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-            "a",
-          ),
-        ),
+    it("Shouldn't shorten a path that's not inside the folder whose path relative path would be longer", async () => {
+      // We define a folder so nested that the relative path will be longer
+      // than the absolute one, due to too many ".." in the path
+      const cwd = path.join(
         import.meta.dirname,
+        "fixture-projects",
+        "fs",
+        "shorten-path",
+        ...Array(30).fill("nested"),
       );
+      await ensureDir(cwd);
+      process.chdir(cwd);
+
+      assert.equal(shortenPath(import.meta.dirname), import.meta.dirname);
     });
   });
 });
