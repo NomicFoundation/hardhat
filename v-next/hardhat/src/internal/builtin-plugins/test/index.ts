f9b426b97da2ee34d7c380242a636bc40b2ab504
Luis Schaab <schaable@gmail.com>
2025-07-02 14:23:51 +0100

    feat: forward chainType parameter in test task

--- v-next/hardhat/src/internal/builtin-plugins/test/index.ts
+++ v-next/hardhat/src/internal/builtin-plugins/test/index.ts
@@ -9,9 +9,15 @@ const hardhatPlugin: HardhatPlugin = {
   id: "builtin:test",
   tasks: [
     task("test", "Runs all your tests")
-      .addFlag({
-        name: "noCompile",
-        description: "Don't compile the project before running the tests",
+      .addVariadicArgument({
+        name: "testFiles",
+        description: "List of specific files to run tests on",
+        defaultValue: [],
+      })
+      .addOption({
+        name: "chainType",
+        description: "The chain type to use by the solidity test runner",
+        defaultValue: "l1",
       })
       .addOption({
         name: "grep",
@@ -19,10 +25,9 @@ const hardhatPlugin: HardhatPlugin = {
         type: ArgumentType.STRING_WITHOUT_DEFAULT,
         defaultValue: undefined,
       })
-      .addVariadicArgument({
-        name: "testFiles",
-        description: "List of specific files to run tests on",
-        defaultValue: [],
+      .addFlag({
+        name: "noCompile",
+        description: "Don't compile the project before running the tests",
       })
       .setAction(import.meta.resolve("./task-action.js"))
       .build(),
