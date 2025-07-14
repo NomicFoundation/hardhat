c32c3d57fae8d57c1bfc344a195a7af1e1595350
Armando Andini <armando.andini@gmail.com>
2025-07-14 17:27:34 -0300

    Optimize hashing of compilation jobs for cache purposes

--- v-next/hardhat/src/internal/builtin-plugins/solidity/build-system/build-system.ts
+++ v-next/hardhat/src/internal/builtin-plugins/solidity/build-system/build-system.ts
@@ -362,6 +362,7 @@ export class SolidityBuildSystemImplementation implements SolidityBuildSystem {
 
     // build job for each root file. At this point subgraphsWithConfig are 1 root file each
     const indexedIndividualJobs: Map<string, CompilationJob> = new Map();
+    const contentHashes = new Map<string, string>();
     await Promise.all(
       subgraphsWithConfig.map(async ([config, subgraph]) => {
         const solcLongVersion = solcVersionToLongVersion.get(config.version);
@@ -376,6 +377,7 @@ export class SolidityBuildSystemImplementation implements SolidityBuildSystem {
           config,
           solcLongVersion,
           this.#hooks,
+          contentHashes,
         );
 
         await compilationJob.getBuildId(); // precompute
@@ -471,6 +473,7 @@ export class SolidityBuildSystemImplementation implements SolidityBuildSystem {
 
       subgraphsWithConfig = [...mergedSubgraphsByConfig.entries()];
     } else {
+      // isolated mode
       subgraphsWithConfig = subgraphsWithConfig.filter(
         ([_config, subgraph]) => {
           assertHardhatInvariant(
@@ -499,6 +502,7 @@ export class SolidityBuildSystemImplementation implements SolidityBuildSystem {
         solcConfig,
         solcLongVersion,
         this.#hooks,
+        contentHashes,
       );
 
       for (const [userSourceName, root] of subgraph.getRoots().entries()) {
