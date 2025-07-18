f207e165e34e7d756e6510facacff8f5e3625379
Luis Schaab <schaable@gmail.com>
2025-07-18 19:33:28 +0100

    feat: allow apiKey to be undefined when enabled is false

--- v-next/hardhat-verify/src/internal/hook-handlers/config.ts
+++ v-next/hardhat-verify/src/internal/hook-handlers/config.ts
@@ -12,7 +12,9 @@ import type {
   HardhatUserConfigValidationError,
 } from "hardhat/types/hooks";
 
+import { isObject } from "@nomicfoundation/hardhat-utils/lang";
 import {
+  conditionalUnionType,
   sensitiveStringSchema,
   validateUserConfigZodType,
 } from "@nomicfoundation/hardhat-zod-utils";
@@ -31,12 +33,27 @@ const userConfigType = z.object({
           enabled: z.boolean().optional(),
         })
         .optional(),
-      etherscan: z
-        .object({
-          apiKey: sensitiveStringSchema,
-          enabled: z.boolean().optional(),
-        })
-        .optional(),
+      etherscan: conditionalUnionType(
+        [
+          [
+            (data) => isObject(data) && data.enabled === false,
+            z.object({
+              apiKey: sensitiveStringSchema.optional(),
+              enabled: z.literal(false),
+            }),
+          ],
+          [
+            (data) =>
+              isObject(data) &&
+              (data.enabled === undefined || data.enabled === true),
+            z.object({
+              apiKey: sensitiveStringSchema,
+              enabled: z.literal(true).optional(),
+            }),
+          ],
+        ],
+        "Expected an object with an 'apiKey' property and an optional 'enabled' boolean property",
+      ).optional(),
     })
     .optional(),
 });
@@ -88,7 +105,7 @@ function resolveEtherscanConfig(
   resolveConfigurationVariable: ConfigurationVariableResolver,
 ): EtherscanConfig {
   return {
-    apiKey: resolveConfigurationVariable(etherscanConfig.apiKey),
+    apiKey: resolveConfigurationVariable(etherscanConfig.apiKey ?? ""),
     enabled: etherscanConfig.enabled ?? true,
   };
 }
