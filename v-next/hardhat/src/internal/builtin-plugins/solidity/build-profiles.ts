1ae14194ccea91e7c264b0fd9780fb15a20d2f28
Luis Schaab <schaable@gmail.com>
2025-08-06 21:28:56 +0100

    refactor: remove unused build profiles

--- v-next/hardhat/src/internal/builtin-plugins/solidity/build-profiles.ts
+++ v-next/hardhat/src/internal/builtin-plugins/solidity/build-profiles.ts
@@ -1,8 +1,3 @@
 export const DEFAULT_BUILD_PROFILE = "default";
 
-export const DEFAULT_BUILD_PROFILES = [
-  "default",
-  "production",
-  "solidity-tests",
-  "javascript-tests",
-] as const;
+export const DEFAULT_BUILD_PROFILES = ["default", "production"] as const;
