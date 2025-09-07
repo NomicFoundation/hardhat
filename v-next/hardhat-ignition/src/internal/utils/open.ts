938cc6764cd0a67fa6eb85e51c2ceff9c5098955
sashaodessa <140454972+sashaodessa@users.noreply.github.com>
2025-09-07 23:16:08 +0200

    Update open.ts

--- v-next/hardhat-ignition/src/internal/utils/open.ts
+++ v-next/hardhat-ignition/src/internal/utils/open.ts
@@ -1,21 +1,18 @@
-import { execSync } from "node:child_process";
+import { execFileSync, spawnSync } from "node:child_process";
 import os from "node:os";
 
 export function open(filePath: string): void {
-  let command: string;
-  switch (os.platform()) {
-    case "win32":
-      command = "start";
-      break;
-    case "darwin":
-      command = "open";
-      break;
-    default:
-      command = "xdg-open";
-  }
-
   try {
-    execSync(`${command} ${filePath}`, { stdio: "ignore" });
+    switch (os.platform()) {
+      case "win32":
+        spawnSync("cmd", ["/c", "start", "", filePath], { stdio: "ignore", windowsHide: true });
+        break;
+      case "darwin":
+        execFileSync("open", [filePath], { stdio: "ignore" });
+        break;
+      default:
+        execFileSync("xdg-open", [filePath], { stdio: "ignore" });
+    }
   } catch {
     // do nothing
   }
