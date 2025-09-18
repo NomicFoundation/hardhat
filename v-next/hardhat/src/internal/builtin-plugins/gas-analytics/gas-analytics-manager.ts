a7d68b57f07276ab369a0bba61dab0e4578088fd
Luis Schaab <schaable@gmail.com>
2025-09-18 15:29:44 +0100

    feat: calculate gas stats

--- v-next/hardhat/src/internal/builtin-plugins/gas-analytics/gas-analytics-manager.ts
+++ v-next/hardhat/src/internal/builtin-plugins/gas-analytics/gas-analytics-manager.ts
@@ -5,6 +5,8 @@ import path from "node:path";
 
 import {
   ensureDir,
+  getAllFilesMatching,
+  readJsonFile,
   remove,
   writeJsonFile,
 } from "@nomicfoundation/hardhat-utils/fs";
@@ -14,9 +16,18 @@ const gasStatsLog = debug(
   "hardhat:core:gas-analytics:gas-analytics-manager:gas-stats",
 );
 
+interface GasStats {
+  min: number;
+  max: number;
+  avg: number;
+  median: number;
+  calls: number;
+}
+
 export class GasAnalyticsManagerImplementation implements GasAnalyticsManager {
   public gasMeasurements: GasMeasurement[] = [];
   readonly #gasStatsPath: string;
+  #gasStatsReportEnabled = true;
 
   constructor(gasStatsPath: string) {
     this.#gasStatsPath = gasStatsPath;
@@ -48,8 +59,24 @@ export class GasAnalyticsManagerImplementation implements GasAnalyticsManager {
     gasStatsLog("Saved gas measurements", id, filePath);
   }
 
-  public async reportGasStats(..._ids: string[]): Promise<void> {
-    // TODO
+  public async reportGasStats(...ids: string[]): Promise<void> {
+    if (!this.#gasStatsReportEnabled) {
+      return;
+    }
+
+    await this._loadGasMeasurements(...ids);
+
+    const _report = this._calculateGasStats();
+
+    // TODO size: only for deployments
+  }
+
+  public enableGasStatsReport(): void {
+    this.#gasStatsReportEnabled = true;
+  }
+
+  public disableGasStatsReport(): void {
+    this.#gasStatsReportEnabled = false;
   }
 
   async #getGasMeasurementsPath(id: string): Promise<string> {
@@ -57,4 +84,102 @@ export class GasAnalyticsManagerImplementation implements GasAnalyticsManager {
     await ensureDir(dataPath);
     return dataPath;
   }
+
+  /**
+   * @private exposed for testing purposes only
+   */
+  public async _loadGasMeasurements(...ids: string[]): Promise<void> {
+    this.gasMeasurements = [];
+    for (const id of ids) {
+      const gasStatsPath = await this.#getGasMeasurementsPath(id);
+      const filePaths = await getAllFilesMatching(gasStatsPath);
+      for (const filePath of filePaths) {
+        const entries = await readJsonFile<GasMeasurement[]>(filePath);
+        for (const entry of entries) {
+          this.gasMeasurements.push(entry);
+        }
+        gasStatsLog("Loaded gas measurements", id, filePath);
+      }
+    }
+  }
+
+  /**
+   * @private exposed for testing purposes only
+   */
+  public _calculateGasStats(): Map<string, Map<string, GasStats>> {
+    const report: Map<string, Map<string, GasStats>> = new Map();
+    const measurementsByContract = this._aggregateGasMeasurements();
+
+    for (const [contractFqn, functions] of measurementsByContract) {
+      const contractGasStats = new Map<string, GasStats>();
+
+      for (const [functionOrDeployment, gasValues] of functions) {
+        const stats: GasStats = {
+          min: Math.min(...gasValues),
+          max: Math.max(...gasValues),
+          avg: avg(gasValues),
+          median: median(gasValues),
+          calls: gasValues.length,
+          // TODO size: only for deployments
+        };
+
+        contractGasStats.set(functionOrDeployment, stats);
+      }
+
+      report.set(contractFqn, contractGasStats);
+    }
+
+    return report;
+  }
+
+  /**
+   * @private exposed for testing purposes only
+   */
+  public _aggregateGasMeasurements(): Map<string, Map<string, number[]>> {
+    const measurementsByContract = new Map<
+      string,
+      Map<string, number[]> // functionSig or "deployment"
+    >();
+
+    for (const currentMeasurement of this.gasMeasurements) {
+      let contractMeasurements = measurementsByContract.get(
+        currentMeasurement.contractFqn,
+      );
+      if (contractMeasurements === undefined) {
+        contractMeasurements = new Map<string, number[]>();
+        measurementsByContract.set(
+          currentMeasurement.contractFqn,
+          contractMeasurements,
+        );
+      }
+
+      const key =
+        currentMeasurement.type === "deployment"
+          ? "deployment"
+          : currentMeasurement.functionSig;
+
+      let measurements = contractMeasurements.get(key);
+      if (measurements === undefined) {
+        measurements = [];
+        contractMeasurements.set(key, measurements);
+      }
+
+      measurements.push(currentMeasurement.gas);
+    }
+
+    return measurementsByContract;
+  }
+}
+
+export function avg(values: number[]): number {
+  return values.reduce((a, c) => a + c, 0) / values.length;
+}
+
+export function median(values: number[]): number {
+  const sorted = [...values].sort((a, b) => a - b);
+  const mid = Math.floor(sorted.length / 2);
+
+  return sorted.length % 2 === 1
+    ? sorted[mid]
+    : (sorted[mid - 1] + sorted[mid]) / 2;
 }
