aed0c5bff4ab9e0526eab3feab81e837b36d4edc
Luis Schaab <schaable@gmail.com>
2025-09-19 15:04:08 +0100

    feat: show basic markdown report

--- v-next/hardhat/src/internal/builtin-plugins/gas-analytics/gas-analytics-manager.ts
+++ v-next/hardhat/src/internal/builtin-plugins/gas-analytics/gas-analytics-manager.ts
@@ -3,6 +3,7 @@ import type { GasAnalyticsManager, GasMeasurement } from "./types.js";
 import crypto from "node:crypto";
 import path from "node:path";
 
+import { formatMarkdownTable } from "@nomicfoundation/hardhat-utils/format";
 import {
   ensureDir,
   getAllFilesMatching,
@@ -66,9 +67,13 @@ export class GasAnalyticsManagerImplementation implements GasAnalyticsManager {
 
     await this._loadGasMeasurements(...ids);
 
-    const _report = this._calculateGasStats();
+    const gasStatsReport = this._calculateGasStats();
 
     // TODO size: only for deployments
+    const markdownReport = this._formatGasStatsMarkdownReport(gasStatsReport);
+
+    console.log(markdownReport);
+    gasStatsLog("Printed markdown report");
   }
 
   public enableGasStatsReport(): void {
@@ -110,10 +115,10 @@ export class GasAnalyticsManagerImplementation implements GasAnalyticsManager {
     const report: Map<string, Map<string, GasStats>> = new Map();
     const measurementsByContract = this._aggregateGasMeasurements();
 
-    for (const [contractFqn, functions] of measurementsByContract) {
+    for (const [contractFqn, measurements] of measurementsByContract) {
       const contractGasStats = new Map<string, GasStats>();
 
-      for (const [functionOrDeployment, gasValues] of functions) {
+      for (const [functionOrDeployment, gasValues] of measurements) {
         const stats: GasStats = {
           min: Math.min(...gasValues),
           max: Math.max(...gasValues),
@@ -168,6 +173,44 @@ export class GasAnalyticsManagerImplementation implements GasAnalyticsManager {
     }
 
     return measurementsByContract;
+  }
+
+  /**
+   * @private exposed for testing purposes only
+   */
+  public _formatGasStatsMarkdownReport(
+    gasStatsReport: Map<string, Map<string, GasStats>>,
+  ): string {
+    const rows: string[][] = [];
+    for (const [contractFqn, contractGasStats] of gasStatsReport) {
+      rows.push([contractFqn]);
+      for (const [functionOrDeployment, gasStats] of contractGasStats) {
+        if (functionOrDeployment === "deployment") {
+          rows.push(["Deployment Cost", "Deployment Size"]);
+          rows.push([`${gasStats.avg}`, ""]);
+          rows.push([
+            "Function name",
+            "Min",
+            "Average",
+            "Median",
+            "Max",
+            "#calls",
+          ]);
+        } else {
+          rows.push([
+            functionOrDeployment,
+            `${gasStats.min}`,
+            `${gasStats.avg}`,
+            `${gasStats.median}`,
+            `${gasStats.max}`,
+            `${gasStats.calls}`,
+          ]);
+        }
+      }
+      rows.push([]);
+    }
+
+    return formatMarkdownTable(undefined, rows, undefined);
   }
 }
 
