c6f1e72316722a2f17be2311f33ab17acd53f924
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-10-27 11:13:18 +0100

    remove dist folder

--- v-next/hardhat-vendored/dist/src/coverage/index.js
+++
@@ -1,7 +0,0 @@
-import libCoverage from "./lib-coverage/index.cjs";
-export { libCoverage };
-import libReport from "./lib-report/index.cjs";
-export { libReport };
-import reports from "./istanbul-reports/index.cjs";
-export { reports };
-//# sourceMappingURL=index.js.map
