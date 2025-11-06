f625f6f5c7e96ed62e8f0a32b660beb48482a247
Patricio Palladino <email@patriciopalladino.com>
2025-11-06 00:28:36 +0000

    Also use the user interruptions in the viem/ethers helpers

--- packages/hardhat-ignition-viem/src/internal/viem-ignition-helper.ts
+++ packages/hardhat-ignition-viem/src/internal/viem-ignition-helper.ts
@@ -22,6 +22,7 @@ import type {
 } from "@nomicfoundation/ignition-core";
 import type { ArtifactManager } from "hardhat/types/artifacts";
 import type { HardhatConfig } from "hardhat/types/config";
+import type { HookManager, UserInterruptionHooks } from "hardhat/types/hooks";
 import type { NetworkConnection, ChainType } from "hardhat/types/network";
 import type { UserInterruptionManager } from "hardhat/types/user-interruptions";
 
@@ -35,6 +36,7 @@ import {
   HardhatArtifactResolver,
   PrettyEventHandler,
   errorDeploymentResultToExceptionMessage,
+  getUserInterruptionsHandlers,
   readDeploymentParameters,
   resolveDeploymentId,
 } from "@nomicfoundation/hardhat-ignition/helpers";
@@ -55,6 +57,7 @@ export class ViemIgnitionHelperImpl<ChainTypeT extends ChainType | string>
   readonly #artifactsManager: ArtifactManager;
   readonly #connection: NetworkConnection<ChainTypeT>;
   readonly #userInterruptions: UserInterruptionManager;
+  readonly #hooks: HookManager;
   readonly #config: Partial<DeployConfig> | undefined;
   readonly #provider: EIP1193Provider;
 
@@ -65,6 +68,7 @@ export class ViemIgnitionHelperImpl<ChainTypeT extends ChainType | string>
     artifactsManager: ArtifactManager,
     connection: NetworkConnection<ChainTypeT>,
     userInterruptions: UserInterruptionManager,
+    hooks: HookManager,
     config?: Partial<DeployConfig> | undefined,
     provider?: EIP1193Provider,
   ) {
@@ -72,6 +76,7 @@ export class ViemIgnitionHelperImpl<ChainTypeT extends ChainType | string>
     this.#artifactsManager = artifactsManager;
     this.#connection = connection;
     this.#userInterruptions = userInterruptions;
+    this.#hooks = hooks;
     this.#config = config;
     this.#provider = provider ?? this.#connection.provider;
   }
@@ -131,6 +136,8 @@ export class ViemIgnitionHelperImpl<ChainTypeT extends ChainType | string>
 
     this.#mutex = true;
 
+    let userInterruptionsHandlers: UserInterruptionHooks | undefined;
+
     try {
       // eslint-disable-next-line @typescript-eslint/consistent-type-assertions -- eth_accounts returns a string array
       const accounts: string[] = (await this.#connection.provider.request({
@@ -171,6 +178,17 @@ export class ViemIgnitionHelperImpl<ChainTypeT extends ChainType | string>
       const executionEventListener = displayUi
         ? new PrettyEventHandler(this.#userInterruptions)
         : undefined;
+
+      if (executionEventListener !== undefined) {
+        userInterruptionsHandlers = getUserInterruptionsHandlers(
+          executionEventListener,
+        );
+
+        this.#hooks.registerHandlers(
+          "userInterruptions",
+          userInterruptionsHandlers,
+        );
+      }
 
       let deploymentParameters: DeploymentParameters;
 
@@ -231,6 +249,13 @@ export class ViemIgnitionHelperImpl<ChainTypeT extends ChainType | string>
         result,
       );
     } finally {
+      if (userInterruptionsHandlers !== undefined) {
+        this.#hooks.unregisterHandlers(
+          "userInterruptions",
+          userInterruptionsHandlers,
+        );
+      }
+
       this.#mutex = false;
     }
   }
