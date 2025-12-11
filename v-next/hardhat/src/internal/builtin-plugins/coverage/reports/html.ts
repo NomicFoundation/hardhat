8aaecf864ff5037d7ad10c8c5023d76ff9ac12a8
ChrisD <18092467+ChristopherDedominici@users.noreply.github.com>
2025-12-11 16:14:35 +0100

    first iteration

---
+++ v-next/hardhat/src/internal/builtin-plugins/coverage/reports/html.ts
@@ -0,0 +1,56 @@
+import type { Report } from "../coverage-manager.js";
+import type { FileCoverageData } from "@nomicfoundation/hardhat-vendored/coverage/types";
+
+import path from "node:path";
+
+import { mkdir } from "@nomicfoundation/hardhat-utils/fs";
+import {
+  istanbulLibCoverage,
+  istanbulLibReport,
+  istanbulReports,
+} from "@nomicfoundation/hardhat-vendored/coverage";
+
+export async function generateHtmlReport(
+  report: Report,
+  htmlReportPath: string,
+): Promise<void> {
+  const baseDir = process.cwd();
+  const coverageMap = istanbulLibCoverage.createCoverageMap({});
+
+  // Construct coverage data for each tested file,
+  // detailing whether each line was executed or not.
+  for (const [p, fileCoverageInput] of Object.entries(report)) {
+    const testedFilePath = path.join(baseDir, p);
+
+    const fc: FileCoverageData = {
+      path: testedFilePath,
+      statementMap: {},
+      fnMap: {}, // Cannot be derived from current report data
+      branchMap: {}, // Cannot be derived from current report data
+      s: {},
+      f: {}, // Cannot be derived from current report data
+      b: {},
+    };
+
+    for (const [line, count] of fileCoverageInput.lineExecutionCounts) {
+      fc.statementMap[line] = {
+        start: { line, column: 0 },
+        end: { line, column: 0 },
+      };
+
+      // TODO: currently EDR does not provide per-statement coverage counts
+      fc.s[line] = count > 0 ? 1 : 0; // mark as covered if hit at least once
+    }
+
+    coverageMap.addFileCoverage(fc);
+  }
+
+  await mkdir(htmlReportPath);
+
+  const context = istanbulLibReport.createContext({
+    dir: htmlReportPath,
+    coverageMap,
+  });
+
+  istanbulReports.create("html", undefined).execute(context);
+}
