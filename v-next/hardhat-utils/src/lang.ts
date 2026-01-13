522e6fdf3da8494ee1b81d4fcdec3c3d91a7f9f2
ChristopherDedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2026-01-13 13:27:37 +0000

    use `isDeepStrictEqual` from `node:util`

--- v-next/hardhat-utils/src/lang.ts
+++ v-next/hardhat-utils/src/lang.ts
@@ -1,3 +1,5 @@
+import { isDeepStrictEqual } from "node:util";
+
 import { deepMergeImpl, getDeepCloneFunction } from "./internal/lang.js";
 
 /**
@@ -20,9 +22,7 @@ export async function deepClone<T>(value: T): Promise<T> {
  * @returns True if the values are deeply equal, false otherwise.
  */
 export async function deepEqual<T>(x: T, y: T): Promise<boolean> {
-  const { deepEqual: _deepEqual } = await import("fast-equals");
-
-  return _deepEqual(x, y);
+  return isDeepStrictEqual(x, y);
 }
 
 /**
