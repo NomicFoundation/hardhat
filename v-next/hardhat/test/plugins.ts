99aa28abfc7378b0c4891a7bb6ff185558a16145
Patricio Palladino <email@patriciopalladino.com>
2026-02-23 13:53:09 +0000

    Also trap in `hardhat/plugins`'s functions

---
+++ v-next/hardhat/test/plugins.ts
@@ -0,0 +1,43 @@
+import assert from "node:assert/strict";
+import { describe, it } from "node:test";
+
+import { assertThrows } from "@nomicfoundation/hardhat-test-utils";
+
+import { UsingHardhat2PluginError } from "../src/internal/using-hardhat2-plugin-errors.js";
+import { lazyFunction, lazyObject } from "../src/plugins.js";
+
+describe("Hardhat 2 plugin compatibility", () => {
+  it("should throw when calling lazyFunction", () => {
+    assertThrows(
+      () => lazyFunction(),
+      (error) => {
+        assert.ok(
+          error instanceof UsingHardhat2PluginError,
+          "Should be a UsingHardhat2PluginError",
+        );
+        assert.ok(
+          error.callerRelativePath?.includes("test/plugins.ts") === true,
+          "Should have the caller path",
+        );
+        return true;
+      },
+    );
+  });
+
+  it("should throw when calling lazyObject", () => {
+    assertThrows(
+      () => lazyObject(),
+      (error) => {
+        assert.ok(
+          error instanceof UsingHardhat2PluginError,
+          "Should be a UsingHardhat2PluginError",
+        );
+        assert.ok(
+          error.callerRelativePath?.includes("test/plugins.ts") === true,
+          "Should have the caller path",
+        );
+        return true;
+      },
+    );
+  });
+});
