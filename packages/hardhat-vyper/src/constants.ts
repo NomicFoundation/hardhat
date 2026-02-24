996a899a24d50824cef20ac5608597ff2aa58832
Patricio Palladino <email@patriciopalladino.com>
2026-02-24 18:48:53 +0000

    Update hardhat-vyper

--- packages/hardhat-vyper/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const DEFAULT_VYPER_VERSION = "0.3.1";
-export const ARTIFACT_FORMAT_VERSION = "hh-vyper-artifact-1";
-export const DEBUG_NAMESPACE = "hardhat:plugin:vyper";
-export const CACHE_FORMAT_VERSION = "hh-vy-cache-1";
-export const VYPER_FILES_CACHE_FILENAME = "vyper-files-cache.json";
-export const OUTPUT_BREAKABLE_VYPER_VERSION = "0.4.0";
--- packages/hardhat-vyper/test/fixture-projects/generates-gas-field/contracts/A.vy
+++
@@ -1,5 +0,0 @@
-# @version 0.2.12
-
-@external
-def test() -> int128:
-  return 42
