654127f8186e6f1f4dda13890166cd3f12709c49
Patricio Palladino <email@patriciopalladino.com>
2026-02-24 16:20:04 +0000

    Add more docs

--- v-next/hardhat/src/internal/deprecated-module-imported-from-hardhat2-plugin.ts
+++ v-next/hardhat/src/internal/deprecated-module-imported-from-hardhat2-plugin.ts
@@ -6,3 +6,7 @@
 // The reason this module can be empty is that Hardhat 2 plugins are CJS modules
 // so they can destructure the require and get `undefined` values, instead of
 // a load-time error.
+
+// We could also throw from this file, but if it gets imported by an ESM module
+// you don't get an import-stack-trace, so you loose the possibility of figuring
+// out which plugin is triggering the error.
