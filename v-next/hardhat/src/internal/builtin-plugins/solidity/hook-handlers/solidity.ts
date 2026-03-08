5b5fe524fe1ad26f8ba30eec03557c553d7eef3a
Patricio Palladino <email@patriciopalladino.com>
2026-03-08 18:39:29 +0000

    Use new name fore a function

--- v-next/hardhat/src/internal/builtin-plugins/solidity/hook-handlers/solidity.ts
+++ v-next/hardhat/src/internal/builtin-plugins/solidity/hook-handlers/solidity.ts
@@ -1,12 +1,14 @@
 import type { SolidityHooks } from "../../../../types/hooks.js";
 
-import { isSolcConfig } from "../build-system/build-system.js";
+import { isSolcSolidityCompilerConfig } from "../build-system/build-system.js";
 import { downloadSolcCompilers } from "../build-system/compiler/index.js";
 
 export default async (): Promise<Partial<SolidityHooks>> => ({
   downloadCompilers: async (_context, compilerConfigs, quiet) => {
     const solcVersions = new Set(
-      compilerConfigs.filter(isSolcConfig).map((c) => c.version),
+      compilerConfigs
+        .filter(isSolcSolidityCompilerConfig)
+        .map((c) => c.version),
     );
 
     if (solcVersions.size > 0) {
