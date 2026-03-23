03ae643e467f9b811496493f1a8528c03daa79bd
John Kane <john@kanej.me>
2026-03-23 17:26:27 +0000

    chore: upgrade the devcontainer to trixie

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,10 +7,14 @@ sudo apt update
 sudo apt install -y libudev-dev
 
 # aha and wkhtmltoimage (included in the wkhtmltopdf package) are used in the integration tests of the node:test reporter
-sudo apt install -y aha wkhtmltopdf
+sudo apt install -y aha
+
+curl -sL https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6.1-3/wkhtmltox_0.12.6.1-3.bookworm_amd64.deb -o /tmp/wkhtmltox.deb
+sudo apt install -y /tmp/wkhtmltox.deb
+rm /tmp/wkhtmltox.deb
 
 # Used for performance measurement
-cargo install hyperfine
+sudo apt install -y hyperfine
 
 # Make sure bun is available at the cli
 npm install -g bun
