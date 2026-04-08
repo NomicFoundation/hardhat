e10fddcaf5d0c62604539f8d7abbce21b944bee7
ChristopherDedominici <18092467+ChristopherDedominici@users.noreply.github.com>
2026-04-08 12:07:04 +0000

    lint:fix

--- packages/hardhat/test/internal/builtin-plugins/network-manager/network-manager.ts
+++ packages/hardhat/test/internal/builtin-plugins/network-manager/network-manager.ts
@@ -2649,8 +2649,7 @@ describe("NetworkManagerImplementation", () => {
           assertValidationErrors(validationErrors, [
             {
               path: ["networks", "hardhat", "forking", "blockNumber"],
-              message:
-                "Expected a positive safe int or a positive bigint",
+              message: "Expected a positive safe int or a positive bigint",
             },
           ]);
 
