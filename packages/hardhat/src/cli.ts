598e4e3177f43e5bc20d8ae97c27f53c14b3d6aa
Patricio Palladino <email@patriciopalladino.com>
2026-04-20 15:08:52 -0300

    Setup the new global error handlers in the cli's entrypoint

--- packages/hardhat/src/cli.ts
+++ packages/hardhat/src/cli.ts
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
 import { printNodeJsVersionWarningIfNecessary } from "./internal/cli/node-version.js";
+import { setupGlobalUnhandledErrorHandlers } from "./internal/cli/telemetry/error-reporter/global-error-handlers.js";
+
+// We setup the global unhandled errors first
+setupGlobalUnhandledErrorHandlers();
 
 // We enable the sourcemaps before loading main, so that everything except this
 // small file is loaded with sourcemaps enabled.
