83d813b4972108ccc8afbc077695f7e6780bfa31
Luis Schaab <schaable@gmail.com>
2026-04-20 16:36:58 +0000

    refactor(hardhat): use createLazyLoader in network-manager HRE hook

--- packages/hardhat/src/internal/builtin-plugins/network-manager/hook-handlers/hre.ts
+++ packages/hardhat/src/internal/builtin-plugins/network-manager/hook-handlers/hre.ts
@@ -1,9 +1,7 @@
-import type {
-  HardhatRuntimeEnvironmentHooks,
-  HookContext,
-} from "../../../../types/hooks.js";
-import type { HardhatRuntimeEnvironment } from "../../../../types/hre.js";
+import type { HardhatRuntimeEnvironmentHooks } from "../../../../types/hooks.js";
 import type { NetworkManager } from "../../../../types/network.js";
+
+import { createLazyLoader } from "@nomicfoundation/hardhat-utils/lang";
 
 import { DEFAULT_NETWORK_NAME } from "../../../constants.js";
 
@@ -11,37 +9,44 @@ export default async (): Promise<Partial<HardhatRuntimeEnvironmentHooks>> => ({
   created: async (context, hre) => {
     let networkManager: NetworkManager | undefined;
 
+    const getNetworkManagerImpl = createLazyLoader<NetworkManager>(async () => {
+      const { NetworkManagerImplementation } = await import(
+        "../network-manager.js"
+      );
+
+      networkManager = new NetworkManagerImplementation(
+        hre.globalOptions.network !== undefined
+          ? hre.globalOptions.network
+          : DEFAULT_NETWORK_NAME,
+        hre.config.defaultChainType,
+        hre.config.networks,
+        context.hooks,
+        context.artifacts,
+        hre.userConfig,
+        hre.config.chainDescriptors,
+        hre.globalOptions.config,
+        hre.config.paths.root,
+        hre.globalOptions.verbosity,
+      );
+
+      return networkManager;
+    });
+
     hre.network = {
       async create(networkConnectionParams) {
-        if (networkManager === undefined) {
-          networkManager = await createNetworkManager(hre, context);
-        }
-
-        return networkManager.create(networkConnectionParams);
+        return (await getNetworkManagerImpl()).create(networkConnectionParams);
       },
 
       async connect(networkConnectionParams) {
-        if (networkManager === undefined) {
-          networkManager = await createNetworkManager(hre, context);
-        }
-
-        return networkManager.connect(networkConnectionParams);
+        return (await getNetworkManagerImpl()).connect(networkConnectionParams);
       },
 
       async getOrCreate(networkOrParams) {
-        if (networkManager === undefined) {
-          networkManager = await createNetworkManager(hre, context);
-        }
-
-        return networkManager.getOrCreate(networkOrParams);
+        return (await getNetworkManagerImpl()).getOrCreate(networkOrParams);
       },
 
       async createServer(...params) {
-        if (networkManager === undefined) {
-          networkManager = await createNetworkManager(hre, context);
-        }
-
-        return networkManager.createServer(...params);
+        return (await getNetworkManagerImpl()).createServer(...params);
       },
     };
 
@@ -58,27 +63,3 @@ export default async (): Promise<Partial<HardhatRuntimeEnvironmentHooks>> => ({
     });
   },
 });
-
-async function createNetworkManager(
-  hre: HardhatRuntimeEnvironment,
-  context: HookContext,
-): Promise<NetworkManager> {
-  const { NetworkManagerImplementation } = await import(
-    "../network-manager.js"
-  );
-
-  return new NetworkManagerImplementation(
-    hre.globalOptions.network !== undefined
-      ? hre.globalOptions.network
-      : DEFAULT_NETWORK_NAME,
-    hre.config.defaultChainType,
-    hre.config.networks,
-    context.hooks,
-    context.artifacts,
-    hre.userConfig,
-    hre.config.chainDescriptors,
-    hre.globalOptions.config,
-    hre.config.paths.root,
-    hre.globalOptions.verbosity,
-  );
-}
