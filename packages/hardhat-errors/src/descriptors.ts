cbad8226d074545c5ccdd4748e2abeef29c46d17
Patricio Palladino <email@patriciopalladino.com>
2026-04-30 12:09:23 -0300

    Remove redundant message

--- packages/hardhat-errors/src/descriptors.ts
+++ packages/hardhat-errors/src/descriptors.ts
@@ -1483,7 +1483,7 @@ Please use the fully qualified name of the contract to disambiguate it.`,
         number: 1400,
         messageTemplate: `Failed to install the project dependencies.
 
-Please take a look at the error, and try again. If it persists, please open an issue in the Hardhat repository.`,
+Please try again. If it persists, please open an issue in the Hardhat repository.`,
         websiteTitle: "Failed to install the project dependencies",
         websiteDescription: `The project initialization failed to install the required dependencies`,
       },
